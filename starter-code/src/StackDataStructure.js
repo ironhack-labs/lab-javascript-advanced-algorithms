ee83972f70409bc6a8a8b7310ca59d665e0078d5
ahuertam <ahuertam@yahoo.es>
2017-01-25 22:08:54 +0100

    starting

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,11 @@
 function StackDataStructure () {
+  var number=0;
+  this.stackControl=[];
+  this.MAX_SIZE=number;
 }
+StackDataStructure.prototype.isEmpty=function(){
+  if(this.stackControl.length === 0){return ;}else{return false;}
+};
+StackDataStructure.prototype.canPush=function(){
+  if(this.stackControl.length === 0){return ;}else{return false;}
+};
