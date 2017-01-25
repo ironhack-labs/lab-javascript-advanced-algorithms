3cf0a5d812c3aee97a9feb2d73fd21225702cd9d
Gonzalo Sanz <gonzalo.orio@gmail.com>
2017-01-25 23:47:45 +0100

    test ok

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
+
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
 }
+
+var stack = new StackDataStructure();
+
+StackDataStructure.prototype.isEmpty = function(){
+  return this.stackControl.length === 0 ? true : false;
+  // if(this.stackControl.length === 0){
+  //   return true;
+  // }else{
+  //   return false;
+  // }
+};
+
+StackDataStructure.prototype.canPush = function(){
+  return this.stackControl.length<this.MAX_SIZE ? true : false;
+  // if(this.stackControl.length<this.MAX_SIZE){
+  //   return true;
+  // }else{
+  //   return false;
+  // }
+};
+
+StackDataStructure.prototype.push = function(numToPush){
+  if(this.canPush()){
+    this.stackControl.push(numToPush);
+    return this.stackControl;
+  }else{
+    return 'Stack Overflow';
+  }
+};
+
+StackDataStructure.prototype.pop = function(){
+  return this.isEmpty() ? 'Stack Underflow' : this.stackControl.pop();
+    // if(this.isEmpty()){
+    //   return 'Stack Underflow';
+    // }else{
+    //   return this.stackControl.pop();
+    // }
+};
