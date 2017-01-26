914d8d8ca75f85d3b98ff6c0d5f8b59b328a3377
Carlos Olives <carlosolives@gmail.com>
2017-01-26 12:26:38 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
 function QueueDataStructure () {
+    this.queueControl=[];
+    this.MAX_SIZE=8;
+
+    this.isEmpty= function(){
+      return this.queueControl.length===0;
+    };
+    this.canEnqueue= function(){
+      return this.queueControl.length===this.MAX_SIZE ? false:true;
+    };
+    this.enqueue= function(){
+      if(this.queueControl.length===this.MAX_SIZE){
+        return "Queu Underflow";
+      }else{
+        this.queueControl.unshift(item);
+        return this.queueControl;
+      }
+      };
+    this.dequeue= function(item){
+      return this.isEmpty() ? 'Queue Underflow': this.queueControl.pop(item);
+    };
+
+
 }
