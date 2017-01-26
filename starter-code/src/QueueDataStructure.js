2e3094996e166353624409a050f904a1015260e8
Saturnino Villaverde <savianco@hotmail.com>
2017-01-26 12:45:37 +0100

    half done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+    this.isEmpty = function () {
+        return this.queueControl.length === 0;
+    };
+     this.canEnqueue = function (i) {
+         this.queueControl.lenght === this.MAX_SIZE ? false : true;
+     };
+     this.enqueue = function (i) {
+        if (this.queueControl.length === this.MAX_SIZE) {
+            return ("Queue Overflow");
+        } else {
+           this.queueControl.unshift(i);
+         return this.queueControl; 
+        };
+     };
+     this.dequeue = function (i) {
+         if (this.queueControl.length === 0) {
+            return ("Queue Underflow");
+        } else {
+            return this.queueControl.push(i);
+       };
+ };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+    this.isEmpty = function () {
+        return this.stackControl.length === 0;
+    };
+     this.canPush = function (i) {
+         this.stackControl.lenght === this.MAX_SIZE ? false : true;
+    };
+     this.push = function (i) {
+        if (this.stackControl.length === this.MAX_SIZE){
+            return ("Stack Overflow");
+        } else {
+           this.stackControl.push(i);
+         return this.stackControl; 
+        };
+     };
+     this.pop = function (i) {
+         if (this.stackControl.length === 0){
+            return ("Stack Underflow");
+        } else {
+            return this.stackControl.pop(i);
+   };
+ };
 }
+
+
+
+
