431aed4aa737b0a98a967546e98cd56ab740b1c3
Manuel Lintz <manuel.lintz@gmail.com>
2017-01-26 09:24:44 +0100

    passes the test, still have to do the html and css

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,53 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE= 8;
 }
+
+
+StackDataStructure.prototype.isEmpty =function(){
+    return this.stackControl.length === 0;
+  };
+
+
+StackDataStructure.prototype.canPush = function(){
+    if (this.stackControl.length==this.MAX_SIZE){
+      return false;}
+      else{return true;}
+    };
+
+StackDataStructure.prototype.push = function(i){
+    if (this.canPush()){
+      this.stackControl.push(i);
+      return this.stackControl;
+      }
+    else{
+      return "Stack Overflow";
+      }
+      };
+
+StackDataStructure.prototype.pop = function(){
+    var len= this.stackControl.length;
+    if(this.isEmpty()){
+      return 'Stack Underflow';
+        }
+      else{return this.stackControl[len-1];}
+      };
+
+/*  };
+  this.push = function(i){
+    if (this.stackControl.length===this.MAX_SIZE){
+      return "Stack Overflow";
+    }
+    else{
+      this.stackControl.push(i);
+      return this.stackControl;}
+
+  };
+  this.pop = function(){
+    var len= this.stackControl.length;
+    if(len===0){
+      return 'Stack Underflow';
+    }
+    else{return this.stackControl[len-1];}
+  };
+}*/
