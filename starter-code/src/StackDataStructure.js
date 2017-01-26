ef1a37fc7c58593dd213d7d5d4135b4035639df0
inka43 <enrikeglegarra@gmail.com>
2017-01-26 12:25:25 +0100

    casi ni empezado

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,22 @@
-function StackDataStructure () {
+function StackDataStructure (stackControl,MAX_SIZE,isEmpty,canPush,push,pop) {
+  this.stackControl =[];
+  this.MAX_SIZE =0;
+  this.isEmpty = function() {
+// devuelve true o false porque es una comparacion
+  return this.stackControl.length === 0;
+
+  };
+  this.canPush = function(item){
+    this.stackControl.push(item);
+    return true;
+  };
+  this.push = function(stack){
+    
+      return stack;
+  };
+  this.pop = function (){
+
+  };
+
+
 }
