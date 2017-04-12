d25e324c25cab2b40f09c386c8778ca7c90fe4da
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-12 22:35:45 +0200

    1st & 2nd Iterations complete

---
+++ starter-code/js/stack.js
@@ -0,0 +1,23 @@
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 0;
+
+}
+
+Stack.prototype.isEmpty = function() {
+
+};
+
+Stack.prototype.canPush = function() {
+
+};
+
+Stack.prototype.push = function() {
+
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+var stack = new StackDataStructure(true);
