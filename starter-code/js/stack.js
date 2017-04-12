8106c58c4ffaa3ff56de631c2707095d02e1ff8d
Joana Rosa <joanamram@gmail.com>
2017-04-12 22:22:01 +0100

    falta jquery

---
+++ starter-code/js/stack.js
@@ -0,0 +1,22 @@
+/*$(document).ready(function(){
+
+  $(".stack-number").on("click",function(){
+      console.log(click);
+      $("#number" : first-child).css("background-color","purple");
+    })
+
+});*/
+
+
+/*$(window).load(function(){
+    $("#number").click(function () {
+        $(this).toggle("selected").css("background-color","purple");
+    });
+});*/
+
+
+
+$(".btn-blue").on("click", function() {
+  $(this).toggleClass("active");
+    $(".number:first-child").toggle()
+     });
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
+
 function StackDataStructure () {
+  this.stackControl=[];
+  this.MAX_SIZE=100;
+}
+
+StackDataStructure.prototype.isEmpty= function(){
+    if (this.stackControl.length == 0) {
+    return true;
+  } else{
+    return false;
+  }
+  };
+
+StackDataStructure.prototype.canPush= function(){
+  return (this.stackControl.length<this.MAX_SIZE)
+};
+
+StackDataStructure.prototype.push=function(number){
+  if (this.canPush()){
+    this.stackControl.push(number);
+    return this.stackControl;
+  }else{
+    return "Stack Overflow";
+  }
+}
+
+StackDataStructure.prototype.pop=function(number){
+  if (!this.isEmpty()) {
+    return this.stackControl[this.stackControl.length-1];
+  }else{
+    return "Stack Underflow";
+  }
 }
