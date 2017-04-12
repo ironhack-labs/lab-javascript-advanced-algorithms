743238bd7e21d3cc6a053018873fe0ad0ca07d84
Carlos Luis <clburguete@gmail.com>
2017-04-12 18:54:23 +0200

    htmlnotpainted

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
+
+//declare stack constructor function
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
 }
+//checks if stack is empty
+StackDataStructure.prototype.isEmpty = function(){
+    return this.stackControl.length === 0;
+};
+//check if free space
+StackDataStructure.prototype.canPush = function(){
+    return this.stackControl.length < this.MAX_SIZE;
+  };
+//push method
+StackDataStructure.prototype.push = function(number){
+    if (this.canPush()) {
+       this.stackControl.push(number);
+       return this.stackControl;
+    }else {
+      return "Stack Overflow";
+    }
+};
+//pop method
+StackDataStructure.prototype.pop = function(){
+  if (!this.isEmpty()) {
+    return this.stackControl.pop();
+  }else {
+    return "Stack Underflow";
+  }
+};
