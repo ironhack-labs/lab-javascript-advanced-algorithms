31b34e4466d4fae43b7ac5289ed013feaa4fad09
edsonjmv <edsonjmv@gmail.com>
2017-04-13 20:48:04 +0200

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
 function StackDataStructure () {
+this.stackControl = [];
+this.MAX_SIZE = 10;
 }
+
+StackDataStructure.prototype.isEmpty = function(){
+  var activeBars = this.stackControl.length;
+  if (activeBars === 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.canPush = function(){
+  var activeBars = this.stackControl.length;
+  if (activeBars < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.push = function(bar){
+  if (this.canPush()){
+    this.stackControl.push(bar);
+    return this.stackControl;
+  } else {
+    return "Stack Overflow";
+  }
+};
+
+StackDataStructure.prototype.pop = function(){
+  if (this.isEmpty()){
+    return "Stack Underflow";
+  } else {
+    return this.stackControl[this.stackControl.length - 1];
+  }
+};
