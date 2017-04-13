6441596ba0bb36d7fb46347a24242a3403461f51
sermartin <sermartin79@gmail.com>
2017-04-13 09:19:59 +0200

    Madrid_Sergio_V01

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,46 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function(){
+      return this.stackControl.length === 0;
+
+    };
+  this.canPush = function(){
+    if (this.stackControl.length < this.MAX_SIZE){
+      return true;
+    } else  {
+      return false;
+    }
+    /*refactorizando puedo hacer:
+    return this.stackControl.length < this.MAX_SIZE
+    si es menos me devolver true si no false
+    */
+    };
+  this.push = function(elements){
+    this.stackControl.push(elements);
+
+    if (this.stackControl.length > this.MAX_SIZE){
+      return "Stack Overflow";
+    }
+    return this.stackControl;
+  };
+
+
+  this.pop = function(){
+    this.stackControl.pop();
+    if(this.stackControl.length === 0){
+      return "Stack Underflow";
+    }
+  };
+
+
 }
+/*
+this.push = function(x){
+  this.stackControl.push(x);
+
+El codigo anterior es lo mismo qeu el siguiente:
+
+StackDataStructure.prototype.push = function(x){
+  this.stackControl.push(x);
+*/
