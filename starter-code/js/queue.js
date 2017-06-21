9db7f0f31452efbae2a54a68dd95251a3aee29ff
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-06-21 22:00:33 +0200

    needs fixing

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-var queue = new QueueDataStructure();
-var id = queue.MAX_SIZE;
+  var queue = new QueueDataStructure();
+  var upQueue = 1;
+  var downQueue = queue.MAX_SIZE;
 
   $('#add-queue').click(function(){
-    var response = queue.enqueue(id);
-    $("div.queue:nth-child(" + id + ")").css('background-color','blue');
-    id--;
+    var response = queue.enqueue(upQueue);
+    $("div.queue:nth-child(" + upQueue + ")").css('background-color','blue');
+    upQueue++;
     console.log(response);
+    if(upQueue > 11){
+      upQueue = 1;
+    }
   });
 
   $('#take-queue').click(function(){
-    id++;
-    $("div.queue:nth-child(" + id + ")").css('background-color','#e1e5ed');
+    $("div.queue:nth-child(" + downQueue + ")").css('background-color','#e1e5ed');
+    downQueue--;
     var response = queue.dequeue();
     console.log(response);
+    if(downQueue < 0){
+      upQueue = queue.MAX_SIZE;
+    }
   });
 });
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
   var stack = new StackDataStructure();
-  var id = stack.MAX_SIZE;
+  var downStack = stack.MAX_SIZE;
+  var upStack = 1;
 
   $('#add-stack').click(function(){
-    var response = stack.push(id);
-    $("div.stack:nth-child(" + id + ")").css('background-color','blue');
-    id--;
+    var response = stack.push(downStack);
+    $("div.stack:nth-child(" + downStack + ")").css('background-color','blue');
+    downStack--;
     console.log(response);
   });
-  
+
   $('#take-stack').click(function(){
-    id++;
-    $("div.stack:nth-child(" + id + ")").css('background-color','#e1e5ed');
+    $("div.stack:nth-child(" + upStack + ")").css('background-color','#e1e5ed');
+    upStack++;
     var response = stack.pop();
     console.log(response);
   });
