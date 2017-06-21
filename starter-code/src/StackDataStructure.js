50bd8a689e2940e6baa33d3d53be12ff3b6f7850
tyrannogina <tyrannogina@gmail.com>
2017-06-21 17:43:05 +0200

    Complete spec for the stack

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,26 @@
 function StackDataStructure () {
+  this.MAX_SIZE = 100;
+  this.stackControl = [];
+  this.isEmpty = function () {
+    return this.stackControl.length === 0;
+  };
+  this.canPush = function () {
+    return this.stackControl.length < this.MAX_SIZE;
+  };
+  this.push = function(element) {
+    console.log(this.canPush);
+    if (this.canPush()) {
+      this.stackControl[this.stackControl.length] = element;
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow';
+    }
+  };
+  this.pop = function() {
+    if (this.isEmpty()) {
+      return 'Stack Underflow';
+    } else {
+      return this.stackControl[this.stackControl.length - 1];
+    }
+  };
 }
