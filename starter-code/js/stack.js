82c5068fe5e554cb2f7329ae1fa72be72939abd6
Jorge Pedrejon <jgpedrejon@gmail.com>
2017-06-21 23:33:10 +0200

    done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ $(".allstack .btn .btn.add").click(function(){
+   $(".stack.boxes").css("background-color" , "blue");
+ });
+ $(".allstack .btn .btn.take").click(function(){
+  $(".stack.boxes").css("background-color" , "white");
+ });
+});
