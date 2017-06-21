cc0a04e2a2a236080e730c488b4a2b91a15b34b4
JB-Tellez <jb.tellez@gmail.com>
2017-06-21 15:59:31 +0200

    Tests pass for Stack and Queue

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
 function StackDataStructure () {
+
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+
+    this.isEmpty = function() {
+        return this.stackControl.length === 0;
+    }
+
+    this.canPush = function() {
+        return this.stackControl.length < this.MAX_SIZE;
+    }
+
+    this.push = function(val) {
+
+        if (this.canPush()) {
+
+            this.stackControl.push(val);
+
+            return this.stackControl;
+
+        } else {
+            return 'Stack Overflow';
+        }
+        
+    }
+
+    this.pop = function() {
+
+        if (this.isEmpty()) {
+
+            return 'Stack Underflow';
+
+        } else {
+            
+            return this.stackControl.pop();
+        }
+
+        
+    }
+
+
 }
