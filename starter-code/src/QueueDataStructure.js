3c39002a7b329bf83e0c498872d721ee9cfccbc5
Carlos Dueñas <carlosdss84@gmail.com>
2017-06-21 20:59:48 +0200

    version final

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+}
+
+function queueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 20;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+  return (this.queueControl.length == 0);
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  return (this.queueControl.length < this.MAX_SIZE);
+}
+
+QueueDataStructure.prototype.enqueue = function(element){
+  if (!this.canEnqueue()) {
+    return("Queue Overflow");
+  } else {
+    this.queueControl.unshift(element);
+    var itemQueued = [this.queueControl[0]];
+    return itemQueued;
+  }
+}
+
+QueueDataStructure.prototype.dequeue = function(element){
+  if (this.isEmpty()) {
+    return("Queue Underflow");
+  } else {
+    var firstItem = this.queueControl.pop();
+    return firstItem;
+  }
 }
