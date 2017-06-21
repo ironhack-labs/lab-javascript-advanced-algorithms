a9ca68c47646839e6134d4535b2219d7103d5319
Marc Palatsí <mpalatsi@me.com>
2017-06-21 18:22:07 +0200

    pasados los tests

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
 }
+
+function QueueDataStructure () {
+  this.queueControl = [],
+  this.MAX_SIZE = 8,
+  this.isEmpty = function() {
+    if (this.queueControl.length === 0) {
+      return true;
+    }else {
+      return false;
+    }
+  },
+  this.canEnqueue = function() {
+    // if (this.stackControl.length < this.MAX_SIZE) {
+    //   return true;
+    // }else {
+    //   return false;
+    // }
+    return this.queueControl.length < this.MAX_SIZE;
+  },
+  this.enqueue = function(element) {
+    if (this.canEnqueue()) {
+    this.queueControl.unshift(element);
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+
+  },
+  this.dequeue = function() {
+    if (this.isEmpty() === false) {
+      return this.queueControl[this.queueControl.length-1];
+    }else {
+      return 'Queue Underflow';
+    }
+  }
+}
