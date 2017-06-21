bec36e9aec12ff438c7952de77e1699729db3029
Juan Macias <yo@soyjuanmacias.com>
2017-06-21 19:03:49 +0200

    Stacking oferflow in my land means 'te has pasao colega' - Working good the JS & Jasmine

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 15;
 }
+
+StackDataStructure.prototype.isEmpty = function(){
+  return this.stackControl.length === 0;
+};
+
+StackDataStructure.prototype.canPush = function() {
+  if (this.stackControl.length < this.MAX_SIZE) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.push = function(e) {
+  if (this.canPush()) {
+    this.stackControl.push(e);
+    return this.stackControl;
+  } else {
+    return "Stack Overflow";
+  }
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.isEmpty()) {
+    return "Stack Underflow";
+  }
+  return this.stackControl.pop();
+};
