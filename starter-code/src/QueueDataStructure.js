e7e3348ec00c51861fdc56e1469fe8ab7f39fafe
Maria Mayer <mayermaru@gmail.com>
2017-06-21 22:16:51 +0200

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,32 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+    
+    this.isEmpty = function () {
+        return this.queueControl.length === 0;
+    };
+
+    this.canEnqueue = function () {
+        return this.queueControl.length < this.MAX_SIZE;
+    };
+
+    this.enqueue = function (val) {
+
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(val);
+            return this.queueControl;
+        } else {
+            return 'Queue Overflow';
+        }
+    };
+
+    this.dequeue = function (val) {
+
+        if (this.isEmpty()) {
+            return 'Queue Underflow';
+        } else {
+            return this.queueControl.pop(val);
+        }
+    };
 }
