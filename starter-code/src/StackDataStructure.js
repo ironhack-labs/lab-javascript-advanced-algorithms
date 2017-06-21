6466f7eab9d7824e72e4d7ef08e448c09bcacd39
Jordi Manchon <jordimanchon@me.com>
2017-06-21 18:25:59 +0200

    Test passed (:

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
+  this.isEmpty = function() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.canPush = function() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.push = function(element) {
+    if (this.canPush()) {
+      this.stackControl.push(element);
+      return (this.stackControl);
+    } else {
+      return "Stack Overflow";
+    }
+  };
+  this.pop = function() {
+    if (!this.isEmpty()) {
+      return (this.stackControl[this.stackControl.length-1]);
+    } else {
+      return "Stack Underflow";
+    }
+  };
 }
