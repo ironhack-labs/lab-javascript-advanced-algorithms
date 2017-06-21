f590317a3bcc01da6e813e41256ee3db689b0282
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-06-21 17:17:36 +0200

    logic done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,46 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  var response;
+  if(this.queueControl.length === 0){
+    response = true;
+  } else {
+    response = false;
+  }
+  return response;
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  var response;
+  if(this.queueControl.length < this.MAX_SIZE){
+    response = true;
+  } else {
+    response = false;
+  }
+  return response;
+};
+
+QueueDataStructure.prototype.enqueue = function(element) {
+  var response,
+      canEnqueue = this.canEnqueue();
+  if(canEnqueue){
+    this.queueControl.unshift(element);
+    response = this.queueControl;
+  } else {
+    response = "Queue Overflow";
+  }
+  return response;
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  var response;
+  if(this.queueControl.length !== 0){
+    response = this.queueControl.pop();
+  } else {
+    response = 'Queue Underflow';
+  }
+  return response;
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  var response;
+  if(this.stackControl.length === 0){
+    response = true;
+  } else {
+    response = false;
+  }
+  return response;
+};
+
+StackDataStructure.prototype.canPush = function() {
+  var response;
+  if(this.stackControl.length < this.MAX_SIZE){
+    response = true;
+  } else {
+    response = false;
+  }
+  return response;
+};
+
+StackDataStructure.prototype.push = function(element) {
+  var canPush = this.canPush(),
+      response;
+  if(canPush){
+    this.stackControl.push(element);
+    response = this.stackControl;
+  } else {
+    response = "Stack Overflow";
+  }
+  return response;
+};
+
+StackDataStructure.prototype.pop = function() {
+  var response;
+  if(this.stackControl.length !== 0){
+    response = this.stackControl.pop();
+  } else {
+    response = 'Stack Underflow';
+  }
+  return response;
+};
