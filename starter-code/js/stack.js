6c228af8b325bfdda7c2d9b315322f242a39ddaf
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-21 20:36:06 +0200

    MAD Claudia Duarte

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,11 +1,24 @@
-$(document).ready( function() {
-$("add.stack").click(function(){
+$(document).ready(function(){
+
+  var stack;
+  $('.stack .add').on('click', function() {
+    stack = new StackDataStructure();
+    stack.isEmpty();
+    stack.canPush();
+    stack.push(0);
+
+    $(".stack .unpint:last").addClass('active');
+    $(".stack .unpint:last").removeClass('disabled');
+  });
+
+  $('.stack .take').on('click', function() {
+    stack = new StackDataStructure();
+    stack.isEmpty();
+    stack.canPush();
+    stack.pop();
+
+    $(".stack .pint:first").addClass('disabled');
+    $(".stack .pint:first").removeClass('active');
+  });
 
 });
-
-
-
-
-
-
-} );
