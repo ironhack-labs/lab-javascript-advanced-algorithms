ada485f473032ca66149883ceddbce7bf3357b01
rosadl <rosadl84@gmail.com>
2017-06-22 09:12:44 +0200

    First version

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,42 @@
-function QueueDataStructure () {
-}
+  function QueueDataStructure() {
+    this.queueControl = [];
+    this.MAX_SIZE = parseInt();
+
+
+    QueueDataStructure.prototype.isEmpty = function() {
+      if (this.queueControl.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    QueueDataStructure.prototype.canEnqueue = function() {
+      if (this.queueControl.length === this.MAX_SIZE) {
+
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    QueueDataStructure.prototype.enqueue = function(a) {
+      if (this.canEnqueue()) {
+        this.queueControl.unshift(a);
+        return this.queueControl;
+      } else {
+        return "Queue Overflow";
+      }
+
+    };
+
+    QueueDataStructure.prototype.dequeue = function() {
+      if (this.isEmpty()) {
+        return 'Queue Underflow';
+      } else {
+
+        return this.queueControl.pop();
+      }
+
+    };
+  }
