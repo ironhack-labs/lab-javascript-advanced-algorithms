82134865557769b93df968623ff978a5382313e8
Salvatore Dziekan <sdziekan@gmail.com>
2017-07-05 19:44:07 +0200

    Paris Salvatore-Dziekan, not finished yet

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
 function StackDataStructure () {
-}
+    // var stack = [];
+    this.stackControl = [];
+    this.MAX_SIZE = 42;
+    this.isEmpty = function () {
+      if (this.stackControl.length === 0) {
+        return true;
+      } else return false;
+    };
+    this.canPush = function () {
+      if (this.stackControl.length < this.MAX_SIZE) {
+        return true;
+      } else {
+        return  false;
+      }
+    }
+    this.push = function (a) {
+      if (this.canPush() === true) {
+        this.stackControl.push(a);
+      } else {
+        return "Stack is full"
+      }
+    }
+    this.pop = function () {
+      if (this.stackControl.length > 0) {
+        this.stackControl.pop();
+      } else {
+        return "Nothing to remove, Stack is empty"
+      }
+    }
+
+
+};
+
+// StackDataStructure.prototype.stackControl = [];
+// StackDataStructure.prototype.MAX_SIZE = 42;
+
+// var MAX_SIZE = 5;
+// stack.length = MAX_SIZE;
+
+var stack = new StackDataStructure();
+console.log(stack);
+console.log(stack.MAX_SIZE);
+console.log(stack.MIN_SIZE);
+
+
+// stack.stackConrol.push(1);
