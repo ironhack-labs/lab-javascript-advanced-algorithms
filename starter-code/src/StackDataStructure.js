72f9c2a889e8e5ba435ee82d20fe35620f92e5bd
pablo fuentes <pablofuentesg1@yahoo.es>
2017-08-23 19:53:49 +0200

    faltan 3 del test

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,19 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 25;
+  this.isEmpty = function() {
+    return this.stackControl.length == 0 ? true : false;
+  };
+  this.canPush = function() {
+    return this.stackControl.length == this.MAX_SIZE ? false : true;
+  };
+
+  this.push = function(element) {
+    var m = this.stackControl.length;
+    this.stackControl[m] = element;
+    return this.stackControl;
+
+  };
+  this.pop = function() {};
+return this.stackControl.pop();
 }
