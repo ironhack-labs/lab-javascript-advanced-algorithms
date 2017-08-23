a674d1c2e2e2b24d804b1598da12efbaac9fdf07
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-08-23 23:15:08 +0200

    more or less..

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE=10;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+  if(this.queueControl.length == 0){
+
+   return true;
+ }else{
+   return false;
+ }
+};
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  if(this.MAX_SIZE==this.queueControl.length){
+    return false;
+  }else{
+    return true;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(value){
+  if(this.MAX_SIZE==this.queueControl.length){
+    return "Queue Overflow";
+  }else{
+  this.queueControl.unshift(value);
+  }
+  return this.queueControl;
+};
+
+QueueDataStructure.prototype.dequeue = function(value){
+  if(this.queueControl.length==0){
+    return "Queue Underflow";
+  }
+    return this.queueControl.pop(value);
+};
