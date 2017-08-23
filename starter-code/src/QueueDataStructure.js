8cbece9cfe94980324931ee4a02a283c670b3e35
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-08-23 19:38:31 +0200

    testing done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,21 @@
 function QueueDataStructure () {
+  this.queueControl = []
+  this.MAX_SIZE = 1
+}
+
+QueueDataStructure.prototype.isEmpty = function() {
+  return this.queueControl.length == 0 ? true : false 
+}
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  return this.queueControl.length < this.MAX_SIZE ? true : false 
+}
+
+QueueDataStructure.prototype.enqueue = function(x) {
+  this.queueControl.unshift(x) 
+  return this.queueControl.length > this.MAX_SIZE ? 'Queue Overflow' : this.queueControl
+}
+
+QueueDataStructure.prototype.dequeue = function() {
+  return this.queueControl.length == 0 ? 'Queue Underflow' : this.queueControl[this.queueControl.length - 1]
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,23 @@
 function StackDataStructure () {
+  this.stackControl = [] 
+  this.MAX_SIZE = 1
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length > 0 ? false : true
+}
+
+StackDataStructure.prototype.canPush = function() {
+  return this.stackControl.length >= this.MAX_SIZE ? false : true
+}
+
+StackDataStructure.prototype.push = function(x) {
+  this.stackControl.push(x)
+  return this.stackControl.length > this.MAX_SIZE ? 'Stack Overflow' : this.stackControl
+}
+
+
+StackDataStructure.prototype.pop = function() {
+  return this.stackControl.length == 0 ? 'Stack Underflow' : this.stackControl[this.stackControl.length - 1] 
+}
+
