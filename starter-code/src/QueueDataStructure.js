b3f466e0fb4ae639e641820eb270aa7f6ddf19c9
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-23 20:10:40 +0200

    Queue error -> reverse array

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -4,18 +4,20 @@ function QueueDataStructure () {
 }
 
 QueueDataStructure.prototype.isEmpty = function () {
-  return (this.queueControl <= 0) ? true : false
+  return (this.queueControl.length === 0) ? true : false
 }
 
 QueueDataStructure.prototype.canEnqueue = function () {
-  return (this.MAX_SIZE == this.queueControl) ? true : false
+  return (this.queueControl.length === this.MAX_SIZE) ? false : true
 }
 
 QueueDataStructure.prototype.enqueue = function (a) {
-  this.queueControl.push(a)
-  if (this.canEnqueue()) { return this.queueControl } else { return 'Queue Overflow'}
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(a)
+    return this.queueControl
+   } else { return 'Queue Overflow'}
 }
 
 QueueDataStructure.prototype.dequeue = function () {
-  if (this.isEmpty()) { return 'Queue Underflow' } else { return this.queueControl }
+  if (this.isEmpty()) { return 'Queue Underflow' } else { return this.queueControl.pop() }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,21 +1,24 @@
 function StackDataStructure () {
   this.stackControl = [ ]
-  this.MAX_SIZE = 0
+  this.MAX_SIZE = 10
 }
 
 StackDataStructure.prototype.isEmpty = function () {
-  return (this.stackControl <= 0) ? true : false
+  return (this.stackControl.length === 0) ? true : false
 }
 
 StackDataStructure.prototype.canPush = function () {
-  return (this.MAX_SIZE <= this.stackControl) ? true : false
+  return (this.stackControl.length === this.MAX_SIZE ) ? false : true
 }
 
 StackDataStructure.prototype.push = function (a) {
-  this.stackControl.push(a)
-  if (this.canPush()) { return this.stackControl  } else { return 'Stack Overflow' }
+
+  if (this.canPush()) {
+    this.stackControl.push(a)
+    return this.stackControl
+  } else { return 'Stack Overflow' }
 }
 
-StackDataStructure.prototype.pop = function (a) {
-  if (this.isEmpty()) { return 'Stack Underflow' } else { return this.stackControl.pop(a) }
+StackDataStructure.prototype.pop = function () {
+  if (this.isEmpty()) { return 'Stack Underflow' } else { return this.stackControl.pop() }
 }
