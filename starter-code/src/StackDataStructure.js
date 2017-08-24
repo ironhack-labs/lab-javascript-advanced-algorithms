f72fe65f09333e7db2fb257d62dc7eb1451a4b92
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-08-24 07:59:52 +0200

    first commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 1;
 }
+
+var stack = new StackDataStructure();
+
+StackDataStructure.prototype.isEmpty = function(){
+
+return (this.stackControl.length > 0) ?  false : true;
+
+};
+
+
+StackDataStructure.prototype.canPush = function(){
+  return (this.stackControl.length > this.MAX_SIZE) ?  false : true;
+
+};
+
+StackDataStructure.prototype.push = function(a){
+  this.stackControl.push(a);
+  return this.stackControl;
+
+};
+
+
+StackDataStructure.prototype.pop = function(){
+
+};
