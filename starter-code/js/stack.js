a745e2a308918baf438de4c998ac93b6305c6b6e
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-24 10:00:25 +0200

    Iteration 3 - ADD Done

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-  var stack = new StackDataStructure();
+  var stack = new StackDataStructure()
 
-  $("#stack-add").on("click", function() {
+  $("#stack-add").on("click", function(e) {
+    e.preventDefault()
     if (stack.canPush()) {
-      var a = $("#stack-value").val()
-      console.log(a)
-      stack.push(a)
-
+      var $a = $("#stack-value").val()
+      stack.push($a)
       var b = ".block-" + stack.stackControl.length
-      console.log(b)
       $(b).removeClass("grey")
       $(b).addClass("btn-primary")
-
+      $.each(stack.stackControl, function(a, i){
+          $(b).html(i)
+      })
+    } else {
+      $('.block-8').removeClass('grey')
+      $('.block-8').addClass('red')
+      $('.block-8').html(stack.push())
     }
   })
 })
