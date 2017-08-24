7f4ed8437951aeb3caf4f48acd292d93d6a934e3
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-08-24 09:54:52 +0200

    TERMINADO

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 
-      $('.add-queue').on("click", function() {
-        var add = $('.form-queue').val();
-        if (queue.canEnqueue()) {
-          queue.enqueue(add);
+  $('.add-queue').on("click", function() {
+    var add = $('.form-queue').val();
+    if (queue.canEnqueue()) {
+      queue.enqueue(add);
+      console.log(queue.queueControl);
 
-          $('.content-queue').children().each(function(index) {
-            if (queue.queueControl[index]) {
-              $(this).html(queue.queueControl[index]).removeClass("queue-grey queue-underflow").addClass("queue-div");
-            } else {
-              $(this).html("").removeClass("queue-underflow").addClass("queue-grey");
-            }
-          });
-
+      $($('.content-queue').children().get().reverse()).each(function(index) {
+        if (queue.queueControl[index]) {
+          $(this).html(queue.queueControl[queue.queueControl.length - index - 1]).removeClass("queue-grey queue-underflow").addClass("queue-div");
         } else {
-          $('.content-queue').children().html("Queue Overflow").removeClass('queue-grey queue-div').addClass("queue-underflow");
+          $(this).html("").removeClass("queue-underflow").addClass("queue-grey");
         }
-
       });
+
+    } else {
+      $('.content-queue').children().html("Queue Overflow").removeClass('queue-grey queue-div').addClass("queue-underflow");
+    }
+
+  });
 
   $('.take-queue').on("click", function() {
     if (queue.isEmpty()) {
@@ -26,7 +27,7 @@ $(document).ready(function() {
       queue.dequeue();
       console.log(queue.queueControl);
 
-      $('.content-queue').children().each(function(index) {
+      $($('.content-queue').children().get().reverse()).each(function(index) {
         if (queue.queueControl[index]) {
           $(this).html(queue.queueControl[index]).removeClass("queue-grey queue-underflow").addClass("queue-div");
         } else {
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 
-      $('.add-stack').on("click", function() {
-        var add = $('.form-stack').val();
-        if (stack.canPush()) {
-          stack.push(add);
+  $('.add-stack').on("click", function() {
+    var add = $('.form-stack').val();
+    if (stack.canPush()) {
+      stack.push(add);
 
 
-          $($('.content-stack').children().get().reverse()).each(function(index) {
-            if (stack.stackControl[index]) {
-              $(this).html(stack.stackControl[index]).removeClass("grey-div overflow").addClass("blue-div");
-            } else {
-              $(this).html("").removeClass("blue-div overflow").addClass("grey-div");
-            }
-          });
+      $($('.content-stack').children().get().reverse()).each(function(index) {
+        if (stack.stackControl[index]) {
+          $(this).html(stack.stackControl[index]).removeClass("grey-div overflow").addClass("blue-div");
         } else {
-          $('.content-stack').children().html("STACK OVERFLOW").removeClass("blue-div grey-div").addClass("overflow");
+          $(this).html("").removeClass("blue-div overflow").addClass("grey-div");
         }
-
-
       });
+    } else {
+      $('.content-stack').children().html("STACK OVERFLOW").removeClass("blue-div grey-div").addClass("overflow");
+    }
+
+
+  });
   $('.take-stack').on("click", function() {
     if (stack.isEmpty()) {
       $('.content-stack').children().html("Stack Underflow").removeClass("grey-div").addClass("overflow");
