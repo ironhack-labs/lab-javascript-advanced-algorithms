763ef42e6d7be0fbbb318070f444284b4f6f80c3
trigoporres <santiagotrigoporres@gmail.com>
2017-08-24 09:13:33 +0200

    Acabado testing completo

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 5;
 }
+
+QueueDataStructure.prototype.isEmpty = function (){
+  return this.queueControl.length == 0 ? true : false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function (){
+  if (this.queueControl.length === this.MAX_SIZE) {
+    return false;
+  }
+  else{
+    return true;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function (a){
+
+  if (this.queueControl.length < this.MAX_SIZE) {
+    this.queueControl.unshift(a);
+    return this.queueControl;
+   }
+   else{
+     return "Queue Overflow";
+   }
+};
+
+QueueDataStructure.prototype.dequeue = function (){
+  if (this.queueControl.length === 0) {
+
+    return "Queue Underflow";
+
+   }
+   else{
+     return this.queueControl.pop();
+   }
+};
