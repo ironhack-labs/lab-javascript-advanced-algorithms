9c005f59d0ad1938377b62c2786704951c5e00d0
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-24 08:08:32 +0200

    Changing stack.js

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,8 +1,17 @@
-$(document).ready(function () {
-  var stackStructure = new StackDataStructure()
+$(document).ready(function() {
+  var stack = new StackDataStructure();
 
-  $('#add-stack', function () {
-    var a = $(this).attr('#input-text').val()
-    console.log(a)
+  $("#stack-add").on("click", function() {
+    if (stack.canPush()) {
+      var a = $("#stack-value").val()
+      console.log(a)
+      stack.push(a)
+
+      var b = ".block-" + stack.stackControl.length
+      console.log(b)
+      $(b).removeClass("grey")
+      $(b).addClass("btn-primary")
+
+    }
   })
 })
