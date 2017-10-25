24100b9e3944dbc840234db9eaffe19fd2e6fbb7
acampos91 <acampos91@hotmail.com>
2017-10-25 21:23:25 +0200

    Stack funciones, html y CSS

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
 }
+
+StackDataStructure.prototype.isEmpty = function(){
+ return this.stackControl.length === 0;
+
+};
+
+StackDataStructure.prototype.canPush = function(){
+if(this.stackControl.length >= this.MAX_SIZE){
+  return false;
+} else {
+  return true;
+}
+};
+
+StackDataStructure.prototype.push = function (e){
+   if(this.stackControl.length == this.MAX_SIZE) {
+     return "Stack Overflow";
+  } else {
+    this.stackControl.push(e);
+}
+return this.stackControl;
+};
+StackDataStructure.prototype.pop = function (){
+if (this.stackControl.length === 0){
+  return 'Stack Underflow';
+} else{
+  return this.stackControl[this.stackControl.length - 1]
+}
+};
