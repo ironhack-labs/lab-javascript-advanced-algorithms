45ac53ac31b8f5b66b4ecd5b8794346a8debabd0
Isak La Fleur <isak.engdahl@gmail.com>
2017-10-26 00:20:05 +0200

    fixed the StackDataStructure and part of QueueDataStructure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,14 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 100;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,37 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 100;
+  // check if we can add new elements to the stack (to avoid the stack overflow)
+  // check if we can take an element from the stack (to avoid the stack underflow)
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.canPush = function() {
+  return this.stackControl < this.MAX_SIZE;
+};
+
+StackDataStructure.prototype.push = function(item) {
+  if (this.stackControl.length === this.MAX_SIZE) {
+    return "Stack Overflow";
+  } else {
+    this.stackControl.push(item);
+    return [item];
+  }
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.stackControl.length === 0) {
+    return "Stack Underflow";
+  } else {
+    const lastValueInStack = this.stackControl[this.stackControl.length - 1];
+    this.stackControl.pop();
+    return lastValueInStack;
+  }
+};
