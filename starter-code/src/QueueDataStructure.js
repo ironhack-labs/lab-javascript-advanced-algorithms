b3fe933e9a203e039392ee4434b3d7bd44d23275
popelini <ccastillon92@gmail.com>
2017-10-25 23:02:17 +0200

    Finished with bonuses

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,31 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+    if(this.queueControl.length == 0) return true;
+    return false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+    if(this.queueControl.length < this.MAX_SIZE) return true;
+      return false;
+
+};
+
+QueueDataStructure.prototype.enqueue = function(element) {
+    if(this.canEnqueue()) {
+      this.queueControl.unshift(element);
+      return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  if(this.queueControl.length == 0) return "Queue Underflow";
+  var item = this.queueControl[this.queueControl.length - 1];
+  this.queueControl.pop();
+  return item;
+};
