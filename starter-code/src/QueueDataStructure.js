b84b835ca97bbf14c757bc1cfcb819ec6d0b129d
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-25 22:13:33 +0200

    first iteration done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
+var queue;
+
+
 function QueueDataStructure () {
+this.queueControl=[];
+this.MAX_SIZE= 9;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+ return this.queueControl.length === 0;
+};
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  return this.queueControl.length < this.MAX_SIZE;
+};
+
+QueueDataStructure.prototype.enqueue = function(a){
+  this.queueControl.unshift(a);
+  return this.queueControl.length < this.MAX_SIZE;
+};
+
+QueueDataStructure.prototype.dequeue = function(){
+  return Underflow;
+ };
