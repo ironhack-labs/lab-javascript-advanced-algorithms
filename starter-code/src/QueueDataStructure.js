39b07377bb05482eaa0b4d04de7681ba7606c9d0
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-10-25 20:34:56 +0200

    finished second iteration

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,41 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 10;
+    }
+    isEmpty() {
+        if (this.queueControl.length === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    canEnqueue() {
+        if (this.queueControl.length < this.MAX_SIZE) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    enqueue(element) {
+        if (this.queueControl.length == this.MAX_SIZE) {
+            return "Queue Overflow";
+        }
+        else {
+            this.queueControl.unshift(element);
+            return this.queueControl;
+        }
+    }
+    dequeue() {
+        if (this.isEmpty()) {
+            return 'Queue Underflow';
+        }
+        else {
+            return this.queueControl[this.queueControl.length-1];
+        }
+    }
 }
+
+
