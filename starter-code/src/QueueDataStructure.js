9e741316afd8120e2574e691e605dbecb46648f4
Camila Maya <camilamaya21@gmail.com>
2017-10-26 00:00:14 +0200

    1st Iteration Completed, 2nd Iteration 95% completed: 1 Spec left (line 159 in the DataStructureSpec.js)

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
-function QueueDataStructure () {
-}
+//2nd Iteration
+
+//Queues
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+};
+
+//this method check if the queue is empty
+QueueDataStructure.prototype.isEmpty = function() {
+  return this.queueControl.length == 0;
+};
+
+//this method check if we can add a new element in the queue
+QueueDataStructure.prototype.canEnqueue = function() {
+  return this.queueControl.length < this.MAX_SIZE;
+};
+
+//this method allow us to enqueue elements into the queue
+QueueDataStructure.prototype.enqueue = function(e) {
+this.queueControl.push(e)
+  if (this.canEnqueue()) {
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+};
+
+
+QueueDataStructure.prototype.dequeue = function(e) {
+     if (this.isEmpty()) {
+     return 'Queue Underflow';
+   } else {
+     return this.queueControl.shift(e);
+   }
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,32 +1,39 @@
+//1st Iteration
+
+//Stack
 function StackDataStructure() {
   this.stackControl = [];
   this.MAX_SIZE = 8;
 };
 
+//this method check if the stack is empty
 StackDataStructure.prototype.isEmpty = function() {
   return this.stackControl.length == 0;
 };
 
+//this method check if we can add a new element in the stack
 StackDataStructure.prototype.canPush = function() {
   return this.stackControl.length < this.MAX_SIZE;
 };
 
+//this method let us add new elements to the stack:
+//1. check if we can add the elements, if is TRUE... will add the element
+// if is FALSE, will return "Stack Overflow"
 StackDataStructure.prototype.push = function(e) {
   this.stackControl.push(e);
-
-  if (this.canPush()){
-      return this.stackControl;
+  if (this.canPush()) {
+    return this.stackControl;
   } else {
-        return "Stack Overflow";
+    return "Stack Overflow";
   }
 };
 
+//This method let us delete elements from the stack
 StackDataStructure.prototype.pop = function() {
-   this.stackControl[this.stackControl.length -1] ;
-
-   if (this.isEmpty()){
-     return 'Stack Underflow';
-   } else {
-     return this.stackControl.pop();
-   }
+  this.stackControl[this.stackControl.length - 1];
+  if (this.isEmpty()) {
+    return 'Stack Underflow';
+  } else {
+    return this.stackControl.pop();
+  }
 };
