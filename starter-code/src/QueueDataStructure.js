07a376324e77d1ff4131291434abfa74bef76b76
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-25 22:19:40 +0200

    iteration 1 stack

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,11 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.queue.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+
+};
+QueueDataStructure.prototype.canEnqueue = function(){
+
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+};
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length == 0) {
+    return true;
+} else {
+  return false;
 }
+};
+
+StackDataStructure.prototype.canPush = function() {
+  if(this.stackControl.length < this.MAZ_SIZE){
+  return true;
+} else {
+ return false;
+}
+};
+
+StackDataStructure.prototype.push = function(number) {
+  if (this.stackControl.length >= this.MAX_SIZE) {
+    return "Stack Overflow";
+  }  this.stackControl.push(number);
+  return this.stackControl;
+};
+
+StackDataStructure.prototype.pop = function() {
+  if(this.stackControl.length == 0){
+    return "Stack Underflow";
+  };
+var number = this.stackControl[this.stackControl.length-1];
+this.stackControl.pop();
+return number;
+};
+
+
+
+//stackDataStructure.prototype
