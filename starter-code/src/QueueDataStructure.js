890f2f61b3feadd79713ce7806bca21198a534c8
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-10-25 18:33:47 +0200

    all jasmine test complete

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+  if (this.queueControl.length == 0){
+    return true;
+  }else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  if (this.queueControl.length < this.MAX_SIZE){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(item){
+  if (this.canEnqueue()){
+    this.queueControl.unshift(item);
+    return [this.queueControl[0]];
+  }else{
+    return 'Queue Overflow';
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function(){
+  if(this.isEmpty()){
+    return 'Queue Underflow';
+  }
+  else{
+    return this.queueControl.pop();
+  }
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
-}
+function StackDataStructure(){
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+};
+
+StackDataStructure.prototype.isEmpty = function(){
+  if (this.stackControl.length == 0){
+    return true;
+  }else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.canPush = function(){
+  if (this.stackControl.length < this.MAX_SIZE){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+StackDataStructure.prototype.push = function(item){
+  if (this.canPush()){
+    this.stackControl.push(item);
+    return [this.stackControl[this.stackControl.length - 1]];
+  }else{
+    return 'Stack Overflow';
+  }
+};
+
+StackDataStructure.prototype.pop = function(){
+  if(this.isEmpty()){
+    return 'Stack Underflow';
+  }
+  else{
+    return this.stackControl.pop();
+  }
+};
