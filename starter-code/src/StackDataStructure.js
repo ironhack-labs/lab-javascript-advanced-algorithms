5220df77a3a0f90bd50880ee51ec2fbe5537e8ea
Camila Maya <camilamaya21@gmail.com>
2017-10-25 20:03:58 +0200

    16 Specs completed

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
-}
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
+};
+
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length == 0;
+};
+
+StackDataStructure.prototype.canPush = function() {
+  return this.stackControl.length < this.MAX_SIZE;
+};
+
+StackDataStructure.prototype.push = function(e) {
+  this.stackControl.push(e);
+
+  if (this.canPush()){
+      return this.stackControl;
+  } else {
+        return "Stack Overflow";
+  }
+};
+
+StackDataStructure.prototype.pop = function() {
+   this.stackControl[this.stackControl.length -1] ;
+
+   if (this.isEmpty()){
+     return 'Stack Underflow';
+   } else {
+     return this.stackControl.pop();
+   }
+};
