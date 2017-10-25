f8e3e0258c52a9a0cbff99e8f5664eff3d9a9374
Abraham Castro <cucharadepalo@gmail.com>
2017-10-26 00:04:17 +0200

    Methods in prototype

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,35 +1,39 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
   this.queueControl = [];
   this.MAX_SIZE = 8;
-  this.isEmpty = function(){
-    if (this.queueControl.length == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  this.canEnqueue = function(){
-    if (this.queueControl.length < this.MAX_SIZE) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  this.enqueue = function(e){
-    if (this.queueControl.length < this.MAX_SIZE) {
-      this.queueControl.unshift(e);
-      return this.queueControl;
-    } else {
-      return 'Queue Overflow';
-    }
-  };
-  this.dequeue = function(){
-    if (this.isEmpty()) {
-      return 'Queue Underflow';
-    } else {
-      e = this.queueControl[this.queueControl.length - 1];
-      this.queueControl.pop();
-      return e;
-    }
-  };
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(e) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(e);
+    return this.queueControl;
+  } else {
+    return 'Queue Overflow';
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  if (this.isEmpty()) {
+    return 'Queue Underflow';
+  } else {
+    e = this.queueControl[this.queueControl.length - 1];
+    this.queueControl.pop();
+    return e;
+  }
+};
