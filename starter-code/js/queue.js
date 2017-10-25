f9c8fc3d64389e86dbaf3da2a6b7598f857d1398
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-10-25 23:47:05 +0200

    little bug fixed

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -18,11 +18,11 @@ $('document').ready(function(){
     if (result == 'Queue Overflow'){
           console.log($('.queuelist .queue:first'));
         $('.queue:first').addClass('warning');
-        $('.queue:first > p').html('Queue Overflow');
+        $('.queue:first').html('Queue Overflow');
     }
     else{
       $('.queue').removeClass('warning');
-      $('.queue.empty:last > p').html(result);
+      $('.queue.empty:last').html(result);
       $('.queue.empty:last').toggleClass("empty full");
     }
   });
@@ -31,17 +31,26 @@ $('document').ready(function(){
     var items = myQueue.dequeue();
     if(items == 'Queue Underflow'){
       $('.queue:last').addClass('warning');
-      $('.queue:last > p').html('Queue Overflow');
+      $('.queue:last').html('Queue Overflow');
       // Paint last item red
 
     }else{
+      var arrReversed = [];
       $('.queue').removeClass('warning');
-      $('.queue.full:first > p').html('EMPTY');
       $('.queue.full').toggleClass('empty full');
+      myQueue.queueControl.forEach(function(e){
+        if (e==""){
+          e="NO DATA";
+        }
+        arrReversed.push(e);
+      });
+      arrReversed.reverse();
+      console.log("arrReversed vale: "+arrReversed+'y queueControl: '+myQueue.queueControl);
+      // console.log(arrReversed);
       for (var z = 0; z < myQueue.queueControl.length; z++){
-        $('.queue.empty:last').toggleClass('empty full');
-        // myQueue.queueControl.queueControl[z];
-      }
+         $('.queue.empty:last').html(arrReversed[z]).toggleClass('empty full');;
+       }
+       $('.queue.empty').html('EMPTY');
       console.log(items);
       if (items == ""){
           items = "NO DATA";
