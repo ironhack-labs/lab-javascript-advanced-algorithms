8c8c5937184013ca952e9c206e41535a09dc4bbb
gemaderus <hi@gemaderus.com>
2017-10-26 09:26:57 +0200

    Create warnings in stacks and fixed heigth.

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -9,14 +9,15 @@ function init() {
     if(stack.push(input.val()) !== "Stack Overflow")  {
       console.log("you have space");
       if(stack.stackControl.length === 1) {
-        item.last().addClass("is-selected").text(input.val());
-
+        item.last().addClass("is-selected").text(input.val()).removeClass("warning");
       } else {
         $(".is-selected:first").prev().addClass("is-selected").text(input.val());
       }
 
     } else {
       console.log("Stack Overflow");
+      item.first().addClass("warning").text("Stack overflow");
+      // $(".is-selected:first").addClass("is-selected").text(input.val();
     }
 
   })
@@ -28,7 +29,6 @@ function init() {
     } else {
       $(".is-selected").first().removeClass("is-selected").contents().remove();
       stack.pop();
-
     }
   });
 }
