193f67621717214f06251033eb87db2482a9ec41
Virginia Álvarez <virginiaalvarez@MacBook-Pro-de-Virginia.local>
2017-10-26 09:33:23 +0200

    1ª interación pendiente terminar

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
 function StackDataStructure () {
+
+this.stackControl = [];
+this.MAX_SIZE = 5;
+};
+
+StackDataStructure.prototype.isEmpty = function isEmpty(){
+if (this.stackControl.length = 0){
+    return true;
+  }else {
+    return false;
+  }
+};
+StackDataStructure.prototype.canPush = function canPush(){
+  if (this.stackControl.lenth < this.MAX_SIZE){
+    return true;
 }
+  };
+StackDataStructure.prototype.push = function(name) {
+  if(this.stackControl.length == this.MAX_SIZE){
+    return "Stack Overflow";
+  }else{
+    this.stackControl.push(name);
+  }
+    return this.stackControl;
+
+};
+
+StackDataStructure.prototype.pop = function(){
+
+};
