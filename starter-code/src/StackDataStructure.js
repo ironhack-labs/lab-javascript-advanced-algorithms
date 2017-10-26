2c75ea253fb994a8b83beb8316e3f3024c5caae8
Asier Cisneros <ascisneros14@gmail.com>
2017-10-26 02:14:40 +0200

    Stack done Queue only jasmine

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,25 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
 }
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length === 0;
+};
+StackDataStructure.prototype.canPush = function() {
+  return this.stackControl.length < this.MAX_SIZE;
+};
+StackDataStructure.prototype.push = function(qty) {
+  if(this.canPush()) {
+    this.stackControl.push(qty);
+    return this.stackControl;
+  } else {
+    return "Stack Overflow";
+  }
+};
+StackDataStructure.prototype.pop = function() {
+  if(this.stackControl.length === 0) {
+    return 'Stack Underflow';
+  } else {
+    return this.stackControl.pop();
+  }
+};
