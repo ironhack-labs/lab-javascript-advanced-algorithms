63d31acfefbb1763e33c8116e50a5c548336fdef
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-10-26 11:48:47 +0200

    Push value imput inside stack.stackControl

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -3,12 +3,15 @@ window.onload = function() {
   stack.MAX_SIZE = 4;
 
   $('#add-num-stack').on('click', function(e){
-    var results = stack.push(10);
+    var results = stack.push($('#stack-num').val());
     if (results == "Stack Overflow"){
       return alert("Stack Overflow");
     }
-    results.forEach(function(num, i){
-      $('#stack .well')[i].innerText = num;
-    });
+    for(var i = (stack.MAX_SIZE - 1); i >= 0; i--){
+      for(var j = 0; j < results.length; j++){
+        $('#stack .well')[i].innerText = results[j];
+        $('#stack-num').val("").innerText = "";
+      }
+    }
   });
 }
