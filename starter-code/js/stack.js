e03e0bba88a9efbe21259f785a6523d74e0976cc
FranRom <maildefranz@gmail.com>
2017-10-26 09:48:54 +0200

    Still working on linking CSS with JS

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -3,14 +3,27 @@
 $(document).ready(function(){
 var juanStack = new StackDataStructure();
 
-$("btn-add").on("click", function(){
+$("btn-primary").on("click", function(){
   if (juanStack.canPush()){
     juanStack.push(1);
     $("#stack").children().text("");
     $("#stack .colorTake:last").addClass("colorAdd");
     $("#stack .colorTake:last").removeClass("colorTake");
-    console.log(juanStack);
+    if(juanStack.stackControl.length == 0){
+       $('#stack').children().addClass('colorTake');
+       $('#stack').children().removeClass('colorError');
   }
-});
 
+ } else {
+
+    $('#stack').children().addClass('colorError');
+    $('#stack').children().removeClass('colorTake');
+    $('#stack').children().removeClass('colorAdd');
+    $('#stack').children().text('STACK OVERFLOW').css({
+      color: "red",
+      fontSize: "18px",
+      textAlign: "center"
+  });
+ }
+});
 });
