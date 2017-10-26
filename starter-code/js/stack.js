3d68cf78727b5a951b099ceba7b74e550077db38
Nikolaas Verlee <nikotomad@gmail.com>
2017-10-26 09:39:09 +0200

    Started to implement dynamic li creation from MAX_SIZE, not finished

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -2,6 +2,10 @@ $(function() {
   stack = new StackDataStructure();
 
   console.log("Max stack size: " + stack.MAX_SIZE);
+  // Creamos tantos li como defina el MAX_SIZE
+    // for(var i=0; i < stack.MAX_SIZE, i++){
+    //   $('ul').html('<li class="item not-piled"></li>');
+    // }
 
   $('.stack .add').click(function() {
     var input_text = $('.stack .controls input').val();
