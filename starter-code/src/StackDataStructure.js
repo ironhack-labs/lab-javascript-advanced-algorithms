9284f2f74902e3ae8e2f267757ffa31674e84cfe
asalria <ameliasalria@gmail.com>
2017-11-18 17:00:24 +0100

    feat: StackDataStructure

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,23 @@
 function StackDataStructure () {
+
+    this.stackControl = [];
+    this.MAX_SIZE = 20;
+    
 }
+
+StackDataStructure.prototype.isEmpty = function(){
+  return (this.stackControl.length > 0) ? 0 : 1;
+}
+
+StackDataStructure.prototype.canPush = function(){
+    
+}
+
+StackDataStructure.prototype.push = function(){
+    
+}
+
+StackDataStructure.prototype.pop = function(){
+    
+}
+
