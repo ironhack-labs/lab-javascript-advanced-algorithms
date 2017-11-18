84532575e7dc7a873a255e9a1ade734d31ec16fd
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2017-11-18 17:13:14 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,31 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 90;
+}
+
+QueueDataStructure.prototype.isEmpty = function() {
+  return this.queueControl.length === 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  return this.queueControl.length < this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue = function(elem) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(elem);
+    return this.queueControl;
+  }
+  else {
+    return "Queue Overflow";
+  }
+}
+
+QueueDataStructure.prototype.dequeue = function() {
+  if (this.isEmpty()){
+    return "Queue Underflow";
+  }
+  else {
+    return this.queueControl.pop();
+  }
 }
