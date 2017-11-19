33c7b7a5dd6aa329658d3b85a35ad29ef7828b13
asalria <ameliasalria@gmail.com>
2017-11-19 21:20:44 +0100

    feat: Queue

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -6,18 +6,33 @@ function StackDataStructure () {
 }
 
 StackDataStructure.prototype.isEmpty = function(){
-  return (this.stackControl.length > 0) ? 0 : 1;
+  return (this.stackControl.length > 0) ? false : true;
 }
 
 StackDataStructure.prototype.canPush = function(){
-    
+    if(this.stackControl.length < this.MAX_SIZE)
+    {   
+        return true;
+    }
+    else return false;
 }
 
-StackDataStructure.prototype.push = function(){
-    
+StackDataStructure.prototype.push = function(newEle){
+    if ( this.canPush() ){
+        this.stackControl[this.stackControl.length] = newEle;
+        return this.stackControl;
+
+    } else return "Stack Overflow";
+
 }
 
 StackDataStructure.prototype.pop = function(){
-    
+
+    if ( !this.isEmpty() ){
+        element = this.stackControl[this.stackControl.length-1];
+        this.stackControl.splice(-1,1);
+        return element;
+
+    } else return "Stack Underflow";
 }
 
