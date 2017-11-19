af2465ee40808752321aa9f09a217b498d5b022b
pabloonieva <pablo@ironhack.com>
2017-11-19 20:49:07 +0100

    Just started, not finnished yet

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,26 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+
 }
+
+StackDataStructure.prototype.isEmpty = function () {
+  if(this.stackControl.length == 0){
+    return true;
+  }else{
+      return false;
+  }
+};
+StackDataStructure.prototype.canPush = function () {
+  if(this.stackControl.length < this.MAX_SIZE){
+    return true;
+  }else{
+    return false;
+  }
+};
+StackDataStructure.prototype.push = function () {
+
+};
+StackDataStructure.prototype.pop = function () {
+
+};
