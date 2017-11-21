92fd873de20a80f26b98b4d023038628d669e895
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-21 16:41:16 +0100

    DOne the first two classes, just need to finish the representation

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 500;
 }
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length === 0) {
+    return true;
+  }
+  return false;
+};
+QueueDataStructure.prototype.canEnqueue = function() {
+  if (this.MAX_SIZE > this.queueControl.length) {
+    return true;
+  }
+  return false;
+};
+QueueDataStructure.prototype.enqueue = function(element) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(element);
+    return this.queueControl;
+  }
+  return "Queue Overflow";
+};
+QueueDataStructure.prototype.dequeue = function() {
+  if(!this.isEmpty()){
+    return this.queueControl[this.queueControl.length-1];
+  }
+  return 'Queue Underflow'; 
+};
