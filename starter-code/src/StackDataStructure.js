2d40b59d3c65ea3af0a27d2e8eb80b929dee1d8b
Evapavo <32174314+Evapavo@users.noreply.github.com>
2017-11-23 18:49:38 +0100

    feat:first commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
-function StackDataStructure () {
-}
+
+  function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+
+  StackDataStructure.prototype.isEmpty = function(){
+    return this.stackControl.length === 0 ? true : false;
+  };
+
+  StackDataStructure.prototype.canPush = function () {
+    return this.stackControl.length < this.MAX_SIZE ? true : false;
+  };
+
+
+  StackDataStructure.prototype.push = function(item){
+    if (this.canPush() ){
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow';
+    }
+  };
+
+  StackDataStructure.prototype.pop = function(){
+  if (this.isEmpty()) {
+   return 'Stack Underflow';
+   } else {
+   return this.stackControl.pop();
+   }
+   };
