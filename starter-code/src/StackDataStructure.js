b3c993fea1d1e452d55a5fbc0b55a369d290d8cc
Bart Smet <bartsmet30@gmail.com>
2017-11-23 18:55:43 +0100

    feat: Initial and final commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE=80;
 }
+
+StackDataStructure.prototype.isEmpty= function(){
+  if(this.stackControl.length === 0){
+   return true;
+ } else {
+   return false;
+ }
+};
+
+StackDataStructure.prototype.canPush = function(){
+  if(this.stackControl.length < this.MAX_SIZE){
+    return true;
+  }else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.push = function(elem){
+  this.stackControl.push(elem);
+  if(this.stackControl.length > this.MAX_SIZE) {
+    return "Stack Overflow";
+  }else {
+  return this.stackControl;
+}
+};
+StackDataStructure.prototype.pop = function(){
+  if(this.stackControl.length > 0){
+    return this.stackControl.pop();
+  }else {
+    return "Stack Underflow";
+  }
+};
