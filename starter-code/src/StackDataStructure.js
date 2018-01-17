f1beb7ae92b52f28bcba9f15065248cef924727e
ngoc0905 <lebichngoc090589@gmail.com>
2018-01-17 17:43:09 +0100

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 20;
+    this.isEmpty = function () {
+        if (this.stackControl.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    this.canPush = function () {
+        if (this.stackControl.length >= this.MAX_SIZE) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    this.push = function (ele) {
+        if (this.stackControl.length === this.MAX_SIZE) {
+            return 'Stack Overflow';
+        } else {
+            this.stackControl.push(ele);
+            return this.stackControl;
+        }
+    };
+    this.pop = function () {
+        if (this.stackControl.length === 0) {
+            return 'Stack Underflow';
+        } else {
+            var last = this.stackControl.length - 1;
+            return this.stackControl.pop(last);
+        }
+    };
 }
