de3918621391ed2e8ddc40f1776e0cdcc5665bc4
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-01-17 20:29:27 +0100

    Stack y Queue done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+   return this.queueControl.length === 0;
+};
+
+QueueDataStructure.prototype.canEnqueue = function(){
+   return this.queueControl.length < this.MAX_SIZE;
+};
+
+QueueDataStructure.prototype.enqueue = function(numero){
+    if(this.canEnqueue()){
+        this.queueControl.unshift(numero);
+        return this.queueControl;
+    } else{
+        return "Queue Overflow";
+    }
+    
+};
+
+QueueDataStructure.prototype.dequeue = function(){
+    if(this.isEmpty()){
+        return "Queue Underflow";
+    }else {
+        return this.queueControl.pop();
+    }
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
 }
+
+
+
+StackDataStructure.prototype.isEmpty = function(){
+    return this.stackControl.length == 0;
+}
+
+StackDataStructure.prototype.canPush = function(){
+    return this.stackControl.length < this.MAX_SIZE; //Gracias Pelotudo por esta maravillosa "nueva" forma de comparar true/false. tkm
+  
+}
+
+StackDataStructure.prototype.push = function(numero){
+    if(this.canPush()){
+        this.stackControl.push(numero);
+        return this.stackControl;
+    }else{
+        return 'Stack Overflow';
+    }
+}
+
+StackDataStructure.prototype.pop = function(){
+    // return this.stackControl.pop([this.stackControl.length - 1]);
+    if(this.stackControl == 0){
+        return "Stack Underflow";
+    }else {
+        return this.stackControl.pop()
+    }
+}
+
+///////// VAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMOS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (soy feliz con poco)
