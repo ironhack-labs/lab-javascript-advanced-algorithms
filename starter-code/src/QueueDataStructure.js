39cf108f7b3366b933a5b79bde2e42988d06a06e
Carlos San Martin <carlosanmartin@gmail.com>
2018-01-17 19:27:59 +0100

    falta todo el jquery

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(a) {
+  if (this.queueControl.length >= this.MAX_SIZE) {
+    return "Queue Overflow";
+  } else {
+    this.queueControl.push(a);
+    return this.queueControl.reverse();
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  if (this.queueControl.length <= 0) {
+    return "Queue Underflow";
+  } else {
+    return this.queueControl.pop();
+  }
+};
