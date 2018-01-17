8c781bb024913e1dcb5be8cffd338c4886242d53
Henri Delozanne <hnr.delozanne@gmail.com>
2018-01-17 23:36:14 +0100

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function() {
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.canPush = function() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.push = function(element) {
+    if (!this.canPush()) {
+      return "Stack Overflow";
+    }
+    this.stackControl.push(element);
+    return this.stackControl;
+  };
+
+  this.pop = function() {
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      return "Stack Underflow";
+    }
+  };
 }
