bf071dc5c2b4f11c496d7e74438ff5d4bba3df26
Ironhack <ironhack@Ironhacks-Mac-mini.local>
2018-01-17 20:43:33 +0100

    buen intento

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,1 +1,21 @@
- c 
+ $(document).ready(function(){
+     $('.btn-push').click(function(e){
+         $('.stack').addClass('active');
+     })
+     $('.btn-pop').click(function(e){
+        $('.stack').removeClass('active');
+    })
+ })
+ 
+ 
+ $(".btn-push").click(function() {
+
+
+
+    // if ($(".btn-pop").hasClass("active")){
+    //     $(".").hide();
+    //     renderPrice(-5);
+    // } else {
+    //     $(".price > ul > li:nth-child(5)").show();
+    //     renderPrice(5);
+})
