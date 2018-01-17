c0de8ff1ecf9ec34944441306b6a0bc1c01f7799
Arthur M <arthur.mossa@gmail.com>
2018-01-18 00:25:04 +0100

    PAR - Arthur M - Algorithms done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 6;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  return this.queueControl.length === 0 ? true : false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  return this.queueControl.length < this.MAX_SIZE ? true : false;
+};
+
+QueueDataStructure.prototype.enqueue = function(arg) {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    this.queueControl.unshift(arg);
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function(rmvarg) {
+  if (this.queueControl.length === 0) {
+    return "Queue Underflow";
+  } else {
+    return this.queueControl.pop(rmvarg);
+  }
+};
