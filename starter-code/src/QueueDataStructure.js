374203942754181ff27f3c9281580e22128ba5ba
Ana Recuero <anarecsa@gmail.com>
2018-01-17 17:44:45 +0100

    QueueDataStructure constructor and methods

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 5;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+  return this.queueControl.length == 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  return this.queueControl.length < this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue = function(elem){
+  if(this.canEnqueue()){
+    this.queueControl.splice(0,0,elem);
+    return this.queueControl;
+  }
+  else{
+    return "Queue Overflow";
+  }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+  if(this.queueControl.length > 0){
+    return this.queueControl[this.queueControl.length - 1];
+  } else{
+    return "Queue Underflow";
+  }
 }
