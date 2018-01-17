c7eca9d72e1a5d3768af64a2f0b2224e3be5b6f8
JurgenBlitz <jorge.gopae@gmail.com>
2018-01-17 21:56:10 +0100

    Halfway

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,28 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 9;
+}
+QueueDataStructure.prototype.isEmpty = function() {
+    return this.queueControl.length == 0;
+}
+QueueDataStructure.prototype.canEnqueue = function() {
+    if(this.QueueControl<this.MAX_SIZE) {
+        return true;
+    } else {
+        return false;
+    }
+}  
+QueueDataStructure.prototype.enqueue = function(a) {
+    if (this.canEnqueue()) {
+        this.queueControl.unshift(a); 
+    return this.queueControl;
+} else {
+    return 'Queue Overflow'
+}
+}
+QueueDataStructure.prototype.dequeue = function() {
+    if (this.isEmpty()) {
+        return 'Queue Underflow'
+    }
+    return this.queueControl.pop(a);
 }
