9580b3c46b06d320247a712e553f5d7d0ac235cb
Jean-Nicolas Piketty <jnpiketty@BDLCM.local>
2018-01-17 22:24:18 +0100

    PAR - WD - Stack-Queue - Jean-Nicolas

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function() {
+    return this.stackControl.length === 0 ? true : false;
+  };
+  this.canPush = function() {
+    return this.stackControl.length === this.MAX_SIZE ? false : true;
+  };
+  this.push = function(element) {
+    if (this.canPush() === true) {
+      this.stackControl.push(element);
+    } else {
+      return "Stack Overflow";
+    }
+    return this.stackControl;
+  };
+  this.pop = function() {
+    if (this.isEmpty() === true) {
+      return "Stack Underflow";
+    } else {
+      var takenElement = this.stackControl[this.stackControl.length - 1];
+      this.stackControl.splice(this.stackControl.length - 1, 1);
+      return takenElement;
+    }
+  };
 }
