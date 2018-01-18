8b303976b4689ffd7ed45cb6db60a1d69f5decf4
Alberto Salmerón Tapia <alberto@MacBook-Pro-de-Alberto.local>
2018-01-18 09:18:40 +0100

    ME falta última interacción

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,35 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 20;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+    return this.queueControl == 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function(a){
+    if (this.queueControl.length >= this.MAX_SIZE){
+        return false;
+    }
+    else {
+        this.queueControl.push(a)
+        return true;
+    }
+}
+
+QueueDataStructure.prototype.enqueue = function(a){
+    if(this.queueControl.length >= this.MAX_SIZE ){
+        return 'Queue Overflow';
+    }else{
+        this.queueControl.unshift(a);
+        return this.queueControl;
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+    if( this.queueControl.length <= 0){
+        return "Queue Underflow"
+    } else {
+        return this.queueControl.pop()
+    }
 }
