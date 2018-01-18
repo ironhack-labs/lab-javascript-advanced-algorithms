57d6cb677198017ea3a1101b909a524ad6ac104e
Ana Recuero <anarecsa@gmail.com>
2018-01-18 08:14:45 +0100

    document.ready added

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,42 +1,44 @@
-var stack = new StackDataStructure();
+$(document).ready(function() {
+  var stack = new StackDataStructure();
 
-function updateStackShowcase(elems) {
-  for (i = 0; i < stack.MAX_SIZE; i++) {
-    var datas = $("#stack-container .data");
-    if (!elems[i]) {
-      $(datas[i]).html("");
-    } else {
-      $(datas[i]).html(elems[i]);
+  function updateStackShowcase(elems) {
+    for (i = 0; i < stack.MAX_SIZE; i++) {
+      var datas = $("#stack-container .data");
+      if (!elems[i]) {
+        $(datas[i]).html("");
+      } else {
+        $(datas[i]).html(elems[i]);
+      }
     }
   }
-}
 
-function emptyInputAlert() {
-  $("#stack-form-group").addClass("has-error");
-}
+  function emptyInputAlert() {
+    $("#stack-form-group").addClass("has-error");
+  }
 
-$("#add-stack").click(function() {
-  if ($("#stack-elem").val() == "") {
-    emptyInputAlert();
-  } else {
-    $("#stack-form-group").removeClass("has-error");
-    var addElem = stack.push($("#stack-elem").val());
-
-    if (addElem == "Stack Overflow") {
-      $("#stack-overflow").removeClass("hidden");
+  $("#add-stack").click(function() {
+    if ($("#stack-elem").val() == "") {
+      emptyInputAlert();
     } else {
-      $("#stack-underflow").addClass("hidden");
-      updateStackShowcase(addElem);
-    }
-  }
-});
-$("#remove-stack").click(function() {
-  var removeElem = stack.pop();
+      $("#stack-form-group").removeClass("has-error");
+      var addElem = stack.push($("#stack-elem").val());
 
-  if (removeElem == "Stack Underflow") {
-    $("#stack-underflow").removeClass("hidden");
-  } else {
-    $("#stack-overflow").addClass("hidden");
-    updateStackShowcase(stack.stackControl);
-  }
+      if (addElem == "Stack Overflow") {
+        $("#stack-overflow").removeClass("hidden");
+      } else {
+        $("#stack-underflow").addClass("hidden");
+        updateStackShowcase(addElem);
+      }
+    }
+  });
+  $("#remove-stack").click(function() {
+    var removeElem = stack.pop();
+
+    if (removeElem == "Stack Underflow") {
+      $("#stack-underflow").removeClass("hidden");
+    } else {
+      $("#stack-overflow").addClass("hidden");
+      updateStackShowcase(stack.stackControl);
+    }
+  });
 });
