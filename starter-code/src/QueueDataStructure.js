6c7990442479b51eefbfaa086eed91b6eefec8a4
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-18 09:28:43 +0100

    .

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,34 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function () {
+    if (this.queueControl == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+QueueDataStructure.prototype.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+QueueDataStructure.prototype.enqueue = function (numero) {
+    if (this.canEnqueue()) {
+        this.queueControl.unshift(numero)
+        return this.queueControl;
+    } else {
+        return "Queue Overflow";
+    }
+}
+QueueDataStructure.prototype.dequeue = function () {
+    if(this.queueControl == 0){
+        return 'Queue Underflow';
+    }else{
+        return this.queueControl.pop();
+    }
+    }
