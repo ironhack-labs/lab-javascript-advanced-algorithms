8df73dadad35aedd83af5bf9a7e67b0702e9d532
cut-throat <rober.strada@outlook.com>
2018-01-18 11:53:28 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 5;
 }
+QueueDataStructure.prototype.isEmpty = function() {
+
+    if (this.queueControl == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+QueueDataStructure.prototype.canEnqueue = function() {
+    if (this.queueControl.length < this.MAX_SIZE){
+        return true;
+    }else{
+        return false;
+    }
+};
+QueueDataStructure.prototype.enqueue = function(a) {
+    this.queueControl.push(a);
+    this.queueControl.reverse();
+    if (this.queueControl.length > this.MAX_SIZE) {
+        return "Queue Overflow";
+    } else {
+        return this.queueControl;
+    }
+};
+QueueDataStructure.prototype.dequeue = function() {
+    if (this.queueControl.length == 0) {
+
+        return "Queue Underflow";
+    } else {
+        return this.queueControl.pop();
+    }
+
+};
