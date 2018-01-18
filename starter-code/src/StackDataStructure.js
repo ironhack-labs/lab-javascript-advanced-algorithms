2c4ef6c47b6a783fbe76d243f474500c3868e775
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-01-18 09:55:24 +0100

    done js y first botton Jquery

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 19;
 }
+
+StackDataStructure.prototype.isEmpty = function () {
+  if (this.stackControl.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+StackDataStructure.prototype.canPush = function () {
+  if (this.stackControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+StackDataStructure.prototype.push = function (element) {
+
+  if (this.stackControl.length > this.MAX_SIZE) {
+    return 'Stack Overflow';
+  } else {
+    this.stackControl.push(element);
+    return this.stackControl;
+  }
+}
+
+StackDataStructure.prototype.pop = function () {
+
+  if (this.stackControl.length == 0) {
+    return 'Stack Underflow';
+  } else {
+    return this.stackControl.pop();
+  }
+}
+
