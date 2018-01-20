3ab8ef5942bc89d81266bdcc587e429c01a9c816
Alvaro <echanovevaro@gmail.com>
2018-01-20 06:02:36 +0100

    completado

---
+++ starter-code/src/QueueDataStructure.js
@@ -0,0 +1,27 @@
+function QueueDataStructure() {
+	this.queueControl = [];
+	this.MAX_SIZE = 8;
+}
+
+QueueDataStructure.prototype.isEmpty = function () {
+	return this.queueControl.length === 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+	return this.queueControl.length < this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue = function (element) {
+	if (!this.canEnqueue()) {
+		return "Queue Overflow";
+	}
+	this.queueControl.unshift(element);
+	return this.queueControl;
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+	if (this.isEmpty()) {
+		return "Queue Underflow";
+	}
+	return this.queueControl.pop();
+}
