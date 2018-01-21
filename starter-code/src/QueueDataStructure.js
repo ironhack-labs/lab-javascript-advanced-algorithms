67561189827bdf707706bd4e4e6d064ce1829802
JoniWanKenobi <joniwankenoby@gmail.com>
2018-01-21 19:04:52 +0100

    data structures and visual layout done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,34 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+    return this.queueControl.length === 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+    return this.queueControl.length < this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue = function(el){
+    if(this.canEnqueue()){
+        this.queueControl.unshift(el);
+        return this.queueControl;
+    }
+    else {
+        return 'Queue Overflow';
+    }
+    
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+    if(this.isEmpty()){
+        return 'Queue Underflow';
+    }
+    else {
+        return this.queueControl.pop();
+    }
+    
+    
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+function StackDataStructure (max) {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
 }
+
+StackDataStructure.prototype.isEmpty = function (){
+    return this.stackControl.length === 0;
+}
+
+StackDataStructure.prototype.canPush = function(){
+    return this.stackControl.length < this.MAX_SIZE;
+}
+
+StackDataStructure.prototype.push = function(el){
+    if(this.canPush()){
+        this.stackControl.push(el);
+        return this.stackControl;
+    }
+    else {
+        return 'Stack Overflow';
+    }    
+}
+
+StackDataStructure.prototype.pop = function(){
+    if(this.isEmpty()){
+        return 'Stack Underflow';
+    }
+    else {
+        return this.stackControl.pop();  
+    }      
+}
+
+var stack = new StackDataStructure();
+stack.push(1);
