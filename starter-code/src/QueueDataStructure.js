d76922431a36869113cf7074e66bc16f23869e8c
Bet Mendoza <sbm2391@hotmail.com>
2018-01-31 19:43:55 -0600

    logic done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
+    this.queueControl =[];
+    this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty= function () {
+    if (this.queueControl.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue= function () {
+    if (this.queueControl.length >= this.MAX_SIZE) {
+        return false
+        
+    }
+    return true;
+    
+}
+QueueDataStructure.prototype.enqueue= function (element) {
+    this.queueControl.push(element);
+    this.queueControl.reverse()
+    if (this.queueControl.length >= this.MAX_SIZE) {
+        return "Queue Overflow"
+        
+    }
+    return this.queueControl
+
+   
+}
+QueueDataStructure .prototype.dequeue= function () {
+    if (this.queueControl.length ===  0) {
+        return "Queue Underflow"
+    }
+    return this.queueControl[this.queueControl.length -1];
 }
