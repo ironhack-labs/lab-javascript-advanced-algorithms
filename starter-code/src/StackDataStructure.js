f8025395f55aeaa805358abc8f00eb1d33494e59
Juanmagos <juanmagoss@hotmail.com>
2018-02-01 13:21:23 -0600

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,16 @@
 function StackDataStructure () {
+this.stackControl=[];
+this.MAX_SIZE=10;
+this.isEmpty =function(){
+    return this.stackControl.length===0;
+}
+this.canPush = function(){ 
+    if (this.stackControl.length <= 10){
+        StackDataStructure === true;
+    }
+}
+this.push = function(){
+}
+this.pop = function(){
+}
 }
