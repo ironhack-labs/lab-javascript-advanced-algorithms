f2b0afb0186db3645f4f192922fb16eee7d5b4c5
MacBook <macbook@Macbook.local>
2018-01-31 23:10:47 -0600

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
-}
+    this.queueControl = []
+    this.MAX_SIZE = 10
+
+    this.isEmpty = function(){
+        if (this.queueControl.length===0){
+            return true}
+         else {return false}
+         }
+     
+     this.canEnqueue = function(){
+     return (this.MAX_SIZE>this.queueControl.length)
+     
+    }
+     
+     this.enqueue = function(num){ 
+ 
+         if (this.canEnqueue()){
+ 
+         this.queueControl.unshift(num)
+         return this.queueControl
+         } else {
+             return "Queue Overflow"
+         }
+         
+     
+     }
+    this.dequeue = function(num){
+ 
+       if (this.isEmpty()){
+     return 'Queue Underflow' 
+ 
+    } else {
+ 
+       return this.queueControl.pop(num)
+    }
+ 
+ }}
