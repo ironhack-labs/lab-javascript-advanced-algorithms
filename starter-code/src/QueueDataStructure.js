a5d68041536429c34e96e833b8c2b3b9cb9a0eac
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-03-28 19:20:58 +0200

    Jasmine done al fin

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -14,13 +14,21 @@ QueueDataStructure.prototype.canEnqueue = function() {
 }
 
 QueueDataStructure.prototype.enqueue = function(i) {
+  if(this.queueControl.length < this.MAX_SIZE) {
     this.queueControl.unshift(i);
     return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
     
 }
 
 QueueDataStructure.prototype.dequeue = function() {
-    
+  if(this.queueControl.length == 0) {
+    return "Queue Underflow";
+  } else if (this.queueControl.length < this.MAX_SIZE) {
+    return this.queueControl[this.queueControl.length-1];
+  }
 }
 
 
