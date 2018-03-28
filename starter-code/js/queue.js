2d15facf43f46ef5b0c1233eeae4a7382554276e
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-03-28 20:42:41 +0200

    ...

---
+++ starter-code/js/queue.js
@@ -0,0 +1,17 @@
+/*StackDataStructure.prototype.push = function(stc){
+    if(this.canPush()){
+    this.stackControl.unshift(stc)
+    return this.stackControl
+    }
+    else if (!this.canPush()){
+        return 'Stack Overflow'
+    }
+}*/
+/*if(this.isEmpty()){
+    this.stackControl.pop(stc)
+    return this.stackControl   
+    }
+    /*else if(!this.isEmpty()){
+        return 'Stack Underflow'
+    }*/
+} */
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,9 +1,9 @@
 function StackDataStructure () {
     this.stackControl = [],
-    this.MAX_SIZE = 1
+    this.MAX_SIZE = 30
 }
 StackDataStructure.prototype.isEmpty = function(){
-    if (this.stackControl.length == 0){
+    if (this.stackControl.length === 0){
         return true
     }else{
         return false;
@@ -11,12 +11,26 @@ StackDataStructure.prototype.isEmpty = function(){
 }
 
 
-StackDataStructure.prototype.canPush = function(){
+StackDataStructure.prototype.canPush = function(stc){
+    if (this.stackControl.length < this.MAX_SIZE){
+        return true
+    } else {
+        return false
+    }
+}
+StackDataStructure.prototype.push = function(stc){
+    if(this.canPush()){
+    this.stackControl.push(stc)
+    return this.stackControl
+    }
+    else if (!this.canPush()){
+        return 'Stack Overflow'
+    }
+   }
 
-}
-StackDataStructure.prototype.push = function(){
-    
-}
-StackDataStructure.prototype.pop = function(){
-    
+StackDataStructure.prototype.pop = function(stc){
+    if(this.isEmpty()){
+        this.stackControl.shift(stc)
+        return this.stackControl   
+        }
 }
