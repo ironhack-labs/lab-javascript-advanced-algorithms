dad2865face62738447ce9e14278d7c9bea1bea8
davidtexmex814 <davidtexmex814.com>
2018-03-28 20:54:52 +0200

    primer commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
 function StackDataStructure () {
+this.stackControl = [];
+this.MAX_SIZE = 3;
 }
+StackDataStructure.prototype.isEmpty = function(){
+    if(this.stackControl == 0){
+        return true;
+    }else {
+        return false;
+    }
+};
+
+StackDataStructure.prototype.canPush = function(){
+    if(this.stackControl.length < this.MAX_SIZE){
+        return true;
+    }else {
+        return false;
+    }
+};
+StackDataStructure.prototype.push = function(){
+    if(this.stackControl.length< this.MAX_SIZE){
+        return this.stackControl.push(19);
+    }else {
+        return false;
+    }
+};
+StackDataStructure.prototype.pop = function(){
+
+};
