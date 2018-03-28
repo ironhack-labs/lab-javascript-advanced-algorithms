6a45bb653c31aecf9229b5336cd81554bf8bb8be
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-28 18:10:09 +0200

    QueueDataStructure object created

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length == 0) {
+    return true;
+  }
+  return false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+QueueDataStructure.prototype.enqueue = function(el) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(el);
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  if (!this.isEmpty()) {
+    return this.queueControl.pop();
+  } else {
+    return "Queue Underflow";
+  }
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,36 +1,35 @@
-function StackDataStructure () {
+function StackDataStructure() {
   this.stackControl = [];
   this.MAX_SIZE = 10;
 }
 
 StackDataStructure.prototype.isEmpty = function() {
-  if( this.stackControl.length == 0 ) {
+  if (this.stackControl.length == 0) {
     return true;
   }
   return false;
-}
+};
 
 StackDataStructure.prototype.canPush = function() {
-  if( this.stackControl.length < this.MAX_SIZE ) {
+  if (this.stackControl.length < this.MAX_SIZE) {
     return true;
   }
   return false;
-}
+};
 
 StackDataStructure.prototype.push = function(el) {
-  if( this.canPush() ) {
+  if (this.canPush()) {
     this.stackControl.push(el);
+    return this.stackControl;
   } else {
     return "Stack Overflow";
   }
-
-  return this.stackControl;
-}
+};
 
 StackDataStructure.prototype.pop = function() {
-  if( !this.isEmpty() ) {
+  if (!this.isEmpty()) {
     return this.stackControl.pop();
   } else {
     return "Stack Underflow";
   }
-}
+};
