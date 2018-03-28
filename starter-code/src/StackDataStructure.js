e12723940490ce40a2c127906a90b8e11f777167
lloren27 <llorenteyuste@hotmail.com>
2018-03-28 18:33:38 +0200

    Creados los metodos Stack

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,4 +1,28 @@
-function StackDataStructure () {
-
-    
+    function StackDataStructure () {
+        this.stackControl = [];
+        this.MAX_SIZE = 1;
 }
+
+    StackDataStructure.prototype.isEmpty = function(){
+        
+        if (this.stackControl=[]){
+            return true;
+        }
+    }
+    StackDataStructure.prototype.canPush = function(){
+        
+        if (this.stackControl< this.MAX_SIZE){
+            return true;
+        }
+    } 
+    StackDataStructure.prototype.push = function(){
+        if(this.canPush=true){
+            return this.stackControl.length
+        }else{
+            return "Stack Overflow"
+        }
+        
+    } 
+    StackDataStructure.prototype.pop = function(){
+        return stackControl = [];
+    } 
