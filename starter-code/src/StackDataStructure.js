ec139f3477ef8d784f6ac2e91bacba1557e4a740
JMBarcelo <jmbbazal@gmail.com>
2018-03-28 22:06:11 +0200

    Estoy con el stack todavía

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,30 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length === 0;
+}
+
+StackDataStructure.prototype.canPush = function() {
+  return (this.stackControl.length - 1 < this.MAX_SIZE);
+}
+
+StackDataStructure.prototype.push = function(element) {
+  if(this.canPush()) {
+    this.stackControl.push(element);
+  
+    return this.stackControl;
+  } else {
+    return 'Stack Overflow';
+  }
+}
+
+StackDataStructure.prototype.pop = function() {
+  if(this.isEmpty()) {
+    return 'Stack Underflow';
+  } else {
+    return this.stackControl.pop();
+  }
 }
