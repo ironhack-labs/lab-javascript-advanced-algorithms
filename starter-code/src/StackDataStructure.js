603db22f6e81cc7c0b6d70f91d337c647a67c7ae
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-03-28 18:34:55 +0200

    Hecho media primera interacción

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,22 @@
 function StackDataStructure () {
+    this.stackControl = [],
+    this.MAX_SIZE = 1
+}
+StackDataStructure.prototype.isEmpty = function(){
+    if (this.stackControl.length == 0){
+        return true
+    }else{
+        return false;
+    }
+}
+
+
+StackDataStructure.prototype.canPush = function(){
+
+}
+StackDataStructure.prototype.push = function(){
+    
+}
+StackDataStructure.prototype.pop = function(){
+    
 }
