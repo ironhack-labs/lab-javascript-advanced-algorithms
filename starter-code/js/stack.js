0ddabfe19b966a64109f1cad9baf1d9870bb8cde
A-Tr <alvarotrancon@gmail.com>
2018-03-28 21:05:26 +0200

    rellena el stack

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
+$( document ).ready(function(){
+  var stacker = new StackDataStructure();
+  console.log(stacker)
+
+  var stackNumber = 0
   $("#add-stack").click(function() {
-      
-  }
-}
+    console.log("#stack-boxes")
+    console.log($('#stack' + (10 - stackNumber)))
+    $('#stack' + (10 - stackNumber)).css("background-color", "blue")
+    stackNumber++;
+
+  });
+
+
+
+
+
+
+});
