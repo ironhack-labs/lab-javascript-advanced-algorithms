b5a796b896c08eec7c32ad3825488dfcd94d77ff
Borja Arana <barana1977@hotmail.com>
2018-03-28 22:34:32 +0200

    Terminado iteracion 2.Y me voy para casa

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+    if (this.queueControl.length == 0){
+        return true;
+    }
+    return false;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+    if (this.canEnqueue.length < this.MAX_SIZE) {
+        return true;
+    }
+    return false;
+}
+
+QueueDataStructure.prototype.enqueue = function (elem) {
+    if (this.canEnqueue() == true) {
+        this.queueControl.unshift(elem);
+        return this.queueControl;
+    } else {
+        return "Queue Overflow";
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function (elem) {
+    if (this.isEmpty() == true) {
+        return 'Queue Underflow';
+    } else {
+        return this.queueControl.pop(elem);
+    }   
+
+
+
+
 }
