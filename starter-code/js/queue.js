40e287fb007d8a4861fb92237295ae80d80a6b44
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-28 22:26:38 +0200

    HTML & CSS created

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var queue = new QueueDataStructure();
+
   var $container = $(".container");
 
   var $queueContainer = $("<div class='col-md-5' id='queue'>");
@@ -9,4 +11,36 @@ $(document).ready(function() {
   $("<button class='btn btn-primary col-md-12' id='addQueue'>ADD</butto>").appendTo($queueContainer);
   $("<button class='btn btn-danger col-md-12' id='takeQueue'>TAKE</button>").appendTo($queueContainer);
 
+  for(var i = 0; i < queue.MAX_SIZE; i++) {
+    $("<div class='empty form-control col-md-12' id='queue-" + i + "'>").appendTo($queueContainer);
+  }
+
+  // show dequeue value
+  var $dequeue = $("<div class='col-md-12' id='dequeue'>");
+  $dequeue.appendTo($queueContainer);
+
+  $("#addQueue").on("click", function(e) {
+    var item = $("#queueItem").val();
+    
+    var resultEnqueue = queue.enqueue(item);
+
+    if( resultEnqueue ) {
+      printQueue(queue);
+    } else if( resultEnqueue == "Queue Overflow" ) {
+
+    } else if( resultEnqueue == "Queue Underflow") {
+      
+    }
+
+    $("#queueItem").val("");
+  });
+
+  function printQueue(queue) {
+    for(var i = 0; i < queue.queueControl.length; i++) {
+      if(queue.queueControl[i] != undefined) {
+        $("div#queue-"+i).addClass("full");
+        $("div#queue-"+i).text(queue.queueControl[i]);
+      }
+    }
+  }
 });
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -11,6 +11,64 @@ $(document).ready(function() {
 
   $("<h2>Stack</h2>").appendTo($stackContainer);
   $("<input class='form-control' id='stackItem' placeholder='Add the element to the stack...'>").appendTo($stackContainer);
-  $("<button class='btn btn-primary col-md-12' id='addStack'>ADD</butto>").appendTo($stackContainer);
+  $("<button class='btn btn-primary col-md-12' id='addStack'>ADD</button>").appendTo($stackContainer);
   $("<button class='btn btn-danger col-md-12' id='takeStack'>TAKE</button>").appendTo($stackContainer);
+
+  for (var i = stack.MAX_SIZE - 1; i >= 0; i--) {
+    $("<div class='empty form-control col-md-12' id='stack-" + i + "'>").appendTo($stackContainer);
+  }
+
+  // show pop value
+  var $stackPop = $("<div class='col-md-12' id='stack-pop'>");
+  $stackPop.appendTo($stackContainer);
+
+  $("#addStack").on("click", function(e) {
+    var item = $("#stackItem").val();
+
+    if (item != "") {
+      var resultPush = stack.push(item);
+
+      if (resultPush) {
+        printStack(stack);
+      } else if (resultPush == "Stack Overflow") {
+      }
+
+      $("#stackItem").val("");
+    }
+  });
+
+  $("#takeStack").on("click", function(e) {
+    var resultPop = stack.pop();
+    
+    $("#stack-pop").text(resultPop);
+
+    if (resultPop != "Stack Underflow") {
+      $("div#stack-0").addClass("danger");
+      $("div#stack-0").text("Stack Underflow");
+    } else {
+      clearStack(stack);
+    }
+  });
+
+  function printStack(stack) {
+    for (var i = 0; i < stack.stackControl.length; i++) {
+      if (stack.stackControl[i] != undefined) {
+        $("div#stack-" + i)
+          .removeClass("empty")
+          .addClass("full");
+        $("div#stack-" + i).text(stack.stackControl[i]);
+      }
+    }
+  }
+
+  function clearStack(stack) {
+    for (var i = 0; i < stack.stackControl.length; i++) {
+      if (stack.stackControl[i] == undefined) {
+        $("div#stack-" + i)
+          .removeClass("full")
+          .addClass("empty");
+        $("div#stack-" + i).text("");
+      }
+    }
+  }
 });
