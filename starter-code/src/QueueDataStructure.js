a7da7bbdfa958a71dbeabf195a8ce20e304585bd
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-03-28 20:37:40 +0200

    js done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,45 @@
 function QueueDataStructure () {
+    this.queueControl=[];
+    this.MAX_SIZE=10;
+}
+
+QueueDataStructure.prototype.isEmpty = function() {
+    if (this.queueControl.length == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+
+
+
+QueueDataStructure.prototype.canEnqueue = function(){
+    if(this.queueControl.length < this.MAX_SIZE){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+QueueDataStructure.prototype.enqueue = function (a){
+    if (this.canEnqueue()){
+        this.queueControl.unshift(a);
+        return this.queueControl;
+    } 
+    else {
+    
+    return "Queue Overflow";
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function (){
+    if (!this.isEmpty()){
+        return this.queueControl.pop();
+    }
+    else {
+         return "Queue Underflow";
+    }
 }
