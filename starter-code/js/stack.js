bd7fd877db40ff8b3728e8039e24cb33d9ee2cc9
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-03-29 01:02:10 +0200

    logica de stack y queue y controlador de stack

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,43 +1,42 @@
 var stackData = new StackDataStructure();
   stackData.MAX_SIZE = 8;
 $( document ).ready(function() {
-  console.log("ddd")
   
 
 
+  
   $(".lleno").hide();
   $(".vacio").hide();
 
-$(".btn-add").on("click", function () {
-  var texto =  $(".entrada").val();
-  stackData.push(texto);
-   
-  if (stackData.stackControl.length == 8) {
-    $(".lleno").show();
-    $(".lleno").addClass("overflow");
-  }
-  else if (stackData.stackControl.length == 0) {
-    $(".vacio").addClass("underflow");
-  }
+  $(".btn-add").on("click", function () {
+    if ($(".vacio").css("visibility") == "visible") { //alguna otra forma de hacer esto???
+      $(".vacio").hide();
+    } 
+    var texto =  $(".entrada").val();
+    if (stackData.stackControl.length == 8) { 
+      $(".lleno").show();
+    }
+    stackData.push(texto);
+    
+    
+    
+  
   var index = stackData.MAX_SIZE - stackData.stackControl.length;
   $(".pila").eq(index).removeClass("pila-vacia");
   $(".pila").eq(index).addClass("pila-llena");
   $(".pila").eq(index).text(texto);
-
-
-
 });
 
 $(".btn-take").on("click", function () {
+  if ($(".lleno").css("visibility") == "visible") {
+    $(".lleno").hide();
+  }
   if (stackData.stackControl.length == 0) {
-    $(".vacio").addClass("underflow");
     $(".vacio").show();
   }
- 
   var index = stackData.MAX_SIZE - stackData.stackControl.length;
   var texto =  $(".entrada").eq(index).val();
   stackData.pop(texto);
-  
   $(".pila").eq(index).removeClass("pila-llena");
   $(".pila").eq(index).addClass("pila-vacia");
   $(".pila").eq(index).text("");
