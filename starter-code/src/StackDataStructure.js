4b049d9292061f945aad4ebd5b8bf4f89c6ab15b
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-28 17:45:00 +0200

    StackDataStructure object created

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+  if( this.stackControl.length == 0 ) {
+    return true;
+  }
+  return false;
+}
+
+StackDataStructure.prototype.canPush = function() {
+  if( this.stackControl.length < this.MAX_SIZE ) {
+    return true;
+  }
+  return false;
+}
+
+StackDataStructure.prototype.push = function(el) {
+  if( this.canPush() ) {
+    this.stackControl.push(el);
+  } else {
+    return "Stack Overflow";
+  }
+
+  return this.stackControl;
+}
+
+StackDataStructure.prototype.pop = function() {
+  if( !this.isEmpty() ) {
+    return this.stackControl.pop();
+  } else {
+    return "Stack Underflow";
+  }
 }
