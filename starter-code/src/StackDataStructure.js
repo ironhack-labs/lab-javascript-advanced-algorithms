a17bd8058278585e028ae5c281618dcdd3b197ae
Tanis Pan <e.pan.gar@gmail.com>
2018-03-28 21:13:21 +0200

    Iteracion1

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
+function StackDataStructure(stack) {
+  this.stackControl=[];
+  this.MAX_SIZE=this.stackControl.length;
+
+  this.isEmpty = function() {
+    if (this.stackControl.length==0){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  this.canPush = function(){
+    if(this.stackControl){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  this.push = function(e){
+    this.stackControl.push(e);
+    this.isEmpty();
+    return [e];
+  };
+  this.pop = function(){
+    
+  };
+
+
 }
+
