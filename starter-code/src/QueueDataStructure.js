1f4f53888e69a6d50e44d26370172560556a313b
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-03-28 19:04:16 +0200

    Jasmine a 3 del final

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -6,6 +6,21 @@ function QueueDataStructure () {
 }
 
 QueueDataStructure.prototype.isEmpty = function() {
+  return (this.queueControl.length == 0)
+}
+
+QueueDataStructure.prototype.canEnqueue = function() {
+    return (this.queueControl.length < this.MAX_SIZE  )
+}
+
+QueueDataStructure.prototype.enqueue = function(i) {
+    this.queueControl.unshift(i);
+    return this.queueControl;
     
 }
+
+QueueDataStructure.prototype.dequeue = function() {
+    
+}
+
 
