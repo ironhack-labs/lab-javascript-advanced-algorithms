7fb494db5222c3e678e2343ad718e054b4c17d01
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-28 20:59:01 +0200

    Back-end done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,43 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+    if(this.queueControl.length == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+    if(this.isEmpty() || this.queueControl.length < this.MAX_SIZE){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+QueueDataStructure.prototype.enqueue = function(elment){
+   
+    if(this.canEnqueue()){
+        this.queueControl.unshift(elment);
+        this.isEmpty();
+        return this.queueControl;
+    }
+    else{
+        return "Queue Overflow";
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+    if(!this.isEmpty()){
+        return this.queueControl[this.queueControl.length-1];
+    }
+    else{
+        return "Queue Underflow";
+    }   
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,45 @@
-function StackDataStructure () {
+//DO NOT USE append or prepend
+
+function StackDataStructure(){
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+}
+
+StackDataStructure.prototype.isEmpty = function(){
+    if(this.stackControl.length == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+StackDataStructure.prototype.canPush = function(){
+    if(this.isEmpty() || this.stackControl.length < this.MAX_SIZE){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+StackDataStructure.prototype.push = function(elment){
+   
+    if(this.canPush()){
+        this.stackControl.push(elment);
+        this.isEmpty();
+        return this.stackControl;
+    }
+    else{
+        return "Stack Overflow";
+    }
+}
+
+StackDataStructure.prototype.pop = function(){
+    if(!this.isEmpty()){
+        return this.stackControl[this.stackControl.length-1];
+    }
+    else{
+        return "Stack Underflow";
+    }   
 }
