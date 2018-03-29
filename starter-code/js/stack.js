e69d2cb67778f127c991ae0bdf01c898e01ba1da
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-29 02:04:49 +0200

    Stack working full

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,44 +1,58 @@
-$(function(){
+$(function() {
+  var stack = new StackDataStructure();
 
+  //REMOVE ELEMENT FROM STACK
+  $("#take-element-stack").on("click", function() {
+    removeFromStack();
+  });
+  //ADD ELEMENT TO STACK
+  $("#add-element-stack").on("click", function() {
+    addToStack();
+  });
+  $("#stack-clear-input").on("click", function() {
+    $("#input-element-stack").val('');
+  });
 
-    var stack = new StackDataStructure();
+  function addToStack() {
+    var valueToStack = $("#input-element-stack").val();
 
-    //REMOVE ELEMENT FROM STACK
-    $("#take-element-stack").on("click", function(){
-        removeFromStack();
-    });
-
-    //ADD ELEMENT TO STACK
-    $("#add-element-stack").on("click", function(){
-        addToStack();
-    });
-
-
-    function addToStack(){
-        if(stack.push(2)){
-            debugger;
-            var freeCell = 10 - stack.stackControl.length;
-            //Complete...
-        }
-        else{
-            //Add code...
-        }
+    if (!valueToStack) {
+      alert("Please input a value to stack!");
+    } else {
+      if (stack.push(valueToStack)) {
+        valueToStack = "<p>" + valueToStack + "</p>";
+        var freeCell = stack.MAX_SIZE + 1 - stack.stackControl.length;
+        $(".stack-elements > ul > li")
+          .eq(freeCell)
+          .addClass("occupied")
+          .html(valueToStack);
+        $("#stack-bottom").removeClass("over");
+      } else {
+        $("#stack-top")
+          .html("<p>Stack Overflow</p>")
+          .addClass("over")
+          .removeClass("hidden");
+      }
     }
+  }
 
+  function removeFromStack() {
+    if (!stack.pop()) {
+      $("#stack-bottom")
+        .html("<p>Stack Underflow</p>")
+        .addClass("over");
+    } else {
+        debugger;
+        var freeCell = (stack.MAX_SIZE) - stack.stackControl.length;
+        $(".stack-elements > ul > li")
+          .eq(freeCell)
+          .removeClass("occupied")
+          .empty();
 
-    function removeFromStack(){
-        if(!stack.pop()){
-            $("#stack-bottom").html("<p>Stack Underflow</p>").addClass("over");
-        }
-        else{
-            //Add code...
-        }
+          $("#stack-top")
+          .html("<p>Stack Overflow</p>")
+          .removeClass("over")
+          .addClass("hidden");
     }
-
-
-
-
-
-
-
+  }
 });
