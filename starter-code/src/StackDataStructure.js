c73be7b034d0ad4b1887fa9366e225b5885ec312
esthvega <esthvega@hotmail.es>
2018-03-29 09:04:13 +0200

    hecho hasta jasmine, empezada la siguiente parte pero muy poco

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 2;
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+StackDataStructure.prototype.canPush = function() {
+  if (this.stackControl.length < this.MAX_SIZE || this.isEmpty() == true) {
+        return true;
+    }
+else {
+    return false;
+  }
+}
+
+StackDataStructure.prototype.push = function(e) {
+if (this.canPush() == true) {
+    this.stackControl.push(e);
+
+    return this.stackControl;
+} else {
+    return "Stack Overflow";
+}
+
+}
+
+StackDataStructure.prototype.pop = function(e) {
+    if (this.isEmpty() == true) {
+        return "Stack Underflow";
+    } else { 
+    return this.stackControl.pop(e);
+    }
+};
