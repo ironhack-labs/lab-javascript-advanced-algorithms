789ae24dff201fdc51c7159ca6f33864595a11eb
Javier Valiente <javara00@gmail.com>
2018-03-29 09:34:03 +0200

    Añado elementos y muestro bloque en top

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,15 +1,25 @@
 $( document ).ready(function (){
     
-   
     var lastChild = $(".empty:last-child");
     var list = [];
+
     $(".add").click(function(){
-        console.log(lastChild);
+        // Guardo el valor del input
         var inputVal = $("input").val();
-        lastChild.text(inputVal) ;
-        $("#empty8").html= inputVal;
-        list.push(inputVal);
-        console.log(list);
+        // Pusheo el value del input en el array base
+        
+        // A cada box una posición del array
+        if (list.length < 8) {
+            list.push(inputVal);
+            $("#stack-over").css("display", "none");
+            for (var i = 0; i < list.length; i++) { 
+                var conQuery = $("#empty" + i);
+                conQuery.text(list[i]).addClass("blue");
+                $("input").val("");
+            }    
+        } else {
+            $("#stack-over").css("display", "block");
+        } 
     });
 
     $(".remove").click(function(){
