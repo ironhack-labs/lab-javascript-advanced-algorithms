eedcb062ddb721ec33973656d8bebf4969aab33c
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-03-29 09:24:38 +0200

    lo mismo que el anterior con formateado

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,9 +1,7 @@
 var stackData = new StackDataStructure();
   stackData.MAX_SIZE = 8;
+
 $( document ).ready(function() {
-  
-
-
   
   $(".lleno").hide();
   $(".vacio").hide();
@@ -18,31 +16,26 @@ $( document ).ready(function() {
     }
     stackData.push(texto);
     
-    
-    
-  
   var index = stackData.MAX_SIZE - stackData.stackControl.length;
   $(".pila").eq(index).removeClass("pila-vacia");
   $(".pila").eq(index).addClass("pila-llena");
   $(".pila").eq(index).text(texto);
 });
 
-$(".btn-take").on("click", function () {
-  if ($(".lleno").css("visibility") == "visible") {
-    $(".lleno").hide();
-  }
-  if (stackData.stackControl.length == 0) {
-    $(".vacio").show();
-  }
-  var index = stackData.MAX_SIZE - stackData.stackControl.length;
-  var texto =  $(".entrada").eq(index).val();
-  stackData.pop(texto);
-  $(".pila").eq(index).removeClass("pila-llena");
-  $(".pila").eq(index).addClass("pila-vacia");
-  $(".pila").eq(index).text("");
 
+  $(".btn-take").on("click", function () {
+    if ($(".lleno").css("visibility") == "visible") {
+      $(".lleno").hide();
+    }
+    if (stackData.stackControl.length == 0) {
+      $(".vacio").show();
+    }
+    var index = stackData.MAX_SIZE - stackData.stackControl.length;
+    var texto =  $(".entrada").eq(index).val();
+    stackData.pop(texto);
+    $(".pila").eq(index).removeClass("pila-llena");
+    $(".pila").eq(index).addClass("pila-vacia");
+    $(".pila").eq(index).text("");
 
-
-});
-  
+  });
 });
