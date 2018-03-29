2d64d9ca3f694651400348b2e89f0ac96c04d84c
Antea Martín <anteamartinacinas@MacBook-Air-de-Antea.local>
2018-03-29 09:21:14 +0200

    hasta la segunda iteracion incluida

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,28 @@
 function QueueDataStructure () {
-}
+    this.queueControl = [];
+    this.MAX_SIZE     = 8;
+}  
+  QueueDataStructure.prototype.isEmpty = function () {
+    return this.queueControl.length === 0;
+  };
+  
+  QueueDataStructure.prototype.canEnqueue = function () {
+    return this.queueControl.length < this.MAX_SIZE;
+  };
+  
+  QueueDataStructure.prototype.enqueue = function (element) {
+    if (!this.canEnqueue()) {
+      return "Queue Overflow";
+    }
+  
+    this.queueControl.reverse().push(element);
+    return this.queueControl.reverse();
+  };
+  
+  QueueDataStructure.prototype.dequeue = function (element) {
+    if (this.isEmpty()) {
+      return "Queue Underflow";
+    }
+  
+    return this.queueControl.pop();
+  };
