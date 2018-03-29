2acb00c47d73fe0ec79b8b464a9fb267d8b8e489
aacecandev <aacecan.dev@gmail.com>
2018-03-29 09:16:43 +0200

    Hasta aquí he llegado

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,35 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+  if(this.queueControl.length > 0){
+    return false;
+  }
+  return true;
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  if(this.queueControl.length === this.MAX_SIZE){
+    return false;
+  }
+  return true;
+}
+
+QueueDataStructure.prototype.enqueue = function(element){
+  if(this.canEnqueue()){
+    this.queueControl.push(element);
+    return this.queueControl;
+  } else {
+    return "Queue Overflow";
+  }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+  if(!this.isEmpty()){
+    return this.queueControl.shift()
+  } else {
+    return "Queue Underflow";
+  }
 }
