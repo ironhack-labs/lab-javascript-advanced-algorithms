efb4fc8cf7253a49c2eedb88cf59aff97d082102
Daniel Lozano <lozanodaniel02@gmail.com>
2018-04-11 11:55:43 -0400

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 50;
+
+    this.isEmpty = function(){
+        return (this.queueControl.length === 0);
+    };
+
+    this.canEnqueue = function(){
+      return (this.queueControl.length + 1 < this.MAX_SIZE);
+    };
+
+    this.enqueue =  function (value) {
+        if (this.canEnqueue()) {
+            this.queueControl.push(value);
+            return this.queueControl.reverse();
+        }
+        return "Queue Overflow";
+    };
+
+    this.dequeue = function (){
+        if (this.queueControl.length > 0) {
+            return this.queueControl.pop();
+        }
+        return 'Queue Underflow';
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
 function StackDataStructure () {
+	this.stackControl = [];
+	this.MAX_SIZE = 50;
+    var undo = [];
+
+	this.push = function(val){
+		if (this.canPush()){
+			this.stackControl.push(val);
+			return this.stackControl;
+		}
+
+		return 'Stack Overflow';
+	};
+
+	this.pop = function(){
+		if (this.stackControl.length > 0)
+	        return this.stackControl.pop();
+		return 'Stack Underflow'
+	};
+
+    this.isEmpty = function(){
+	  return (this.stackControl.length === 0);
+    };
+
+    this.canPush = function(){
+      return (this.stackControl.length + 1 <= this.MAX_SIZE);
+    };
 }
