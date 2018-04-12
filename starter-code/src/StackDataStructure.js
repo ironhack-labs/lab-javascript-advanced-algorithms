8a3c51b50a3ff1d863dab72a8fd4c2f7cf7b7d8b
Alina Rodríguez <alinagriselrodriguez@gmail.com>
2018-04-11 23:59:55 -0500

    Almost Done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+
+    this.isEmpty = function() {
+        return (this.stackControl.length === 0);
+    };
+
+    this.canPush = function() {
+        return (this.stackControl.length + 1 <= this.MAX_SIZE);
+    };
+
+    this.push = function(element) {
+        if (this.canPush()) {
+            this.stackControl.push(element);
+            return this.stackControl;
+        }
+        return 'Stack Overflow';
+    };
+
+
+    this.pop = function() {
+        if (this.stackControl.length > 0)
+            return this.stackControl.pop();
+        return "Stack Underflow";
+    };
+
 }
