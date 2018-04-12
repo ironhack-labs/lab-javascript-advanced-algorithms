85c7e0af5fd38587f567bde29b873977131919b7
Franco Vargas <francovargas25@gmail.com>
2018-04-12 00:01:17 -0500

    still working on it..

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
+//function creates an array,
+
 function StackDataStructure () {
-}
+    this.stackControl = []; 
+    this.MAX_SIZE = 10;
+    this.isEmpty = function(){
+        var i = 0;
+         if(i < array.length){
+            console.log("vacio");
+            }
+         } 
+    this.canPush = function(){
+        if(array.lenght < 10){
+            console.log("todavia le cabe");
+            }
+        }
+    this.push = function(){ 
+    }
+    this.pop = function(){
+        return this.stackControl.pop();
+    }
+    
+    var array= [];
+
+};
+
+
