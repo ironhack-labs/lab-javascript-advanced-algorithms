769baadb2faba36ce2bf323044db0a358029a277
Fernanda <mfernandasrmz8201@gmail.com>
2018-04-11 19:44:51 -0500

    MX-FERNANDA

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 9;
+
+this.isEmpty = function(){
+  return this.queueControl.length === 0 ;
+};
+
+this.canEnqueue = function(){
+  return this.queueControl.length + 1 < this.MAX_SIZE;
 }
+
+this.enqueue = function (value){
+  if(this.canEnqueue()){
+    this.queueControl.push(value);
+    return this.queueControl.reverse();
+  }
+  return 'Queue Overflow'
+}
+
+this.dequeue = function (){
+  if (this.queueControl.length >= 1){
+    return this.queueControl.pop();
+  }
+  return 'Queue Underflow'
+  }
+
+ 
+}
+  
+
+
+  
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
-function StackDataStructure () {
+function StackDataStructure (stack) {
+this.stackControl = [];
+this.MAX_SIZE= 9;
+
+
+
+this.isEmpty = function (){
+  return this.stackControl.length === 0;
+} 
+
+this.push = function(val){
+  if (this.canPush()){
+    this.stackControl.push(val);
+    return this.stackControl;
+  }
+  return "Stack Overflow"; 
+
 }
+this.canPush = function (){
+return this.stackControl.length + 1 < this.MAX_SIZE;
+}
+this.pop = function (val){
+  if (this.stackControl.length >= 1){
+    return this.stackControl.pop(val);
+  } else {
+    return 'Stack Underflow';
+  }   
+
+}
+}
+
+
+
+
+
