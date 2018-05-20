8cda08b31eb9f197c37fc2c555f77215f539a916
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-20 22:15:59 +0200

    second iteration

---
+++ starter-code/js/stack.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+    var stack = new StackDataStructure();
+
+/*     $("#add-stack-button").click(function(){
+        var newLi = $("#stack-elements").append("<li></li>");
+        newLi.innerText = $("input [name='add-element'").val;
+    }); */
+
+    
+});
--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,41 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty = function() {
+    if (this.queueControl.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue = function() {
+    if (this.queueControl.length == this.MAX_SIZE) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+QueueDataStructure.prototype.enqueue = function(element) {
+    if (this.queueControl.length == this.MAX_SIZE) {
+        return "Queue Overflow";
+    } else {
+        this.queueControl.unshift(element);
+        return this.queueControl;
+    }
+
+    
+} 
+
+QueueDataStructure.prototype.dequeue = function() {
+    if (this.queueControl.length == 0) {
+        return "Queue Underflow";
+    } else {
+        return this.queueControl.pop();
+    }
+    
 }
