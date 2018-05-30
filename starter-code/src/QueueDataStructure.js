b7356ca3a6c5404401fe8507f8edf02be15f99e7
Chris Manthey <calined@gmx.de>
2018-05-30 13:58:44 +0200

    Queue halfway done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,16 @@
-function QueueDataStructure () {
+//FIFO
+function QueueDataStructure() {
+
+    this.queueControl = [];
+    this.MAX_SIZE = 0;
+
+    this.isEmpty = function () {
+        return this.queueControl.length ? false : true;
+    }
+
+    this.canEnqueue = function () { return true; }
+
+    this.enqueue = function (element) { this.queueControl.unshift(element); }
+
+    this.dequeue = function () { }
 }
