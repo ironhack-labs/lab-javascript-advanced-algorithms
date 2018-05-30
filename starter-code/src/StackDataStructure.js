39a8f05938b70ec34fd554a5c7531234af8cdc74
Sari <freja.sender@gmail.com>
2018-05-30 18:35:55 +0200

    complete StackDataStructure

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,26 @@
-function StackDataStructure () {
+function StackDataStructure(maxItems = 5) {
+  this.MAX_SIZE = maxItems;
+  this.stackControl = [];
 }
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length === 0;
+};
+
+StackDataStructure.prototype.canPush = function() {
+  return this.MAX_SIZE !== this.stackControl.length;
+};
+
+StackDataStructure.prototype.push = function(item) {
+  if (this.canPush()) {
+    this.stackControl.push(item);
+    return this.stackControl;
+  }
+  return "Stack Overflow";
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.isEmpty()) {
+    return "Stack Underflow";
+  }
+  return this.stackControl.pop();
+};
