5fa2f2dcc0ba4440a6c9adabf57b363292518881
Sari <freja.sender@gmail.com>
2018-05-30 18:37:26 +0200

    complete QueueDataStructure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
-function QueueDataStructure () {
+function QueueDataStructure(maxSize = 5) {
+  this.queueControl = [];
+  this.MAX_SIZE = maxSize;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  return this.queueControl.length === 0;
+};
+QueueDataStructure.prototype.canEnqueue = function() {
+  return this.queueControl.length !== this.MAX_SIZE;
+};
+QueueDataStructure.prototype.enqueue = function(item) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(item);
+    return this.queueControl;
+  }
+  return "Queue Overflow";
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+  if (this.isEmpty()) {
+    return "Queue Underflow";
+  }
+  return this.queueControl.pop();
+};
