6279b3c20322072fe2f86b16ec8cbd9cea7f36ce
Chris Manthey <calined@gmx.de>
2018-05-30 13:31:38 +0200

    stack halfway done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,3 +1,23 @@
 //LIFO
 function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 2;
+
+    this.isEmpty = function () { return this.stackControl.length ? false : true; }
+
+    this.canPush = function () { return this.stackControl.length < this.MAX_SIZE; }
+
+    this.push = function (element) {
+
+        if (this.stackControl.length < this.MAX_SIZE) {
+            this.stackControl.push(element);
+            return this.stackControl;
+        }
+        else {
+            return "Stack Overflow";
+        }
+    }
+
+    this.pop = function () { }
+
 }
