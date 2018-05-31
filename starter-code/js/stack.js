78fe84c45be9aaa387d09f6068b221fa6bc86efa
Sari <freja.sender@gmail.com>
2018-05-31 09:01:35 +0200

    add stack visualisation

---
+++ starter-code/js/stack.js
@@ -0,0 +1,69 @@
+var stack = new StackDataStructure(8);
+
+$(document).ready(function() {
+  var inputText = $("#new-element-input");
+  var stackUI = $("#stack");
+
+  $("#btn-add").click(function() {
+    bindStackControl(stack.push(inputText.val()));
+  });
+
+  $("#btn-take").click(function() {
+    bindStackControl(stack.pop());
+  });
+
+  function bindStackControl(stackControl) {
+    if (stackControl === "Stack Overflow") {
+      // add Stack Overflow
+      setOverflow(stackControl);
+    } else if (stackControl === "Stack Underflow") {
+      // add Stack Underflow
+      setUnderflow(stackControl);
+    } else if (typeof stackControl === "object") {
+      // fill stacks
+      setAllStackElementsToEmpty();
+      stackControl.forEach(function(element, index) {
+        setFilled(stackUI.children().eq(index), element);
+      });
+    }
+  }
+
+  function setOverflow(text) {
+    var overflowChild = stackUI.children().eq(stack.MAX_SIZE - 1);
+    setAlert(overflowChild, text);
+  }
+
+  function setAlert(child, text) {
+    child.addClass("alert");
+    child.removeClass("empty filled");
+    child.text(text);
+  }
+
+  function setUnderflow(text) {
+    var underflowChild = stackUI.children().eq(0);
+    setAlert(underflowChild, text);
+  }
+
+  function setEmpty(child) {
+    child.addClass("empty");
+    child.removeClass("filled alert");
+    child.text("");
+  }
+
+  function setFilled(child, text) {
+    child.addClass("filled");
+    child.removeClass("empty alert");
+    child.text(text);
+  }
+
+  function setAllStackElementsToEmpty() {
+    stackUI.children().each(function() {
+      setEmpty($(this));
+    });
+  }
+  function removeFromStack() {
+    var child;
+    if (stack.isEmpty) {
+    }
+  }
+});
