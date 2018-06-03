3db5669a91123c66059552f71145932cb568658f
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-06-03 13:07:03 +0200

    logic done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function () {
+  if (this.queueControl.length === 0) { return true; }
+  return false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function () {
+  if (this.queueControl.length < this.MAX_SIZE) { return true; }
+  return false;
+};
+
+QueueDataStructure.prototype.enqueue = function (element) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(element);
+    return this.queueControl;
+  }
+  return 'Queue Overflow';
+};
+
+QueueDataStructure.prototype.dequeue = function () {
+  if (!this.isEmpty()) { return this.queueControl.pop(); }
+  return 'Queue Underflow';
+};
