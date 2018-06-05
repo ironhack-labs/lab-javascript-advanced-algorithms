ff673c61a3f295f819223269b9371e358e06d586
Mariona Roca <mariona.roca@gmail.com>
2018-06-06 01:39:10 +0200

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+    //FIFO Structure
+    this.MAX_SIZE = 5;
+    this.queueControl = [];
+}
+
+QueueDataStructure.prototype.enqueue = function(element){
+  if (this.canEnqueue())
+  {
+    this.queueControl.push(element);
+    return this.queueControl;
+  }
+  else
+  {
+    console.log('could not add ' + element);
+    return 'Queue Overflow';
+  }
+}
+QueueDataStructure.prototype.dequeue = function(){
+  if (this.isEmpty())
+  {
+    return 'Queue Underflow';
+  }
+  else
+  {
+    var element = this.queueControl.splice(0,1);
+    return element;
+  } 
+}
+QueueDataStructure.prototype.canEnqueue = function(){
+  if(this.queueControl.length === this.MAX_SIZE) return false;
+  else return true;
+}
+QueueDataStructure.prototype.isEmpty = function(){
+  if(this.queueControl.length === 0) return true;
+  else return false;
 }
