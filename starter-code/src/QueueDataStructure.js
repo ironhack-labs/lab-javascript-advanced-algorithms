5d18b1204d03a5e000770a0ddef774eb490342a0
Jorge L <kolmax@gmail.com>
2018-06-19 23:06:54 +0200

    Start the daily exercise

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,13 @@
 function QueueDataStructure () {
+  this.queueControl = []
+  this.MAX_SIZE = 10
+}
+
+QueueDataStructure.prototype.enqueue = function(item) {
+  this.queue.unshift(item)
+}
+
+
+QueueDataStructure.prototype.dequeue = function() {
+  this.queue.pop()
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,23 @@
 function StackDataStructure () {
+  this.stackControl = []
+  this.MAX_SIZE = 10
+  this.isEmpty = function () {
+    return stackControl.length < 1
+  }
+}
+
+StackDataStructure.prototype.isEmpty = function () {
+  return stackControl.length < 1
+}
+
+StackDataStructure.prototype.canPush = function () {
+  return isEmpty()
+}
+
+StackDataStructure.prototype.push = function (item) {
+  this.stackControl.push(item)
+}
+
+StackDataStructure.prototype.pop = function () {
+  this.stackControl.pop()
 }
