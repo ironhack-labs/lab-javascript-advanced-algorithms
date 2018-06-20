45d6cbd2ac6a4c67638ca14e7b0954f3faf59f1e
Abel Alonso <abel.alonso@gmail.com>
2018-06-20 16:16:44 +0200

    second iteration

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+}
+
+QueueDataStructure.prototype.isEmpty = function () {
+    if (this.queueControl.length === 0){
+        return true;
+    }
+    return false;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE) {
+        return true;
+    }
+    return false;
+}
+
+QueueDataStructure.prototype.enqueue = function (e) {
+    if (this.canEnqueue()){
+        this.queueControl.unshift(e);
+        return this.queueControl;
+    }
+    return "Queue Overflow";    
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+    if (this.isEmpty()){
+        return "Queue Underflow";
+    }
+    return this.queueControl.pop();
 }
