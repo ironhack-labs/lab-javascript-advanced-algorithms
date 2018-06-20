e68809d058e560dd24768fc1fa7591df13772fb4
MateoReparaz <mateoreparaz1989@gmail.com>
2018-06-20 21:29:34 +0200

    Primeros cambios

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,20 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;   
+}
+StackDataStructure.prototype.isEmpty = function (){
+    this.stack = [];
+
+}
+
+StackDataStructure.prototype.canPush = function(){
+
+}
+
+StackDataStructure.prototype.push = function(){
+    this.stackControl.length = 1;
+}
+
+StackDataStructure.prototype.pop = function(){
+
 }
