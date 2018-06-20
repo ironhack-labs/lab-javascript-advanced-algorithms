ef5630f24b8207a93c33fe6d8e5d6fd457997776
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-06-20 19:43:25 +0200

    con_queue

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,5 +1,6 @@
 
 var stack = new StackDataStructure()
+var queue = new QueueDataStructure()
 
 $(".add").on('click',function(){
   var val = $('#val').val();
@@ -17,20 +18,39 @@ $(".add").on('click',function(){
   }
 });
 
-$(".take").on('click',function(){
-  if (stack.isEmpty()==false) {
-      var nuevostack = $(".stackelementfull:first")
-      stack.pop()
-      nuevostack.text("");
-      nuevostack.removeClass("stackelementfull");
-      nuevostack.addClass("stackelementempty");
+
+$(".addq").on('click',function(){
+  var val = $('#val2').val();
+  if (queue.canEnqueue()) {
+      var nuevoqueue = $(".stackelementemptyq:first")
+      queue.enqueue()
+      nuevoqueue.text(val);
+      nuevoqueue.removeClass("stackelementemptyq");
+      nuevoqueue.addClass("stackelementfullq");
   } else {
-    var overflow = $(".stackelementstatus")
-    overflow.text("Stack Underflow!!")
-    overflow.removeClass("stackelementstatus")
-    overflow.addClass("stackunderflow")
+    var overflow = $(".stackelementstatusq")
+    overflow.text("Queue OverFloW!!")
+    overflow.removeClass("stackelementstatusq")
+    overflow.addClass("stackoverflowq")
   }
 });
+
+$(".takeq").on('click',function(){
+  if (queue.isEmpty()==false) {
+      var nuevoqueue = $(".stackelementfullq:last")
+      queue.dequeue()
+      nuevoqueue.text("");
+      nuevoqueue.removeClass("stackelementfullq");
+      nuevoqueue.addClass("stackelementemptyq");
+  } else {
+    var overflow = $(".stackelementstatusq")
+    overflow.text("Queue Underflow!!")
+    overflow.removeClass("stackelementstatusq")
+    overflow.addClass("stackunderflowq")
+  }
+});
+
+
   
 
 
