21aa16f62163f7517ae2ba817ca2e38cc0946ad4
Enrique Pernia <Epernia96@gmail.com>
2018-06-20 23:03:12 +0200

    Depurado el codigo

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,62 +1,61 @@
-var contadorPrimero = 8;
-var contadorUltimo = 8;
-var contadorElementos = 0;
+var contadorPrimero = 8; //Variable que apunta al primer elemento
+var contadorUltimo = 8; //Variable que apunta al ultimo elemento
+var contadorElementos = 0; //Numero de elementos en la lista
 $(document).ready(function () {
-     var padreLista = $(".inside-lista-queue");
-     var hijo = padreLista.children();
-     var input = $(".valor-queue");
-     $(".sacar-queue").click(function () {
-          $(hijo[8]).removeClass("red");
+     var padreLista = $(".inside-lista-queue"); //UL
+     var hijo = padreLista.children(); //conjunto html de li
+     var input = $(".valor-queue"); //Valor introducido
+     $(".sacar-queue").click(function () { //Si clickamos en take
+          $(hijo[8]).removeClass("red"); //Operaciones que debes realizar en cada operacion para eliminar los colores de los elementos stack overflow y underflow
           $(hijo[0]).removeClass("red");
-          $(hijo[8]).text("");
-          $(hijo[0]).text("");
-          if (contadorElementos <= 0) {
+          $(hijo[0]).text(""); //Si se muestra stack overflow y despues se hace un take para sacar un elemento, esta operacion elimina el texto de la novena casilla
+          if (contadorElementos <= 0) { //Si la lista esta vacia 
                $(hijo[8]).text("STACK   UNDERFLOW");
                $(hijo[8]).addClass("red")
-          } else {
-               $(hijo[contadorPrimero]).text(" ");
-               contadorUltimo++;
-               for (i = 0; i < 7; i++) {
-                    if (i == 6) {
+          } else { //Si no
+               $(hijo[contadorPrimero]).text(" "); //Se elimina el primer elemento (FIFO)
+               contadorUltimo++; //El puntero al ultimo elemento avanza ya que la lista se va a contraer
+               for (i = 0; i < 7; i++) { //Bucle que hace avanzar una posicion a cada elemento de la lista
+                    if (i == 6) { //En caso de ser el ultimo elemento, debemos eliminar el azul de su casilla
                          $(hijo[contadorPrimero]).text($(hijo[contadorPrimero - 1]).text());
                          $(hijo[contadorPrimero - 1]).text("")
                          $(hijo[contadorPrimero - 1]).removeClass("blue")
                     } else {
                          $(hijo[contadorPrimero]).text($(hijo[contadorPrimero - 1]).text());
                          $(hijo[contadorUltimo]).removeClass("blue")
-                         contadorPrimero--;
                     }
                }
-               contadorElementos--;
-               contadorPrimero = 8;
+               contadorElementos--; //Elementos totales -1
+               contadorPrimero = 8; //Posicionamos otra vez el primero
           }
      })
 
      $(".meter-queue").click(function () {
-          $(hijo[8]).removeClass("red");
+          $(hijo[8]).removeClass("red"); //Operaciones que debes realizar en cada operacion para eliminar los colores de los elementos stack overflow y underflow
           $(hijo[0]).removeClass("red");
-          $(hijo[0]).text("");
-          var value = input.val();
-          if (value == "") {
-               return false;
-          }
-          if (contadorElementos > 7) {
+          $(hijo[0]).text(""); //Si se muestra stack overflow y despues se hace un add para sacar un elemento, esta operacion elimina el texto de la novena casilla
+          var value = input.val(); //Valor del input
+          if (value == "" && $(hijo[8]).hasClass("blue")) { //Si se clicka en add sin haber introducido texto   pero ya hay algo en la primera posicion
+
+          } else if (value == "") {
+               $(hijo[8]).text(""); //Esto se hace por si se ha producido previamente stack underflow, de esta forma se elimina el texto del primer elemento
+          } else if (contadorElementos > 7) { //Si se ha llegado al maximo de elementos
                $(hijo[0]).text("STACK   OVERFLOW");
                $(hijo[0]).addClass("red")
-               for (i = 1; i < 9; i++) {
+               for (i = 1; i < 9; i++) { //Para vaciar el array
                     $(hijo[i]).text("");
                     $(hijo[i]).removeClass("blue")
                }
-               contadorElementos = 0;
+               contadorElementos = 0; //Reseteamos los valores ya que se ha vaciado el array
                contadorPrimero = 8;
                contadorUltimo = 8;
           } else {
-               $(hijo[0]).text("");
+               $(hijo[0]).text(""); //Si no
                $(hijo[contadorUltimo]).text(value);
                $(hijo[contadorUltimo]).addClass("blue");
                input.val("");
-               contadorUltimo--;
-               contadorElementos++;
+               contadorUltimo--; //Actualizamos la ultima posicion 
+               contadorElementos++; // Elementos + 1
           }
      })
 });
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,52 +1,52 @@
-var stack=new StackDataStructure();
-var contador=8;
+var stack = new StackDataStructure();
+var contador = 8;
 
 $(document).ready(function () {
-     var padreLista=$(".inside-lista-stack");
-     var hijo=padreLista.children();
-     var input=$(".valor-stack");
+     var padreLista = $(".inside-lista-stack"); //UL
+     var hijo = padreLista.children(); //HTML de li
+     var input = $(".valor-stack"); //Valor del input
 
 
-     $(".sacar-stack").click(function(){
-          $(hijo[8]).removeClass("red");
+     $(".sacar-stack").click(function () {
+          $(hijo[8]).removeClass("red"); //Operaciones que debes realizar en cada operacion para eliminar los colores de los elementos stack overflow y underflow
           $(hijo[0]).removeClass("red");
-          $(hijo[0]).text("");
-          if(contador>=8){
-               $(hijo[8]).text("STACK   UNDERFLOW");
+          $(hijo[0]).text(""); //Si se muestra stack overflow y despues se hace un take para sacar un elemento, esta operacion elimina el texto de la novena casilla
+          if (contador >= 8) { //Si la lista esta vacia
+               $(hijo[8]).text("STACK   UNDERFLOW"); //Mostramos stack underflow en rojo
                $(hijo[8]).removeClass("blue")
                $(hijo[8]).addClass("red")
-               contador=8;
+               contador = 8; //Lista vacia
+          } else { //Si no
+               $(hijo[contador + 1]).text(" "); //Como el contador apunta al hueco vacio en el que va el proximo elemento, eliminamos el elemento con posicion contador+1(Ultimo elemento añadido)
+               $(hijo[contador + 1]).removeClass("blue")
+               contador++;
           }
-         else{ $(hijo[contador+1]).text(" ");
-         $(hijo[contador+1]).removeClass("blue")
-          contador++;
-     }
      })
 
 
-     $(".meter-stack").click(function(){
-          $(hijo[8]).removeClass("red");
+     $(".meter-stack").click(function () {
+          $(hijo[8]).removeClass("red"); //Operaciones que debes realizar en cada operacion para eliminar los colores de los elementos stack overflow y underflow
           $(hijo[0]).removeClass("red");
-          $(hijo[0]).text("");
-          var value=input.val();
-          if(value==""){
-               return false;
-          }
-          if(contador<1){
-               $(hijo[0]).text("STACK   OVERFLOW");
+          $(hijo[0]).text(""); //Si se muestra stack overflow y despues se hace un take para sacar un elemento, esta operacion elimina el texto de la novena casilla
+          var value = input.val();
+          if (value == "" && $(hijo[8]).hasClass("blue")) {
+               //Si no se ha introducido un valor pero ya hay al menos uno en la primera posicion no se hace nada
+          } else if (value == "") { //Esto se ejecuta si se ha pulsado el add sin u imput y previamente esta vacio o contiene stack underflow
+               $(hijo[8]).text(""); //Para eliminar el texto de stack underflow en caso de que estuviese
+          } else if (contador < 1) { //Si esta lleno
+               $(hijo[0]).text("STACK   OVERFLOW"); //Mostramos stack overflow en rojo
                $(hijo[0]).addClass("red");
-               for(i=1;i<9;i++){
+               for (i = 1; i < 9; i++) { //Vaciamos la lista
                     $(hijo[i]).text("");
                     $(hijo[i]).removeClass("blue");
                }
-               contador=8;
-          }
-          else{
-          input.text(" ");
-          $(hijo[contador]).text(value);
-          $(hijo[contador]).addClass("blue");
-          input.val("");
-          contador--;
+               contador = 8; //Reiniciamos contador de elementos
+          } else { //Si no
+               input.text(" ");
+               $(hijo[contador]).text(value); //Introducimos el valor, coloreamos la casilla y actualizamos el contador
+               $(hijo[contador]).addClass("blue");
+               input.val("");
+               contador--;
           }
      })
 });
