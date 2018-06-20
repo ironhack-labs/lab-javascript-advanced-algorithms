7830b93dcf3c881ef1bc8bea644f2ca529d18d37
Jesus Martinez <jesmart1969@gmail.com>
2018-06-20 23:44:43 +0200

    Iteración: Solo hasta HTML con Stack - Sin Queue

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,38 +1,39 @@
-function StackDataStructure () {
+function StackDataStructure() {
   this.stackControl = [];
-  this.MAX_SIZE = 10;
+  this.MAX_SIZE = 8;
 
-  this.isEmpty = function(){
-    if (this.stackControl.length > 0){
+  this.isEmpty = function() {
+    if (this.stackControl.length > 0) {
       return false;
-    } else{
+    } else {
       return true;
     }
-  }
+  };
 
-  this.canPush = function(){
-    if (this.stackControl.length < this.MAX_SIZE){
+  this.canPush = function() {
+    if (this.stackControl.length < this.MAX_SIZE) {
       return true;
     } else {
       return false;
     }
-  }
+  };
 
-  this.push = function(element){
-    if (this.canPush()){
+  this.push = function(element) {
+    if (this.canPush()) {
       this.stackControl.push(element);
+      console.log(element)
       return this.stackControl;
     } else {
+      console.log("Stack Overflow");
       return "Stack Overflow";
     }
-  }
+  };
 
-  this.pop = function(){
-    if (!this.isEmpty()){
+  this.pop = function() {
+    if (!this.isEmpty()) {
       return this.stackControl.pop();
     } else {
       return "Stack Underflow";
     }
-  }
-
+  };
 }
