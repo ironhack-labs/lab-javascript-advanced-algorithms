4a11daea7b07ca42f3fae4709eab80bd871c8baa
Jesus Martinez <jesmart1969@gmail.com>
2018-06-20 19:11:21 +0200

    Iteracion 1: Solo logica

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = function(){
+    if (this.queueControl.length > 0){
+      return false;
+    } else{
+      return true;
+    }
+  }
+
+  this.canEnqueue = function(){
+    if (this.queueControl.length < this.MAX_SIZE){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.enqueue = function(element){
+    if (this.canEnqueue()){
+      this.queueControl.unshift(element);
+      return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+  }
+
+  this.dequeue = function(){
+    if (!this.isEmpty()){
+      return this.queueControl.pop();
+    } else {
+      return "Queue Underflow";
+    }
+  }
+
 }
+
