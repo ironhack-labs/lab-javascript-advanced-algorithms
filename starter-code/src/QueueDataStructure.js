058ced48cd4d370bfd245e5f1012f1d0c474c1e0
Fernanda Velazquez <fvel4z@gmail.com>
2018-06-20 22:13:26 -0500

    almost done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queue = [];
+    this.size = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+    if ((this.queue.length == 0)) {
+        return true;
+    }
+};
+
+QueueDataStructure.prototype.canPush = function() {
+    if (queue.length < this.size) {
+        return true;
+    }
+};
+
+QueueDataStructure.prototype.push = function(e) {
+    if (this.canPush()) {
+        this.queue.push(e);
+    }
+};
+
+QueueDataStructure.prototype.pop = function () {
+    if (!this.isEmpty()) {
+        this.queue.pop();
+    }
+}
+
+var myqueue = new QueueDataStructure();
