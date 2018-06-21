24071d52970c5f731ee2a7592b933313d04b94d9
Javier Martin <fjmn2c@gmail.com>
2018-06-21 09:29:53 +0200

    version final

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,46 @@
 function StackDataStructure () {
-}
+
+    this.stackControl=[];
+    this.MAX_SIZE=8;
+    
+    
+};
+
+StackDataStructure.prototype.canPush=function(){
+
+    return (this.stackControl.length<this.MAX_SIZE)
+
+
+};
+
+
+StackDataStructure.prototype.isEmpty=function(){
+
+    return (this.stackControl.length==0)
+ 
+ };
+
+StackDataStructure.prototype.push=function(element){
+
+    if (this.canPush()){
+        this.stackControl.push(element);
+        return this.stackControl;
+    }
+    else 
+        return "Stack Overflow";
+};
+
+StackDataStructure.prototype.pop=function(){
+
+    var last=this.stackControl[this.stackControl.length-1]
+    if (!this.isEmpty()){
+       this.stackControl.pop();
+        return last;
+    }
+
+    else return "Stack Underflow"
+
+};
+
+
+
