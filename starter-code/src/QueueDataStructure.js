6e8fa27a45f5b0265aaa5749901d5ad819dbc975
Pablo Rezola <pablorezola20@yahoo.es>
2018-06-21 06:59:47 +0200

    stack

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
+
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+    
 }
+
+
+QueueDataStructure.prototype.isEmpty = function(){
+    if(this.queueControl.length<=0){
+             return true;
+          }
+           return false;
+     
+};
+QueueDataStructure.prototype.canEnqueue = function(){
+    if(this.queueControl.length==this.MAX_SIZE){
+                  return false;
+             }
+             return true;
+};
+QueueDataStructure.prototype.enqueue = function(element){
+    if(this.canEnqueue()){
+              this.queueControl.unshift(element);
+                  return this.queueControl;
+             }
+             return "Queue Overflow";
+     
+};
+QueueDataStructure.prototype.dequeue = function(element){
+    if (this.isEmpty()) {
+           return 'Queue Underflow';
+          }
+          return this.queueControl.pop();
+
+
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
 function StackDataStructure () {
+ 
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+    
 }
+
+
+StackDataStructure.prototype.isEmpty = function(){
+     return this.stackControl.length == 0;
+     
+};
+StackDataStructure.prototype.canPush = function(){
+    return this.stackControl.length < this.MAX_SIZE;
+};
+StackDataStructure.prototype.push = function(element){
+    if (this.stackControl.length >= this.MAX_SIZE ){
+        return "Stack Overflow";
+    } else { 
+    this.stackControl.push(element);
+    return this.stackControl; }
+     
+};
+StackDataStructure.prototype.pop = function(element){
+    var lastElement = this.stackControl[this.stackControl.length -1];
+    this.stackControl.pop(element);
+    if (this.stackControl.length == 0){
+        return "Stack Underflow";
+    }
+    return lastElement;
+};
+
+
+    
