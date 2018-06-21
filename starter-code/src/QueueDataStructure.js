26b40a727c93962effe2539b17d0ca9821cf3f0c
Lili Cadena <anlica_99@hotmail.com>
2018-06-21 08:27:10 -0500

    Logré imprimir elementos en la página, me falto recorrerlos

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
 function QueueDataStructure () {
-}
+	this.queueControl= [];
+	this.MAX_SIZE= 10;
+	this.isEmpty= function(){
+		 if(this.queueControl.length === 0){
+		 	return true;
+		 } else {
+		 	return false;
+		 }
+	};
+
+	this.canEnqueue= function(){
+		if(this.queueControl.length === this.MAX_SIZE){
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	this.enqueue= function(e){
+		if (this.queueControl.length === this.MAX_SIZE ){
+			return "Queue Overflow";
+		} else {
+			this.queueControl.unshift(e);
+			return this.queueControl;
+		}	
+	};
+
+	this.dequeue= function(){
+		if (this.queueControl.length === 0){
+			return "Queue Underflow";
+		} else{
+			return this.queueControl.pop();
+		}
+	};
+};
