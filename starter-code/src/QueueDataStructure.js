3c0c788cbd7c8196979f417504ab9539d9c7d9a7
Pepe Lora <jose.maria.lora.alarcon@gmail.com>
2018-06-21 09:27:19 +0200

    tercera sin terminar

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,44 @@
-function QueueDataStructure () {
+
+
+function QueueDataStructure() {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
 }
+
+QueueDataStructure.prototype.isEmpty = function () {
+    if (this.queueControl.length == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+    if (this.queueControl.length == this.MAX_SIZE) {
+        return false
+    }
+    else {
+        return true
+    }
+
+}
+QueueDataStructure.prototype.enqueue = function (e) {
+
+    if(this.canEnqueue()){
+        this.queueControl.unshift(e)
+        return this.queueControl
+    } else {
+        return "Queue Overflow"
+    }
+
+}
+QueueDataStructure.prototype.dequeue = function () {
+    if (this.queueControl.length == 0) {
+        return "Queue Underflow"
+    }
+    else {
+    return this.queueControl.pop()
+}
+}
+
