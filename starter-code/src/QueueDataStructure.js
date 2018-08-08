1142240bb28651a75fc1d67032377a8ccb759b32
adiyathereh <ad.kydy@gmail.com>
2018-08-08 19:35:53 +0200

    complete the logic part

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = function() {
+    if (this.queueControl.length) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  this.canEnqueue = function() {
+    if (this.queueControl.length >= this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  this.enqueue = function(newItem) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(newItem);
+      return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+  };
+
+  this.dequeue = function() {
+    if (this.queueControl.length > 0) {
+      this.queueControl.shift();
+      return this.queueControl.shift();
+    } else {
+      return "Queue Underflow";
+    }
+  };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = function() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Should have a method to check if we can push elements into the stack
+  this.canPush = function() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.push = function(newItem) {
+    //"Should return 'Stack Overflow' if the stack is full"
+    if (this.canPush()) {
+      this.stackControl.push(newItem);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  };
+
+  this.pop = function() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
+  };
 }
+let stack = new StackDataStructure();
+stack.push(19);
+
+console.log(stack);
