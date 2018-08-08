fb1b9b7ccaee8706ee925f113a1f93f524b87259
Anais Plancoulaine <anais.plancoulaine@gmail.com>
2018-08-08 19:16:33 +0200

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,46 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 1;
+    this.isEmpty =
+        function() {
+            if (this.stackControl.length === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } || true;
+
+    this.canPush = function() {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.push = function(element) {
+        if (this.canPush) {
+            this.stackControl.push(element);
+            return this.stackControl;
+        } else {
+            return "Stack Overflow";
+        }
+    };
+
+    this.pop = function(element) {
+        if (!this.isEmpty) {
+            this.stackControl.pop(element);
+            return this.stackControl.pop();
+        } else {
+            return "Stack Underflow";
+        }
+    };
+
+    this.isArray = function() {
+        if (this.isArray.stackControl) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
