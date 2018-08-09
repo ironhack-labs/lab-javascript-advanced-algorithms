5958d84981accd4778c71a7c314122747b877081
AlinaKr <mirgehendieideenaus@gmail.com>
2018-08-09 18:43:03 +0200

    BER Alina-Kraft

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+    this.isEmpty = function(){
+        if (this.queueControl.length === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+this.canEnqueue = function(){
+    if (this.queueControl.length < this.MAX_SIZE){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+this.enqueue = function(elem){
+    if(this.canEnqueue()){
+        this.queueControl.unshift(elem);
+        return this.queueControl;
+    }
+    else{
+        return 'Queue Overflow';
+    }
+}
+this.dequeue = function(){
+    if(this.queueControl.length > 0){
+        return this.queueControl.pop();
+    }
+    else{
+        return 'Queue Underflow';
+    }
+}
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+    this.isEmpty = function(){
+        if (this.stackControl.length === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    this.canPush = function(){
+        if (this.stackControl.length < this.MAX_SIZE){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    this.push = function(elem){
+        if(this.canPush()){
+            this.stackControl.push(elem);
+            return this.stackControl;
+        }
+        else{
+            return 'Stack Overflow';
+        }
+    }
+    this.pop = function(){
+        if(this.stackControl.length > 0){
+            return this.stackControl.pop();
+        }
+        else{
+            return 'Stack Underflow';
+        }
+    }
+
 }
+
+stack1 = new StackDataStructure();
+stack1.push(102)
+
+//this.stackControl.length > 0
