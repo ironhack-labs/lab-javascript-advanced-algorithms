60b791e4a90c675bfe92bfcb1000eaa6ce02c345
Adolfo Vela <vela@tekplanner.com>
2018-08-19 10:31:19 -0500

    Terminado Ok

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+}
+
+QueueDataStructure.prototype.isEmpty = function () {
+  if (this.queueControl.length === 0) return true;
+  else return false;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+  if (this.queueControl.length < this.MAX_SIZE) return true;
+  else return false
+}
+
+QueueDataStructure.prototype.enqueue = function (element) {
+  this.queueControl.unshift(element);
+  if (this.queueControl.length > this.MAX_SIZE) {
+    return "Queue Overflow"
+  };
+  return this.queueControl
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+  if (this.queueControl.length === 0) {
+    return "Queue Underflow"
+  }
+  return this.queueControl[this.queueControl.length-1];
 }
