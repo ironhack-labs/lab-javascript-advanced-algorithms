3d982ca4e536cd960a66405b5eb20bc1f3f91697
Ricardo Mayo <rmayo.1995@gmail.com>
2018-08-21 12:59:38 -0500

    Complete

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,27 +1,47 @@
+// New queue obj.
 var stack = new StackDataStructure();
 
+// Add element to the queue
 $("#add-stack").click(function() {
-    var val = $("#stackelement").val();
-    if(stack.canPush()) {
-        $(".error-stack:last").hide();
-        $("#delete-stack").prop('disabled', false);
-        stack.push(val);
-        $(".stack-element:empty:last").text(val);
+  // MISSING CHECK IF EMPTY INPUT !!!
+    if ($("#stackelement").val() != '') {
+        var element = $("#stackelement").val();
+        if (stack.canPush()) {
+            // Hide Stack Underflow warning
+            $(".error-stack:last").hide();
+            // Enable stack-push button
+            $("#delete-stack").prop("disabled", false);
+            stack.push(element);
+            // Adds text to the last empty stack-element
+            $(".stack-element:empty:last").text(element);
+        } else {
+            // Show Stack Overflow warning
+            $(".error-stack:first").show();
+            // Disable add-stack button
+            $("#add-stack").prop("disabled", true);
+        }
     } else {
-        $(".error-stack:first").show();
-        $("#add-stack").prop('disabled', true);
+        alert('Empty input');
     }
+
+    // Empty input field
     $("#stackelement").val('');
 });
 
+// Delete element from the stack
 $("#delete-stack").click(function() {
-    if(stack.isEmpty()) {
-        $(".error-stack:last").show();
-        $("#delete-stack").prop('disabled', true);
-    } else {
-        $(".error-stack:first").hide();
-        $("#add-stack").prop('disabled', false);
-        stack.pop();
-        $(".stack-element:not(:empty):first").text('');
-    }
+  if (stack.isEmpty()) {
+    // Show Stack Underflow warning
+    $(".error-stack:last").show();
+    // Disable delete-stack button
+    $("#delete-stack").prop("disabled", true);
+  } else {
+    // Hide Stack Overflow warning
+    $(".error-stack:first").hide();
+    // Enable add-stack button
+    $("#add-stack").prop("disabled", false);
+    stack.pop();
+    // Empty first non empty stack-element
+    $(".stack-element:not(:empty):first").text('');
+  }
 });
