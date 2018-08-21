e32ada8c654ee796b88e1ca4993d63514dcdff45
sannchez-Mx <sannchez-mx@outlook.com>
2018-08-20 20:17:43 -0500

    incomplete

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
-function QueueDataStructure () {
+class QueueDataStructure  {
+    constructor(){
+    this.queueControl =[];
+    this.MAX_SIZE = 10;
 }
+isEmpty(){
+    if (this.queueControl.length === 0) {
+        return true;
+      } else {
+        return false;
+      };
+};
+
+canEnqueue(){
+    if (this.queueControl.length === this.MAX_SIZE) {
+        return false;
+      } else {
+        return true;
+      };
+};
+
+enqueue(e){ 
+    if (this.queueControl.length >= this.MAX_SIZE) {
+        return 'Queue Overflow';
+      };
+      this.queueControl.unshift(e);
+      return this.queueControl;
+};
+
+dequeue(){
+    if (this.queueControl.length === 0) {
+        return 'Queue Underflow';
+      }
+      var vas = this.queueControl.length -1;
+      return this.queueControl[vas];
+    }
+
+};
