b17afb8a708eece07aefb4c6eb1c3cb891d50b76
Mapno <mariorvinas@gmail.com>
2018-08-29 17:11:31 +0200

    jasmine passed

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 2;
+}
+
+QueueDataStructure.prototype.isEmpty = function () {
+    return this.queueControl.length === 0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+    return this.queueControl.length < this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue = function (e) {
+    if(this.canEnqueue()){
+        this.queueControl.unshift(e);
+        return this.queueControl;
+    } else {
+        return "Queue Overflow";
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+    if(this.isEmpty()){
+        return "Queue Underflow";
+    } else {
+        return this.queueControl.splice(-1,1)[0];
+    }
 }
