d6629c07940c12413dc9cd0a1cee8dced5fd9f20
Teo Diaz <tdiazsan@gmail.com>
2018-08-29 22:15:47 +0200

    conseguido

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -2,9 +2,19 @@ var stack = new StackDataStructure();
 
 $("#add-btn-stack").click(function() {
   stack.push(1);
-  console.log(stack.stackControl);
   var i = (stack.MAX_SIZE) - stack.stackControl.length;
+  var x = $("#inputGroup-sizing-sm").prop('value');
+
     $($(".div-sec")[i]).addClass("stack-div");
+    $($(".div-sec")[i]).html(x);
+
     
-    
+});
+
+
+$("#remove-btn-stack").click(function() {
+stack.pop(x);
+var x = (stack.MAX_SIZE-1) - stack.stackControl.length;
+  $($(".div-sec")[x]).removeClass("stack-div");
+  $($(".div-sec")[x]).html("");
 });
