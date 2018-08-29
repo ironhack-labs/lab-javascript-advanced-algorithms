39d3dbdd9e4431ef9d8338f3b54e7b193a0d90a4
FonRiego <fonriego@gmail.com>
2018-08-29 23:05:03 +0200

    Iterations 1 & 2

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
 function StackDataStructure () {
+
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  return this.stackControl.length === 0;
+}
+
+StackDataStructure.prototype.canPush = function () {
+  return this.stackControl.length < this.MAX_SIZE; 
+}
+
+StackDataStructure.prototype.push = function (data) {
+  if (this.canPush()) {
+    this.stackControl.push(data);
+    return this.stackControl;
+  } else {
+    return "Stack Overflow";
+  }
+}
+
+StackDataStructure.prototype.pop = function() {
+  if (this.isEmpty()) {
+      return 'Stack Underflow';
+  } else {
+      return this.stackControl.pop();
+  }
+}
+
