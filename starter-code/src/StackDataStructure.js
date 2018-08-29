2a3c9623b697702813d9ca0cb964ed6c33a30adf
agdwm <almudenaguerraswm@gmail.com>
2018-08-29 20:22:02 +0200

    Queue Structure and Functionality done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,30 +1,42 @@
 //LIFO (Last-in First-out)
-var StackDataStructure = function () {
+function StackDataStructure () {
 	this.stackControl = [];
 	this.MAX_SIZE = 8;
 }
 
-
-StackDataStructure.prototype.push = function(element) {
-	//Always to add to the end fo the array
-	this.stackControl.push(element);
-	return this.maxNumberOfItems;
-}
-
-StackDataStructure.prototype.pop = function() {
-	//Always remove the lastElement inserted
-	this.stackControl.splice[-1];
-}
-
 StackDataStructure.prototype.isEmpty = function() {
-	if (this.stackControl.length === 0){
+	if (this.stackControl.length === 0) {
 		return true;
+	} else {
+		return false;
 	}
 }
 
 StackDataStructure.prototype.canPush = function() {
-	if (this.stackControl.length < this.MAX_SIZE){
+	if (this.stackControl.length < this.MAX_SIZE) {
 		return true;
+	} else {
+		return false;
 	}
-	return false;
 }
+
+StackDataStructure.prototype.push = function(element) {
+	//Last In
+	if (this.canPush() === false) {
+		return 'Stack Overflow';
+	} else {
+		this.stackControl.push(element);
+		return this.stackControl;
+	}	
+}
+
+StackDataStructure.prototype.pop = function() {
+	//First Out
+	if (this.isEmpty()) {
+		return 'Stack Underflow';
+	}
+	return this.stackControl.pop();
+}
+
+
+
