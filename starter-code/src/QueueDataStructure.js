8334671433c1d3524cc8ece85d675919a51834ec
David Sirit <davidsirit@yahoo.com>
2018-08-29 21:47:08 +0200

    Second Interation Done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,31 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+
 }
+
+QueueDataStructure.prototype.isEmpty = function (){
+  return this.queueControl.length === 0;
+};
+
+QueueDataStructure.prototype.canEnqueue = function (){
+  return this.queueControl.length < this.MAX_SIZE;
+};
+
+QueueDataStructure.prototype.enqueue = function (elem){
+  if (this.canEnqueue() === true){
+    this.queueControl.unshift(elem);
+    return this.queueControl;
+  } else {
+    return 'Queue Overflow';
+  }
+};
+
+QueueDataStructure.prototype.dequeue = function (){
+  if (this.queueControl.length > 0){
+    return this.queueControl.pop();
+  } else{
+    return 'Queue Underflow';
+  }
+};
+
