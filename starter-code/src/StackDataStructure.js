7e5b83ddb23ce71b8d91b13bc74c0fb9bb8b5c84
agdwm <almudenaguerraswm@gmail.com>
2018-08-29 17:48:27 +0200

    Stack methods default values done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,30 @@
-function StackDataStructure () {
+//LIFO (Last-in First-out)
+var StackDataStructure = function () {
+	this.stackControl = [];
+	this.MAX_SIZE = 8;
+}
+
+
+StackDataStructure.prototype.push = function(element) {
+	//Always to add to the end fo the array
+	this.stackControl.push(element);
+	return this.maxNumberOfItems;
+}
+
+StackDataStructure.prototype.pop = function() {
+	//Always remove the lastElement inserted
+	this.stackControl.splice[-1];
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+	if (this.stackControl.length === 0){
+		return true;
+	}
+}
+
+StackDataStructure.prototype.canPush = function() {
+	if (this.stackControl.length < this.MAX_SIZE){
+		return true;
+	}
+	return false;
 }
