b2d0b1b6c500e0fb7713fcc66b82cd09f5a98e76
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-08-29 21:43:01 +0200

    Stack

---
+++ starter-code/js/stack.js
@@ -0,0 +1,51 @@
+function init() {
+
+  var myStack = new StackDataStructure();
+  
+
+  $("#add-stack").click(function(){ //adds the input to the last div
+    var input = $("#input-stack").prop("value");
+
+    if (input==="") {
+      return
+    }
+  
+    if(myStack.canPush()){
+      myStack.push(input);
+      $(".underflow").css("display", "none");
+      for (let i = 7; i >= 0; i--){
+        $($(".cell")[7-i]).text(myStack.stackControl[i]);
+      }
+
+    } else {
+      $(".overflow").css("display", "block");
+    }  
+  })
+
+  //-----------------------------------------------------------------
+
+  $("#take-stack").click(function(){ //removes last element
+  
+    if(!myStack.isEmpty()){
+
+      let divs = $(".cell");
+      myStack.stackControl.pop();
+      $(".overflow").css("display", "none");
+      
+      for (let i = 7; i >= 0; i--){
+        if(myStack.stackControl[i] == undefined) {
+          $(divs[7-i]).text("");
+        } else {
+          $(divs[7-i]).text(myStack.stackControl[i]);        
+        }
+      }
+    
+    }else {
+      $(".underflow").css("display", "block");
+
+    }
+  });
+   
+}
+
+$(document).ready(init());
