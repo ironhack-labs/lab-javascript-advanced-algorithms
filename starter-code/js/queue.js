cdd1b28e6b9d4941eb784268c4075e3ff2ea7642
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-08-30 15:13:05 +0200

    Errores arreglados

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,51 +1,61 @@
 var queue = new QueueDataStructure(size);
 var $queue = document.getElementById("queue");
-var $queueElements;
 var $queueAdd = document.getElementById("queue-add");
 var $queueTake = document.getElementById("queue-take");
 var $queueInput = document.getElementById("queue-input");
 var $elQueueTake = document.getElementById("el-queue-take");
+var $queueElements;
+
 for (var i = -1; i <= queue.MAX_SIZE; i++) {
   $queue.appendChild(document.createElement("li"));
 }
 
 $queueElements = $queue.getElementsByTagName("li");
+
 $queueTake.onclick = function() {
   var i = 0;
   var element = queue.dequeue();
   if (element != "Queue Underflow") {
+    // Ocultamos el mensaje Queue Overflow
     $queueElements[0].innerHTML = "";
     $queueElements[0].className = "";
+    //Vaciamos el primer elemento añadido
     i = queue.MAX_SIZE - queue.queueControl.length;
-
     $queueElements[i].innerHTML = "";
     $queueElements[i].className = "";
-    console.log($elQueueTake, element);
+    //Mostramos el elemento eliminado
     $elQueueTake.innerHTML = element;
   } else {
+    // "Queue Underflow"
     $queueElements[queue.MAX_SIZE + 1].innerHTML = "Queue Underflow";
     $queueElements[queue.MAX_SIZE + 1].className = "underflow";
-    // "Queue Underflow"
   }
 };
 $queueAdd.onclick = function() {
+  var i = 0;
   $queueInput.focus()
   if($queueInput.value==""){
       alert("no puede añadir un elemento vacio")
   
     return
   }
-  var i = 0;
   if (queue.enqueue($queueInput.value) != "Queue Overflow") {
-    i = queue.MAX_SIZE;
+    //Dejamos de ver el ultimo elemento eliminado
     $elQueueTake.innerHTML = "";
+    // Quitamos el mensaje Queue Underflow
+    $queueElements[queue.MAX_SIZE + 1].innerHTML = "";
+    $queueElements[queue.MAX_SIZE + 1].className = "";
+    // Añadimos el valor del nuevo elemento en el segundo hueco
+    // El primero lo guardamos para el mensaje stack overflow
+    i = queue.MAX_SIZE;
     $element=$queueElements[1];
     $element.innerHTML = $queueInput.value;
     $element.className = "lleno";
+    // movemos al elemento añadido al final
     $element.parentNode.appendChild($element);
+    // Añadimos al penultimo elemento al final
+    // Es el mensaje oculto Queue
     $element.parentNode.appendChild($element.previousSibling);
-    $queueElements[queue.MAX_SIZE + 1].innerHTML = "";
-    $queueElements[queue.MAX_SIZE + 1].className = "";
     $queueInput.value = "";
   } else {
     $queueElements[0].innerHTML = "Queue Overflow";
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,52 +1,63 @@
-size=prompt("the default length is 8.\n if you want to change it, write the new length")
-var stack=new StackDataStructure(size)
+size = prompt("the default length is 8.\n if you want to change it, write the new length")
+console.log(parseInt(size))
+size = (parseInt(size)!=NaN)?parseInt(size):8;
+console.log(size)
+var stack = new StackDataStructure(size)
 var $stack = document.getElementById('stack')
-var $stackElements;
-var $stackAdd= document.getElementById("stack-add")
-var $stackTake= document.getElementById("stack-take")
+var $stackAdd = document.getElementById("stack-add")
+var $stackTake = document.getElementById("stack-take")
 var $stackInput = document.getElementById("stack-input")
 var $elStackTake = document.getElementById("el-stack-take")
-for(var i=-1;i<=stack.MAX_SIZE;i++){
+var $stackElements;
+
+for (var i = -1; i <= stack.MAX_SIZE; i++) {
   $stack.appendChild(document.createElement('li'))
 }
 
-$stackElements=$stack.getElementsByTagName('li')
-$stackTake.onclick=function(){
-  var i=0;
-  var element=stack.pop() 
-  if(element!="Stack Underflow"){
-    $stackElements[0].innerHTML=""
-    $stackElements[0].className=""
-    i=stack.MAX_SIZE-stack.stackControl.length
+$stackElements = $stack.getElementsByTagName('li')
 
-    $stackElements[i].innerHTML=""
-    $stackElements[i].className=""
-    console.log($elStackTake,element)
-    $elStackTake.innerHTML=element
-  }else{
-    $stackElements[stack.MAX_SIZE+1].innerHTML="Stack Underflow"
-    $stackElements[stack.MAX_SIZE+1].className="underflow"
+$stackTake.onclick = function () {
+  var i = 0;
+  var element = stack.pop()
+  if (element != "Stack Underflow") {
+    // ocultamos el mensaje Stack Overflow
+    $stackElements[0].innerHTML = ""
+    $stackElements[0].className = ""
+    //vaciamos el ultimo elemento añadido
+    i = stack.MAX_SIZE - stack.stackControl.length
+    $stackElements[i].innerHTML = ""
+    $stackElements[i].className = ""
+    //Mostramos el elemento eliminado
+    $elStackTake.innerHTML = element
+  } else {
     // "Stack Underflow"
+    $stackElements[stack.MAX_SIZE + 1].innerHTML = "Stack Underflow"
+    $stackElements[stack.MAX_SIZE + 1].className = "underflow"
   }
 }
-$stackAdd.onclick=function(){
+$stackAdd.onclick = function () {
+  var i = 0;
   $stackInput.focus()
-  if($stackInput.value==""){
-      alert("no puede añadir un elemento vacio")
-  
+  if ($stackInput.value == "") {
+    alert("no puede añadir un elemento vacio")
+
     return
   }
-  var i=0;
-  if(stack.push($stackInput.value)!="Stack Overflow"){
-    $elStackTake.innerHTML=""
-    $stackElements[stack.MAX_SIZE+1].innerHTML=""
-    $stackElements[stack.MAX_SIZE+1].className=""
-    i=stack.MAX_SIZE-stack.stackControl.length+1
-    $stackElements[i].innerHTML=$stackInput.value;
-    $stackElements[i].className="lleno"
-    $stackInput.value=""
-  }else{
-    $stackElements[0].innerHTML="Stack Overflow"
-    $stackElements[0].className="overflow"
+  if (stack.push($stackInput.value) != "Stack Overflow") {
+    // Dejamos de ver el ultimo elmento eliminado
+    $elStackTake.innerHTML = ""
+    // Quitamos el mensaje Stack underflow
+    $stackElements[stack.MAX_SIZE + 1].innerHTML = ""
+    $stackElements[stack.MAX_SIZE + 1].className = ""
+    // Añadimos el valor del nuevo elemento
+    i = stack.MAX_SIZE - stack.stackControl.length + 1
+    $stackElements[i].innerHTML = $stackInput.value;
+    $stackElements[i].className = "lleno"
+    // Vaciamos el input
+    $stackInput.value = ""
+  } else {
+    // Stack Overflow
+    $stackElements[0].innerHTML = "Stack Overflow"
+    $stackElements[0].className = "overflow"
   }
 }
