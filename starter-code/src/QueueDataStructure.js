893cac3841b8aa939a28d34f71ce9bf1f87b8835
cyril.attie <attiecyril@gmail.com>
2018-08-30 10:08:57 +0300

    MAD DE Cyril Attie

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,26 @@
 function QueueDataStructure () {
-}
+    this.queueControl = [];
+    this.MAX_SIZE= 55;
+    this.enqueue = function(item) {
+      if (this.canEnqueue()==false) {
+        return "Queue Overflow"};
+      this.queueControl.unshift(item);
+      return this.queueControl;
+    }.bind(this);
+    this.dequeue = function(){
+      if (this.isEmpty()==true){
+      return "Queue Underflow"};
+      return this.queueControl.pop();
+    }.bind(this);
+    
+    this.canEnqueue = function() {
+      if (this.queueControl.length==this.MAX_SIZE){
+        return false;
+      } else {return true};
+    }.bind(this);
+    this.isEmpty = function() {
+      return this.queueControl.length == 0;
+    }
+  
+  }
+
