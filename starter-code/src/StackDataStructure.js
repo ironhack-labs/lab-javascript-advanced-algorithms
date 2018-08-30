a9d5e870e87fdc0fa48ba7785eb5436435a710d5
Elvair <elvair@mountain>
2018-08-30 09:11:54 +0200

    Incompleted

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,18 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 0;
+    
 }
+StackDataStructure.prototype.isEmpty = function (){
+    
+    return true;
+    
+}
+StackDataStructure.prototype.canPush = function () {
+    return true;
+}    
+StackDataStructure.prototype.push = function (){
+    this.stackControl.push ();
+}
+StackDataStructure.prototype.pop = function () {
+}    
