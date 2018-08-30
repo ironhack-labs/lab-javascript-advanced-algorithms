754a84518e6800d1652dd6d528c963b665b6ded0
Esperanza Amaya <eamayag@gmail.com>
2018-08-30 08:48:36 +0200

    pending jQuery and DOM

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+}
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length == 0) {
+    return true 
+  } else
+  return false
+}
+
+QueueDataStructure.prototype.canEnqueue = function() {
+  if (this.queueControl.length < this.MAX_SIZE) {
+    return true
+  } else {
+    return false
+}
+}
+
+QueueDataStructure.prototype.enqueue = function(element) {
+  if (this.canEnqueue() == false) {
+    return "Queue Overflow";
+  } else {
+  this.queueControl.unshift(element);
+  this.isEmpty;
+  return this.queueControl;
+}
+}
+
+QueueDataStructure.prototype.dequeue = function() {
+  var first = this.queueControl.pop(); 
+  if (this.isEmpty() == true) {
+    return "Queue Underflow"
+  } else {
+    return first;
+  }
 }
