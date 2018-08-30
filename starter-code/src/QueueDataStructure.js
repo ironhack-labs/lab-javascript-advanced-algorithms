838299d83b350fb820523b3a0b6639a09fd3cffc
Celia Arnelas <37745928+Carnelas@users.noreply.github.com>
2018-08-30 09:05:17 +0200

    Add files via upload

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
 function QueueDataStructure () {
+  this.queueControl = [],
+  this.MAX_SIZE = 8
 }
+
+QueueDataStructure.prototype.isEmpty = function (){
+  if (this.queueControl.length == 0){
+    return true;
+  } else {return false};
+};
+
+
+
+QueueDataStructure.prototype.canEnqueue = function () {
+  return (this.queueControl.length < this.MAX_SIZE)
+}
+
+QueueDataStructure.prototype.enqueue = function (a) {
+  if (this.canEnqueue()){
+    this.queueControl.unshift(a);
+    return this.queueControl;
+  } else {
+    return 'Queue Overflow';
+  }
+  }
+
+  QueueDataStructure.prototype.dequeue = function (popedElement) {
+    if (this.isEmpty()){
+      return 'Queue Underflow';
+    } else {
+      return this.queueControl.pop();
+    }
+    }
