f90ad71a0a472722d87030df2eae6301732c48c2
Francis Wardley <fwardley90@gmail.com>
2018-08-30 09:07:43 +0200

    initial commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,41 @@
 function StackDataStructure () {
+  
+ this.stackControl = [];
+ console.log(this.stackControl);
+ 
+ this.MAX_SIZE = 10;
+
+ this.isEmpty = function () {
+   if (this.stackControl.length == 0) {
+     return true;
+   } else {
+     return false;
+   }
+ };
+
+ this.canPush = function () {
+   if (this.stackControl.length >= this.MAX_SIZE) {
+     return false;
+   } else {
+     return true;
+   }
+ };
+
+ this.push = function (e) {
+   if (this.canPush()) {
+     this.stackControl.push(e);
+     return this.stackControl;
+   } else {
+     return "Stack Overflow";
+   }
+ }
+
+ this.pop = function () {
+  if (this.isEmpty() !== true) {
+  return this.stackControl.pop();
+ } else {
+  return "Stack Underflow";
+ }
+}
+
 }
