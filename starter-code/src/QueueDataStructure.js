4d4e2ba027871da913d298afff5e2bb3a44a79e5
Eliot Cano <eliot7cano@gmail.com>
2018-08-29 20:50:56 -0500

    DE Mex- Eliot Cano #Francisco's team la verdad se me complico mucho, solo llegue hasta que agregara al stack, francisco estuvo ayudandonos.

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor(){
+    this.queueControl =[]
+    this.MAX_SIZE = 8
+  }
+
+  isEmpty(){
+    if(this.queueControl.length>0){
+      return false
+    }
+    return true
+  }
+
+  canEnqueue(){
+    if(this.queueControl.length<this.MAX_SIZE){
+      return true
+    }
+    return false
+  }
+
+  enqueue(queAñadir){
+    if(this.canEnqueue()){
+      this.queueControl.unshift(queAñadir)
+    }else{
+      return 'Queue Overflow'
+    }
+    return this.queueControl
+  }
+
+  dequeue(){
+    if(this.isEmpty()){
+      return "Queue Underflow"
+    }
+    this.queueControl.splice(0,1)
+    return this.queueControl[this.queueControl.length-1]
+  }
+
 }
