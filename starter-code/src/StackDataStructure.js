65a93d90b9d2f8bb5a574688cef121297a088f68
Ferran <ferranlao@hotmail.com>
2018-10-01 21:13:21 +0200

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 5;
+}
+StackDataStructure.prototype.isEmpty = function(){
+    if (this.stackControl.length === 0){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+StackDataStructure.prototype.canPush = function(){
+    if (this.stackControl.length < this.MAX_SIZE){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+StackDataStructure.prototype.push = function(input){
+    if (this.canPush()){
+        this.stackControl.push(input);
+        return this.stackControl;
+    }else{
+    return "Stack Overflow";
+    }
+}
+
+StackDataStructure.prototype.pop = function(){
+    if(this.isEmpty()){
+        return 'Stack Underflow';
+    }
+    return this.stackControl.pop()
+    
+
 }
