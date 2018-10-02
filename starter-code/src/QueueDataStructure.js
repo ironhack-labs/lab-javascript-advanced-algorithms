2cf37607bbb130e5d655b20eb8814d685f94b532
Isabel Gavidia <isabeltgavidia@gmail.com>
2018-10-02 17:29:13 +0200

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,14 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 9;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+  if (this.queueControl.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
