773d1d67b0ff1aa0c6379c91095f72c1ae532211
alfredohina <41444583+alfredohina@users.noreply.github.com>
2018-10-02 16:11:45 +0200

    Update StackDataStructure.js

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
 function StackDataStructure () {
-}
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+};
+
+StackDataStructure.prototype.isEmpty = function () {
+    return this.stackControl.length == 0;
+};
+
+StackDataStructure.prototype.canPush = function () {
+    return this.stackControl.length < this.MAX_SIZE;
+};
+
+StackDataStructure.prototype.push = function (e) {
+    if (this.canPush() == true) {
+        this.stackControl.push(e);
+        return this.stackControl;
+    } else if (this.canPush() == false) {
+        return "Stack Overflow";
+    }
+};
+
+StackDataStructure.prototype.pop = function () {
+    if (this.isEmpty()) {
+        return "Stack Underflow";
+    } else {
+        return this.stackControl.pop();
+    }
+};
