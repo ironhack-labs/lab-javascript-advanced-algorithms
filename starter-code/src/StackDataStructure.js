25861554dfd6ac2ace8023a3db14edca9a275f32
Elaine Henrique <silva.elainehenrique@gmail.com>
2018-10-25 09:56:20 -0300

    jquery incomplete

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+}
+
+StackDataStructure.prototype.isEmpty = function () {
+  if (this.stackControl.length === 0) {
+    return true
+  } 
+      return false
+}
+
+StackDataStructure.prototype.canPush = function (item) {
+  if (this.stackControl.length < this.MAX_SIZE) {
+    return true
+  }
+      return false
+}
+
+StackDataStructure.prototype.push = function (item) {
+  if (this.canPush()) {
+    this.stackControl.push(item);
+     return this.stackControl
+  }
+      return ("Stack Overflow")
+}
+
+StackDataStructure.prototype.pop = function (item) {
+  if (this.isEmpty()) {
+    return ("Stack Underflow")
+  } 
+      return this.stackControl.pop(item);
 }
