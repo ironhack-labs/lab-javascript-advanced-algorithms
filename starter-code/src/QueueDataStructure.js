0366ff4edb3dc841aa9b2a34fcca246b8b8e9f2b
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-10-25 09:51:00 -0300

    falta integração jquery~html

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+QueueDataStructure.prototype.isEmpty = function () {
+  if (this.queueControl.length === 0) {
+    return true;
+  }
+  return false;
+};
+
+QueueDataStructure.prototype.canEnqueue = function () {
+  if (this.queueControl.length === this.MAX_SIZE) {
+    return false;
+  }
+  return true;
+};
+
+QueueDataStructure.prototype.enqueue = function (add) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(add);
+
+    return this.queueControl;
+  }
+  return 'Queue Overflow';
+};
+
+QueueDataStructure.prototype.dequeue = function () {
+  if (this.isEmpty()) {
+    return 'Queue Underflow';
+  }
+  return this.queueControl.pop();
+};
+
+
+// col-sm-12
+// col-sm-4 ou 3
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
 }
+
+StackDataStructure.prototype.isEmpty = function () {
+  if (this.stackControl.length === 0) {
+    return true;
+  }
+  return false;
+};
+
+StackDataStructure.prototype.canPush = function () {
+  if (this.stackControl.length === this.MAX_SIZE) {
+    return false;
+  }
+  return true;
+};
+
+StackDataStructure.prototype.push = function (add) {
+  if (this.canPush()) {
+    this.stackControl.push(add);
+
+    return this.stackControl;
+  }
+  return 'Stack Overflow';
+};
+
+StackDataStructure.prototype.pop = function () {
+  if (this.isEmpty()) {
+    return 'Stack Underflow';
+  }
+  return this.stackControl.pop();
+};
