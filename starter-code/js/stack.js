0d6e189a49f37233c521fc134e6812f80c11eca6
Sarah Connor <sarahchamorro@gmail.com>
2018-10-25 06:21:15 -0300

    update input on keypress enter

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -3,21 +3,28 @@ $(document).ready(function() {
 
   let stackText;
 
- 
+  $("#inp-text").focus(function() {
+    stackText = $("#inp-text").prop("value");
+    $(document).keypress(function(e) {
+        stackText = $("#inp-text").prop("value");
+    });
+  });
 
   $("#stack-push").click(function() {
     $("#inp-text").blur(function() {
-        stackText = $("#inp-text").prop("value");
-      });
+      stackText = $("#inp-text").prop("value");
+    });
     if (!stackText) {
-      alert("Write a new stack element name at the input")
+      alert("Write a new stack element name at the input");
     } else {
       stack.push();
       console.log(stack.stackControl.length);
       controlIndex = 8 - stack.stackControl.length + 1;
       console.log(controlIndex);
       if (!stack.canPush()) {
-        $(".stackEl:nth-child(" + controlIndex + ")").addClass("stackEl-in-max");
+        $(".stackEl:nth-child(" + controlIndex + ")").addClass(
+          "stackEl-in-max"
+        );
         $(".stackEl:nth-child(" + controlIndex + ")").text(stack.push());
       } else {
         $(".stackEl:nth-child(" + controlIndex + ")").addClass("stackEl-in");
@@ -34,13 +41,14 @@ $(document).ready(function() {
     console.log(stack.stackControl.length);
     controlIndex = 8 - stack.stackControl.length;
     if (!stack.isEmpty()) {
-        $(".stackEl:nth-child(" + controlIndex + ")").removeClass("stackEl-in-max");
-        $(".stackEl:nth-child(" + controlIndex + ")").removeClass("stackEl-in");
-        $(".stackEl:nth-child(" + controlIndex + ")").text("");
+      $(".stackEl:nth-child(" + controlIndex + ")").removeClass(
+        "stackEl-in-max"
+      );
+      $(".stackEl:nth-child(" + controlIndex + ")").removeClass("stackEl-in");
+      $(".stackEl:nth-child(" + controlIndex + ")").text("");
     } else {
-        $(".stackEl:nth-child(" + controlIndex + ")").addClass("stackEl-in-max");
-        $(".stackEl:nth-child(" + controlIndex + ")").text(stack.pop());
-
+      $(".stackEl:nth-child(" + controlIndex + ")").addClass("stackEl-in-max");
+      $(".stackEl:nth-child(" + controlIndex + ")").text(stack.pop());
     }
   });
 });
