107e14392761ab631d8d80c84539b810ba1f969d
stormamnc <amancioandre@gmail.com>
2018-10-24 21:04:49 -0300

    after fix methods with old school functions

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -4,26 +4,26 @@ const addToQueue = () => {
   let put = document.getElementsByTagName('input')[0].value;
   let el = document.createElement('div');
   el.innerText = put;
-  el.setAttribute('class', 'queue');
-  console.log(el)
+  el.setAttribute('class', put);
   queue.addElement(el);
-  console.log(queue.stack)
-  updateQueue();
+  let queueHTML = document.getElementById('queue');
+
+  queue.stack.forEach(val => {
+    queueHTML.appendChild(val);
+  })
 }
 
 const takeFromQueue = () => {
   queue.removeElement();
-  updateQueue();
-}
-
-const updateQueue = () => {
   let queueHTML = document.getElementById('queue');
-
+  while(queueHTML.firstChild){
+    queueHTML.removeChild(firstChild)
+  }
   queue.stack.forEach(val => {
     queueHTML.appendChild(val)
-  })
+  });
 }
 
-let addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', addToQueue);
+
+let addBtn = document.getElementById('addBtn').addEventListener('click', addToQueue);
 let takeBtn = document.getElementById('takeBtn').addEventListener('click', takeFromQueue);
