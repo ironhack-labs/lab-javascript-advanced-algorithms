8be5e72e12ed2313f1e2f4aeac4b8c697135718d
Mohsan Abbas <moabbas.ch@gmail.com>
2018-10-24 21:14:24 -0300

    done with javascript and html/CSS

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
 }
+//Constructor's methods
+QueueDataStructure.prototype.enqueue = function (value) {
+    if (this.canEnqueue()) {
+        this.queueControl.push(value);
+        return this.queueControl.reverse();
+    }
+    return "Queue Overflow";
+};
+    //Check the space of Queue
+QueueDataStructure.prototype.canEnqueue = function () {
+    return (this.queueControl.length  < this.MAX_SIZE);
+};
+
+QueueDataStructure.prototype.dequeue = function () {
+    if (this.queueControl.length > 0) {
+        return this.queueControl.pop();
+    }
+    return 'Queue Underflow';
+}
+
+
+QueueDataStructure.prototype.isEmpty = function () {
+    return (this.queueControl.length === 0);
+};
+
+
+
+
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+function StackDataStructure() {
+	this.stackControl = [];
+	this.MAX_SIZE = 10;
+	var undo = [];
 }
+
+//Constructor's Methods
+
+
+StackDataStructure.prototype.push = function (arg) {
+	if (this.canPush()) {
+		this.stackControl.push(arg);
+		return this.stackControl;
+	}
+	return 'Stack Overflow';
+};
+StackDataStructure.prototype.canPush = function () {
+	return (this.stackControl.length + 1 <= this.MAX_SIZE);
+};
+
+StackDataStructure.prototype.pop = function () {
+	if (this.stackControl.length > 0)
+		return this.stackControl.pop();
+	return 'Stack Underflow'
+};
+StackDataStructure.prototype.isEmpty = function () {
+	return (this.stackControl.length === 0);
+};
