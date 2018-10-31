4f290c2bd949f2e8795c5fd8a79fb51c030f7910
Sonia Ruiz <yune.vk@gmail.com>
2018-10-31 19:57:22 +0100

    First iteration - Core

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 9;
+
+  this.isEmpty = function() {
+    return !this.queueControl.length;
+  }
+
+  this.canEnqueue = function() {
+    return this.queueControl.length < this.MAX_SIZE;
+  }
+
+  this.enqueue = function(item) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(item);
+      return this.queueControl;
+    } else {
+      return 'Queue Overflow'
+    }
+  }
+
+  this.dequeue = function() {
+    return this.isEmpty() ? 'Queue Underflow' : this.queueControl.pop();
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,25 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 9;
+
+  this.isEmpty = function() {
+    return !this.stackControl.length;
+  }
+
+  this.canPush = function() {
+    return this.stackControl.length < this.MAX_SIZE;
+  }
+
+  this.push = function(item) {
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow'
+    }
+  }
+
+  this.pop = function() {
+    return this.isEmpty() ? 'Stack Underflow' : this.stackControl.pop();
+  }
 }
