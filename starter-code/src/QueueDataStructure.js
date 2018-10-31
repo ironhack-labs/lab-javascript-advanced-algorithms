a0a926fe2ecb765e096819d124f57357db6dfa7a
Sandra Luengo <luchosandra@gmail.com>
2018-10-31 19:19:46 +0100

    Data Structure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,41 @@
-function QueueDataStructure () {
-}
+function QueueDataStructure() {
+
+    this.queueControl = [],
+    this.MAX_SIZE = 9;
+};
+
+QueueDataStructure.prototype.isEmpty = function () {
+
+    if (this.queueControl.length == 0) return true;
+    return false;
+
+};
+
+QueueDataStructure.prototype.canEnqueue = function () {
+
+    if ((this.queueControl.length < this.MAX_SIZE)) return true;
+
+    return false;
+
+
+};
+
+QueueDataStructure.prototype.enqueue = function (newElement) {
+    if (this.canEnqueue()) {
+        this.queueControl.unshift(newElement);
+        return this.queueControl;
+    } else {
+        return 'Queue Overflow';
+    }
+
+};
+
+QueueDataStructure.prototype.dequeue = function (newElement) {
+    if (!this.isEmpty()) {
+        this.queueControl.shift(newElement);
+        return this.queueControl.pop()
+    } else {
+        return 'Queue Underflow';
+    }
+};
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
-function StackDataStructure () {
-}
+function StackDataStructure() {
+    //LIFO 
+    //METODO PARA INSERTAR Y ELIMINAR ELEMENTOS DE LA PILA
+    //METODO PARA COMPROBAR SI PUEDO AÑADIR Y BORRAR ELEMENTOS
+    // CUIDADO CON AÑADIR ELEMENTOS A LA PILLA LLENA Y AL SACAR ELEMENTOS DE UNA PILA VACIA
+    this.stackControl = [],
+        this.MAX_SIZE = 9;
+};
+
+StackDataStructure.prototype.isEmpty = function () {
+
+    if (this.stackControl.length == 0) return true;
+    return false;
+
+};
+
+StackDataStructure.prototype.canPush = function () {
+
+    if ((this.stackControl.length < this.MAX_SIZE)) return true;
+
+    return false;
+
+
+};
+
+StackDataStructure.prototype.push = function (newElement) {
+    if (this.canPush()) {
+        this.stackControl.push(newElement);
+        return this.stackControl;
+    } else {
+        return 'Stack Overflow';
+    }
+
+};
+
+StackDataStructure.prototype.pop = function (newElement) {
+    if (!this.isEmpty()) {
+        this.stackControl.shift(newElement);
+        return this.stackControl.slice(-1)[0] 
+    } else {
+        return 'Stack Underflow';
+    }
+};
