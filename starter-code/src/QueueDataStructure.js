e739496be4c7c2119596c6cd73b140f3c8b598a3
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-10-31 21:08:31 +0100

    iteration 1

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,31 @@
-function QueueDataStructure () {
+class QueueDataStructure{
+  constructor(){
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+
+
+  }
+  isEmpty()  {
+    return this.queueControl.length > 0 ?  false : true;
+    
+  }
+  canEnqueue()  {
+    return this.MAX_SIZE >= 8  ? true : false
+  }
+  enqueue(ele)  {
+    if (this.canEnqueue()){
+      this.queueControl.unshift(ele);
+      return this.queueControl;    
+    }else{
+      return 'Queue Overflow';
+    }
+  }
+  dequeue(ele)  {
+    if (this.isEmpty()){
+      return "Queue Underflow";
+    }else{
+      return this.queueControl.pop(ele)
+    }
+
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor(){
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+
+
+  }
+  
+  isEmpty()  {
+    return this.stackControl.length > 0 ?  false : true;
+    
+  }
+  canPush()  {
+    return this.MAX_SIZE >= 8  ? true : false
+  }
+  push(ele)  {
+    if (this.canPush()){
+      this.stackControl.push(ele);
+      return this.stackControl;    
+    }else{
+      return 'Stack Overflow';
+    }
+  }
+  pop(ele)  {
+    if (this.isEmpty()){
+      return "Stack Underflow";
+    }else{
+      return this.stackControl.pop(ele)
+    }
+
+  }
 }
+
+
+
+
