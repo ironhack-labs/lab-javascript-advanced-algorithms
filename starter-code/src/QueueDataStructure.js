890f27d0d4bec9dc9d58d6a9c482da80ffec1185
RuddyLotar <ruddylotar@gmail.com>
2018-10-31 18:53:21 +0100

    Iteration 1 and 2

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,47 @@
 function QueueDataStructure () {
+  
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+
+
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+  if (this.queueControl.length === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  if (this.queueControl.length === this.MAX_SIZE){    
+    return false;
+  }else{
+    return true;
+  }
+
+}
+QueueDataStructure.prototype.enqueue = function(newItem){
+
+  if (this.queueControl.length === this.MAX_SIZE){
+    return 'Queue Overflow';
+  }else{
+
+    this.queueControl.unshift(newItem);
+    return this.queueControl;
+  }
+     
+  
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+  
+  if(this.queueControl.length === 0){
+    return 'Queue Underflow';
+  }else{
+    return this.queueControl[this.queueControl.length - 1];
+
+  }
+  
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,51 @@
 function StackDataStructure () {
+
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+     
+
 }
+var stack = new StackDataStructure();
+
+StackDataStructure.prototype.isEmpty = function(){
+  if (this.stackControl.length === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+StackDataStructure.prototype.canPush = function(){
+  if (this.stackControl.length === this.MAX_SIZE){    
+    return false;
+  }else{
+    return true;
+  }
+
+}
+StackDataStructure.prototype.push = function(newItem){
+
+  if (this.stackControl.length === this.MAX_SIZE){
+    return 'Stack Overflow';
+  }else{
+
+    this.stackControl.push(newItem);
+    return this.stackControl;
+  }
+       
+}
+
+
+StackDataStructure.prototype.pop = function(){
+  
+  if(this.stackControl.length === 0){
+    return 'Stack Underflow';
+  }else{
+    
+    return this.stackControl[this.stackControl.length - 1];
+
+  }
+  
+}
+
+
