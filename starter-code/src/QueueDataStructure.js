b762c38a91f29c5fccc7fce47ac5298a4fea40e4
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-01 00:16:39 +0100

    formatted the code on some .js files

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -2,38 +2,38 @@ function QueueDataStructure() {
     this.MAX_SIZE = 4
     this.queueControl = []
     this.isEmpty = function () {
-        if (this.queueControl.length > 0) {return false};
+        if (this.queueControl.length > 0) { return false };
         return true;
     }
     this.canEnqueue = function () {
-        if (this.queueControl.length >= this.MAX_SIZE) {return false};
+        if (this.queueControl.length >= this.MAX_SIZE) { return false };
         return true;
-     }
+    }
     this.enqueue = function (value) {
-        if (this.queueControl.length >= this.MAX_SIZE) {return "Queue Overflow"};
+        if (this.queueControl.length >= this.MAX_SIZE) { return "Queue Overflow" };
         this.queueControl.unshift(value);
         return this.queueControl;
     }
     this.dequeue = function () {
-        if (this.queueControl.length === 0) {return "Queue Underflow"};
+        if (this.queueControl.length === 0) { return "Queue Underflow" };
         return this.queueControl.pop()
     }
 
-    this.printQueue = function(){
-        for (i=0; i<this.MAX_SIZE; i++){
-            if(this.queueControl[i]!==undefined){
-               $('.queue-element').eq(this.MAX_SIZE-1-i).text(this.queueControl[i]);
-               $('.queue-element').eq(this.MAX_SIZE-1-i).addClass('full');
-               if (this.canEnqueue()===false) {
-                $('.queue-element').eq(0).text("Queue Oveflow")
-                $('.queue-element').eq(0).addClass('overflow');
+    this.printQueue = function () {
+        for (i = 0; i < this.MAX_SIZE; i++) {
+            if (this.queueControl[i] !== undefined) {
+                $('.queue-element').eq(this.MAX_SIZE - 1 - i).text(this.queueControl[i]);
+                $('.queue-element').eq(this.MAX_SIZE - 1 - i).addClass('full');
+                if (this.canEnqueue() === false) {
+                    $('.queue-element').eq(0).text("Queue Oveflow")
+                    $('.queue-element').eq(0).addClass('overflow');
                 }
             } else {
-               $('.queue-element').eq(this.MAX_SIZE-1-i).text(``);
-               $('.queue-element').eq(this.MAX_SIZE-1-i).removeClass('full');
-               $('.queue-element').eq(this.MAX_SIZE-1-i).removeClass('overflow');
+                $('.queue-element').eq(this.MAX_SIZE - 1 - i).text(``);
+                $('.queue-element').eq(this.MAX_SIZE - 1 - i).removeClass('full');
+                $('.queue-element').eq(this.MAX_SIZE - 1 - i).removeClass('overflow');
             }
-            
+
         }
     }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -6,35 +6,33 @@ function StackDataStructure() {
         return false;
     }
     this.canPush = function () {
-        if (this.stackControl.length >= this.MAX_SIZE) {return false};
+        if (this.stackControl.length >= this.MAX_SIZE) { return false };
         return true;
     }
     this.push = function (value) {
-        if (this.stackControl.length >= this.MAX_SIZE) {return "Stack Overflow"};
+        if (this.stackControl.length >= this.MAX_SIZE) { return "Stack Overflow" };
         this.stackControl.push(value);
         return this.stackControl;
     }
     this.pop = function () {
-        if (this.stackControl.length === 0) {return "Stack Underflow"}; 
+        if (this.stackControl.length === 0) { return "Stack Underflow" };
         return this.stackControl.pop()
-     }
+    }
 
-     this.printStack = function(){
-         for (i=0; i<this.MAX_SIZE; i++){
-             if(this.stackControl[i]!==undefined){
-                $('.stack-element').eq(this.MAX_SIZE-1-i).text(this.stackControl[i]);
-                $('.stack-element').eq(this.MAX_SIZE-1-i).addClass('full');
-                if (this.canPush()===false) {
+    this.printStack = function () {
+        for (i = 0; i < this.MAX_SIZE; i++) {
+            if (this.stackControl[i] !== undefined) {
+                $('.stack-element').eq(this.MAX_SIZE - 1 - i).text(this.stackControl[i]);
+                $('.stack-element').eq(this.MAX_SIZE - 1 - i).addClass('full');
+                if (this.canPush() === false) {
                     $('.stack-element').eq(0).text("Stack Oveflow")
                     $('.stack-element').eq(0).addClass('overflow');
                 }
-             } else {
-                $('.stack-element').eq(this.MAX_SIZE-1-i).text(``);
-                $('.stack-element').eq(this.MAX_SIZE-1-i).removeClass('full');
-                $('.stack-element').eq(this.MAX_SIZE-1-i).removeClass('overflow');
-             }
-             
-         }
-     }
-    
+            } else {
+                $('.stack-element').eq(this.MAX_SIZE - 1 - i).text(``);
+                $('.stack-element').eq(this.MAX_SIZE - 1 - i).removeClass('full');
+                $('.stack-element').eq(this.MAX_SIZE - 1 - i).removeClass('overflow');
+            }
+        }
+    }
 }
