d9e4e58634d6ec03639bf1e8156dc1196db20c97
Sonia Bravo <rinbra0@hotmail.com>
2018-10-31 20:59:29 +0100

    datastructures + queudatastructures

--- starter-code/spec/DataStructuresSpec.js
+++ starter-code/spec/DataStructuresSpec.js
@@ -1,98 +1,98 @@
-describe("Stack", function() {
-  var stack;
+// describe("Stack", function() {
+//   var stack;
 
-  beforeEach (function() {
-    stack = new StackDataStructure();
-  });
+//   beforeEach (function() {
+//     stack = new StackDataStructure();
+//   });
 
-  describe ("Stack properties", function () {
-    it ("Should has an array to add the elements to the stack", function () {
-      expect(Array.isArray(stack.stackControl)).toBe(true);
-    });
+//   describe ("Stack properties", function () {
+//     it ("Should has an array to add the elements to the stack", function () {
+//       expect(Array.isArray(stack.stackControl)).toBe(true);
+//     });
 
-    it ("Should has a max number of items to avoid the stack overflow", function () {
-      expect(typeof(stack.MAX_SIZE)).toBe('number');
-    });
-  });
+//     it ("Should has a max number of items to avoid the stack overflow", function () {
+//       expect(typeof(stack.MAX_SIZE)).toBe('number');
+//     });
+//   });
 
-  describe ("Stack methods", function () {
-    it ("Should has a method to check if the current stack is empty", function () {
-      expect(typeof(stack.isEmpty)).toBe('function');
-    });
+//   describe ("Stack methods", function () {
+//     it ("Should has a method to check if the current stack is empty", function () {
+//       expect(typeof(stack.isEmpty)).toBe('function');
+//     });
 
-    it ("Should has a method to check if we can push elements into the stack", function () {
-      expect(typeof(stack.canPush)).toBe('function');
-    });
+//     it ("Should has a method to check if we can push elements into the stack", function () {
+//       expect(typeof(stack.canPush)).toBe('function');
+//     });
 
-    it ("Should has a method to push elements into the stack", function () {
-      expect(typeof(stack.push)).toBe('function');
-    });
+//     it ("Should has a method to push elements into the stack", function () {
+//       expect(typeof(stack.push)).toBe('function');
+//     });
 
-    it ("Should has a method to take elements from the stack", function () {
-      expect(typeof(stack.pop)).toBe('function');
-    });
-  });
+//     it ("Should has a method to take elements from the stack", function () {
+//       expect(typeof(stack.pop)).toBe('function');
+//     });
+//   });
 
-  describe ("Stack methods default values", function () {
-    it ("Should has an empty stack", function () {
-      expect(stack.isEmpty()).toBe(true);
-    });
+//   describe ("Stack methods default values", function () {
+//     it ("Should has an empty stack", function () {
+//       expect(stack.isEmpty()).toBe(true);
+//     });
 
-    it ("Should be able to add a new element in the stack", function () {
-      expect(stack.canPush()).toBe(true);
-    });
-  });
+//     it ("Should be able to add a new element in the stack", function () {
+//       expect(stack.canPush()).toBe(true);
+//     });
+//   });
 
-  describe ("Stack methods usage", function () {
-    it ("Should add the indicated element to the stack", function () {
-      stack.push(19);
+//   describe ("Stack methods usage", function () {
+//     it ("Should add the indicated element to the stack", function () {
+//       stack.push(19);
 
-      expect(stack.stackControl.length).toBe(1);
-    });
+//       expect(stack.stackControl.length).toBe(1);
+//     });
 
-    it ("Should return that stack is not empty when adding an element", function () {
-      stack.push(19);
+//     it ("Should return that stack is not empty when adding an element", function () {
+//       stack.push(19);
 
-      expect(stack.isEmpty()).toBe(false);
-    });
+//       expect(stack.isEmpty()).toBe(false);
+//     });
 
-    it ("Should return the stack when an element is inserted", function () {
-      expect(stack.push(19)).toEqual([19]);
-    });
+//     it ("Should return the stack when an element is inserted", function () {
+//       expect(stack.push(19)).toEqual([19]);
+//     });
 
-    it ("Should insert the elements in the received order", function () {
-      stack.push(19);
-      stack.push(88);
+//     it ("Should insert the elements in the received order", function () {
+//       stack.push(19);
+//       stack.push(88);
 
-      expect(stack.stackControl).toEqual([19, 88]);
-    });
+//       expect(stack.stackControl).toEqual([19, 88]);
+//     });
 
-    it ("Should return 'Stack Overflow' if the stack is full", function () {
-      stack.MAX_SIZE = 1;
-      stack.push(19);
+//     it ("Should return 'Stack Overflow' if the stack is full", function () {
+//       stack.MAX_SIZE = 1;
+//       stack.push(19);
 
-      expect(stack.push(88)).toEqual("Stack Overflow");
-    });
+//       expect(stack.push(88)).toEqual("Stack Overflow");
+//     });
 
-    it ("Should return false if the stack is full", function () {
-      stack.MAX_SIZE = 1;
-      stack.push(19);
+//     it ("Should return false if the stack is full", function () {
+//       stack.MAX_SIZE = 1;
+//       stack.push(19);
 
-      expect(stack.canPush()).toBe(false);
-    });
+//       expect(stack.canPush()).toBe(false);
+//     });
 
-    it ("Should return the last element inserted in the stack", function () {
-      stack.push(19);
-      stack.push(88);
+//     it ("Should return the last element inserted in the stack", function () {
+//       stack.push(19);
+//       stack.push(88);
 
-      expect(stack.pop()).toBe(88);
-    });
+//       expect(stack.pop()).toBe(88);
+//     });
 
-    it ("Should return 'Stack Underflow' if there are no elements in the stack", function () {
-      expect(stack.pop()).toBe('Stack Underflow');
-    });
-  });
-});
+//     it ("Should return 'Stack Underflow' if there are no elements in the stack", function () {
+//       expect(stack.pop()).toBe('Stack Underflow');
+//     });
+//   });
+// });
 
 describe("Queues", function() {
   var queue;
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
 function StackDataStructure () {
+  this.stackControl =[]
+  this.MAX_SIZE = 8
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length === 0)return true;
+  else return false
+}
+
+StackDataStructure.prototype.canPush = function() {
+if(this.stackControl.length < this.MAX_SIZE) return true;
+else return false
+
+}
+
+StackDataStructure.prototype.push = function(arr) {
+  if(this.canPush()){
+    this.stackControl.push(arr)
+    return this.stackControl
+  } 
+  else return 'Stack Overflow'
+  } 
+  
+  
+StackDataStructure.prototype.pop = function (arr) {
+  if(this.isEmpty()){
+    return 'Stack Underflow'
+  }
+  else return this.stackControl.pop(arr)
+  
 }
