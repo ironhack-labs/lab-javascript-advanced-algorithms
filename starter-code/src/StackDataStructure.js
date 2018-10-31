16eed9b9ccc76a45d033d7651bafd2c594e41b6a
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-10-31 20:42:12 +0100

    StackDataStructure.js

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,38 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+}
+
+StackDataStructure.prototype.isEmpty = function (){
+    if (this.stackControl.length === 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+StackDataStructure.prototype.canPush = function(){
+    if (this.stackControl.length < this.MAX_SIZE){
+        return true
+    } else {
+        return false
+    }
+}
+
+StackDataStructure.prototype.push = function(archive){
+    if (this.canPush()){
+        this.stackControl.push(archive)
+        return this.stackControl
+    } else {
+        return "Stack Overflow"
+    }
+}
+
+StackDataStructure.prototype.pop = function(){
+    if (this.isEmpty()){
+        this.stackControl.pop()
+        return "Stack Underflow"
+    } else {
+        return this.stackControl.pop()
+    }
 }
