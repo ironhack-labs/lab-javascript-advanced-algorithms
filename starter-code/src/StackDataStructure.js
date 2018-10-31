811fbaf6143d2e6d0340becda103293b504270df
jrecio <recio.juan@gmail.com>
2018-10-31 16:10:20 +0100

    Iteration 1

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
 function StackDataStructure () {
+  this.MAX_SIZE= 50;
+  this.stackControl=[];
+  
 }
+
+StackDataStructure.prototype.isEmpty= function(){
+  return this.stackControl.length===0;
+}
+
+StackDataStructure.prototype.canPush = function(){
+  return this.stackControl.length!==this.MAX_SIZE;
+}
+
+StackDataStructure.prototype.push= function(elt){
+  if (this.canPush()){
+    this.stackControl.push(elt);
+    return this.stackControl;
+  }
+  else{
+    return "Stack Overflow";
+}
+}
+
+StackDataStructure.prototype.pop = function(){
+  return (this.isEmpty()?`Stack Underflow`:this.stackControl.pop());
+}
+
