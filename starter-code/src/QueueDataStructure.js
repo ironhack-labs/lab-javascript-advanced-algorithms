0bd1ebe97c2a8e1b03bfa647133ab330fb78e608
jrecio <recio.juan@gmail.com>
2018-10-31 16:35:30 +0100

    Iteration 2

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
 function QueueDataStructure () {
+  this.MAX_SIZE= 50;
+  this.queueControl=[];
+  
+}
+
+QueueDataStructure.prototype.isEmpty= function(){
+  return this.queueControl.length===0;
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  return this.queueControl.length!==this.MAX_SIZE;
+}
+
+QueueDataStructure.prototype.enqueue= function(elt){
+  if (this.canEnqueue()){
+    this.queueControl.unshift(elt);
+    return this.queueControl;
+  }
+  else{
+    return "Queue Overflow";
+}
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+  return (this.isEmpty()?`Queue Underflow`:this.queueControl.pop());
 }
