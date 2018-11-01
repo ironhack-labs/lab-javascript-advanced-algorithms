1e3f21dde5f7fd649ba680adf6b84bbf32cecc2c
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-01 09:25:06 +0100

    JavaScript, CSS & HTML done.

---
+++ starter-code/js/stack.js
@@ -0,0 +1,23 @@
+// var button1 = document.querySelector(".button1")
+// var button2 = document.querySelector(".button2")
+// var input = document.querySelector(".input")
+// var box= document.querySelector(".stack1")
+
+// button1.onclick = function(){
+// box.innerText = document.querySelector("input").value
+// }
+// button1.onClick = function(){
+// stackDataStructure.isEmpty();
+// }
+
+stack = new StackDataStructure();
+varr arr=
+$(document).ready(function(){
+    $(".button1").click(function(){
+        // stack.push($("#stack-push").val());
+        $(".input").val(".stack-test").text
+        
+        console.log($(".stack-test").text(".stackInput"))
+    })
+})
+
