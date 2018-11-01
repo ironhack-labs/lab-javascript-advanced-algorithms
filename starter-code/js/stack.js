ab81531e32c7a64cf892766fda77a9b6273d256f
MiguelColo <mig_colo@yahoo.es>
2018-11-01 09:01:20 +0100

    Version1

---
+++ starter-code/js/stack.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+var addButton = document.getElementsByClassName("btn-primary");
+addButton.onclick = function(){ StackDataStructure.prototype.push(document.getElementsByClassName("form-control").innerHTML);
+}
+
+var itemAdded = document.createTextNode(addButton.onclick);
+
+  
+
+
+
+var itemAdded = itemNode.getElementsByTagName("input").value;
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+
+function StackDataStructure (numItem) {
+this.stackControl =[]
+this.MAX_SIZE=10
+}
+
+StackDataStructure.prototype.isEmpty=function(){
+    if (this.stackControl.length===0){
+        return true
+    }else{
+        return false
+    }
+}
+StackDataStructure.prototype.canPush=function(){
+    if (this.stackControl.length<this.MAX_SIZE){
+        return true
+    }else{
+        return false
+    }
+}
+
+StackDataStructure.prototype.push=function(ele) {
+    if(this.stackControl.length===this.MAX_SIZE){
+        return "Stack Overflow"
+    }else{
+    this.stackControl.push(ele);
+    return this.stackControl
+    }
+    
+}
+
+StackDataStructure.prototype.pop=function(){
+    if (this.stackControl.length===0){
+        return "Stack Underflow"}
+        else{
+    return this.stackControl.pop();
+    }
+    
 }
