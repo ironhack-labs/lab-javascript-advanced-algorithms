0be4236ac89143847dbf861057d323b78e98b351
sitobusnan <sito@citytonight.es>
2018-11-01 09:12:44 +0100

    falta pulir la salida visual de las colas

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -7,8 +7,6 @@ $(function() {
 
   myQueue = new QueueDataStructure();
 
-// FALTA ADAPTAR LA LÓGICA VISUAL A LA ENTRADA Y SALIDA DE LA COLA
-// MOVER LOS ELEMENTOS DENTRO DE LOS DIV A MEDIDA QUE ENTRE O SALE UNO
 
   qadd.onclick = function() {
     var element = qitem.value;
@@ -27,13 +25,20 @@ $(function() {
     }
   };
 
+  // AUN NO FUNCIONA CORRECTAMENTE
+
   qremove.onclick = function() {
     if (myQueue.queueControl.length === 0) {
       qempty.classList.remove("hide");
     } else {
-      
-      document.getElementById(`q${myQueue.queueControl.length}`).textContent = "";
-      
+      newdiv = document.createElement('div');
+      currentdiv = document.getElementById('q8')
+      console.log(newdiv);
+      parent= document.getElementById('qcontent');
+      console.log(parent)
+      document.getElementById(`qcontent`).removeChild(document.getElementById('q1'));
+      document.body.insertBefore(newdiv,currentdiv);
+
       document.getElementById(`q${myQueue.queueControl.length}`).classList.remove("fullbox");
       myQueue.dequeue();
       if (myQueue.queueControl.length != myQueue.MAX_SIZE) {
