69b154b01f9640187737710d77691a7da3496c25
Sandra Luengo <luchosandra@gmail.com>
2018-11-01 09:21:56 +0100

    Parte de funcionamiento incompleta

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,28 +1,51 @@
 $(function () {
+    var numElemen = $('.elemt').length;
+    var take=0;
     myStack = new StackDataStructure();
     $('.btn-addStack').on('click', function () {
         add();
     });
-    $('.btn-addStack').on('click', function () {
-        take();
-    })
-});
+    $('.btn-takeStack').on('click', function () {
+        takeElem();
+    });
 
-function add(){
-    var element = myStack.push($('#stackIn').val());
+    function add() {
+
+        var element = myStack.push($('#stackIn').val());
+       
         if (typeof (element) === "object") {
             element = element[0];
         }
         if (element == "Stack Overflow") {
             $('.overflow').text(element);
-            $('.btn-addStack').off('click')
+            $('.btn-addStack').off('click');
         } else {
-            $('.elemt').each(function () {
-                if ($(this).text() === "") {
-                    $(this).text(element);
+            $(`.elemt:eq(${numElemen - 1})`).text(element);
 
-                    exit;
-                }
-            })
         }
-}
+        numElemen--;
+    };
+
+    function takeElem() {
+        var element = myStack.push($('#stackIn').val());
+        if (typeof (element) === "object") {
+            element = element[0];
+           
+        }
+        if (element == "Stack Underflow") {
+            $('.underflow').text(element);
+            $('.btn-addStack').off('click');
+        } else {
+            $('.overflow').text(" ");
+            $(`.elemt:eq(${take})`).text(" ");
+
+        }
+        take++;
+    }
+
+
+
+
+
+});
+
