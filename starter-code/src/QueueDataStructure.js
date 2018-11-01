6995220a5591ab5a8bbf3e2381b508eb2beb8103
Patricia LG <patri.lopg5@gmail.com>
2018-11-01 09:24:27 +0100

    1ª parte

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,40 @@
-function QueueDataStructure () {
-}
+  function QueueDataStructure() {
+    this.queueControl = []
+    this.MAX_SIZE = 10
+ }
+ 
+ QueueDataStructure.prototype.isEmpty = function () {
+    if (this.queueControl.length === 0) {
+        return true
+    } else {
+        return false
+    }
+ }
+ QueueDataStructure.prototype.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE) {
+        return true
+    } else {
+        return false
+    }
+ }
+ 
+ QueueDataStructure.prototype.enqueue = function (ele) {
+    if (this.queueControl.length === this.MAX_SIZE) {
+        return "Queue Overflow"
+    } else {
+        this.queueControl.unshift(ele);
+        return this.queueControl
+    }
+ }
+ QueueDataStructure.prototype.dequeue = function () {
+    if (this.queueControl.length === 0) {
+        return "Queue Underflow"
+    }
+    else {
+        return this.queueControl.pop();
+    }
+ 
+ }
+
+  
+
