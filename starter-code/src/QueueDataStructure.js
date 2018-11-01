f708e5e320c9a7448b7d70be81db4a897ee67f38
Bianca Castro <biiianca_00@hotmail.com>
2018-11-01 09:23:55 +0100

    1/2 done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,36 @@
 function QueueDataStructure () {
+    this.queueControl= [];
+    this.MAX_SIZE= 5;
+    this.isEmpty = function(){
+        if( this.queueControl.length===0){
+         return true
+        } else {
+            return false
+        }
+ 
+     };
+     this.canEnqueue=function(){
+      return this.queueControl.length!==this.MAX_SIZE
+     };
+     this.enqueue=function(valor){
+        if (this.canEnqueue()){
+            this.queueControl.unshift(valor);
+            return this.queueControl }
+            else {
+                return 'Queue Overflow'
+            }
+            
+
+     };
+     this.dequeue=function(){
+        if(this.isEmpty()===true){
+            return 'Queue Underflow'
+        }else {
+            return this.queueControl.pop();
+        }
+
+     }
+
+
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
+
 function StackDataStructure () {
+    
+   this.stackControl= [];
+    this.MAX_SIZE= 5;
+    this.isEmpty= function (){
+       if( this.stackControl.length===0){
+        return true
+       } else {
+           return false
+       }
+
+    };
+    this.canPush =function(){
+        return this.stackControl.length!==this.MAX_SIZE
+    };
+    this.push=function(valor){
+        if (this.canPush()){
+        this.stackControl.push(valor);
+        return this.stackControl }
+        else {
+            return    'Stack Overflow'
+        }
+        
+      
+        
+    };
+    this.pop=function(){
+        if(this.isEmpty()===true){
+           
+            return 'Stack Underflow' 
+
+        } else {
+            return this.stackControl.pop();
+        }
+
+    }
+   
 }
+
