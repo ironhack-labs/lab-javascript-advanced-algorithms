46d0926e5d5b01e9cbbb3d083f504b810e310afe
Janine Weber <janineweber@Janines-MacBook-Pro.local>
2018-11-08 21:41:51 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,34 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor (){
+        this.queueControl= [];
+        this.MAX_SIZE= 10;
+    }
+    isEmpty (){
+        if (this.queueControl.length === 0){
+            return true;
+        } else {
+            return false;
+        }
+     };
+     canEnqueue (){
+        if (this.queueControl.length < this.MAX_SIZE) {
+            return true;
+        } else {  
+            return false;
+        }
+    };
+    enqueue (element){
+        if (this.canEnqueue()){
+            this.queueControl.unshift(element) ; 
+            return this.queueControl;
+         } else {
+             return "Queue Overflow";
+         }
+    }
+    dequeue (){
+        if (this.queueControl.length > 0) {
+         return this.queueControl[this.queueControl.length-1];
+        } else {return "Queue Underflow"
+        } 
+     };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
-function StackDataStructure () {
-}
+class StackDataStructure {
+constructor ()
+ {
+     this.stackControl = [];
+     this.MAX_SIZE = 10; 
+    }
+     isEmpty (){
+        if (this.stackControl.length === 0){
+            return true;
+        } else {
+            return false;
+        }
+     };
+     canPush (){
+         if (this.stackControl.length < this.MAX_SIZE) {
+             return true;
+         } else {  
+             return false;
+         }
+     };
+     push (element){
+         if (this.canPush()){
+            this.stackControl.push(element) ; 
+            return this.stackControl;
+         } else {
+             return "Stack Overflow";
+         }
+     }; 
+     pop (){
+        if (this.stackControl.length > 0) {
+         return this.stackControl[this.stackControl.length-1]; 
+        } else {return "Stack Underflow"
+    } 
+     };
+    }
+    
