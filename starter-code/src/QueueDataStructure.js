36da460192ffdd62fb5aaa35a352a62d51747997
nirtzur <nirtzur89@gmail.com>
2018-11-08 21:28:41 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,31 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor(){
+        this.queueControl=[]
+        this.MAX_SIZE = 5
+    }
+
+    isEmpty(){
+        return (this.queueControl.length===0)
+    }
+    canEnqueue(){
+        return (this.queueControl.length<this.MAX_SIZE)
+    }
+    enqueue(elem){
+        if (this.canEnqueue()){
+            this.queueControl.unshift(elem)
+            return this.queueControl
+        }
+        else{
+             return "Queue Overflow"
+        }
+        
+    }
+    dequeue(){
+         if (this.isEmpty()){
+             return "Queue Underflow"
+         }
+         else {
+             return this.queueControl.pop()
+         }
+    }
 }
