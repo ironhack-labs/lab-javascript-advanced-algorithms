7cbf9b93b74ca63f0ffc321dfd5f2f3cba1f0e3c
Sarah Dröse <sarahdroese@Sarah.local>
2018-11-08 21:42:28 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,44 @@
-function QueueDataStructure () {
-}
+class QueueDataStructure  {
+    constructor(){
+        this.queueControl=[];
+        this.MAX_SIZE=10;
+
+    }
+    isEmpty(){
+        if(this.queueControl.length===0){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    canEnqueue(){
+        if(this.queueControl.length< this.MAX_SIZE){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    enqueue(el){
+        if(this.canEnqueue()){
+            this.queueControl.unshift(el);
+        }
+        else{
+            return "Queue Overflow"
+        }
+        return this.queueControl;
+    }
+    dequeue(el){
+        if(this.isEmpty()===false){
+            return this.queueControl.pop(el);
+        }
+        else{
+            return "Queue Underflow"
+        }
+    }
+    }
+
+
+
+
