29284b2372b57c1941ed482238c716629f19d181
luicasc <luicasc@gmail.com>
2018-11-08 09:08:27 -0600

    MEX - DE Luis Cascajares JS Advanced Algorithms

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,22 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 0;
+  this.isEmpty = function(){
+    return (this.queueControl.length === 0)
+  }
+  this.canPush = function(){
+    return (this.isEmpty());
+  }
+  this.push = function(element){
+    
+    if (this.queueControl.length === this.MAX_SIZE && this.MAX_SIZE !=0) 
+    return 'Queue Overflow';
+    this.queueControl.push(element);
+    return this.queueControl;
+  }
+  this.pop = function(){
+    if (this.queueControl.length === 0) return "QUeue Underflow";
+    var index = this.queueControl.length-1
+    return this.queueControl.pop(this.queueControl[index]);
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,23 @@
-function StackDataStructure () {
-}
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 0;
+  this.isEmpty = function(){
+    return (this.stackControl.length === 0)
+  }
+  this.canPush = function(){
+    return (this.isEmpty());
+  }
+  this.push = function(element){
+    
+    if (this.stackControl.length === this.MAX_SIZE && this.MAX_SIZE !=0)
+    return 'Stack Overflow';
+    this.stackControl.push(element);
+    return this.stackControl;
+  }
+  this.pop = function(){
+    if (this.stackControl.length === 0) return "Stack Underflow";
+    var index = this.stackControl.length-1
+    return this.stackControl.pop(this.stackControl[index]);
+  }
+  }
+  
