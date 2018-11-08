91d600ff71c1303f1ec8fdae0948482b13592066
Migu8 <lopezcamiro.m8@gmail.com>
2018-11-08 00:28:39 -0600

    Ahí vamo

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+    this.queueControl=[]
+    this.MAX_SIZE=9
+
+    this.isEmpty = function(){
+        if(this.queueControl===0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    this.canEnqueue = function(){
+        if(this.queueControl.length<this.MAX_SIZE){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    this.enqueue = function(){
+        if(this.canEnqueue){
+            this.queueControl.unshift(elemento)
+            return this.queueControl
+        }else{
+            return "Queue overflow"
+        }
+    }
+
+    this.dequeue = function(){
+        if(this.isEmpty()===true){
+            return "Queue underflow"
+        }else{
+            return this.queueControl.pop()
+        }
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,61 @@
 function StackDataStructure () {
+    this.stackControl =[]
+    this.MAX_SIZE = 9
+
+    this.isEmpty = function(){
+        if(this.stackControl.length ===0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    this.canPush = function(){
+        if(this.stackControl.length<this.MAX_SIZE){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    this.push = function(){
+        if(this.canPush()){
+            this.stackControl.push(element)
+            return this.stackControl
+        }else{
+            return "Stack overflow"
+        }
+    }
+
+    this.pop = function(){
+        if(this.isEmpty()=== true){
+            return "Stack underflow"
+        }else{
+            return this.stackControl.pop()
+        }
+    }
 }
+/*
+var stack = [];
+
+var Stack = function(){
+    this.storage = [];
+    this.size = 0;
+}
+
+Stack.prototype.add = function(value){
+    this.storage[this.size]=value
+    this.size ++
+}
+
+Stack.prototype.remove = function(){
+    this.size && this.size --
+    var result = this.storage[this.size]
+    delete this.storage[this.size]
+    return result;
+}
+
+Stack.prototype.getSize = function(){
+    return this.size;
+}
+*/
