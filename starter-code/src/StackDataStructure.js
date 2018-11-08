e12f7a323da23aede65bf3afe8f31d1ab71a1493
annearbor <betting.anne@gmail.com>
2018-11-08 21:42:02 +0100

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,37 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+
+  isEmpty() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  push(el) {
+    if (this.canPush()) {
+      this.stackControl.push(el);
+    } else {
+      return "Stack Overflow";
+    }
+    return this.stackControl;
+  }
+
+  pop(el) {
+    if (this.isEmpty() === false) {
+      return this.stackControl.pop(el);
+    } else {
+      return "Stack Underflow";
+    }
+  }
 }
