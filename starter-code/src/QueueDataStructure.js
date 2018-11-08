dded5799846f45ba3ac176d91f706158f04d2263
Katharina Koenig <k.koenig1811@gmail.com>
2018-11-08 21:34:04 +0100

    Berlin-Katharina-quiteWell

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
-function QueueDataStructure () {
-}
+class QueueDataStructure {
+    constructor(){
+      this.queueControl = [];
+      this.MAX_SIZE = 10;
+    }
+    isEmpty() {
+      if(this.queueControl.length!==0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    canEnqueue() {
+      if(this.queueControl.length < this.MAX_SIZE){
+        return true;
+      } else {
+        return false;
+      }
+    }
+    // push(item)
+    enqueue(element){
+      if(this.queueControl.length < this.MAX_SIZE){
+        this.queueControl.push(element);
+        return this.queueControl;
+      } else {
+        return "Queue Overflow";
+      }
+    }
+   // pop()
+    dequeue(){
+      if (this.queueControl.length !== 0) {
+        return this.queueControl.shift();
+      } else {
+        return 'Queue Underflow';
+      }
+    }
+   }
+   
+   
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        if (this.stackControl.length !== 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+        canPush() {
+        if(this.stackControl.length < this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    push(element) {
+        if(this.stackControl.length < this.MAX_SIZE) {
+            this.stackControl.push(element);
+            return this.stackControl;
+        } else {
+            return 'Stack Overflow';
+        }
+    }
+
+    pop(){
+        if (this.stackControl.length !== 0) {
+          return this.stackControl.pop();
+        } else {
+          return 'Stack Underflow'
+        }
+      }
+
+   
+
+
+
+
+
+
 }
