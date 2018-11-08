f4f3f3353e30dd8ebc2458a7091d19c180faf916
Susanne Tünker <susanne.tuenker@hotmail.de>
2018-11-08 21:56:38 +0100

    almost done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,35 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+  constructor() {
+    this.MAX_SIZE = 10;
+    this.queueControl = []
+  }
+
+isEmpty() {
+  if(this.queueControl.length > 0) {
+    return false
+  } else {return true}
+
 }
+
+canEnqueue() {
+return true
+}
+
+enqueue(element) {
+  
+
+  if (this.queueControl.length === this.MAX_SIZE) {
+    return "Queue Overflow"
+  } else {this.queueControl.unshift(element);}
+
+  return this.queueControl;
+
+}
+
+dequeue() {
+
+}
+
+}
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+
+  isEmpty() {
+    if (this.stackControl.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  canPush(element) {
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return false;
+    } else {this.push(element)}
+    
+  }
+  push(element) {
+    
+  
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return "Stack Overflow" } else {
+        this.stackControl.push(element);
+      }
+   
+    return this.stackControl;
+
+  }
+
+  pop() {
+    if (this.stackControl.length === 0) {
+      return 'Stack Underflow'
+    }
+    return this.stackControl[this.stackControl.length-1]
+  }
 }
+
+
