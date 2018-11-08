c2a71d832a92c5826f86d760edc0e71546c26f45
MefitHp <mefitDev@gmail.com>
2018-11-08 11:47:58 -0600

    first iteration

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,22 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = []
+    this.MAX_SIZE = 100
+    this.isEmpty = () => {
+        if (this.queueControl.length > 0) return false
+        return true
+    }
+    this.canEnqueue = () => {
+        if (this.queueControl.length === this.MAX_SIZE) return false
+        return true
+    }
+    this.enqueue = (element) => {
+
+        if (!this.canEnqueue()) { return 'Queue Overflow' }
+        this.queueControl.unshift(element)
+        return this.queueControl
+    }
+    this.dequeue = () => {
+        if (this.queueControl.length < 1) return 'Queue Underflow'
+        return this.queueControl[this.queueControl.length - 1]
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = []
+    this.MAX_SIZE = 100
+    this.MIN_SIZE = 0
+    this.isEmpty = () => {
+        if (this.stackControl.length > 0) return false
+        return true
+    }
+    this.canPush = () => {
+        if (this.stackControl.length === this.MAX_SIZE) return false
+        return true
+    }
+    this.push = (element) => {
+        if (!this.canPush()) {
+            return 'Stack Overflow'
+        }
+        this.stackControl.push(element)
+        return this.stackControl
+    }
+    this.pop = () => {
+        var lastElement = this.stackControl.length - 1
+        if (this.stackControl.length === this.MIN_SIZE) {
+            return 'Stack Underflow'
+        }
+        return this.stackControl[lastElement]
+    }
 }
