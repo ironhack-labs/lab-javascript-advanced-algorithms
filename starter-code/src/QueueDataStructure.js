3fff029d09efdef27901c2d636a2435e8b195a67
sogchs <sogchs@gmail.com>
2018-11-18 19:59:50 +0100

    Jasmine, html, and css ready

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+    this.isEmpty = function() {
+        if (this.queueControl.length === 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    this.canEnqueue = function() {
+        return (this.queueControl.length >= this.MAX_SIZE) ? false : true;
+    }
+
+    this.enqueue = function (arg) {
+        if (this.canEnqueue()) {
+          this.queueControl.unshift(arg); //Mete y no retorna
+          return this.queueControl;
+        }
+        else  return "Queue Overflow";
+      
+    }
+
+    this.dequeue = function() {
+        return (this.queueControl.length === 0) ? "Queue Underflow": // Si está vacío
+        this.queueControl.pop(); //Saca y retorna
+      }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+    this.isEmpty = function() {
+        if (this.stackControl.length === 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    this.canPush = function() {
+        return (this.stackControl.length >= this.MAX_SIZE) ? false : true;
+    }
+
+    this.push = function (arg) {
+        if (this.canPush()) {
+          this.stackControl.push(arg); //Mete y no retorna
+          return this.stackControl;
+        }
+        else  return "Stack Overflow";
+      
+    }
+
+    this.pop = function() {
+        return (this.isEmpty()) ? "Stack Underflow" : // Si está vacío
+        this.stackControl.pop(); //Saca y retorna
+      }
 }
+
