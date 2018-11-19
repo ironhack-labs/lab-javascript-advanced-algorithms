3b7404255c731009531d51b1c8199e41f5b7bcbb
Cdelatorre <carlosdelatorrediaz@gmail.com>
2018-11-19 22:43:26 +0100

    StackDataStructure.js done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,38 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
+
+  this.isEmpty =  function(){
+    if(this.stackControl.length === 0){
+      return true
+    } else {
+    return false
+    }
+  }
+
+  this.canPush = function(){
+    if(this.stackControl.length === this.MAX_SIZE){
+     return false
+    } else {
+     return true
+    }
+  }
+
+  this.push =  function(unit){
+    if (this.canPush()) {
+      this.stackControl.push(unit);
+      return this.stackControl;
+    }
+      else { return "Stack Overflow";
+    }
+
+  }
+  this.pop = function(){
+    if(this.stackControl.length === 0){
+      return 'Stack Underflow';
+    } else {
+      return this.stackControl.pop()
+    }
+  }
+
 }
