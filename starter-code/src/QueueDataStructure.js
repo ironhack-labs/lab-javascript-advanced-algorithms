62e2808bc79b24af2c2195f975bd5eb30091f148
Daniel Goycochea <danielgoycocheardz@gmail.com>
2018-11-19 11:42:35 -0600

    parcial

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
 function QueueDataStructure () {
-}
+  this.queueControl =[];
+  this.MAX_SIZE = 19;
+
+  this.isEmpty= function(){
+    
+    if(this.queueControl.length ===0){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  this.canEnqueue = function(){
+    if (this.MAX_SIZE<=queueControl){
+      return true
+    }
+    return false
+  }
+  this.enqueue = function(){
+
+  }
+
+  this.dequeue= function (){
+
+  }
+
+  }
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
 function StackDataStructure () {
+
+  this.stackControl =[];
+  this.MAX_SIZE= 19;
+  
+  this.isEmpty= function(){
+
+    if(this.stackControl.length===0){
+      return  false
+    }
+    else{
+      return true
+    }
+  }
+  this.canPush= function(){
+    if(this.MAX_SIZE<=this.stackControl.length){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  this.push = function(){
+    if (this.canPush()){
+      this.stackControl.push();
+      return this.stackControl;
+    }
+    else{
+      return false
+    }
+  }
+ this.pop= function(){
+   
+
+
+ }
+
+
+
 }
