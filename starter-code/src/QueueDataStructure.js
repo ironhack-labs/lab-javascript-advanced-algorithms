ea2a0354d568023a242c0495e5cc7bca56deb36c
Cdelatorre <carlosdelatorrediaz@gmail.com>
2018-11-19 23:44:11 +0100

    css stack done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,41 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+
+  this.isEmpty = function(){
+    if(this.queueControl.length === 0){
+      return true
+    } else  {
+      return false;
+  }
+}
+
+  this.canEnqueue =  function(){
+    if(this.queueControl.length === this.MAX_SIZE){
+      return false
+     } else {
+      return true
+     }
+  }
+
+  this.enqueue = function(unit){
+ 
+      if (this.canEnqueue()) {
+        this.queueControl.unshift(unit)
+        return this.queueControl;
+      }
+        else { 
+          return "Queue Overflow";
+      }
+
+  }
+
+  this.dequeue = function(){
+    if(this.queueControl.length === 0){
+      return 'Queue Underflow';
+    } else {
+      return this.queueControl.pop()
+    }
+  }
+
 }
