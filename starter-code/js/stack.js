0b8ea549845bcb220b9964109d77c26c4eccc8a7
Vero Celemin <veronica.celemin@gmail.com>
2018-11-20 17:15:01 +0100

    Stack finished --- Queue in progress

---
+++ starter-code/js/stack.js
@@ -0,0 +1,79 @@
+var stack = new StackDataStructure();
+
+stack.stackControl = [];
+
+function findParent(element, classSelector) {
+  if (!element || element.classList.contains(classSelector)) {
+    return element;
+  } else {
+    return findParent(element.parentNode, classSelector);
+  }
+}
+
+
+var addButton = document.querySelector('.stack .add');
+
+addButton.onclick = function() {
+  var parent = findParent(this, 'stack');
+  var input = parent.querySelector('input');
+  var pushOnStack = stack.push(input.value);
+  var content = '';
+
+  if (pushOnStack === 'Stack Overflow'){
+    var overflow = parent.querySelector('.over');
+    overflow.style.display = 'block';
+    content = document.createTextNode(pushOnStack);
+    overflow.innerHTML = '';
+    overflow.appendChild(content);
+
+  } else {
+    var arrayElementToChange = parent.getElementsByClassName('empty');
+    var elementToChange = arrayElementToChange[(arrayElementToChange.length-1)];
+    elementToChange.classList.add('added');
+    elementToChange.classList.remove('empty');
+    content = document.createTextNode(stack.stackControl[(stack.stackControl.length-1)]);
+    elementToChange.appendChild(content);
+
+    if (stack.stackControl.length === 1) {
+      var underflow = parent.querySelector('.under');
+      underflow.style.display = 'none';
+    }
+  }
+  input.value = '';
+}
+
+
+var takeButton = document.querySelector('.stack .take');
+
+takeButton.onclick = function() {
+  var parent = findParent(this, 'stack');
+  var popFromStack = stack.pop();
+  var content = '';
+
+  if (popFromStack === 'Stack Underflow'){
+    var underflow = parent.querySelector('.under');
+    underflow.style.display = 'block';
+    content = document.createTextNode(popFromStack);
+    underflow.innerHTML = '';
+    underflow.appendChild(content);
+
+  } else {
+    var arrayElementToChange = parent.getElementsByClassName('added');
+    var elementToChange = arrayElementToChange[0];
+    elementToChange.classList.add('empty');
+    elementToChange.classList.remove('added');
+    elementToChange.innerHTML = '';
+
+    var poppedElement = parent.querySelector('.popped-element');
+    poppedElement.innerHTML = ''
+    content = document.createTextNode(popFromStack);
+    poppedElement.appendChild(content);
+
+    if (stack.stackControl.length < stack.MAX_SIZE) {
+      var overflow = parent.querySelector('.over');
+      overflow.style.display = 'none';
+    }
+  }
+}
+
+
