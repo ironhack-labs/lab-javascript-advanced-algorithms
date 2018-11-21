13790b777e4140dc33313c525ebacd20e5916f2a
jose-martinez-albarran <jose.jesus.martz@gmail.com>
2018-11-20 18:48:06 -0600

    Jasmine Done, Pending Visual Part by Jose Martinez

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,28 @@
 function QueueDataStructure () {
+  let nmxq = 10;
+  this.queueControl = []; 
+  this.MAX_SIZE = nmxq;
+  this.isEmpty = function(){
+    let valEmQ =  true;
+    return this.queueControl.length === 0 ? valEmQ : false;
+  }
+  this.canEnqueue = function(){
+    let valPuQ = true;
+    return this.queueControl.length < this.MAX_SIZE ? valPuQ : false;
+  }
+  this.enqueue = function(elem){
+    if(this.queueControl.length < this.MAX_SIZE){
+      this.queueControl.unshift(elem);
+      return this.queueControl;
+    } else {
+      return 'Queue Overflow';
+    }
+  }
+  this.dequeue = function(){
+    if(this.queueControl.length === 0){
+      return 'Queue Underflow';
+    } else {
+      return this.queueControl.reverse()[0];
+    }
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
 function StackDataStructure () {
+  let nmx = 10;
+  this.stackControl = [];
+  this.MAX_SIZE = nmx;
+  this.isEmpty = function(){
+    let valEm =  true;
+    return this.stackControl.length === 0 ? valEm : false;
+  }
+  this.canPush = function(){
+    let valPu = true;
+    return this.stackControl.length < this.MAX_SIZE ? valPu : false;
+  }
+  this.push = function(elem){
+    if(this.stackControl.length < this.MAX_SIZE){
+      this.stackControl.push(elem);
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow';
+    }
+  }
+  this.pop = function(){
+    if(this.stackControl.length === 0){
+      return 'Stack Underflow';
+    } else {
+      return this.stackControl[this.stackControl.length - 1];
+    }
+  }
 }
