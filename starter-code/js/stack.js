3043b15079bd6aad7cffdcf932edeae167037489
Moises Garcia <moisesgarcia83@gmail.com>
2018-11-24 10:21:38 +0100

    work in progress

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,2 +1,57 @@
-var stack;
-stack = new StackDataStructure();
+
+var stack = new StackDataStructure ();
+
+stack.stackControl = [];
+
+window.onload = function(){
+
+
+
+
+var addButton = document.querySelector('.btn-add');
+var takeButton = document.querySelector('.btn-take');
+var underFlow = document.querySelector(".underflow");
+var overFlow = document.querySelector(".overflow");
+var boxStack = document.querySelector(".box-stack");
+var boxOccupied = document.querySelector(".ocuppied");
+
+addButton.onclick = addStack;
+takeButton.onclick = takeStack;
+
+function addStack(){
+
+  var inputStack =  document.querySelector('input[id="stack"]').value;
+  var boxRemove = document.querySelector(".box");
+
+  if(stack.canPush() && inputStack !== ""){
+    stack.push(inputStack);
+    boxStack.insertBefore(createBox(inputStack), boxOccupied);
+    boxStack.removeChild(boxRemove);
+    underFlow.style.display = "none";
+  } 
+  else if(!stack.canPush()){
+    overFlow.style.display = '';
+  }
+}
+
+
+function createBox(inputStack){
+
+  var divTag = document.createElement("div");
+  divTag.classList.add("occupied");
+  divTag.appendChild(document.createTextNode(inputStack));
+  
+  return divTag;
+}
+
+function takeStack(){
+  if(!stack.isEmpty){
+    underFlow.style.display = "";
+  } else {
+    stack.stackControl.pop();
+  boxStack.removeChild(boxOccupied);
+  overFlow.style.display = 'none';}
+ 
+}
+
+};
--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -3,26 +3,26 @@ function QueueDataStructure () {
   this.queueControl = [];
   this.MAX_SIZE = 8;
 
-  this.isEmpty = function(){
+  QueueDataStructure.prototype.isEmpty = function(){
     if (this.queueControl.length === 0){
       return true;
     } else {return false;}
   }
 
-  this.canEnqueue = function(){
+  QueueDataStructure.prototype.canEnqueue = function(){
     if (this.queueControl.length < this.MAX_SIZE){
       return true;
     } else {return false;}
   }
 
-  this.enqueue = function(element){
+  QueueDataStructure.prototype.enqueue = function(element){
     if(this.canEnqueue()){
       this.queueControl.unshift(element);
       return this.queueControl;
     } else {return 'Queue Overflow'}
   }
 
-  this.dequeue = function(){
+  QueueDataStructure.prototype.dequeue = function(){
     if(!this.isEmpty()){
       return this.queueControl.pop()
     } else {return 'Queue Underflow'}
