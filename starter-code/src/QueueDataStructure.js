b2507eadcc139e350fc75da1831a45d48c8a45d8
Sofía <sofianombela@gmail.com>
2019-01-16 20:34:57 +0100

    QueueDataStructure done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,40 @@
 function QueueDataStructure () {
-}
+    this.queueControl = [];
+    this.MAX_SIZE = 0;
+
+    this.isEmpty = function (){
+        if (this.queueControl.length == 0){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.canEnqueue = function (){
+        if (this.MAX_SIZE > this.queueControl.length){
+            return false;
+        }
+
+        return true;
+
+    };
+
+    this.enqueue = function (num){
+        
+        if (this.MAX_SIZE > this.queueControl.length){
+            return 'Queue Overflow';
+        } else {
+            this.queueControl.unshift(num);
+            return this.queueControl;
+        }
+    };
+
+    this.dequeue = function (){
+
+        if (this.queueControl.length === 0){
+            return 'Queue Underflow';
+        }
+        return this.queueControl.pop();
+
+    };
+}   
