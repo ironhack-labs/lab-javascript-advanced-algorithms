3e1f9efd49e96145b3c8778cc769b2f15381ce41
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-16 23:36:25 +0100

    incomplete

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 0;
+
+  this.isEmpty = function () {
+    if (this.queueControl.length === 0) return true;
+    else return false;
+  }
+
+  this.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE) return true;
+    else return false;
+  }
+
+  this.enqueue = function (data) {
+    if (this.canEnqueue === false) return "Queue Overflow";
+    else {
+      this.queueControl.unshift(data);
+      return this.queueControl;
+    }
+  }
+
+  this.dequeue = function () {
+    if (this.isEmpty) return "Queue Underflow";
+    else return this.queueControl.pop();
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 0;
+
+  this.isEmpty = function () {
+    if (this.stackControl.length === 0) return true;
+    else return false;
+  }
+
+  this.canPush = function () {
+    if (this.stackControl.length < this.MAX_SIZE) return true;
+    else return false;
+  }
+
+  this.push = function (data) {
+    if (this.canPush() === true) {
+      this.stackControl.push(data);
+      return this.stackControl;
+    }
+    else return "Stack Overflow";
+  }
+
+  this.pop = function () {
+    if (this.isEmpty) return "Stack Underflow";
+    else return this.stackControl.pop();
+  }
+
 }
