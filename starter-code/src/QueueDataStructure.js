17b8852b62251a0ec8795dbe66a243c821dbd345
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-01-16 23:04:30 +0100

    almost done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
+queue = new QueueDataStructure();
+
 function QueueDataStructure () {
+  this.queueControl = []
+  this.MAX_SIZE = 8;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+  if (this.queueControl.length === 0){
+    return true
+  } else {
+    return false
+  }
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+  if (this.queueControl.length < this.MAX_SIZE){
+    return true
+  } else {
+    return false
+  }
+}
+
+QueueDataStructure.prototype.enqueue = function(canEnqueue){
+  if (this.queueControl.unshift(canEnqueue) > this.MAX_SIZE){
+    return ("Queue Overflow")
+  } else {
+    return this.queueControl;
+  }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+  if (this.isEmpty() === true){
+    return ('Queue Underflow')
+  } else {
+    return this.queueControl.pop();
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,37 @@
+stack = new StackDataStructure();
+
 function StackDataStructure () {
+  this.stackControl = []
+  this.MAX_SIZE = 8;
+}
+
+StackDataStructure.prototype.isEmpty = function(){
+  if (this.stackControl.length === 0){
+    return true
+  } else {
+    return false
+  }
+}
+
+StackDataStructure.prototype.canPush = function(){
+  if (this.stackControl.length < this.MAX_SIZE){
+    return true
+  } else {
+    return false
+  }
+}
+
+StackDataStructure.prototype.push = function(elem){
+  if (this.stackControl.push(elem) > this.MAX_SIZE){
+    return ("Stack Overflow")
+  } else {
+    return this.stackControl;
+  }
+}
+StackDataStructure.prototype.pop = function(){
+  if (this.isEmpty() === true){
+    return ('Stack Underflow')
+  } else {
+    return this.stackControl.pop();
+  }
 }
