0a97ae6fd07459411c0d7f2ef35a77a9da2620ff
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-01-16 22:02:09 +0100

    added comments

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -5,19 +5,25 @@ $(document).ready(function(){
     stack.MAX_SIZE = 8;
 
     $('.btn-add').click(function() {
+        //remove if there was stack underflow displayed
         $('.underflow').removeClass('item').text("")
+        //if there is no stack overflow, push item to stack and change the last empty div
         if(stack.canPush()) {
             stack.push($('input').val());
             $('.empty:last').removeClass('empty').addClass('full').text($('input').val());
             $('.last-element').text("");
         } else 
+        //if stack is full, display stack overflow
         $('.overflow').addClass('item').text("Stack Overflow");
     })
     $('.btn-take').click(function(){
+        //remove if there was stack overflow displayed
         $('.overflow').removeClass('item').text("")
+        //if the stack is empty, display stack underflow
         if(stack.isEmpty()){
             $('.underflow').addClass('item').text("Stack Underflow");
         } else {
+            //if there is at least one item, write it's value beside and remove it from stack
             stack.pop();
             $('.last-element').text($('.full:first').text());
             $('.full:first').text("");
