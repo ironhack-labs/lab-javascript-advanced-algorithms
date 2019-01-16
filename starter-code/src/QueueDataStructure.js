500285e4c88badaf8c441c6909deaed7bc4c7475
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-16 23:39:08 +0100

    just jasmin

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+
+    this.queueControl=[];
+    this.MAX_SIZE=10;
+
+}
+
+
+QueueDataStructure.prototype.isEmpty=function(){
+    if(this.queueControl.length===0){
+        return true
+    }else return false
+}
+
+QueueDataStructure.prototype.canEnqueue=function(){
+    if(this.queueControl.length<this.MAX_SIZE){
+        return true
+    }else return false
+}
+
+QueueDataStructure.prototype.enqueue=function(nextItem){
+        
+    
+    if(this.canEnqueue()){
+
+      this.queueControl.unshift(nextItem)
+     return this.queueControl
+     } else return 'Queue Overflow'
+    
+   
+}
+QueueDataStructure.prototype.dequeue=function(){
+    if(this.isEmpty()){
+        return 'Queue Underflow'
+    } else return this.queueControl.pop()
+    
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
-}
+function StackDataStructure (stackControl,MAX_SIZE) {
+    this.stackControl=[];
+    this.MAX_SIZE=10;
+    
+
+    }
+    StackDataStructure.prototype.isEmpty=function(){
+        if(this.stackControl.length===0){
+            return true
+        }else return false
+    }
+    StackDataStructure.prototype.canPush=function(){
+        if(this.stackControl.length<this.MAX_SIZE){
+            return true
+        }else return false
+    }
+    StackDataStructure.prototype.push=function(nextItem){
+        
+        if(this.canPush()){
+         this.stackControl.push(nextItem)
+         return this.stackControl
+         } else return 'Stack Overflow'
+        
+        
+    }
+        StackDataStructure.prototype.pop=function(){
+        if(this.isEmpty()){
+            return 'Stack Underflow'
+        } else return this.stackControl.pop()
+        
+    }
+    
+    
+    
