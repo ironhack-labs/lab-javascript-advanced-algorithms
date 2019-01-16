123a76d30d383654253744cc0b3d8f3795ccdc57
André Sebastian <a.sebastian@posteo.de>
2019-01-16 22:38:35 +0100

    pass all spec tests, missing visual

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor(){
+    this.queueControl = [];
+    this.MAX_SIZE = 20;
+  }
+
+  isEmpty(){
+    if (this.queueControl.length === 0) return true;
+    else return false;
+  }
+  
+  canEnqueue(){
+    if (this.queueControl.length === this.MAX_SIZE) return false;
+    else return true;
+  }
+
+  enqueue(x){
+    if (this.queueControl.length === this.MAX_SIZE) return "Queue Overflow";
+    else {
+      this.queueControl.unshift(x);
+      return this.queueControl;
+    }
+  }
+
+  dequeue(){
+    if (this.isEmpty()) return "Queue Underflow";
+    else return this.queueControl.pop();
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+      this.stackControl = [];
+      this.MAX_SIZE = 20;
+  }
+
+  isEmpty(){
+    if (this.stackControl.length != 0) return false
+    else return true;
+  }
+  canPush(){
+    if (this.stackControl.length === this.MAX_SIZE) return false;    
+    return true;
+  }
+  push(x){
+    if (!this.canPush()) return "Stack Overflow"
+    else {
+      this.stackControl.push(x);
+      return this.stackControl;
+    }
+
+  }
+  pop(){
+    if (this.isEmpty()) return "Stack Underflow";
+    else return this.stackControl.pop();
+  }
 }
+
