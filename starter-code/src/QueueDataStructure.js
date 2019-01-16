03c736f84942e6d0e2bb9c36fa7c7d041568f8bf
Bruno Dolce <brudolce@gmail.com>
2019-01-16 18:20:11 -0300

    tenho que ir pra academia, opções da vida

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,23 @@
 function QueueDataStructure () {
+    this.queueControl=[];
+    this.MAX_SIZE=7;
+}
+
+QueueDataStructure.prototype.isEmpty = function(){           return this.queueControl.length>0?false:true
+}    
+
+QueueDataStructure.prototype.canPush =function(){
+    return  this.isEmpty() || this.queueControl.length<this.MAX_SIZE}
+
+QueueDataStructure.prototype.push = function(item) {
+    if (this.canPush()){
+        this.queueControl.push(item);
+        return this.queueControl
+    }
+        return 'Stack Overflow'
+}   
+QueueDataStructure.prototype.pop =function(){
+    if (!this.isEmpty()){
+        this.queueControl.pop();
+    } else { 'Stack Underflow'}
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,24 @@
 function StackDataStructure () {
+    this.stackControl=[];
+    this.MAX_SIZE=7;
 }
+
+StackDataStructure.prototype.isEmpty = function(){           return this.stackControl.length>0?false:true
+}    
+
+StackDataStructure.prototype.canPush =function(){
+    return  this.isEmpty() || this.stackControl.length<this.MAX_SIZE}
+
+StackDataStructure.prototype.push = function(item) {
+    if (this.canPush()){
+        this.stackControl.push(item);
+        return this.stackControl
+    }
+        return 'Stack Overflow'
+}   
+StackDataStructure.prototype.pop =function(){
+    if (!this.isEmpty()){
+        this.stackControl.pop();
+    } else { 'Stack Underflow'}
+}
+
