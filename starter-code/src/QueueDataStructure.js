998e81832eece0d7973350cd83a7b89e0a2c21b9
mocuishla <misabellanes@gmail.com>
2019-01-16 20:36:54 +0100

    stack constructor done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
 }
+
+QueueDataStructure.prototype.isEmpty = function() {
+if (this.queueControl === 0){
+  return true
+}
+else {
+  return false
+}
+};
+
+QueueDataStructure.prototype.canEnqueue = function() {
+
+};
+
+QueueDataStructure.prototype.enqueue = function() {
+
+};
+
+QueueDataStructure.prototype.dequeue = function() {
+
+};
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE =10;
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.canPush = function() {
+  if (this.stackControl.length < this.MAX_SIZE) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.push = function(element) {
+  if (this.canPush()) {
+    this.stackControl.push(element);
+    return this.stackControl;
+  } else {
+    return "Stack Overflow";
+  }
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.isEmpty() === true) {
+    return "Stack Underflow";
+  } else {
+  return this.stackControl.pop();
+    
+  }
+};
+
