bedf37beec864d9ac59a9d24b538e30e39bc4aeb
Mr.X <seniorito.x@icloud.com>
2019-01-16 20:48:18 +0100

    First commit (=____=)

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
+stack = new StackDataStructure();
+
 function StackDataStructure () {
+  this.stackControl = []
+  this.MAX_SIZE = 20
 }
+
+StackDataStructure.prototype.isEmpty = function(){
+  if (this.stackControl.length === 0){
+    return true
+  } return false 
+  }
+
+StackDataStructure.prototype.canPush = function(){
+    if (this.stackControl.length < this.MAX_SIZE){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  StackDataStructure.prototype.push = function(elem){
+    if (this.canPush() === true){
+      this.stackControl.push(elem)
+      return this.stackControl
+    } else {
+      return 'Stack Overflow'
+    }
+  }
+
+  StackDataStructure.prototype.pop = function (){
+    if (this.isEmpty() === true){
+      return 'Stack Underflow'
+    } return this.stackControl.pop() 
+  }
