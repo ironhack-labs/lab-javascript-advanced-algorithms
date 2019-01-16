ba9347b5feb5ddf462f68f98c62153cabbe0b153
Sofía <sofianombela@gmail.com>
2019-01-16 19:58:42 +0100

    StackDataStructure done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,41 @@
 function StackDataStructure () {
+    this.stackControl = [];
+    this.MAX_SIZE = 0;
+
+    this.isEmpty = function(){
+        if (this.stackControl.length == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.canPush = function(){
+        if (this.MAX_SIZE > this.stackControl.length){
+            return false;
+        }
+
+        return true;
+    }
+
+    this.push = function(num){
+
+        if (this.MAX_SIZE > this.stackControl.length){
+            return 'Stack Overflow';
+       
+        } else {
+            this.stackControl.push(num);
+            return this.stackControl;
+        }
+
+    }
+
+    this.pop = function(){
+
+        if (this.stackControl.length === 0){
+            return 'Stack Underflow';
+        }
+        return this.stackControl.pop()
+    }
 }
+
