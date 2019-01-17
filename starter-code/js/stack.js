d7e588bc43e4e2c6c68fcbb8d47d1e19128f492a
krls13 <krlsalberto13@hotmail.com>
2019-01-17 11:17:45 +0100

    first commit

---
+++ starter-code/js/stack.js
@@ -0,0 +1,13 @@
+
+var input = document.createElement("input");
+var MAX_SIZE = 4;
+for(input = 4; input == MAX_SIZE; input++){
+
+}
+
+
+var father = document.querySelector(".container");
+father.appendChild(input);
+
+
+
--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,21 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 4;
+    this.isEmpty = function(){
+        if(this.queueControl.length == 0) return true; 
+        return false;
+    };
+    this.canEnqueue = function(){
+        if(this.queueControl.length < this.MAX_SIZE) return true; return false;
+    };
+    this.enqueue = function(arg){
+        if(this.queueControl.length == this.MAX_SIZE) return "Queue Overflow"
+        this.queueControl.unshift(arg);
+        return this.queueControl;
+    };
+    this.dequeue = function(){
+        if(this.queueControl.length == 0) return "Queue Underflow"
+        return this.queueControl.pop();
+    };
+
 }
