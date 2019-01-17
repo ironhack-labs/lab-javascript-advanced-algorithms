be3aa171379ffac0d288eac3f70d27c737f9cc35
Inés Guerra <inesguerra@MacBook-Air-de-Ines.local>
2019-01-17 10:03:52 +0100

    done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,5 @@
+
+var stack
+for (i = 0, i < stackControl.lenght, i++){
+
+}
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,31 @@
 function StackDataStructure () {
+
+     this.stackControl = []
+     this.MAX_SIZE = 9
+
+     this.isEmpty = function (){
+         if (this.stackControl.length === 0) {
+             return true
+         } else return false
+     }
+
+    this.canPush = function(){
+        if(this.stackControl.length<this.MAX_SIZE) return true
+        return false
+    } 
+
+    this.pop = function(){
+        if(this.stackControl.length == 0) return "Stack Underflow"
+        return this.stackControl.pop();
+    }
+
+    this.push = function(element){
+        if(this.stackControl.length >= this.MAX_SIZE) return "Stack Overflow"
+        else {
+        this.stackControl.push(element) 
+        return this.stackControl
+        }
+    }
 }
+
+
