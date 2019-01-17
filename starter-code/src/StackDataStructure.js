5d99e2d536c16c18be920c4340d8122e84f9dd15
Thilo Sperber <t.sperber@gmx.net>
2019-01-17 09:20:02 +0100

    first commit

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
 function StackDataStructure () {
-}
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+};
+
+StackDataStructure.prototype.isEmpty = function (){
+  if (this.stackControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+StackDataStructure.prototype.canPush = function () {
+  if (this.stackControl.length < this.MAX_SIZE) {
+    return true;
+  } else { 
+    return false;
+  }
+};
+StackDataStructure.prototype.push = function (i){
+  if (this.canPush() === false) {
+    return 'Stack Overflow'
+  } else {
+    this.stackControl.push(i);
+    return this.stackControl;
+  }
+    
+};
+StackDataStructure.prototype.pop = function (){
+    if (this.isEmpty() === true) {
+      return 'Stack Underflow'
+    } else {
+      return this.stackControl.pop();
+    }
+};
+
+
+
+
+
+
+
+
