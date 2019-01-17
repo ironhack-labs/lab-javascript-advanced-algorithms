c10b0f6ac8f703bf0acb65f6a43683b3b1dbd49c
Patricia Martin <patricia_martin_4@hotmail.com>
2019-01-17 09:00:24 +0100

    Advanced Algorithms

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,48 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 0;
+  
+
+    this.isEmpty = function () {
+        if (this.queueControl.length === 0){
+            return true; 
+        }
+           return false;
+       };
+   
+    this.canEnqueue = function () {
+
+        if (this.MAX_SIZE > this.queueControl.length) {
+            return false
+        }
+        return true
+
+    };
+   
+    this.enqueue = function (num) {
+       
+        if (this.MAX_SIZE > this.queueControl.length) {
+            return "Queue Overflow"
+        }
+
+        this.queueControl.push(num);
+        this.queueControl.reverse(num);
+        return this.queueControl;
+
+
+
+    };
+   
+    this.dequeue = function () {
+
+        if (this.queueControl.length === 0){
+            return 'Queue Underflow'
+        }
+
+        return this.queueControl.pop()
+
+    };
+   
+
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,42 @@
 function StackDataStructure () {
+   this.stackControl = [];
+   this.MAX_SIZE = 0;
+   this.isEmpty = function (){
+    
+    if (this.stackControl.length === 0){
+        return true; 
+    }
+       return false;
+   };
+   
+   this.canPush = function (){
+
+        if (this.MAX_SIZE > this.stackControl.length) {
+            return false;
+        }
+        return true;
+   };
+
+   this.push = function (num){
+
+    if (this.MAX_SIZE > this.stackControl.length){
+        return 'Stack Overflow';
+        }
+
+    this.stackControl.push(num)
+    return this.stackControl;
+
+   }
+   this.pop = function () {
+
+    if (this.stackControl.length === 0){
+        return 'Stack Underflow';
+    }
+       return this.stackControl.pop()
+    
+   }
+
+
+
 }
+
