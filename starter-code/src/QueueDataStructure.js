51b50472132e10f9cd4401bcb0ff06040035df42
CGuillotG <c.guillot@outlook.com>
2019-01-23 17:44:01 -0600

    datastructure js files completed with succesful Jasmine tests

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = () => {
+    return this.queueControl.length === 0;
+  };
+  this.canEnqueue = () => {
+    return this.queueControl.length < this.MAX_SIZE;
+  };
+  this.enqueue = elem => {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(elem);
+      return this.queueControl;
+    }
+    return "Queue Overflow";
+  };
+  this.dequeue = () => {
+    if (!this.isEmpty()) {
+      return this.queueControl.pop();
+    }
+    return "Queue Underflow";
+  };
+
 }
