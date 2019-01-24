7d920937695df8fb5a35aee0a297a138be9a3fb7
Maldonado <henry4frank1@gmail.com>
2019-01-24 08:53:12 -0600

    No lo he acabado por cuestion de tiempo, pero lo retomre para terminarlo

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
-}
+class StackDataStructure  {
+  constructor(stack=[]) {
+    this.stackControl = stack;
+    this.MAX_SIZE = 10;
+  }
+  isEmpty() {
+    return(this.stackControl.length === 0);
+  }
+  canPush() {
+    return(this.stackControl.length < this.MAX_SIZE);
+  }
+  push(val) {
+ 
+     return this.enqueue(val);
+  }
+  pop() {
+    return this.dequeue();screenTop
+  }
+  enqueue(val) {
+    if(this.canPush() === true) {
+      this.stackControl.push(val);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow" ;
+    }
+  }
+  dequeue() {
+    if(this.isEmpty()) {
+      return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
+  }
+ }
