2aac647937101a48bb99b48b4f9e8a4964357a1d
caigma <caigma@gmail.com>
2019-01-28 23:35:00 +0100

    resuelta primera parte DOM visual pila STACK

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,12 +1,42 @@
-var stack = new StackDataStructure()
-stack.MAX_SIZE = 7
+var stack = new StackDataStructure();
 
-var stackDiv = document.getElementById("containerLifo")
+stack.MAX_SIZE = 10;
+stack.stackControl = [];
 
-var newElement = document.createElement("div")
-newElement.setAttribute("class", "element")
+var stackDiv = document.getElementById('containerLifo');
 
-for(var i = 0; i < stack.MAX_SIZE; i++){
-  stackDiv.innerHTML+= newElement.outerHTML
-}
+var newElement = document.createElement('div');
+newElement.setAttribute('class', 'element');
 
+stackDiv.innerHTML = newElement.outerHTML.repeat(stack.MAX_SIZE);
+
+var inElement = document.querySelector('#inputElement');
+var elementAdd = document.querySelector('#buttonAdd');
+var elementTake = document.querySelector('#buttonTake');
+var elementToken = document.querySelector('#elementToken');
+var allElements = document.querySelectorAll('.element');
+
+elementAdd.onclick = function() {
+	if (stack.canPush()) {
+		stack.push(inElement.value);
+		let index = stack.MAX_SIZE - stack.stackControl.length;
+		allElements[index].innerHTML = inElement.value;
+		allElements[index].setAttribute('class', 'elementIn');
+		inElement.value = '';
+	} else {
+		console.log('la pila está llena');
+	}
+};
+
+elementTake.onclick = function() {
+	if (!stack.isEmpty()) {
+		let index = stack.MAX_SIZE - stack.stackControl.length;
+		elementToken.innerHTML = allElements[index].innerHTML;
+		allElements[index].innerHTML = '';
+		allElements[index].setAttribute('class', 'element');
+
+		stack.pop();
+	} else {
+		console.log('la pila está vacía');
+	}
+};
