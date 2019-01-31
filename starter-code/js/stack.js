27462128182018ecc2c8df2b78ed32ef0e7f381c
Ric Taglietti <rictaglietti@gmail.com>
2019-01-31 17:30:51 -0500

    Stack Overflow works

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,32 +1,36 @@
 $(document).ready(function() {
   let newStack = new StackDataStructure(8)
-  // var numLi = $(".list-item-group").length;
+  var numLi = $(".list-item-group").length;
   
 
   for(let i = 0; i < newStack.MAX_SIZE; i++) {
 
     $('<li>', {
-      class: "list-group-item",
+      class: "list-group-item text-center",
       text: ""
     }).appendTo(".list-group")
   }
 
-  $('.add-btn').click(function() {
+  $('#add-btn').click(function() {
     let txtValue = $('#add-to-stack').val();
-    for(let i = $('.list-group-item').length - 1; i > 0; i++) {
+    
+    if($('.list-group-item').first().hasClass('full')) {
+      $('<li>', {
+        class: "list-group-item overflow text-center",
+        text: "Stack Overflow!!"
+      }).prependTo('.list-group')
 
+    } else {
+    $('.list-group-item').not('.full').last().addClass('full').text(txtValue)
     }
   })
 
+  $('#take-btn').click(function() {
+    
+    
+    $('.list-group-item').filter('.full').first().removeClass('full').empty();
+  })
 
-  
-//   $('.btn-primary').click(function () {
-//     $('<li>', {
-//       class: "list-group-item",
-//       text: "Example Page"
-//     }).appendTo(".list-group")
-//   })
-// })
 })
 
 
