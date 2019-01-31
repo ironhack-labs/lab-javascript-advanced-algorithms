efb32dadf17ab658ad80ca7ef6c60ac325ee9797
Victoria Hickam <vhickam@gmail.com>
2019-01-31 11:58:24 -0500

    all jasmine tests completed and html setup done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,49 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+  constructor(maxNum) {
+    this.MAX_SIZE = Number(maxNum);
+     this.queueControl = [];
+  }
+
+  
+  isEmpty() { 
+    if (this.queueControl.length == 0){
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  canEnqueue(){
+    if(this.queueControl.length == this.MAX_SIZE){
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  
+
+  enqueue(element){
+    if (this.canEnqueue() == true){
+    this.queueControl.unshift(element);
+    return this.queueControl;
+    }
+    else {
+      return 'Queue Overflow';
+    }
+  }
+
+  dequeue(){
+    if(!this.isEmpty()){
+    return this.queueControl.pop();
+    }
+    else {
+      return 'Queue Underflow';
+    }
+  }
+
+
 }
+ 
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,51 @@
-function StackDataStructure () {
+
+class StackDataStructure {
+
+  constructor(maxNum) {
+    this.MAX_SIZE = Number(maxNum);
+     this.stackControl = [];
+  }
+
+  
+  isEmpty() { 
+    if (this.stackControl.length == 0){
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  canPush(){
+    if(this.stackControl.length == this.MAX_SIZE){
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  
+
+  push(element){
+    if (this.canPush() == true){
+    this.stackControl.push(element);
+    return this.stackControl;
+    }
+    else {
+      return 'Stack Overflow';
+    }
+  }
+
+  pop(){
+    if(!this.isEmpty()){
+    return this.stackControl.pop();
+    }
+    else {
+      return 'Stack Underflow';
+    }
+  }
+
+
 }
+ 
+
