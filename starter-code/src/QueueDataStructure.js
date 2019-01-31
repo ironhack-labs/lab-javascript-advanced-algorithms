ecdce8794f3798833d9102312e53212bfed5e716
Matthew Keil <malibuss67@gmail.com>
2019-01-31 12:00:17 -0500

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,32 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+    constructor() {
+        this.MAX_SIZE = 10;
+        this.queueControl = [];   
+    }
+
+    isEmpty() {
+        return !this.queueControl.length
+    }
+
+    canEnqueue() {
+        return this.queueControl.length < this.MAX_SIZE
+    }
+
+    enqueue(item) {
+        
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(item)
+            return this.queueControl;
+        }
+
+        return 'Queue Overflow';
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return 'Queue Underflow'
+        }
+        return this.queueControl.pop()
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
+class StackDataStructure {
+
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        return !this.stackControl.length
+    }
+
+    canPush() {
+        return this.stackControl.length < this.MAX_SIZE
+    }
+
+    push(item) {
+        
+        if (this.canPush()) {
+            this.stackControl.push(item)
+            return this.stackControl;
+        }
+
+        return 'Stack Overflow';
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return 'Stack Underflow'
+        }
+        return this.stackControl.pop()
+    }
 }
