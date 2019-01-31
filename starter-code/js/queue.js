7e0362a8b90a9bde00bffa1f0df5fff2e7ad3498
Jack Fischer <jrfisch95@gmail.com>
2019-01-31 18:25:24 -0500

    Finished with stack visualizer. Queue visualizer needs more work.

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,6 +1,36 @@
 
-let testQueue = new QueueDataStructure(10);
+let testQueue = new QueueDataStructure(4);
 
-for(let i = 0; i < testStack.MAX_SIZE; i++){
-  $('#dequeue-queue').after(`<div><span></span></div>`);
+for(let i = 0; i < testQueue.MAX_SIZE; i++){
+  console.log(i);
+$('#dequeue-queue').after(`<div><span></span></div>`);
 }
+
+let divQueueArray = $('#queue div');
+
+$('#enqueue-queue').click(() => {
+  //Add OVERFLOW if an attempt is made to enqueue and there is no more room to enqueue
+  if(testQueue.MAX_SIZE == testQueue.queueControl.length && !$('#queue div:first').hasClass('overflow')){
+    $('#enqueue-queue').after(`<div class="overflow"><span>QUEUE OVERFLOW</span></div>`);
+  }
+  //Remove UNDERFLOW if it exists
+  if($('#queue div:last').hasClass('underflow')){
+    $('#queue div:last').remove();
+  } 
+  queueStack.enqueue($('#value-queue').val());
+  $(divQueueArray[testQueue.MAX_SIZE - testQueue.queueControl.length]).children()[0].innerText = $('#value-queue').val();
+});
+
+$('#dequeue-queue').click(() => {
+  //Remove OVERFLOW if it exists
+  if($('#queue div:first').hasClass('overflow')){
+    $('#queue div:first').remove();
+  }
+  //Add UNDERFLOW if there is nothing left to pop and an attempt is made to pop
+  if(testQueue.queueControl.length == 0 && !$('#queue div:last').hasClass('underflow')){
+    $('#queue div:last').after(`<div class="underflow"><span>QUEUE UNDERFLOW</span></div>`);
+  }
+
+  testQueue.dequeue();
+  $(divQueueArray[testQueue.MAX_SIZE - testQueue.queueControl.length - 1]).children()[0].innerText = "";
+});
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,20 +1,36 @@
 
-let testStack = new StackDataStructure(10);
+let testStack = new StackDataStructure(4);
 
 for(let i = 0; i < testStack.MAX_SIZE; i++){
-  $('#pop-stack').after(`<div><span></span></div>`);
+$('#pop-stack').after(`<div><span></span></div>`);
 }
 
-let divArray = $('#stack div');
+let divStackArray = $('#stack div');
 
 $('#push-stack').click(() => {
+  //Add OVERFLOW if an attempt is made to push and there is no more room to push
+  if(testStack.MAX_SIZE == testStack.stackControl.length && !$('#stack div:first').hasClass('overflow')){
+    $('#pop-stack').after(`<div class="overflow"><span>STACK OVERFLOW</span></div>`);
+  }
+  //Remove UNDERFLOW if it exists
+  if($('#stack div:last').hasClass('underflow')){
+    $('#stack div:last').remove();
+  } 
   testStack.push($('#value-stack').val());
-  $(divArray[testStack.MAX_SIZE - testStack.stackControl.length]).children()[0].innerText = $('#value-stack').val();
+  $(divStackArray[testStack.MAX_SIZE - testStack.stackControl.length]).children()[0].innerText = $('#value-stack').val();
 });
 
 $('#pop-stack').click(() => {
-  testStack.pop();
-  $(divArray[testStack.MAX_SIZE - testStack.stackControl.length - 1]).children()[0].innerText = "";
-});
+  //Remove OVERFLOW if it exists
+  if($('#stack div:first').hasClass('overflow')){
+    $('#stack div:first').remove();
+  }
+  //Add UNDERFLOW if there is nothing left to pop and an attempt is made to pop
+  if(testStack.stackControl.length == 0 && !$('#stack div:last').hasClass('underflow')){
+    $('#stack div:last').after(`<div class="underflow"><span>STACK UNDERFLOW</span></div>`);
+  }
 
+  testStack.pop();
+  $(divStackArray[testStack.MAX_SIZE - testStack.stackControl.length - 1]).children()[0].innerText = "";
+});
 
