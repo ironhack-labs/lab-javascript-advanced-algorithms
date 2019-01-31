b9c079b0318c8c1cbb5fb53631ae95c0e9dddd7e
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-31 11:22:07 -0500

    -18 failures to go

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
-function StackDataStructure () {
+
+
+class StackDataStructure {
+
+  constructor() {
+    this.stackControl = []
+    this.MAX_SIZE = 3
+  }
+
+  pop(){
+    if(this.isEmpty()){
+      this.stackControl.pop()
+    }else{
+      console.log('Undeflow');
+    }
+  }
+
+  push(args){
+    if(this.canPush()){
+      this.stackControl.push(args)
+      return this.stackControl
+    }else{
+      return 'Stack Overflow'
+    }
+  }
+
+  isEmpty(){
+        return (this.stackControl.length<1) ? true:false
+  }
+  canPush(){
+    if (this.stackControl.length >= this.MAX_SIZE){
+      return false
+    }else{
+      return true
+    }
+  }
+  describe(){
+
+  }
+
 }
+
+
+let arr = new StackDataStructure
+arr.push(3)
+arr.push(5)
+console.log(arr.stackControl.pop());
