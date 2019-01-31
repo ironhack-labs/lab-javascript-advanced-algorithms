666792370ee648d78aac140b07319e08a1821d8a
LiranGib <liran.gibor@gmail.com>
2019-01-31 13:31:57 -0500

    code done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,42 @@
-function QueueDataStructure () {
+class QueueDataStructure{
+    constructor(){
+        this.queueControl = [];
+        this.MAX_SIZE = 10;
+    }
+    isEmpty(){
+        if(this.queueControl.length == 0){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    canEnqueue(){
+        if(this.queueControl.length >= 0 && this.queueControl.length < this.MAX_SIZE){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    enqueue(element){
+        let enqueueCondition = this.canEnqueue();
+        if(enqueueCondition == true){
+            this.queueControl.unshift(element);
+            return this.queueControl;
+        }else if(this.queueControl.length == this.MAX_SIZE){
+            return 'Queue Overflow';
+        }else{
+            return this.queueControl;
+        }
+    };
+
+    dequeue(){
+        let dequeueCondition = this.isEmpty();
+        if(dequeueCondition == true){
+            return 'Queue Underflow'
+        }else{
+            return this.queueControl.pop();
+        }
+    };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,46 @@
-function StackDataStructure () {
+
+class StackDataStructure {
+    constructor(){
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }  
+
+    isEmpty(){
+        if(this.stackControl.length == 0){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    canPush(){
+        if(this.stackControl.length >= 0 && this.stackControl.length < this.MAX_SIZE){
+            return true;
+
+        }else if(this.stackControl.length === this.MAX_SIZE) {
+            return false;
+
+        }else{
+            return false;
+        }
+     };
+
+    push(element){
+        let pushCondition = this.canPush();
+        if(pushCondition == true){
+            this.stackControl.push(element);
+        }else{
+            return "Stack Overflow"
+        }
+        return this.stackControl
+      };
+
+    pop(){
+        let popCondition = this.isEmpty();
+        if(popCondition == false){
+            return this.stackControl.pop();
+        }else{
+            return 'Stack Underflow'
+        }
+     };
 }
