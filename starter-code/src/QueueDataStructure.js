80c50f768f9f0b1dffe2d35f03e0ebb52db366cb
nunna19 <nunnapat_ch@yahoo.com>
2019-02-01 12:56:12 -0500

    not done yet

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,48 @@
-function QueueDataStructure () {
+
+class QueueDataStructure{
+  constructor () {
+    this.queueControl= [];
+    this.MAX_SIZE = 10;
+  }
+  isEmpty () {
+    this.emptry = 0;
+    if(this.queueControl.length === this.emptry){
+      
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  canEnqueue () {
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true;
+    } 
+    else {
+      return false
+    }
+  }
+  
+  dequeue(num) {
+    console.log(this.queueControl)
+      if (!this.isEmpty()){
+        return  this.queueControl.pop(num)
+      }else{
+        return 'Queue Underflow'
+      }
+    }
+    
+
+   
+  
+  enqueue(num) {
+    console.log(this.queueControl)
+    if (this.canEnqueue()){
+      this.queueControl.unshift(num);
+      return this.queueControl
+    }
+    else {
+      return "Queue Overflow"
+    }
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,65 @@
-function StackDataStructure () {
+
+
+class StackDataStructure {
+  constructor(){
+    this.stackControl=[]
+    this.MAX_SIZE = 10;
+    
+  }
+
+ 
+  isEmpty(){
+    this.emtry = 0;
+    if (this.stackControl.length === this.emtry ){
+      return true
+  }else{
+    return false
+  }
 }
+  canPush(){
+    if (this.stackControl.length < this.MAX_SIZE){
+      return true
+    }else{
+      return false
+    }
+  }
+    push(num){
+    if(this.canPush()){
+      this.stackControl.push(num)
+      return this.stackControl
+    }else{
+      return 'Stack Overflow'
+    }
+    }
+    pop(){
+      if (!this.isEmpty()){
+        return this.stackControl.pop()
+      }else{
+        return 'Stack Underflow'
+      }
+    }
+}
+
+document.querySelector('.addButton').addEventListener('click',function() {
+  var x = document.querySelector('.input').value
+  var y = document.querySelector('.eight').innerHTML = x;
+ 
+});
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+// function StackDataStructure(){
+//   this.stackControl = []
+
+// 
