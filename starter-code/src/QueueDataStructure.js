c491307965e6c33ea9848dce08a33259d48b0bd5
Tomas Freire Knight <tomasfreire@gmail.com>
2019-02-10 21:25:44 -0600

    jasmine & general layout done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = function() {
+    if (this.queueControl.length == 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  this.canEnqueue = function() {
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  this.enqueue = function(elem) {
+    if (this.canEnqueue() == true) {
+      this.queueControl.unshift(elem);
+      return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+  }
+
+  this.dequeue = function() {
+    if (this.isEmpty() == false) {
+      return this.queueControl.pop();
+    } else {
+      return "Queue Underflow";
+    }
+  }
 }
