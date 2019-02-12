f2cb20cb15d4563fdca34fa3c9b85b9d793a6670
Rafael Torres Escobar <torresc.rafael@gmail.com>
2019-02-12 14:53:07 -0600

    Only Algorithm

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,56 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE=10;
+    this.front = 0;
+    this.rear = 0;
 }
+
+QueueDataStructure.prototype.isEmpty = function(){
+    if(this.queueControl.length == 0){
+	return true
+    }
+    return false
+}
+
+QueueDataStructure.prototype.isEmpty = function(){
+    if(this.queueControl.length == 0){
+	return true
+    }else{
+	return false
+    }
+}
+
+QueueDataStructure.prototype.canEnqueue = function(){
+    if(this.queueControl.length < this.MAX_SIZE){
+	return true
+    }   else{
+	return false
+    }
+}
+
+QueueDataStructure.prototype.enqueue = function(element){
+    if(this.canEnqueue()){
+	this.queueControl[this.rear] = element;
+	this.rear = this.rear + 1;
+	return this.queueControl.reverse()
+    }else{
+	return "Queue Overflow"
+    }
+}
+
+QueueDataStructure.prototype.dequeue = function(){
+    if(this.isEmpty()){
+	return 'Queue Underflow'
+    }else{
+	let data = this.queueControl[this.front]
+	this.front = this.front + 1
+	
+	return this.queueControl[this.queueControl.length-1]
+    }
+	
+}
+
+
+
+
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,49 @@
-function StackDataStructure () {
+function StackDataStructure(){
+    this.stackControl = [];
+    this.MAX_SIZE=10;
+    this.top = 0;
 }
+
+
+StackDataStructure.prototype.isEmpty = function(){
+    if(this.stackControl.length == 0){
+	return true
+    }
+    return false
+}
+
+
+
+
+StackDataStructure.prototype.canPush = function(){
+    if(this.stackControl.length < this.MAX_SIZE){
+	return true
+    }
+    return false
+}
+
+StackDataStructure.prototype.push = function(element){
+    if(this.canPush())
+    {
+	this.stackControl[this.top++] = element
+	return this.stackControl
+
+    }
+    return "Stack Overflow"
+    
+}
+
+
+StackDataStructure.prototype.pop = function(element){
+    if(this.isEmpty()) return 'Stack Underflow';
+    let last = this.stackControl.length - 1
+    return    this.stackControl[last]
+}
+
+
+
+
+
+
+
+
