08fe7de3a1124582e17aa906ffa4cdf391259665
baltasar torres garcia <baltasartorresgarcia@MacBook-Pro-de-amelia.local>
2019-02-14 09:37:44 +0100

    done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,4 @@
+var stack = new StackDataStructure()
+
+var btnAdd = document.getElementById('add');
+var btnTake = document.getElementById('take');
--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,51 @@
 function QueueDataStructure () {
+   
+    this.queueControl = []
+
+    this.MAX_SIZE = 8
+
+    QueueDataStructure.prototype.isEmpty = function () {
+
+        return (this.queueControl.length === 0)
+
+    }
+
+    QueueDataStructure.prototype.canEnqueue= function () {
+
+        return (this.queueControl.length <= (this.MAX_SIZE - 1))
+
+    }
+
+    QueueDataStructure.prototype.enqueue = function (elem) {
+
+        if (this.queueControl.length === this.MAX_SIZE) {
+
+            return 'Queue Overflow'
+
+        } else {
+
+            this.queueControl.unshift(elem)
+
+            return this.queueControl
+        }
+
+    }
+
+    QueueDataStructure.prototype.dequeue = function () {
+
+        if (this.queueControl.length === 0) {
+
+            return 'Queue Underflow'
+
+        } else {
+
+            return this.queueControl.pop()
+
+        }
+
+    }
+
 }
+
+queue = new QueueDataStructure();
+
