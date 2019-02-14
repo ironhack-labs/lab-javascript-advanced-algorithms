c2eabee5bfcf3a6bbb10cae3bb0ff8d198f8377a
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-02-14 09:29:46 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = []
+    this.MAX_SIZE = 8
+    this.isEmpty = function() {
+        return this.queueControl.length === 0
+    }
+    QueueDataStructure.prototype.canEnqueue = function() {
+        return this.queueControl.length < this.MAX_SIZE
+    }
+    QueueDataStructure.prototype.enqueue = function(elem) {
+        if (this.queueControl.length === this.MAX_SIZE) {
+            return "Queue Overflow"
+        } else {
+            this.queueControl.unshift(elem)
+            return this.queueControl
+        }
+    }
+    QueueDataStructure.prototype.dequeue = function() {
+        if (this.queueControl.length === 0) {
+            return "Queue Underflow"
+        } else {
+            return this.queueControl.pop()
+        }
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = []
+    this.MAX_SIZE = 8
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+    return this.stackControl.length === 0
+}
+
+StackDataStructure.prototype.canPush = function() {
+    return this.stackControl.length < this.MAX_SIZE
+}
+
+StackDataStructure.prototype.push = function(elem) {
+    if (this.stackControl.length === this.MAX_SIZE) {
+        return "Stack Overflow"
+    } else {
+        this.stackControl.push(elem)
+        return this.stackControl
+    }
+}
+
+StackDataStructure.prototype.pop = function() {
+    if (this.stackControl.length === 0) {
+        return "Stack Underflow"
+    } else {
+        return this.stackControl.pop()
+    }
+}
+
+var addButton = document.getElementById("add-button")
+var box = document.getElementsByClassName("box")
+
+addButton.onclick = function() {
+    for (var i = 0; i < box.length; i++) {
+        box[i].style.backgroundColor = "green"
+    }
+
 }
