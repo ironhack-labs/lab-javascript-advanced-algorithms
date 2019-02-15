b7f25fd704d0d7eb7479f0f82d0e2c373405b10c
Felipe Martinez <luisf.martinez89@gmail.com>
2019-02-14 18:20:34 -0600

    No lo acabamos

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,16 @@
 function StackDataStructure () {
+   this.stackControl = []
+   this.MAX_SIZE = 8
+   this.isEmpty = function(){
+        
+   }
+   this.canPush = function(){
+
+   }
+   this.push = function(){
+
+   }
+   this.pop = function(){
+
+   }
 }
