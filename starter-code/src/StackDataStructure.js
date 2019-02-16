de823b9949989f3a76324efca0ceb0d9230f1cf8
Dnox1 <david@nestrategia.com>
2019-02-16 20:36:18 +0100

    hecho stack y queue

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 2;
+  this.isEmpty = function () {
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
+  };
+  this.canPush = function () {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
+  };
+  this.push = function (arg) {
+    if (this.canPush() === true){
+    this.stackControl.push(arg);
+    return this.stackControl
+    } else {
+      return 'Stack Overflow'
+    }
+  };
+  this.pop = function () {
+    if (this.isEmpty() === true) {
+      return "Stack Underflow"
+    } else {
+    return this.stackControl.pop();
+    }
+  };
 }
