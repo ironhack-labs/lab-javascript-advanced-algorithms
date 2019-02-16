bc9e8a8fb29753931c504d8bb73aeb8235134ee0
David Gallego Gomez <davizs@hotmail.com>
2019-02-17 00:29:01 +0100

    Commit inicial

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,10 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function () {
+    return this.queueControl.length === 0;
+  };
+  this.canEnqueue = function () {
+    return this.queueControl.length < this.MAX_SIZE;
+  };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,25 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function () {
+    return this.stackControl.length === 0;
+  };
+  this.canPush = function () {
+    return this.stackControl.length < this.MAX_SIZE;
+  };
+  this.push = function (data) {
+    if ( this.canPush() ) {
+      this.stackControl.push(data);
+      return this.stackControl;
+    }
+    else { return 'Stack Overflow'; }
+  };
+  this.pop = function () {
+    if ( !this.isEmpty() ) {
+      var toReturn = this.stackControl[this.stackControl.length - 1];
+      this.stackControl.pop();
+      return toReturn;
+    }
+    else { return 'Stack Underflow'; }
+  };
 }
