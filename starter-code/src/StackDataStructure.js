74dcebf27eab36c0257e205498f71c7f3441c384
amperry <alexp3y@gmail.com>
2019-02-16 12:33:28 +0100

    Implemented stack data structure according to test specs

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,24 @@
 function StackDataStructure () {
+
+    this.MAX_SIZE = 20;
+    this.stackControl = [];
+    this.isEmpty = function() {
+        return this.stackControl.length == 0;
+    }
+    this.canPush = function() {
+        return this.stackControl.length < this.MAX_SIZE;
+    }
+    this.push = function(element) {
+        if (!this.canPush()) {
+            return 'Stack Overflow';
+        }
+        this.stackControl.push(element);
+        return this.stackControl;
+    }
+    this.pop = function() {
+        if (this.isEmpty()) {
+            return 'Stack Underflow';
+        }
+        return this.stackControl.pop();
+    }
 }
