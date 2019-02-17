3d9b03351e03f15ba8347b711e9113b45fed6de0
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-17 21:40:12 +0100

    18failures

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
 function StackDataStructure () {
+  this.stackControl=[];
+  this.MAX_SIZE=10;
+  this.isEmpty = function(){
+    if (this.stackControl.length!=0){
+      return false
+    }
+    return true
+  }
+  this.canPush = function(){
+    if (this.stackControl.length < this.MAX_SIZE){
+      return true
+    }
+    return false
+  }
+  this.push = function (item){
+     if (this.canPush() == true) {
+       this.stackControl.push(item); 
+       return this.stackControl
+     }
+     return 'Stack Overflow'
+  }
+  this.pop = function (){
+
+  }
 }
+
