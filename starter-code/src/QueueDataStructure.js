6bccf9e63e4d8ab03461ddef19f2cdc4cbcded11
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-17 22:22:00 +0100

    iteration 2 finished

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
 function QueueDataStructure () {
+  this.queueControl=[];
+  this.MAX_SIZE=10;
+  this.isEmpty = function(){
+    if (this.queueControl.length!=0){
+      return false
+    }
+    return true
+  }
+  this.canEnqueue = function(){
+    if (this.queueControl.length < this.MAX_SIZE){
+      return true
+    }
+    return false
+  }
+  this.enqueue = function (item){
+    if (this.canEnqueue() == true) {
+      this.queueControl.unshift(item); 
+      return this.queueControl
+    }
+    return 'Queue Overflow'
+ }
+ this.dequeue = function (){
+  if (this.isEmpty() == false){
+    return this.queueControl.pop()
+  }
+  return 'Queue Underflow'
+}
+  
 }
