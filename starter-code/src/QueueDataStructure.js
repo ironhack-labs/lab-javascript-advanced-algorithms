dc63c5e6607b3f3c24d929ff870be436a99df511
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-02-17 22:55:15 +0100

    Queue logic done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 5;
+  this.isEmpty = function() {
+    if(this.queueControl.length ===0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  this.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  this.enqueue = function(newQueue) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(newQueue);
+      return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+  }
+  this.dequeue = function () {
+    if(this.isEmpty()) {
+      return "Queue Underflow";
+    } else {
+      return  this.queueControl.pop();
+    }
+  }
 }
