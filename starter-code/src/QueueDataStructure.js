27be8bd5b620d73527afb911d7784af17f2ee9a8
Fran Enriquez <fjenriglez@gmail.com>
2019-02-17 21:54:59 +0100

    Inicio del ejercicio, Lógica hecha, Jasmine pasado

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+  this.isEmpty = function() {
+    return (this.queueControl.length === 0)
+  };
+  this.canEnqueue = function() {
+    return (this.queueControl.length < this.MAX_SIZE)
+  };
+
+  this.enqueue = function(element) {
+    if (this.canEnqueue()){
+      this.queueControl.push(element);
+      this.queueControl.reverse()
+      return this.queueControl.slice(-1);
+    } else {
+      return 'Queue Overflow';
+    };
+  };
+
+  this.dequeue = function(){
+    if (this.queueControl.length !== 0){
+      var lastItem = this.queueControl.slice(-1)
+      this.queueControl = this.queueControl.slice(0, this.queueControl.length-1);
+      return parseInt(lastItem);
+    } else {
+      return 'Queue Underflow'
+    }
+  };
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,30 @@
 function StackDataStructure () {
+  this.stackControl= [];
+  this.MAX_SIZE = 8;
+  this.isEmpty = function() {
+    return (this.stackControl.length === 0)
+  };
+  this.canPush = function() {
+    return (this.stackControl.length < this.MAX_SIZE)
+  };
+
+  this.push = function(element) {
+    if (this.canPush()){
+      this.stackControl.push(element);
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow';
+    };
+  };
+
+  this.pop = function(){
+    if (this.stackControl.length !== 0){
+      var lastItem = this.stackControl.slice(-1)
+      this.stackControl = this.stackControl.slice(0, this.stackControl.length-1);
+      return parseInt(lastItem);
+    } else {
+      return 'Stack Underflow'
+    }
+  };
+
 }
