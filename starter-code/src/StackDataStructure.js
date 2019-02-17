cc14e4a3898cc2753f9eaab535ae22a6d842c9ec
carlosgsm <45521638+carlosgsm@users.noreply.github.com>
2019-02-17 22:58:12 +0100

    Update StackDataStructure.js

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,44 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 7;
 }
+
+ StackDataStructure.prototype.isEmpty = function (){
+       if (this.stackControl.length === 0){
+           return true
+       }
+       else{
+           return false
+       }
+     }
+
+
+ StackDataStructure.prototype.canPush = function (){
+        if (this.stackControl.length < this.MAX_SIZE){
+            return true
+        }
+        else if (this.stackControl.length === this.MAX_SIZE){
+            return false
+        }
+      }
+ 
+ StackDataStructure.prototype.push = function (s){
+       if (this.stackControl.length < this.MAX_SIZE){
+       this.stackControl.push(s);
+       return this.stackControl
+      }
+      else {
+        return ('Stack Overflow')
+      }}
+
+ StackDataStructure.prototype.pop = function (){
+    if (this.stackControl.length<1){
+        return ('Stack Underflow')
+    } 
+    
+    return this.stackControl.pop()
+     
+       }      
+
+
+
