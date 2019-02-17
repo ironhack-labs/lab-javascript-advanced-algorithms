c5a88948891ff5f8f9e870ef0efe74db0bc4a97c
David Gallego Gomez <davizs@hotmail.com>
2019-02-17 01:05:33 +0100

    Terminada la primera iteración

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -7,4 +7,19 @@ function QueueDataStructure () {
   this.canEnqueue = function () {
     return this.queueControl.length < this.MAX_SIZE;
   };
+  this.enqueue = function (data) {
+    if ( this.canEnqueue() ) {
+      this.queueControl.unshift(data); //ERROR
+      return this.queueControl;
+    }
+    else { return 'Queue Overflow'; }
+  };
+  this.dequeue = function () {
+    if ( !this.isEmpty() ) {
+      var toReturn = this.queueControl[this.queueControl.length - 1];
+      this.queueControl.shift();
+      return toReturn;
+    }
+    else { return 'Queue Underflow'; }
+  };
 }
