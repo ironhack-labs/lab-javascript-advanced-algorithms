334c08adeb4be7fbcd298d42e277ce25b9365abd
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-17 21:52:29 +0100

    return last element in array

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 4;
+  this.isEmpty = function(){
+  if(this.stackControl.length != 0){
+    return false;
+  }
+  return true;
+  };
+  this.canPush = function(){
+    if(this.stackControl.length < this.MAX_SIZE ){
+      return true;
+    }
+    return false;
+  };
+  this.push = function(elem){
+    if(this.canPush()) {
+      this.stackControl.push(elem);
+      return this.stackControl
+    }
+    else {return 'Stack Overflow';}
+    
+  }
+  this.pop = function(){
+    if(!this.isEmpty()){
+      return this.stackControl.pop()
+    }
+    return 'Stack Underflow'
+  }
+
+   
 }
