3e19ddbe80ed0f45587ac6aa517aa62d2ae6e25c
Fran Enriquez <fjenriglez@gmail.com>
2019-02-17 23:25:48 +0100

    html hecho, falta vincular los eventos

---
+++ starter-code/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var memoryGame = new MemoryGame(cards);
+  var html = '';
+  memoryGame.cards.forEach(function (pic) {
+    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '  <div class="back" name="'+ pic.img +'"></div>';
+    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '</div>';
+  });
+
+  // Add all the div's to the HTML
+  $('#memory_board').html(html);
+}
