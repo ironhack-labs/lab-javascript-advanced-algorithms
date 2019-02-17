be27824f50aff916c406d637b055059829c8bc7f
carlosgsm <45521638+carlosgsm@users.noreply.github.com>
2019-02-17 22:58:57 +0100

    commit final

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 7;
+}
+
+QueueDataStructure.prototype.isEmpty = function (){
+    if (this.queueControl.length === 0){
+        return true
+    }
+    else{
+        return false
+    }
+  }
+
+  QueueDataStructure.prototype.canEnqueue = function (){
+    if (this.queueControl.length < this.MAX_SIZE){
+        return true
+    }
+    else if (this.queueControl.length === this.MAX_SIZE){
+        return false
+    }
+  }
+
+  QueueDataStructure.prototype.enqueue = function (s){
+    if (this.canEnqueue()){
+    this.queueControl.unshift(s);
+    return this.queueControl}
+     else {
+         return ('Queue Overflow')
+     }
+}
+
+
+QueueDataStructure.prototype.dequeue = function (){
+    if (this.isEmpty()){
+        return "Queue Underflow"
+    }
+        return this.queueControl.pop()
 }
