33577c4a0313df55882aef5bcba18fb20f866628
Guille LA <guillermolucena90@gmail.com>
2019-02-18 17:28:45 +0100

    finished: still have to add some details

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -1,7 +1,7 @@
 var queueDataContainer = $(".queue-data-container");
 var qOverflow = '<div class="queue-overflow">O<br>v<br>e<br>r<br>f<br>l<br>o<br>w</div>';
 var qUnderflow = '<div class="queue-underflow">U<br>n<br>d<br>e<br>r<br>f<br>l<br>o<br>w<br></div>';
-var inputTop = document.getElementsByClassName("input-bottom");
+var inputBot = document.getElementsByClassName("input-bottom");
 
 function addQDivs (arg) {
   for (var i = 0; i < arg; i++) {
@@ -9,16 +9,46 @@ function addQDivs (arg) {
   }
 }
 
+function addQueue (obj) {
+  if(obj.canEnqueue() === false) {
+    $(queueDataContainer).prepend(qOverflow)
+  }
+  else {
+    var inputVal = inputBot[0].value
+    var queueSlots = document.getElementsByClassName("queue-data-element");
+    var arrQSlots = [].slice.call(queueSlots);
+    $(arrQSlots[arrQSlots.length-1]).toggleClass("fill-queue-element");
+    $(arrQSlots[arrQSlots.length-1]).toggleClass("queue-data-element");
+    $(arrQSlots[arrQSlots.length-1]).html(inputVal);
+    obj.queueControl.push(inputVal)
+  }
+}
+
+function takeQueue (obj) {
+  if(obj.isEmpty() === true) {
+    $(queueDataContainer).append(qUnderflow);
+  }
+  else {
+    var queueSlots = document.getElementsByClassName("fill-queue-element");
+    var arrQSlots = [].slice.call(queueSlots);
+    var inputVal = $(arrQSlots[0]).html()
+    obj.queueControl.shift(inputVal)
+    $(arrQSlots[0]).toggleClass("fill-queue-element");
+    $(arrQSlots[0]).toggleClass("queue-data-element");
+    $(arrQSlots[0]).html("");
+  }
+}
+
 $(document).ready(function(){
-  queueDataStructure = new QueueDataStructure(9);
+  queueDataStructure = new QueueDataStructure(15);
   addQDivs(queueDataStructure.MAX_SIZE);
 
   $('.btn-add-queue').click(function () {
-    console.log("He entrado");
+    addQueue (queueDataStructure);
   });
 
   $('.btn-take-queue').click(function () {
-    console.log("He entrado");
+    takeQueue (queueDataStructure);
   });
 
 });
