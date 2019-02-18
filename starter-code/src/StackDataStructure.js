e23d281d0fa87b72f254308a56a3d31e1cf3caa6
RedBalloon24 <worldtraveler757@gmail.com>
2019-02-18 17:35:05 +0100

    almost done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
 function StackDataStructure () {
+    
+    this.stackControl = [];  
+    this.MAX_SIZE = 8;
+     
+
+    this.isEmpty = function() {
+        if (this.stackControl.length === 0){
+            return true;
+        }else {
+            return false;
+        }
+    }; 
+
+    this.canPush = function() {
+        if (this.stackControl.length < this.MAX_SIZE){
+            return true;
+        }else {
+            return false;
+        }
+    };
+    this.push = function (newElement) {
+        if (this.canPush()){
+            this.stackControl.push(newElement)
+            return this.stackControl;
+        }
+            return "Stack Overflow";
+    };
+    this.pop = function () {
+        if (this.isEmpty()){
+            return "Stack Underflow";
+        }            
+            return this.stackControl.pop();
+    };
 }
