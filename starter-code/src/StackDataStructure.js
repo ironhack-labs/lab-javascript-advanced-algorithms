0539a984924d210aa3b13e6c689bd63f8cab001b
laura <lauraabad83@gmail.com>
2019-03-27 19:57:57 +0100

    added push

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
-function StackDataStructure () {
+class StackDataStructure {
+    stackControl = [];
+    MAX_SIZE = 10;
+
+    isEmpty(params) {
+        if (this.stackControl.length > 0) {
+            return false
+        } else {
+            return true
+        }
+
+    }
+
+
+    canPush(params) {
+        if (this.stackControl.length >= this.MAX_SIZE) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+
+
+    push(params) {
+        if (this.stackControl.canPush()) {
+            return "Stack Overflow"
+        } else {
+            this.stackControl.push(params)
+            return this.stackControl
+        }
+
+    }
+
+    pop(params) {
+
+
+    }
+
 }
