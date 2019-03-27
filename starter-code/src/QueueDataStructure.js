3213c53b3000431a9c4829287d7d8e45836a2e93
Natalia Franquet <n.franquet.lm@gmail.com>
2019-03-27 22:28:13 +0100

    js done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
 function QueueDataStructure () {
+  
+  this.queueControl = [];
+  this.MAX_SIZE = 9;
+
+  this.isEmpty = function () {
+    if (this.queueControl.length === 0)   return true;
+      return false;
+  };
+
+  this.canEnqueue = function () {
+    if (this.queueControl.length < this.MAX_SIZE)   return true;
+      return false;
+  };
+
+  this.enqueue = function (item) {
+    if (this.canEnqueue()) {
+        this.queueControl.unshift(item) ;
+        return this.queueControl;
+    } else {
+      return "Queue Overflow";
+    }
+  };
+
+  this.dequeue = function () {
+    if (this.isEmpty())   return "Queue Underflow";
+      return this.queueControl.pop();
+  };
+
+
+
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
 function StackDataStructure () {
+
+  this.stackControl = [];
+  this.MAX_SIZE = 9;
+
+  this.isEmpty = function () {
+    if (this.stackControl.length === 0)   return true;
+      return false;
+  };
+
+  this.canPush = function () {
+    if (this.stackControl.length < this.MAX_SIZE)   return true;
+      return false;
+  };
+
+  this.push = function (item) {
+    if (this.canPush()) {
+        this.stackControl.push(item);
+        return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  }; 
+
+  this.pop = function () {
+    if (this.isEmpty())   return "Stack Underflow";
+      return this.stackControl.pop();
+       };
 }
