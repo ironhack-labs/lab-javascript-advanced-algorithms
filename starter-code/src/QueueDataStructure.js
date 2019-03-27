b9521d7502d4528e2e17802512edc1e6d22c7c80
Raúl Contreras <raulc98@gmail.com>
2019-03-27 18:28:15 +0100

    Finished the logic of the project

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,26 @@
-function QueueDataStructure () {
+class QueueDataStructure{
+  constructor(){
+    this.queueControl = [];
+    this.MAX_SIZE = 255;
+  }
+
+  isEmpty(){
+    return this.queueControl.length === 0;
+  }
+
+  canEnqueue(){
+    return this.queueControl.length < this.MAX_SIZE;
+  }
+
+  enqueue(el){
+    if(this.queueControl.length >= this.MAX_SIZE){ return 'Queue Overflow'}
+    this.queueControl.unshift(el);
+    return this.queueControl;
+  }
+  
+  dequeue(){
+    if(this.queueControl.length === 0){ return 'Queue Underflow'}
+    this.queueControl.shift();
+    return this.queueControl[0];
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,19 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor(){
+    this.stackControl = [];
+    this.MAX_SIZE = 255;
+  }
+    isEmpty(){
+      return this.stackControl.length === 0;
+    }
+    canPush(){
+      return this.stackControl.length < this.MAX_SIZE;
+    }
+    push(n){
+      this.stackControl.push(n);
+      return this.stackControl.length > this.MAX_SIZE ? 'Stack Overflow' : this.stackControl;
+    }
+    pop(){
+      return this.stackControl.length === 0 ? 'Stack Underflow' : this.stackControl.pop();
+    }
 }
