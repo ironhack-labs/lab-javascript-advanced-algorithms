be079e0d38c393efc71dd1f746947196ebc00671
ernestogl88 <ernestogomezlopez@gmail.com>
2019-03-27 18:53:40 +0100

    Modelo creado

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor() {
+    this.queueControl = [];
+    this.MAX_SIZE = 8;
+  }
+  isEmpty() {
+    if (this.queueControl.length === 0) return true;
+    return false;
+  }
+  canEnqueue() {
+    if (this.queueControl.length < this.MAX_SIZE) return true;
+    return false;
+  }
+  enqueue(element) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(element);
+      return this.queueControl;
+    } else return "Queue Overflow";
+  }
+  dequeue() {
+    if (this.queueControl.length === 0) return "Queue Underflow";
+    return this.queueControl.pop();
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,25 @@
-function StackDataStructure () {
-}
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+  }
+  isEmpty() {
+    if (this.stackControl.length === 0) return true;
+    return false;
+  }
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) return true;
+    return false;
+  }
+  push(element) {
+    if (this.canPush()) {
+      this.stackControl.push(element);
+      return this.stackControl;
+    }
+    return "Stack Overflow";
+  }
+  pop() {
+    if (this.stackControl.length === 0) return "Stack Underflow";
+    return this.stackControl.pop();
+  }
+} 
