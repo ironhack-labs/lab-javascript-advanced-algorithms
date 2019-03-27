ee8a36222583805179f1362c5fbed00b3c388ba8
JlAviles <jlavilesmolina@gmail.com>
2019-03-28 00:20:40 +0100

    Stack and Queue data, completed

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor(){
+        this.queueControl = [];
+        this.MAX_SIZE = 9;
+        }
+
+    isEmpty() {
+        return this.queueControl.length === 0;
+    }
+    
+    canEnqueue() {
+        return this.queueControl.length < this.MAX_SIZE;
+    }
+    
+    enqueue(elem) {
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(elem)
+            return this.queueControl
+                }
+            return "Queue Overflow"; 
+        }
+
+    dequeue(elem) {
+        if (this.isEmpty()) {
+            return "Queue Underflow"
+        }
+        return this.queueControl.pop(elem)
+    }
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor(){
+        this.stackControl = [];
+        this.MAX_SIZE = 8;
+    }
+
+    isEmpty() {
+        return this.stackControl.length === 0
+    }
+
+    canPush() {
+        return this.stackControl.length < this.MAX_SIZE;
+    }
+    
+    push(elem) {
+        if (this.canPush()) {
+            this.stackControl.push(elem)
+            return this.stackControl
+                }
+            return "Stack Overflow"; 
+        }
+
+    pop(elem) {
+        if (this.isEmpty()) {
+            return "Stack Underflow"
+        }
+        return this.stackControl.pop(elem)
+    }
+
 }
+
+   
