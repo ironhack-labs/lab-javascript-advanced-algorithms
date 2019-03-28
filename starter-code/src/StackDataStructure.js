18c50658a8309eda8ecddbf5a0b6fb62f7745604
Ruben Polo <rpoloredondo@outlook.es>
2019-03-28 09:30:53 +0100

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,22 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [""];
+    this.MAX_SIZE = 0;
+  }
+  isEmpty() {
+
+    return true;
+  }
+  canPush() {
+    return true;
+  }
+  push() {
+    this.stackControl.push(19);
+    return this.stackControl;
+  }
+  pop() {
+    if(this.stackControl.length < 0) {
+      return "Stack Underflow";
+    }
+  }
 }
