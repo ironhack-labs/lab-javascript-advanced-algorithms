f004df3a32c02a1a4da99fc03d9f24063cb0a471
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-03-28 08:20:52 +0100

    Jasmine completado y html empezado

---
+++ starter-code/js/stack.js
@@ -0,0 +1,19 @@
+var stackDataStructure = new StackDataStructure();
+ 
+
+
+console.log(stackDataStructure);
+ 
+
+ var arrstack=document.getElementsByClassName('stack');
+console.log(arrstack);
+
+stackDataStructure.push(arrstack);
+console.log(arrstack);
+
+
+
+
+
+
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor(){
+      this.stackControl = [];
+      this.MAX_SIZE = 9;
+
+    }
+
+    isEmpty(){
+      if (this.stackControl.length == 0)
+      return true;
+      else return false;
+
+    }
+
+    canPush(){
+      if (this.stackControl.length < this.MAX_SIZE)
+      return true;
+      else return false;
+
+    }
+
+    push(val){
+      if (this.canPush()){
+        this.stackControl.push(val); return this.stackControl;}
+     else return "Stack Overflow";
+    }
+
+    pop(val){
+        if (this.isEmpty()){
+          return "Stack Underflow";}
+          else return this.stackControl.pop(val);
+       
+    }
+
+    
+
+
 }
+
