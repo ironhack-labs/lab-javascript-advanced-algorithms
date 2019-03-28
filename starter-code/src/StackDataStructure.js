e5796405457901fc7ede90c2c4d870eada1fdfb6
Aledia Ortega <alediaortega@gmail.com>
2019-03-28 09:35:20 +0100

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,42 @@
-function StackDataStructure () {
+class StackDataStructure {
+    stackControl = [];
+    MAX_SIZE = 88;
+    isEmpty() {
+        if(this.stackControl.length == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+    canPush() {
+        if(this.stackControl < this.MAX_SIZE){
+            return true;
+        }
+        else{
+           return false;
+        }
+    };
+    push(el){
+        if (this.canPush()) {
+            this.stackControl.push(el);
+            return this.stackControl;
+
+        } 
+        
+        else{
+            return "Stack Overflow"
+        }
+        
+    };
+    pop(){
+        
+        if (this.stackControl == 0){
+            return "Stack Underflow";
+            
+        }else {
+            return this.stackControl.pop();
+        }
+    };
+    
 }
