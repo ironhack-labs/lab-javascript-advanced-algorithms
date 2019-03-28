69fdb81fb1d5259f2aa7d1a9598e7ed03255cf02
juanmana <juanmana0402@gmail.com>
2019-03-28 09:35:10 +0100

    jasmine terminado

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,40 @@
-function QueueDataStructure () {
+class  QueueDataStructure{
+    constructor(){
+        this.queueControl = [];
+        this.MAX_SIZE = 8;
+        
+    }
+    
+
+
+
+    isEmpty(){
+        if(this.queueControl.length===0) return true
+        
+         else return false
+
+   }
+
+
+
+   canEnqueue(){
+    if(this.queueControl.length < this.MAX_SIZE) return true
+    
+    else return false
+}
+
+
+enqueue(element){
+    if(this.canEnqueue()){
+        this.queueControl.unshift(element); return this.queueControl}
+        else return "Queue Overflow"
+        
+    }
+
+    dequeue(element){
+        if(this.isEmpty()){
+            return 'Queue Underflow'}
+            else return this.queueControl.pop(element); 
+            
+}
 }
