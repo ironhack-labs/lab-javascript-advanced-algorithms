053dccb54c5fe769c82aeb01f10e9eb4b9f5aedc
LadyCrispy <cvg.94@hotmail.com>
2019-05-01 19:17:29 +0200

    Stack & Queue done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor(){
+        this.stackControl=[]
+        this.MAX_SIZE=25
+    }
+    isEmpty(){
+        if(this.stackControl.length==0){
+            return true
+        }else{
+            return false
+        }
+    }
+    canPush(){
+        if(this.stackControl.length<this.MAX_SIZE){
+            return true
+        }else{
+            return false
+        }
+        
+        
+    }
+    push(element){
+
+        
+        if(this.stackControl.length==this.MAX_SIZE){
+            return "Stack Overflow"
+        }else{
+            this.stackControl.push(element)
+            return this.stackControl
+        }
+    }
+    pop(){
+        if(this.stackControl.length==0){
+            return 'Stack Underflow'
+        }else{
+        return this.stackControl.pop()
+        }
+    }
+
 }
