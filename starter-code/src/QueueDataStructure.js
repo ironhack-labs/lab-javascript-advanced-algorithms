a76769d22c0d0529cf3b0d41bdac59df57875444
Paula Filipe <pac.tic.es@gmail.com>
2019-05-02 09:35:48 +0200

    half-done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,28 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+  constructor() {
+    this.queueControl = []
+    this.MAX_SIZE = 8
+  }
+  isEmpty() {
+    return (this.queueControl.length === 0)
+  }
+  canEnqueue() {
+    return (this.queueControl.length < this.MAX_SIZE)
+  }
+  enqueue(element) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(element)
+      return this.queueControl
+    } else {
+      return "Queue Overflow"
+    }
+  }
+  dequeue() {
+    if (!this.isEmpty()) {
+      return this.queueControl.pop()
+    } else {
+      return 'Queue Underflow'
+    }
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+class StackDataStructure {
+
+  constructor() {
+    this.stackControl = []
+    this.MAX_SIZE = 8
+  }
+  isEmpty() {
+    return (this.stackControl.length === 0)
+  }
+  canPush() {
+    return (this.stackControl.length < this.MAX_SIZE)
+  }
+  push(element) {
+    if (this.canPush()) {
+      this.stackControl.push(element)
+      return this.stackControl
+    } else {
+      return "Stack Overflow"
+    }
+  }
+  pop() {
+    if (!this.isEmpty()) {
+      return this.stackControl.pop()
+    } else {
+      return 'Stack Underflow'
+    }
+  }
 }
