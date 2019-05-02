bea2235efd004431197aa31117de262810ba6e9d
root <root@JORGE.localdomain>
2019-05-02 12:16:55 +0200

    Advanced_Algorithms - Dayli_Exercice

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,30 @@
-function StackDataStructure () {
+function StackDataStructure() {
+
+    constructor(stack) {
+        this.MAX_SIZE = 100;
+        this.stackControl = [];
+    }
+    push(ele) {
+        this.stackControl.push(ele);
+    }
+
+    pop() { }
+
+    isEmpty() {
+        if (this.stackControl.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    canPush() {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+
