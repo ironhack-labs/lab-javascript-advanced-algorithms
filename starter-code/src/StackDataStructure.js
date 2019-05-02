b06b0cb99db9f258d46057ba1e2ac0a6ebce1550
Mariel ML <marielml@MacBook-Pro-de-Mariel.local>
2019-05-02 11:05:19 +0200

    no me ha dado tiempo

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor(stack) {
+    this.MAX_SIZE = 100;
+    this.stackControl = [];
+  }
+  push(ele) {
+    this.stackControl.push(ele);
+  }
+
+  pop() {}
+
+  isEmpty() {
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
