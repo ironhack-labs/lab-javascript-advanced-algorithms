ca7aea7402eaeecd1014f3bbda64ad9d96950277
Juan Perez Gago <juan.pgago@gmail.com>
2019-05-02 09:48:11 +0200

    Done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,87 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  const stack = new StackDataStructure()
+  const textInput = document.getElementById('input')
+
+  const buttonAdd = document.getElementById('add')
+  const buttonTake = document.getElementById('take')
+
+
+  // Creacion Stack Container
+  introduceDiv(stack.MAX_SIZE)
+
+
+  // Captura los elementos con clase row
+  const elementsCelda = document.getElementsByClassName('row')
+
+
+  // let currentElementPos = stack.MAX_SIZE - 1
+  let currentElementPos = elementsCelda.length - 1
+
+
+
+
+
+  // Evento Add
+
+  buttonAdd.onclick = () => {
+
+    const elementPushed = stack.push(textInput.value)
+    if (elementPushed === 'Stack Overflow') {
+      alert(elementPushed)
+      return
+    }
+
+    elementsCelda[currentElementPos].innerHTML = textInput.value
+    elementsCelda[currentElementPos].className = 'row elementStack'
+
+    currentElementPos--
+  }
+
+
+  // Evento Take
+
+  const currentStackPosition = 0
+
+  buttonTake.onclick = () => {
+
+    const elementsStack = document.getElementsByClassName('elementStack')
+
+    const elementTaked = stack.pop()
+    if (elementTaked === 'Stack Underflow') {
+      alert(elementTaked)
+      return
+    }
+
+    elementsCelda[currentElementPos + 1].innerHTML = ''
+    elementsCelda[currentElementPos + 1].className = 'row celda'
+
+    currentElementPos++
+
+    // elementsStack[currentStackPosition].innerHTML = ''
+    // elementsStack[currentStackPosition].className = 'celda'
+
+  }
+
+})
+
+
+
+function introduceDiv(numeroDivs) {
+  // let html = `<div class="row celda"></div>`
+  const divContainer = document.getElementById('container')
+
+  // le aplica la clase col-2
+
+
+  for (let i = 0; i < numeroDivs; i++) {
+    // divContainer.innerHTML = html;
+    // console.log("a")
+
+    let html = document.createElement('div')
+    html.setAttribute('class', 'row celda')
+    divContainer.appendChild(html)
+  }
+
+
+}
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+class StackDataStructure {
+
+  constructor() {
+    this.stackControl = []
+    this.MAX_SIZE = 10
+  }
+
+  isEmpty() {
+    return (this.stackControl.length === 0)
+  }
+
+  canPush() {
+    return (this.stackControl.length != this.MAX_SIZE)
+  }
+
+  push(element) {
+    if (this.canPush()) {
+      this.stackControl.push(element)
+      return this.stackControl
+
+    } else {
+      return 'Stack Overflow'
+    }
+  }
+
+  pop(element) {
+    if (!this.isEmpty())
+      return this.stackControl.pop(element)
+
+    else return "Stack Underflow"
+  }
+
 }
+
