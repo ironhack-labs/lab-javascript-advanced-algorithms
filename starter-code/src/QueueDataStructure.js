e55bdea227c55041a3bf6d14c38448f5cbaa1ec9
David H <iq.davidh@gmail.com>
2019-05-24 23:34:01 -0500

    avance 12 failures

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+
+    construnctor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 100;
+    }
+
+
+    isEmpty(){
+        return this.queueControl.length===0;
+    }
+
+    canEnqueue(){
+
+    }
+
+    enqueue(){
+
+    }
+
+    dequeue(){
+
+    }
+
 }
