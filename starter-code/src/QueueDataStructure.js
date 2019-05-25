73f3ce686adff3777b5339ca436505b3b6071b40
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-05-24 20:37:48 -0500

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,47 @@
 function QueueDataStructure () {
+
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+  this.items = 0;
+
 }
+
+QueueDataStructure.prototype.isEmpty = function () {
+  if(this.items < 1){
+    return true;
+  }
+  return false;
+}
+
+QueueDataStructure.prototype.canEnqueue = function () {
+  if(this.items < this.MAX_SIZE){
+    return true;
+  }
+  return false;
+}
+
+
+QueueDataStructure.prototype.enqueue = function (e) {
+  this.queueControl.unshift(e)
+  this.items = this.queueControl.length;
+
+  if(this.items > this.MAX_SIZE){ return 'Queue Overflow' }
+  
+  return this.queueControl
+}
+
+QueueDataStructure.prototype.dequeue = function () {
+
+  let willBeDequeued = this.queueControl[this.queueControl.length - 1];
+  this.queueControl.pop()
+  this.items = this.queueControl.length;
+
+  if(this.items < 1){ return 'Queue Underflow' }
+
+  return willBeDequeued;
+
+}
+
+let queue = new QueueDataStructure();
+
+console.log(queue.isEmpty())
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,41 @@
 function StackDataStructure () {
+
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+    this.items = 0;
+  }
+
+StackDataStructure.prototype.isEmpty = function () {
+  if(this.items < 1){
+    return true;
+  }
+  return false;
 }
+
+StackDataStructure.prototype.canPush = function () {
+  if(this.items < this.MAX_SIZE){
+    return true;
+  }
+  return false;
+}
+
+
+StackDataStructure.prototype.push = function (e) {
+  this.stackControl.push(e)
+  this.items = this.stackControl.length;
+
+  if(this.items > this.MAX_SIZE){ return 'Stack Overflow' }
+  
+  return this.stackControl
+}
+
+StackDataStructure.prototype.pop = function () {
+  let willBePopped = this.stackControl[this.stackControl.length - 1];
+  this.stackControl.pop()
+  this.items = this.stackControl.length;
+
+  if(this.items < 1){ return 'Stack Underflow' }
+
+  return willBePopped;
+}
+
