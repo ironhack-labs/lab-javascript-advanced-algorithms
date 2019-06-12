e48e667a051c727dbbcd72a5385fe854ed939338
Miriloper <miriam.mad.ml@gmail.com>
2019-06-12 22:09:15 +0200

    almost finish jasmine

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
+var queue = new QueueDataStructure();
+
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+
+  this.isEmpty = function() {
+    return this.queueControl.length === 0;
+  };
+
+  this.canEnqueue = function() {
+    return this.queueControl.length < this.MAX_SIZE;
+  };
+
+  this.enqueue = function(elemQueue) {
+    if (this.canEnqueue()) {
+    this.queueControl.unshift(elemQueue);
+    return this.queueControl;
+    }
+    else {
+      return "Stack Overflow";
+    }
+  };
+
+  this.dequeue = function(elemQueue) {
+    if (!this.isEmpty()) {
+      return this.queueControl.pop(elemQueue);
+    } 
+    else {
+      return "Queue Underflow";
+    }
+  };
 }
