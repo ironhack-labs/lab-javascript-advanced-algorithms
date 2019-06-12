c8f2e38b0fe54a6f01c2e9b61887c2926b792be2
Francho Garcia <franchog79@gmail.com>
2019-06-12 19:26:20 +0200

    Jasmine finished

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = []
+    this.MAX_SIZE = 10
+    this.isEmpty = function() {
+        if (this.queueControl.length == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    this.canEnqueue = function() {
+        if (this.queueControl.length < this.MAX_SIZE) {
+            return true
+        } else {
+            return false
+        }
+    }
+    this.enqueue = function(newItem) {
+        if (this.queueControl.length == this.MAX_SIZE) {
+            return 'Queue Overflow'
+        } else {
+            this.queueControl.unshift(newItem)
+            return this.queueControl
+        }
+    }
+    this.dequeue = function() {
+        if (this.queueControl.length == 0) {
+            return "Queue Underflow";
+        } else {
+            return this.queueControl.pop();
+        }
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.MAX_SIZE = 10
+    this.stackControl = []
+    this.isEmpty = function() {
+        return (this.stackControl.length == 0)
+    }
+    this.canPush = function() {
+        if (this.stackControl.length == this.MAX_SIZE) {
+            return false
+        } else {
+            return true
+        }
+
+    }
+    this.push = function(newItem) {
+        if (this.stackControl.length == this.MAX_SIZE) {
+            return 'Stack Overflow'
+        } else {
+            this.stackControl.push(newItem)
+            return this.stackControl
+        }
+    }
+    this.pop = function() {
+        if (this.stackControl.length == 0) {
+            return 'Stack Underflow'
+        } else {
+            return this.stackControl.pop()
+        }
+
+    }
 }
+f
