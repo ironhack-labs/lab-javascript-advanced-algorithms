ddd74e82fc079a6da01dec566e785313319b24cf
MicaelZV <micaelzv@gmail.com>
2019-06-12 23:42:04 +0200

    llegue a intentar lanzar las funciones del obj pero Uncaught todo el rato...

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,37 @@
-function QueueDataStructure () {
+
+
+stack = new QueueDataStructure();
+
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function() {
+    if (this.queueControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.canEnqueue = function() {
+    if (this.queueControl < this.MAX_SIZE) {
+      return true;
+    } 
+      return false;
+    
+  };
+
+  this.enqueue = function (x) {
+    if (this.queueControl.length === this.MAX_SIZE) {
+      return "Queue Overflow"
+    } else if (this.queueControl.length < this.MAX_SIZE) {
+      this.queueControl.unshift(x);
+      return this.queueControl;
+    }
+  }
+  this.dequeue = function () {
+    if (this.queueControl.length === 0) {
+      return "Queue Underflow"
+    }
+    return this.queueControl.pop();
+   }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
-function StackDataStructure () {
+stack = new StackDataStructure();
+
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.canPush = function() {
+    if (this.stackControl < this.MAX_SIZE) {
+      return true;
+    }
+    return false;
+  };
+
+  this.push = function(x) {
+    
+    if (this.stackControl.length === this.MAX_SIZE) {
+      console.log("Stack Overflow");
+    } else if (this.stackControl.length < this.MAX_SIZE) {
+      this.stackControl.push(x);
+      console.log("Stack Overflow");
+      return this.stackControl;
+    }
+  };
+  this.pop = function() {
+    if (this.stackControl.length === 0) {
+      return "Stack Underflow";
+    }
+    return this.stackControl.pop();
+  };
 }
+var AddClick = document.querySelector("#addButton");
+
+AddClick.addEventListener("click", function() {
+  var formInput = document.querySelector("#inputValue2").value;
+  stack.stackControl=1;
+  stack.call.push(1);
+ console.log(stack.stackControl)
+ console.log(stack.MAX_SIZE)
+
+  document.querySelector("#htmlText").innerHTML = formInput;
+});
