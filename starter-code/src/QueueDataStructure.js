c8d34a7801d2044fb2f414690ae89d99d131bd02
root <root@DESKTOP-COVV5F4.localdomain>
2019-06-12 22:15:03 +0200

    ma muero

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,43 @@
 function QueueDataStructure () {
+    
+    this.queueControl = [] 
+    this.MAX_SIZE = 8
+   
+
+this.isEmpty = function (){
+   
+       return this.queueControl.length === 0
+   }
+
+
+this.canEnqueue =function(){
+    return this.queueControl.length < this.MAX_SIZE
+   
+    }
+
+   
+this.enqueue = function (queueElement){
+    if(this.canEnqueue()){
+     this.queueControl.unshift(queueElement)
+     return this.queueControl
+    }
+    else{
+        return "Queue Overflow"
+    }
+
+
+
+}
+
+
+this.dequeue=function(queueElement){
+        if(this.isEmpty()){
+            return "Queue Underflow"
+        }
+        else{
+          return  this.queueControl.pop(queueElement)
+        } 
+}
+
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,48 @@
 function StackDataStructure () {
+    
+            this.stackControl = [] 
+            this.MAX_SIZE = 8
+           
+        
+        this.isEmpty = function (){
+           
+               return this.stackControl.length === 0
+           }
+        
+
+        this.canPush =function(){
+            return this.stackControl.length < this.MAX_SIZE
+           
+            }
+    
+           
+        this.push = function (stackElement){
+            if(this.canPush()){
+             this.stackControl.push(stackElement)
+             return this.stackControl
+            }
+            else{
+                return "Stack Overflow"
+            }
+
+
+
+        }
+        
+        
+        this.pop=function(stackElement){
+                if(this.isEmpty()){
+                    return "Stack Underflow"
+                }
+                else{
+                  return  this.stackControl.pop(stackElement)
+                } 
+        }
+
+        
+        
+        
 }
+
+
+  
