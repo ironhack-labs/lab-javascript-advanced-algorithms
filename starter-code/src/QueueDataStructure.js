25a9eeccc001ec6df65d46696487f9eed450a6ab
carlos.trujillo <carlos@riverettecom>
2019-06-12 19:58:12 +0200

    he terminado el jasmine

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
+  this.queueControl  = []
+  this.MAX_SIZE = 8
+
+  this.isEmpty = function () {
+    return this.queueControl.length === 0
+  }
+  this.canEnqueue = function () {
+     return this.queueControl.length < this.MAX_SIZE
+      
+    
+    
+  }
+   this.enqueue = function (item) {
+    //  this.stackControl.push()
+    //  return stackControl
+    if (!this.canEnqueue()) {
+       return 'Queue Overflow'
+      
+    } else {
+       this.queueControl.unshift(item)
+      return this.queueControl
+    }
+   }
+
+   this.dequeue = function (element) {
+     //  return stackControl
+     if (this.isEmpty()) {
+       
+       
+      return 'Queue Underflow'
+     
+   } else {
+     return this.queueControl.pop(element)
+     
+   }
+   }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,85 @@
 function StackDataStructure () {
+
+  this.stackControl  = []
+  this.MAX_SIZE = 8
+
+  this.isEmpty = function () {
+    return this.stackControl.length === 0
+  }
+  this.canPush = function () {
+     return this.stackControl.length < this.MAX_SIZE
+      
+    
+    
+  }
+   this.push = function (item) {
+    //  this.stackControl.push()
+    //  return stackControl
+    if (!this.canPush()) {
+       return 'Stack Overflow'
+      
+    } else {
+       this.stackControl.push(item)
+      return this.stackControl
+    }
+   }
+
+   this.pop = function (element) {
+     //  return stackControl
+     if (this.isEmpty()) {
+       
+       
+      return 'Stack Underflow'
+     
+   } else {
+     return this.stackControl.pop(element)
+     
+   }
+   }
+  
+
+  
+
 }
+
+
+  this.queueControl  = []
+  this.MAX_SIZE = 8
+
+  this.isEmpty = function () {
+    return this.queueControl.length === 0
+  }
+  this.canPush = function () {
+     return this.queueControl.length < this.MAX_SIZE
+      
+    
+    
+  }
+   this.push = function (item) {
+    //  this.stackControl.push()
+    //  return stackControl
+    if (!this.canPush()) {
+       return 'Stack Overflow'
+      
+    } else {
+       this.stackControl.push(item)
+      return this.stackControl
+    }
+   }
+
+   this.pop = function (element) {
+     //  return stackControl
+     if (this.isEmpty()) {
+       
+       
+      return 'Stack Underflow'
+     
+   } else {
+     return this.stackControl.pop(element)
+     
+   }
+   }
+  
+
+  
+
