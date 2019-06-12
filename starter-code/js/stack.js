d94f1e3818446dd5fe8c237911c82c89a9b5cc33
rubander <ralexanderv89@gmail.com>
2019-06-13 00:04:05 +0200

    logic done, DOM not

---
+++ starter-code/js/stack.js
@@ -0,0 +1,4 @@
+const stack = new StackDataStructure
+let stackContainer = document.getElementById('wrapper')
+console.log(stackContainer)
+console.log(stack.MAX_SIZE)
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() { this.stackControl =[]
+    this.MAX_SIZE = 8};
+
+    isEmpty() {
+      return this.stackControl.length === 0
+    } 
+
+    canPush() {
+      return this.stackControl.length < this.MAX_SIZE
+    } 
+
+    push(item) {
+
+      if (!this.canPush()) { 
+      return  'Stack Overflow'
+    } else {
+      this.stackControl.push(item)
+      return this.stackControl
+    }  
+    } 
+
+    pop(element) {
+
+      if (this.isEmpty()){ 
+        return  'Stack Underflow'
+      } else{
+       return this.stackControl.pop(element)
+      } 
+    } 
+
+
+    
 }
