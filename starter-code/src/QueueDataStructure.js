4318edeb9ed966315eee96e6869eefed373caf80
Javi <javiersalas@outlook.com>
2019-06-14 08:15:47 +0200

    In progress

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,35 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor(){
+    this.queueControl = []
+    this.MAX_SIZE = 8
+  }
+  
+  isEmpty(){
+    return this.queueControl.length === 0
+  }
+  canEnqueue(){
+    if(this.queueControl.length == this.MAX_SIZE){
+    return false
+    }
+    return true
+  }
+
+  enqueue(element){
+    if(this.queueControl.length == this.MAX_SIZE){
+      return "Queue Overflow"
+    }
+    else{
+      this.queueControl.unshift(element)
+      return this.queueControl
+    }
+  }
+  dequeue(element){
+    if(!this.isEmpty()){
+      return this.queueControl.shift(element)
+    }
+    else{
+      return "Queue Underflow"
+    }
+  }
+  
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,37 @@
-function StackDataStructure () {
+
+class StackDataStructure {
+  constructor(){
+    this.stackControl = []
+    this.MAX_SIZE = 10
+  }
+  isEmpty(){
+    return this.stackControl.length === 0
+   
+  }
+  canPush(){
+    if(this.stackControl.length == this.MAX_SIZE){
+    return false
+    }
+    return true
+  }
+
+  push(element){
+    if(this.stackControl.length == this.MAX_SIZE){
+      return "Stack Overflow"
+    }
+    else{
+      this.stackControl.push(element)
+      return this.stackControl
+    }
+  }
+
+  pop(element){
+    if(!this.isEmpty()){
+      return this.stackControl.pop(element)
+    }
+    else{
+      return "Stack Underflow"
+    }
+  }
+  
 }
