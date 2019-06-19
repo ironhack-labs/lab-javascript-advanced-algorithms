36bbec63e49c23f68f1082855d22f69b82b445ce
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-06-19 10:50:15 -0300

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,28 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.MAX_SIZE = 10;
+		this.stackControl = [];
+		this.size = this.stackControl.length;
+  }
+
+  isEmpty() {
+		return (this.size === 0);
+  }
+
+  canPush() {
+		return (this.size < this.MAX_SIZE);
+  }
+
+  push(item) {
+		if (!this.canPush()) return 'Stack Overflow';
+		this.stackControl.push(item)
+		this.size += 1;
+		return this.stackControl;
+	}
+	
+	pop() {
+		if (this.isEmpty()) return 'Stack Underflow';
+		this.size -= 1;
+		return this.stackControl.pop();
+	}
 }
