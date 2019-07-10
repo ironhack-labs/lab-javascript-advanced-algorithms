e7b792493e54e82257217b5b1ac7831603f73ef3
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-11 00:09:47 +0200

    stackAndQueue

---
+++ starter-code/js/stack.js
@@ -0,0 +1,9 @@
+let stack = new StackDataStructure()
+addButton = document.getElementById("button-add")
+
+
+addButton.onclick = () => {
+    let stackedElms = document.getElementsByClassName("stacked-elms")
+    console.log(isEmpty())
+}
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 8
+    }
+
+    isEmpty() {
+        if (this.stackControl.length == 0) { return true }
+        else { return false }
+    }
+
+    canPush() {
+        if (this.stackControl.length < this.MAX_SIZE) { return true }
+        else { return false }
+
+    }
+
+    push(x) {
+        if (this.stackControl.length == this.MAX_SIZE) {
+            return 'Stack Overflow'
+        }
+        this.stackControl.push(x)
+        return this.stackControl
+    }
+
+    pop(x) {
+        if (this.stackControl.length == 0) {
+            return 'Stack Underflow'
+        }
+        return this.stackControl.pop(x)
+
+    }
 }
