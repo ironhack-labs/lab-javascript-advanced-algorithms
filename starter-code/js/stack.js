4d74dc89bfa69f7aa4b47af09ce7615369ccb19f
ckompier <kompi14@hotmail.com>
2019-07-11 10:38:28 +0200

    done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,20 @@
+var add     = document.getElementById('add-buttom');
+var take    = document.getElementById('take-buttom');
+var elemen = document.getElementById("add-element-container") 
+var arr = document.getElementById ("containers")
+
+
+
+add.onclick = () => {
+   
+    push ()
+            }
+
+take.onclick = () => {
+
+   pop ()
+
+
+
+}
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,50 @@
-function StackDataStructure () {
-}
+class StackDataStructure {
+
+        constructor (stackControl, MAX_SIZE) { 
+            this.stackControl = [];
+            this.MAX_SIZE = 8; 
+        }
+
+        isEmpty () {
+            if (this.stackControl.length === 0) {return true
+        } else {
+            return false 
+        }
+          }
+
+
+        canPush () {
+            if (this.stackControl.length < this.MAX_SIZE ) {
+                return true
+        }  else {
+            return false
+        }
+      }
+     
+
+        push (elm) {
+            if (this.stackControl.length === this.MAX_SIZE) 
+            {  return "Stack Overflow" 
+        } else {
+
+             this.stackControl.push(elm)
+             return this.stackControl
+        }
+      }
+
+        pop (elm) {
+            if (this.stackControl.length === 0)
+            { return "Stack Underflow"
+            
+            } else {
+                return this.stackControl.pop(elm)
+            }
+            
+        }
+
+
+    
+
+
+
+    }
