43eb27404d697620e15e5d5b40f3e77c7621d883
Inma <inmanewmail@gmail.com>
2019-07-11 09:30:53 +0200

    con las funciones en el stack.js

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -37,20 +37,3 @@ class StackDataStructure {
   }
 }
 
-const myStack = new StackDataStructure
-
-let addButton = document.getElementById('addBtn')
-let takeButton = document.getElementById('takeBtn')
-let inputCamp = document.getElementById('input')
-let arrP = document.getElementsByTagName('p')
-console.log(arrP) //hagon esto y veo que está cogida
-addButton.onclick = () => {
-myStack.push(inputCamp.value)
-console.log(myStack.stackControl) // Va ok
-// let reverse = myStack.stackControl.reverse()
-// console.log(reverse)
-let insertMe = myStack.stackControl.indexOf(inputCamp.value)
-console.log(insertMe) // ok entra
-arrP[7-insertMe].innerHTML = inputCamp.value
-arrP[7-insertMe].className = 'blueCamp'
-}
