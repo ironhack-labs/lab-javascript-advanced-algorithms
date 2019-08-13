e0f42b237169dd3a85cab15844c693fc91b1c78b
elwiiman <emmanuel.sanro@gmail.com>
2019-08-13 00:01:50 -0500

    Stack class completed

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,45 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 0;
+  }
+
+  isEmpty() {
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  canPush() {
+    if (
+      this.stackControl.length < this.MAX_SIZE ||
+      this.stackControl.length == 0
+    ) {
+      console.log("es pusheable");
+      return true;
+    } else {
+      console.log("noes pusheable");
+      return false;
+    }
+  }
+
+  push(x) {
+    if (this.canPush()) {
+      this.stackControl.push(x);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    } else {
+      let lastElement = this.stackControl.pop();
+      return lastElement;
+    }
+  }
 }
