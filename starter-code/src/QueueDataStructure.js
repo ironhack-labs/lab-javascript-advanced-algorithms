ae04257f86ad5b4adbb3e98e23f53b2e924450e0
Andres Weber <andresmweber@gmail.com>
2019-08-24 16:29:07 -0400

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,25 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+    this.queueControl = []
+    this.MAX_SIZE = 32
+
+    this.isEmpty = function () {
+        return !Boolean(this.queueControl.length)
+    }
+
+    this.canEnqueue = function () {
+        return this.queueControl.length !== this.MAX_SIZE
+    }
+
+    this.dequeue = function (element) {
+        if (!this.isEmpty()) return this.queueControl.pop()
+        else return 'Queue Underflow'
+    }
+
+    this.enqueue = function (element) {
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(element)
+            return this.queueControl
+        }
+        else return 'Queue Overflow'
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,25 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.stackControl = []
+    this.MAX_SIZE = 32
+
+    this.isEmpty = function () {
+        return !Boolean(this.stackControl.length)
+    }
+
+    this.canPush = function () {
+        return this.stackControl.length !== this.MAX_SIZE
+    }
+
+    this.pop = function (element) {
+        if (!this.isEmpty()) return this.stackControl.pop()
+        else return 'Stack Underflow'
+    }
+
+    this.push = function (element) {
+        if (this.canPush()) {
+            this.stackControl.push(element)
+            return this.stackControl
+        }
+        else return 'Stack Overflow'
+    }
 }
