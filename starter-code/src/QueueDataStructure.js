1e6d9da76298f67f063feb7dadd1417875b41ce3
pauromeropau <pauromeropau@gmail.com>
2019-08-28 21:00:43 +0200

    Second iteration

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,23 @@
 function QueueDataStructure () {
+  this.queueControl= [];
+  this.MAX_SIZE = 10;
+
+this.isEmpty = function() {
+    return this.queueControl.length == 0;
 }
+this.canEnqueue = function() {
+  return this.MAX_SIZE > this.queueControl.length;
+};
+this.enqueue= function(element) {
+  if (this.canEnqueue()) {
+    this.queueControl.unshift(element);
+    return this.queueControl;
+  } return "Queue Overflow";
+};
+this.dequeue = function(element) {
+  if (!this.isEmpty()){
+  return this.queueControl.pop();
+  } return "Queue Underflow";
+}
+}
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,20 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+  this.isEmpty = function() {
+    return this.stackControl.length == 0;
+  };
+  this.canPush = function() {
+    return this.MAX_SIZE > this.stackControl.length;
+  };
+  this.push = function(element) {
+    if (this.canPush()) {
+      this.stackControl.push(element);
+      return this.stackControl;
+    } else return "Stack Overflow";
+  };
+  this.pop = function(element) {
+    if (!this.isEmpty()) return this.stackControl.pop();
+    else return "Stack Underflow";
+  };
 }
