65e984da4d79f875bb7e4a91d4b7836a562c62db
“Natalia <natgarea@gmail.com>
2019-08-28 23:49:33 +0200

    iteration2

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+function QueueDataStructure() {
+  this.queueControl = [];
+  this.MAX_SIZE = 8;
+
+  this.isEmpty = function() {
+    return this.queueControl.length == 0;
+  };
+
+  this.canEnqueue = function() {
+    return this.queueControl.length < this.MAX_SIZE;
+  };
+
+  this.enqueue = function(arg) {
+    if (this.canEnqueue()) {
+        this.queueControl.unshift(arg);
+        return this.queueControl;
+      } else {
+        return "Queue Overflow";
+      }
+  };
+
+  this.dequeue = function() {
+      if (!this.isEmpty) {
+        return this.queueControl.shift();
+      } else {
+        return "Queue Underflow";
+      }
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,29 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 8;
+
+  this.isEmpty = function() {
+    return this.stackControl.length == 0;
+  };
+
+  this.canPush = function() {
+    return this.stackControl.length < this.MAX_SIZE;
+  };
+
+  this.push = function(arg) {
+    if (this.canPush()) {
+      this.stackControl.push(arg);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  };
+
+  this.pop = function() {
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      return "Stack Underflow";
+    }
+  };
 }
