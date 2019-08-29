cbf90af800766605cbbb18d59a5a84583ca559d8
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-08-29 09:33:51 +0200

    No interactive buttons

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
-function QueueDataStructure () {
+
+class QueueDataStructure {
+  constructor() {
+    this.queueControl = [];
+    this.MAX_SIZE = 12;
+  }
+
+  isEmpty() {
+    return !Boolean(this.queueControl.length)
+  }
+
+  canEnqueue() {
+    return this.queueControl.length !== this.MAX_SIZE
+  }
+
+  dequeue(element) {
+    return (!this.isEmpty()) ? this.queueControl.pop() : 'Queue Underflow'
+  }
+
+  enqueue(element) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(element)
+      return this.queueControl
+    }
+    else return 'Queue Overflow'
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+  }
+
+  isEmpty() {
+    return !Boolean(this.stackControl.length);
+  }
+
+  canPush() {
+    return this.stackControl.length !== this.MAX_SIZE;
+  }
+
+  pop(element) {
+      if (!this.isEmpty()) return this.stackControl.pop();
+      else return 'Stack Underflow'
+  }
+
+  push(element) {
+      if (this.canPush()) {
+          this.stackControl.push(element)
+          return this.stackControl
+      }
+      else return 'Stack Overflow'
+  }
 }
