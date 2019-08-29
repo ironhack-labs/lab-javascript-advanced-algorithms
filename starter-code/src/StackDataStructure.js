134f1c2ec741b2ce34a908a4fed33047c90d3437
JesusSP <jesus.saints@gmail.com>
2019-08-29 09:29:19 +0200

    v0.1 Código del Stack Column

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,26 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor () {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+  }
+
+  isEmpty () {
+    return this.stackControl.length === 0 ? true : false;
+  }
+
+  canPush () {
+    return this.stackControl.length < this.MAX_SIZE ? true : false;
+  }
+  push (e) {
+    if (this.canPush() === true) {
+      this.stackControl.push(e);
+      return this.stackControl;
+    } else {
+      return 'Stack Overflow';
+    }
+  }
+
+  pop () {
+    return this.stackControl.length === 0 ? 'Stack Underflow' : this.stackControl.pop();
+  }
 }
