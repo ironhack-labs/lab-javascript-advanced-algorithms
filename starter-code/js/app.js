ffc6175a51f96d947cdc23420b1acb1c34ba7b35
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-08-29 09:27:54 +0200

    Add some encapsulation to the JS and edit some CSS styles

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,50 +1,78 @@
-const handleAddStackButtonClick = (stack, element, stackBodyDOMEl) => {
+const handleAddStackButtonClick = (
+  stack,
+  element,
+  stackBodyDOMEl,
+  stackPainter
+) => {
   let itemAdded = addStackElement(stack, element);
 
   if (itemAdded) {
-    stackPainter.addElement(element)
-    stackPainter.message(stackBodyDOMEl.querySelector(".structure-row"), `${element} added!`)
+    stackPainter.addElement(element);
+    stackPainter.message(
+      stackBodyDOMEl.querySelector(".structure-row"),
+      `${element} added!`
+    );
   } else {
-    stackPainter.message(stackBodyDOMEl.querySelector(".structure-row"), "Stack Overflowed")
+    stackPainter.message(
+      stackBodyDOMEl.querySelector(".structure-row"),
+      "Stack Overflowed"
+    );
   }
 };
 
-const addStackElement = (stack, element) => stack.push(element) !== "Stack Overflow" ? true : false;
+const addStackElement = (stack, element) =>
+  stack.push(element) !== "Stack Overflow" ? true : false;
 
-const handleTakeStackButtonClick = (stack, stackDOMEl) => {
+const handleTakeStackButtonClick = (stack, stackDOMEl, stackPainter) => {
   if (takeStackElement(stack)) {
-    stackPainter.deleteElement(stackDOMEl, )
-  }
-
-  if (stack.isEmpty()) stackPainter.message(stackDOMEl.querySelector('.structure-row'), 'Stack Underlow')
+    stackPainter.deleteElement(stackDOMEl);
+  } else
+    stackPainter.message(
+      stackDOMEl.querySelector(".structure-row"),
+      "Stack Underlow"
+    );
 };
 
 const takeStackElement = stack =>
   stack.pop() !== "Stack Underflow" ? true : false;
 
-const handleAddQueueButtonClick = (queue, element, queueBodyDOMEl) => {
+const handleAddQueueButtonClick = (
+  queue,
+  element,
+  queueBodyDOMEl,
+  queuePainter
+) => {
   let queueItemAdded = addQueueElement(queue, element);
-  
+
   if (queueItemAdded) {
-    queuePainter.addElement(element)
-    queuePainter.message(queueBodyDOMEl.querySelector(".structure-row"), `${element} added!`)
+    queuePainter.addElement(element);
+    queuePainter.message(
+      queueBodyDOMEl.querySelector(".structure-row"),
+      `${element} added!`
+    );
   } else {
-    queuePainter.message(queueBodyDOMEl.querySelector(".structure-row"), "Queue Overflowed")
+    queuePainter.message(
+      queueBodyDOMEl.querySelector(".structure-row"),
+      "Queue Overflowed"
+    );
   }
 };
 
 const addQueueElement = (queue, element) =>
   queue.enqueue(element) !== "Queue Overflow" ? true : false;
 
-const handleTakeQueueButtonClick = (queue, queueDOMEl) => {
+const handleTakeQueueButtonClick = (queue, queueDOMEl, queuePainter) => {
   if (takeQueueElement(queue)) {
-    queuePainter.deleteElement(queueDOMEl)
-  }
-
-  if (queue.isEmpty()) queuePainter.message(queueDOMEl.querySelector('.structure-row'), 'Queue Underlow')
+    queuePainter.deleteElement(queueDOMEl);
+  } else
+    queuePainter.message(
+      queueDOMEl.querySelector(".structure-row"),
+      "Queue Underlow"
+    );
 };
 
-const takeQueueElement = queue => queue.dequeue() !== 'Queue Underflow' ? true : false
+const takeQueueElement = queue =>
+  queue.dequeue() !== "Queue Underflow" ? true : false;
 
 const preventFormsToBeSubmited = forms =>
   [...forms].forEach(form =>
@@ -53,38 +81,58 @@ const preventFormsToBeSubmited = forms =>
 
 // --------------------------------
 
-const stackDOMEl = document.querySelector(".structure-body-stack");
-const queueDOMEl = document.querySelector(".structure-body-queue");
+const init = () => {
+  const stackDOMEl = document.querySelector(".structure-body-stack");
+  const queueDOMEl = document.querySelector(".structure-body-queue");
 
-let stack = new StackDataStructure();
-let queue = new QueueDataStructure();
-let stackPainter = new PaintStackStructure(stackDOMEl)
-let queuePainter = new PaintQueueStructure(queueDOMEl)
+  let stack = new StackDataStructure();
+  let queue = new QueueDataStructure();
+  let stackPainter = new PaintStackStructure(stackDOMEl);
+  let queuePainter = new PaintQueueStructure(queueDOMEl);
 
-const buttons = [...document.querySelectorAll(".button")];
+  const buttons = [...document.querySelectorAll(".button")];
 
-const addStackElementDOMEl = buttons[0];
-const takeStackElementDOMEl = buttons[1];
-const addQueueElementDOMEl = buttons[2];
-const takeQueueElementDOMEl = buttons[3];
+  const addStackElementDOMEl = buttons[0];
+  const takeStackElementDOMEl = buttons[1];
+  const addQueueElementDOMEl = buttons[2];
+  const takeQueueElementDOMEl = buttons[3];
 
-const addStackElementInputDOMEl = document.getElementById("stack-added-value");
-const addQueueElementInputDOMEl = document.getElementById("queue-added-value");
+  const addStackElementInputDOMEl = document.getElementById(
+    "stack-added-value"
+  );
+  const addQueueElementInputDOMEl = document.getElementById(
+    "queue-added-value"
+  );
 
-preventFormsToBeSubmited(document.forms);
+  preventFormsToBeSubmited(document.forms);
 
-addStackElementDOMEl.addEventListener("click", e => {
-  handleAddStackButtonClick(stack, addStackElementInputDOMEl.value, stackDOMEl);
-});
+  addStackElementDOMEl.addEventListener("click", e => {
+    handleAddStackButtonClick(
+      stack,
+      addStackElementInputDOMEl.value,
+      stackDOMEl,
+      stackPainter
+    );
+    addStackElementInputDOMEl.value = "";
+  });
 
-addQueueElementDOMEl.addEventListener("click", e => {
-  handleAddQueueButtonClick(queue, addQueueElementInputDOMEl.value, queueDOMEl)
-});
+  addQueueElementDOMEl.addEventListener("click", e => {
+    handleAddQueueButtonClick(
+      queue,
+      addQueueElementInputDOMEl.value,
+      queueDOMEl,
+      queuePainter
+    );
+    addQueueElementInputDOMEl.value = "";
+  });
 
-takeStackElementDOMEl.addEventListener("click", e => {
-  handleTakeStackButtonClick(stack, stackDOMEl);
-});
+  takeStackElementDOMEl.addEventListener("click", e => {
+    handleTakeStackButtonClick(stack, stackDOMEl, stackPainter);
+  });
 
-takeQueueElementDOMEl.addEventListener("click", e => {
-  handleTakeQueueButtonClick(queue, queueDOMEl)
-});
+  takeQueueElementDOMEl.addEventListener("click", e => {
+    handleTakeQueueButtonClick(queue, queueDOMEl, queuePainter);
+  });
+};
+
+init();
