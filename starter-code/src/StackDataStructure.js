b3135f60df30670c8c9ac98fbcdbe13a2891b226
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-21 20:57:21 -0300

    made stack a class and clomplted all needed methods

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 10;
+  }
+  isEmpty() {
+    return this.stackControl.length > 0 ? false : true;
+  }
+  canPush() {
+    return this.stackControl.length < this.MAX_SIZE ? true : false;
+  }
+  push(num) {
+    if (this.canPush()) {
+      this.stackControl.push(num);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
+  }
 }
