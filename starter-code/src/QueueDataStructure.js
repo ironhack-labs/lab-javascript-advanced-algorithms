0e0473d6d39dd4e79a3609e8519883411ea684bc
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-21 21:15:08 -0300

    made queue a class and created all needed functions

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor() {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+  }
+  isEmpty() {
+    return this.queueControl.length > 0 ? false : true;
+  }
+  canEnqueue() {
+    return this.queueControl.length < this.MAX_SIZE ? true : false;
+  }
+  enqueue(item) {
+    if(this.canEnqueue()){
+      this.queueControl.unshift(item)
+      return this.queueControl
+    } else {
+      return "Queue Overflow"
+    }
+  }
+  dequeue() {
+    if (this.isEmpty()){
+      return "Queue Underflow"
+    } else {
+      return  this.queueControl[this.queueControl.length-1]
+    }
+  }
 }
