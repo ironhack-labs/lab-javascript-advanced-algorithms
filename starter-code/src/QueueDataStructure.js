fe4eb62bb3ae971ab4b7d6cec13ed1b04b5867cb
Alfredo del Canto <adelcanto@Alfredos-MacBook-Pro.local>
2019-10-30 22:29:08 +0100

    First and Second Iteration - DONE

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,27 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 8;
+    }
+
+    isEmpty() {
+        return (this.queueControl.length === 0) ? true : false;
+    }
+
+    canEnqueue() {
+        return (this.queueControl.length >= this.MAX_SIZE) ? false : true;
+        
+    }
+
+    enqueue(element) {
+        this.queueControl.unshift(element);
+        return (!this.canEnqueue()) ? "Queue Overflow" : this.queueControl;
+    }
+
+    dequeue() {
+        return this.isEmpty() ? "Queue Underflow" : this.queueControl.pop();
+    }
+
+
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
-}
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 8;
+    }
+
+    isEmpty() {
+        return (this.stackControl.length === 0) ? true : false;
+    }
+
+    canPush() {
+        return (this.stackControl.length >= this.MAX_SIZE) ? false : true;
+        
+    }
+
+    push(element) {
+        this.stackControl.push(element);
+        return (!this.canPush()) ? "Stack Overflow" : this.stackControl;
+    }
+
+    pop() {
+        return this.isEmpty() ? "Stack Underflow" : this.stackControl.pop();
+        
+    }
+
+
+}    
