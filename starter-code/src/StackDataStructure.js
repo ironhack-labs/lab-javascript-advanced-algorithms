84f1f90ac4b4df1da3009eeea098d35c58a44840
Flor Urbinati <flor29600@hotmail.com>
2019-10-30 18:58:28 +0100

    iteration 1

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 1;
+    };
+    isEmpty() {
+        if (this.stackControl.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+    canPush() {
+        if (this.stackControl.length === this.MAX_SIZE) {
+            return false;
+        } else return true;
+    };
+    push(val) {
+        this.stackControl.push(val);
+        if (this.stackControl.length > this.MAX_SIZE) {
+            return "Stack Overflow";
+        }
+        return this.stackControl;
+
+    };
+    pop(val) {
+        if (this.stackControl.length === 0) {
+            return 'Stack Underflow';
+        } else
+            return this.stackControl.pop(val)
+
+    };
+
 }
+
