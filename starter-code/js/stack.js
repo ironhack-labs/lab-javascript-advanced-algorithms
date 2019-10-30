53abc857c71987a40408ee145e1bec61ad414eba
María Simó <maria@coponstudio.es>
2019-10-31 00:48:13 +0100

    Implemented logic & dom manipulation of stack structure

---
+++ starter-code/js/stack.js
@@ -0,0 +1,61 @@
+const stack = new StackDataStructure();
+
+const addBtn = document.querySelector("#add-btn");
+const takeBtn = document.querySelector("#take-btn");
+const inputEl = document.querySelector("#text-input");
+let listEl = document.querySelector(".list-group")
+let messageEl = document.createElement("div");
+
+
+addBtn.onclick = function () {
+    let listGroup = [...document.querySelectorAll('.bg-light')];
+    let lastItemOfList = listGroup[listGroup.length - 1];
+
+    if (inputEl.value) {
+        let inputVal = inputEl.value;
+
+        if (stack.canPush()) {
+            stack.push(inputVal);
+            
+            messageEl.classList.remove('alert','alert-warning');
+            messageEl.innerHTML = "";
+
+            lastItemOfList.innerHTML = inputVal;
+            lastItemOfList.classList.remove("bg-light");
+            lastItemOfList.classList.add("bg-secondary");
+
+            listGroup = listGroup.filter(el => el.classList.contains("bg-light"));
+        } else {
+            messageEl.classList.add("alert", "alert-warning");
+            messageEl.innerText = stack.push();
+            listEl.prepend(messageEl)
+        }
+    }
+}
+
+
+
+takeBtn.onclick = function () {
+    let listRemoveGroup = [...document.querySelectorAll('.bg-secondary')];
+    let firstItemOfList = listRemoveGroup[0];
+    
+
+    if (stack.isEmpty()) {
+        messageEl.classList.add("alert", "alert-warning");
+        messageEl.innerText = stack.pop();
+        listEl.append(messageEl)
+    } else {
+        stack.pop();
+        messageEl.classList.remove('alert','alert-warning');
+        messageEl.innerHTML = "";
+
+        firstItemOfList.innerHTML = " ";
+        firstItemOfList.classList.remove("bg-secondary");
+        firstItemOfList.classList.add("bg-light");
+
+        listRemoveGroup.filter(el => el.classList.contains('bg-secondary'))   
+    }
+}
+
+
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 8;
+    }
+
+    isEmpty() {
+        if (this.stackControl.length <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // checks if stack overflows
+    canPush() {
+        if (this.stackControl.length >= this.MAX_SIZE) {
+            return false;
+        } 
+            return true;
+        
+    }
+
+    push(el) {
+
+        if (this.stackControl.length >= this.MAX_SIZE) {
+            return "Stack Overflow"
+        } else { 
+            this.stackControl.push(el);
+            return this.stackControl; 
+        }
+
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return "Stack Underflow";
+        } else {
+            return this.stackControl.pop();
+        }
+    }
 }
+
