a4588a258e3ab4a40c63bc090b62d19cd51ff7c5
gabriel <gab.zafra@gmail.com>
2019-10-30 18:49:57 +0100

    stack Jasmined

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,32 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        return this.stackControl.length <= 0;
+    }
+
+    canPush() {
+        return !(this.stackControl.length >= this.MAX_SIZE);
+    }
+
+    push(item) {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            this.stackControl.push(item);
+            this.isEmpty();
+            return this.stackControl;
+        } else {
+            return `Stack Overflow`;
+        }
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return `Stack Underflow`;
+        } else {
+            return this.stackControl.pop();
+        }
+    }
 }
