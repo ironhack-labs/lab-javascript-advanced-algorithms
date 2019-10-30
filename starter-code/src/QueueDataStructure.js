c524605e298143b86df7e3e330243ee28f1fa57d
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-10-30 19:15:59 +0100

    Casi está terminado crack

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,45 @@
-function QueueDataStructure () {
-}
+class QueueDataStructure {
+    constructor() {
+      this.queueControl = [];
+      this.MAX_SIZE = 9;
+    }
+  
+    isEmpty() {
+      if (this.queueControl == "") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    canEnqueue() {
+      if (this.queueControl.length === this.MAX_SIZE) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  
+    enqueue(newElem) {
+      if (this.queueControl.length === this.MAX_SIZE) {
+        return "Queue Overflow";
+      }
+      this.queueControl.push(newElem);
+  
+      return this.queueControl.reverse();
+    }
+  
+    dequeue(lastElem) {
+      if (this.queueControl.length === 0) {
+          return "Queue Underflow";
+        }
+  
+        let last = this.queueControl.pop(lastElem);
+    
+        return last;
+    }
+  }
+
+
+// function QueueDataStructure () {
+// }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,45 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 9;
+  }
+
+  isEmpty() {
+    if (this.stackControl == "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  canPush() {
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  push(newElem) {
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return "Stack Overflow";
+    }
+    this.stackControl.push(newElem);
+
+    return this.stackControl;
+  }
+
+  pop(lastElem) {
+    if (this.stackControl.length === 0) {
+        return "Stack Underflow";
+      }
+
+      let last = this.stackControl.pop(lastElem);
+  
+      return last;
+  }
 }
+
+// function StackDataStructure () {
+
+// }º
