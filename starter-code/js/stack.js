fa508c04ae18a18404d7850361d457cb349f5425
root <root@DESKTOP-8FSC3E1.localdomain>
2019-10-30 20:15:45 +0100

    Data logic Done

---
+++ starter-code/js/stack.js
@@ -0,0 +1,15 @@
+class StackDataStructure {
+    constructor(){
+        this.stackControl = [];
+        this.MAX_SIZE = 9;
+    }
+    isEmpty(){
+        if (this.stackControl == ''){return true};
+}
+canPush(){
+}
+push(){
+}
+pop(){
+}
+}
