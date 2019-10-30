7054a2ffa91ca01ea687c41d541698952eb501de
Estefania del Castillo Medrano <ecasmed@gmail.com>
2019-10-30 21:26:58 +0100

    first commmit

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,45 @@
-function QueueDataStructure () {
-}
+class QueueDataStructure {
+     constructor() {
+        this.queueControl = []
+        this.MAX_SIZE = 5
+    }
+    
+    isEmpty() {
+    if (this.queueControl.length >= 1){
+        return false
+    }return true
+    
+    }
+    
+    canEnqueue() {
+    if (this.queueControl.length === this.MAX_SIZE){
+        return false
+    }
+    return true
+    }
+    
+    enqueue(element){
+    
+    if (this.queueControl.length === this.MAX_SIZE){ 
+    return 'Queue Overflow'
+    }
+    this.queueControl.unshift(element)
+    
+    return this.queueControl
+    }
+    
+    dequeue(element){
+        if(this.queueControl.length <= 0){
+            return 'Queue Underflow'
+        }else {
+           return  this.queueControl.pop()
+        }
+    
+    
+    }
+    
+    }
+    
+
+    
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,41 @@
-function StackDataStructure () {
+class StackDataStructure  {
+constructor(){
+    this.stackControl = []
+    this.MAX_SIZE = 5
+}
+
+isEmpty(){
+if (this.stackControl.length >= 1){
+    return false
+}return true
+
+}
+
+canPush(){
+if (this.stackControl.length === this.MAX_SIZE){
+    return false
+}
+return true
+}
+
+push(element){
+
+if (this.stackControl.length === this.MAX_SIZE){ 
+return 'Stack Overflow'
+}
+this.stackControl.push(element)
+
+return this.stackControl
+}
+
+pop(element){
+    if(this.stackControl.length <= 0){
+        return 'Stack Underflow'
+    }else {
+       return  this.stackControl.pop()
+    }
+
+
+}
+
 }
