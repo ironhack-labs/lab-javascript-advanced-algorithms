b85f162777fc12145a464d5ef3062b13dc40eb22
F-naranjo <francisco.naranjorodriguez@gmail.com>
2019-10-30 20:10:14 +0100

    Logic implemented, first html structure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,32 @@
-function QueueDataStructure () {
+class QueueDataStructure  {
+    constructor() {
+        this.queueControl = []
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        if (this.queueControl.length >= 1) {
+            return false
+        }
+        return true
+    }
+    canEnqueue() {
+        if (this.queueControl.length === this.MAX_SIZE) { return false }
+        return true
+    }
+
+    enqueue(element) {
+        if (this.queueControl.length === this.MAX_SIZE) {
+            return 'Queue Overflow'
+        }
+        this.queueControl.unshift(element)
+        return this.queueControl
+    }
+
+    dequeue() {
+        if (this.queueControl.length <= 0) {
+            return 'Queue Underflow'
+        }
+        return this.queueControl[this.queueControl.length - 1]
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        if (this.stackControl.length >= 1) {
+            return false
+        }
+        return true
+    }
+    canPush() {
+        if (this.stackControl.length === this.MAX_SIZE) { return false }
+        return true
+    }
+
+    push(element) {
+        if (this.stackControl.length === this.MAX_SIZE) {
+            return 'Stack Overflow'
+        }
+        this.stackControl.push(element)
+        return this.stackControl
+    }
+
+    pop() {
+        if (this.stackControl.length <= 0) {
+            return 'Stack Underflow'
+        }
+        return this.stackControl.pop()
+         
+    }
 }
+
