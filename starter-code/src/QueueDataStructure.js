f13c318893d039e0204f74ad2c352fbf2bf8a65f
Enrique Montaño Ocaña <enrique@Enriques-MacBook-Pro.local>
2019-10-30 19:32:23 +0100

    first commit

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,34 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 9;
+    }
+    isEmpty() {
+        if (this.queueControl == '') {
+            return true
+        } else {
+            return false;
+        }
+    }
+    canEnqueue() {
+        if (this.queueControl.length != this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    enqueue(elem) {
+        if (this.queueControl.length === this.MAX_SIZE) {
+            return 'Queue Overflow';
+        }
+        this.queueControl.push(elem);
+        return this.queueControl.reverse();
+    }
+    dequeue(lastElem) {
+        if (this.queueControl.length === 0) {
+            return 'Queue Underflow';
+        }
+        let lastElement = this.queueControl.pop(lastElem);
+        return lastElement;
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 9;
+    }
+    isEmpty() {
+        if (this.stackControl == '') {
+            return true
+        } else {
+            return false;
+        }
+    }
+    canPush() {
+        if (this.stackControl.length != this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    push(elem) {
+        if (this.stackControl.length === this.MAX_SIZE) {
+            return 'Stack Overflow';
+        }
+        this.stackControl.push(elem);
+        return this.stackControl;   
+    }
+    pop(lastElem) {
+        if(this.stackControl.length === 0) {
+            return 'Stack Underflow';
+        }
+        let lastElement = this.stackControl.pop(lastElem);
+        return lastElement;
+    }
 }
