349dbc1591c185dea2d9f7aef589b94813e2308c
gabriel <gab.zafra@gmail.com>
2019-10-30 19:07:23 +0100

    queue Jasmined

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,33 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        return this.queueControl.length <= 0;
+    }
+
+    canEnqueue() {
+        return this.queueControl.length < this.MAX_SIZE;
+    }
+
+    enqueue(item) {
+        if (this.queueControl.length < this.MAX_SIZE) {
+            this.queueControl.unshift(item);
+            this.isEmpty();
+            return this.queueControl;
+        } else {
+            return `Queue Overflow`;
+        }
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return `Queue Underflow`;
+        } else {
+            return this.queueControl.pop();
+        }
+    }
+
 }
