822708d6e26143cd2d4d0878dd92a4dc6073f289
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-10-30 19:59:20 +0100

    Completada la parte 'Lógica' :)

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,42 @@
-function StackDataStructure () {
+// Propiedades
+class StackDataStructure {
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 10
+    }
+
+    // Metodos
+    isEmpty() {
+        if (this.stackControl.length == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    canPush() {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            return true
+        } else {
+            return false
+        }
+    }
+    push(elm) {
+
+        if (this.canPush()) {
+            this.stackControl.push(elm)
+            return this.stackControl
+        } else {
+            return 'Stack Overflow'
+        }
+    }
+    pop() {
+        if (this.isEmpty()) {
+            return 'Stack Underflow'
+        } else {
+            return this.stackControl.pop()
+        }
+
+    }
+
 }
