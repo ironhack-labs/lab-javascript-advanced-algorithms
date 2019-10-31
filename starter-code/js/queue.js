34a0ab8de707b810f6fdb4c4b8ecabfa24d3b5ae
Sandra Tejedor delso <sandratejedordelso@MacBook-Pro-de-Sandra.local>
2019-10-31 08:59:31 +0100

    acabado

--- starter-code/js/queue.js
+++ starter-code/js/queue.js
@@ -3,72 +3,34 @@ var queue = new QueueDataStructure();
 let botAddQ = document.getElementsByClassName('container')[1].children[3]
 let botTakeQ = document.getElementsByClassName('container')[1].children[4]
 let meterTextoQ = document.getElementsByClassName('container')[1].children[2]
+let elementosQ = document.getElementsByClassName('container')[1]
+let elementosQDivs = elementosQ.getElementsByTagName('div')
 
-let elementosQ = document.getElementsByClassName('container')[1].children
-//let padre = document.getElementsByClassName('yo')
-console.log(padre)
-//console.log(botAddQ, botTakeQ, meterTextoQ)
 
 botAddQ.addEventListener('click', function () {
-    //console.log("he clicado el add")
     if (queue.MAX_SIZE == queue.queueControl.length) {
         alert("no se pueden meter mas elementos")
     }
     queue.enqueue(meterTextoQ.value)
-    let num = queue.MAX_SIZE - queue.queueControl.length + 5
-    //console.log(num, queue.queueControl.length)
-    elementosQ[num].innerText = meterTextoQ.value
+    let num = queue.MAX_SIZE - queue.queueControl.length
 
-    console.log(num, queue.queueControl.length)
+
+    elementosQDivs[num].innerText = meterTextoQ.value
+
 })
 
 botTakeQ.addEventListener('click', function () {
+    let padre = document.getElementsByClassName('yo')[0]
     if (queue.queueControl.length == 0) {
         alert("no hay elementos que sacar")
     }
-    //console.log("he clicado el take")
-    let num = 14
-    console.log(num, queue.queueControl.length)
-    let nuevoEnlace = document.createElement("div");
-    //padre.append(nuevoEnlace)
-    elementosQ[num].remove()
 
-    //padre.appendChild(nuevoEnlace);
+    let lastDiv = elementosQDivs.length
+    console.log(lastDiv, queue.queueControl.length)
+    let nuevoEnlace = document.createElement("div"); //meto un elemento Div arriba de todos los divs
+    padre.insertBefore(nuevoEnlace, elementosQDivs[0])
+
+    elementosQDivs[lastDiv].remove() //elimino el ultimo div para que saque de la cola el primer elemento que hemos metido
+
     queue.dequeue()
 })
-
-//var nuevoEnlace = document.createElement("a");
-
-/*var queue = new queueDataStructure();
-
-let botAdd = document.getElementsByTagName('button')[0]
-let botTake = document.getElementsByTagName('button')[1]
-let meterTexto = document.getElementById('texto')
-
-let elementos = document.getElementsByTagName('div')
-
-//console.log(botAdd, botTake, meterTexto, elemento3)
-
-botAdd.addEventListener('click', function () {
-    //console.log("he clicado el add")
-    if(stack.MAX_SIZE == stack.stackControl.length){
-        alert("no se pueden meter mas elementos")
-    }
-    stack.push(meterTexto.value)
-    let num = stack.MAX_SIZE - stack.stackControl.length
-    //console.log(num, stack.stackControl.length)
-    elementos[num].innerText = meterTexto.value
-    
-    //console.log(num, stack.stackControl.length)
-})
-
-botTake.addEventListener('click', function () {
-    if (stack.stackControl.length == 0) {
-        alert("no hay elementos que sacar")
-    }
-    //console.log("he clicado el take")
-    let num = stack.MAX_SIZE - stack.stackControl.length
-    //console.log(num, stack.stackControl.length)
-    elementos[num].innerHTML= ""
-    stack.pop()
-})*/
--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -6,28 +6,22 @@ let meterTexto = document.getElementById('texto')
 
 let elementos = document.getElementsByTagName('div')
 
-//console.log(botAdd, botTake, meterTexto, elemento3)
 
 botAdd.addEventListener('click', function () {
-    //console.log("he clicado el add")
-    if(stack.MAX_SIZE == stack.stackControl.length){
+    if (stack.MAX_SIZE == stack.stackControl.length) {
         alert("no se pueden meter mas elementos")
     }
     stack.push(meterTexto.value)
     let num = stack.MAX_SIZE - stack.stackControl.length
-    //console.log(num, stack.stackControl.length)
     elementos[num].innerText = meterTexto.value
-    
-    //console.log(num, stack.stackControl.length)
+
 })
 
 botTake.addEventListener('click', function () {
     if (stack.stackControl.length == 0) {
         alert("no hay elementos que sacar")
     }
-    //console.log("he clicado el take")
     let num = stack.MAX_SIZE - stack.stackControl.length
-    //console.log(num, stack.stackControl.length)
-    elementos[num].innerHTML= ""
+    elementos[num].innerHTML = ""
     stack.pop()
 })
