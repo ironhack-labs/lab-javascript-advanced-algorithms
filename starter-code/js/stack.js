2c1b2811f14d6aa9bcbd25950633702cc940684f
root <root@DESKTOP-8FSC3E1.localdomain>
2019-10-31 09:27:12 +0100

    Faltan iteraciones con el DOM, el finde le meto caña

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,15 +1,18 @@
-class StackDataStructure {
-    constructor(){
-        this.stackControl = [];
-        this.MAX_SIZE = 9;
-    }
-    isEmpty(){
-        if (this.stackControl == ''){return true};
+const stack = new StackDataStructure();
+
+let inputStack = document.getElementById("inputStack");
+let addStackBtn = document.getElementById("addStackbtn");
+let takesStackbtn = addocument.getElementById("takeStackbtn");
+
+let liDomEl = document.createElement("li");
+
+inputStack.onclick = function() {
+let inputValue = inputStack.value;
+let listArray = [...document.querySelectorAll("list-group")];
+listArray.push(inputValue);
+
+stackColumn.appendChild(liDomEl).innerText = inputValue;
 }
-canPush(){
-}
-push(){
-}
-pop(){
-}
-}
+
+
+listArray[listArray.length -1].innerText
