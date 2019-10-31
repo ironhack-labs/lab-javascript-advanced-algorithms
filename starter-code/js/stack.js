37d4952510151b482ad24d34cb57e01940fdb5bc
alejandro alarco <alejandroalarco@alejandros-MacBook-Pro-3040.local>
2019-10-31 09:25:29 +0100

    unfinished

---
+++ starter-code/js/stack.js
@@ -0,0 +1,11 @@
+let addButton = document.getElementsByClassName("add-btn")[0]
+let takeButton = document.getElementsByClassName("take-btn")[0]
+let input = document.getElementById("inputId")
+
+let newStack = new StackDataStructure
+
+function addContent(elm) {
+    if(newStack.canPush()){
+        let
+    }
+}
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,37 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        //Properties
+        this.stackControl = []
+        this.MAX_SIZE = 10
+    }
+    isEmpty() {
+        if(this.stackControl.length === 0) {
+            return true
+        } else {return false}
+    }
+    canPush() {
+        if(this.stackControl.length < this.MAX_SIZE) {
+            return true
+        } else {
+            return false}
+    }
+    push(elm) {
+        
+        if(this.canPush()){
+            this.stackControl.push(elm) 
+            return this.stackControl
+        } else {
+            return 'Stack Overflow'
+        }
+        
+    }
+    pop() {
+
+        if(this.isEmpty()){
+            return 'Stack Underflow'
+        }else {
+            return this.stackControl.pop()
+        }
+    }
 }
+
