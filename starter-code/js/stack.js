7bf02d32c88be1ae48e7dac3dc5d9cab21bc1019
María Simó <maria@coponstudio.es>
2019-10-31 01:27:36 +0100

    Change name of vars

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,18 +1,18 @@
 const stack = new StackDataStructure();
 
-const addBtn = document.querySelector("#add-btn");
-const takeBtn = document.querySelector("#take-btn");
-const inputEl = document.querySelector("#text-input");
-let listEl = document.querySelector("#stack .list-group")
+const stackAddBtn = document.querySelector("#stack .add-btn");
+const stackTakeBtn = document.querySelector("#stack .take-btn");
+const stackInputEl = document.querySelector("#stack .text-input");
+let stackList = document.querySelector("#stack .list-group")
 let messageEl = document.createElement("div");
 
 
-addBtn.onclick = function () {
+stackAddBtn.onclick = function () {
     let listGroup = [...document.querySelectorAll('#stack .bg-light')];
     let lastItemOfList = listGroup[listGroup.length - 1];
 
-    if (inputEl.value) {
-        let inputVal = inputEl.value;
+    if (stackInputEl.value) {
+        let inputVal = stackInputEl.value;
 
         if (stack.canPush()) {
             stack.push(inputVal);
@@ -28,7 +28,7 @@ addBtn.onclick = function () {
         } else {
             messageEl.classList.add("alert", "alert-warning");
             messageEl.innerText = stack.push();
-            listEl.prepend(messageEl)
+            stackList.prepend(messageEl)
         }
     }else {
         alert("Value to input needed")
@@ -38,15 +38,15 @@ addBtn.onclick = function () {
 
 
 
-takeBtn.onclick = function () {
-    let listRemoveGroup = [...document.querySelectorAll('.bg-secondary')];
+stackTakeBtn.onclick = function () {
+    let listRemoveGroup = [...document.querySelectorAll('#stack .bg-secondary')];
     let firstItemOfList = listRemoveGroup[0];
     
 
     if (stack.isEmpty()) {
         messageEl.classList.add("alert", "alert-warning");
         messageEl.innerText = stack.pop();
-        listEl.append(messageEl)
+        stackList.append(messageEl)
     } else {
         stack.pop();
         messageEl.classList.remove('alert','alert-warning');
