a387cb8214d69b0720a7a43aaa3b689eccec4fce
Borja Valdés Jiménez <borja@MacBook-Air-de-Borja.local>
2019-10-31 09:16:26 +0100

    Nada bien

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,35 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = []
+        this.MAX_SIZE = 10
+    }
+
+    isEmpty() {
+        return this.queueControl.length === 0
+
+    }
+
+    canEnqueue() {
+        if (this.queueControl.length < this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    enqueue(err) {
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(err);
+            return this.queueControl;
+        } else {
+            return 'Queue Overflow';
+        }
+    }
+    dequeue() {
+        if (this.isEmpty()) {
+            return 'Queue Underflow'
+        } else {
+            return this.queueControl.pop()
+        }
+    }
 }
+
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = []
+        this.MAX_SIZE = 10
+    }
+
+    isEmpty() {
+        return this.stackControl.length === 0
+
+    }
+
+    canPush() {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    push(err) {
+        if (this.canPush()) {
+            this.stackControl.push(err);
+            return this.stackControl;
+        } else {
+            return 'Stack Overflow';
+        }
+    }
+    pop() {
+        if (this.isEmpty()) {
+            return 'Stack Underflow'
+        } else {
+            return this.stackControl.pop()
+        }
+    }
 }
