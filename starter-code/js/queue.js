d9567ec6fb93f5e4dd6921e6ff4a83d75ee61deb
María Simó <maria@coponstudio.es>
2019-10-31 01:26:22 +0100

    Add vars to queue.js file

---
+++ starter-code/js/queue.js
@@ -0,0 +1,8 @@
+const queue = new QueueDataStructure();
+
+const queueAddBtn = document.querySelector("#queue .add-btn");
+const queueTakeBtn = document.querySelector("#queue .take-btn");
+const queueInputEl = document.querySelector("#queue .text-input");
+let queueList = document.querySelector("#queue .list-group")
+let messageEl = document.createElement("div");
+
