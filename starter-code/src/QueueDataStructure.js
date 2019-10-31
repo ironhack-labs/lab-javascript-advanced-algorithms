441a3e899fa51a096d22881bd117d9047584b48c
María Simó <maria@coponstudio.es>
2019-10-31 01:05:18 +0100

    Added queue logic

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 8
+    }
+
+    isEmpty() {
+        if (this.queueControl.length <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    canEnqueue() {
+        if (this.queueControl.length >= this.MAX_SIZE) {
+            return false;
+        }
+        return true;
+    }
+
+    enqueue(el) {
+        if (this.queueControl.length >= this.MAX_SIZE) {
+            return "Queue Overflow";
+        } else {
+            this.queueControl.unshift(el);
+            return this.queueControl;
+
+        }
+    }
+
+    dequeue() {
+        if (this.queueControl.length <= 0) {
+            return 'Queue Underflow';
+        } else {
+            return this.queueControl.pop();
+        }
+    }
 }
