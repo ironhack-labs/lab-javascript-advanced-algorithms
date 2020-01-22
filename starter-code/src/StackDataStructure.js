7b04976295de9595b468e6fad6f97eb499b347d3
CrisSugar <aguacero18@hotmail.com>
2020-01-22 18:39:38 +0100

    jasmine done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,46 @@
-function StackDataStructure () {
+class StackDataStructure{
+  constructor(){
+    this.stackControl = [],
+    this.MAX_SIZE = 8
 }
+isEmpty(){
+    if (this.stackControl.length===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+canPush(){
+    if (this.stackControl.length<this.MAX_SIZE){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+push(item){
+    if(this.canPush() === true){
+    this.stackControl.push(item)
+    return this.stackControl
+    }
+    else{
+        return "Stack Overflow";
+    }
+}
+
+pop(item){
+    if(this.isEmpty() === true){
+        return 'Stack Underflow';
+    }
+    else{
+        return this.stackControl.pop();
+    }
+    
+} 
+
+}
+
+
