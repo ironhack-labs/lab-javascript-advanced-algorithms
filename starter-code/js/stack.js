8d00d2e30e49df3a471dccd78c42cf9b65573260
alejandrosz <alejandrosz@gmail.com>
2020-01-22 18:40:38 +0100

    html y jasmine hecho

---
+++ starter-code/js/stack.js
@@ -0,0 +1,11 @@
+let Stack = new StackDataStructure();
+let input = document.getElementById("input");
+let add = document.getElementById("add");
+let deleter = document.getElementById("delete");
+let stack = document.getElementById("stack");
+let item = document.createElement("li");
+
+function createStack
+
+deleter.addEventListener("click", () => {
+    console.log(input.value)})
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+  }
+  isEmpty() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  canPush() {
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  push(data) {
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return "Stack Overflow";
+    } else {
+      this.stackControl.push(data);
+    }
+    return this.stackControl;
+  }
+  pop() {
+    if (this.stackControl.length === 0) {
+      return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
+  }
 }
