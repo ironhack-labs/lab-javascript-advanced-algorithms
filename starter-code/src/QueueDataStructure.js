866c7a3a860dd0d176eed64dfd9a8a4c638333b0
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-01-22 19:44:43 +0100

    Jazmina hecho

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    constructor() {
+        this.queueControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        return this.queueControl.length === 0 ? true : false
+        // if (this.queueControl.length === 0) {
+        //     return true
+        // } else
+        //     return false
+    }
+
+
+    canEnqueue() {
+        if (this.queueControl.length < this.MAX_SIZE) {
+            return true
+        } else
+            return false
+    }
+
+
+    enqueue(elm) {
+        if (this.canEnqueue()) {
+            this.queueControl.unshift(elm)
+            return this.queueControl
+        } else if (this.queueControl.length === this.MAX_SIZE) {
+            return "Queue Overflow"
+        }
+    }
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Queue Underflow"
+        } else {
+            return this.queueControl.pop()
+        }
+    }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,44 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty() {
+        return this.stackControl.length === 0 ? true : false
+        // if (this.stackControl.length === 0) {
+        //     return true
+        // } else
+        //     return false
+    }
+
+
+    canPush() {
+        if (this.stackControl.length < this.MAX_SIZE) {
+            return true
+        } else
+            return false
+    }
+
+
+    push(elm) {
+        if (this.canPush()) {
+            this.stackControl.push(elm)
+            return this.stackControl
+        } else if (this.stackControl.length === this.MAX_SIZE) {
+            return "Stack Overflow"
+        }
+    }
+
+
+    pop() {
+        if (this.isEmpty()) {
+            return "Stack Underflow"
+        } else {
+            return this.stackControl.pop()
+        }
+    }
 }
+
+
+
