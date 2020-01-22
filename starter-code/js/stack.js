834adb329b5122986173b43c2a44a9002f2d878b
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-01-23 00:06:07 +0100

    nope......

---
+++ starter-code/js/stack.js
@@ -0,0 +1,20 @@
+
+let stack = new StackDataStructure;
+let getInputValue = document.getElementById('input-value').value;
+
+
+
+
+
+function actionOfAddButton() {
+
+    console.log('FUCK');
+
+}
+
+function addBtn() {
+
+    const addBtnLocation = document.getElementById('#addButtonId'); addBtnLocation.addEventListener('click', actionOfAddButton);
+
+
+}
