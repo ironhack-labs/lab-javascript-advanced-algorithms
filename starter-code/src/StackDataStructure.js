494379897966fda4ba75ad1c1ce147828d807678
Arturo G. Bruno <arturogbruno@gmail.com>
2020-01-22 18:30:45 +0100

    Jasmine tests for Stacks passed

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,43 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 8;
+    }
+    
+    isEmpty() {
+        if(this.stackControl.length === 0) {
+            return true;
+        } 
+        return false;
+    }
+
+    canPush() {
+        if(this.stackControl.length < this.MAX_SIZE) {
+            return true;
+        }
+        return false;
+    }
+
+    push(elem) {
+        if(this.canPush()) {
+            this.stackControl.push(elem);
+            return this.stackControl;
+        } else {
+            return 'Stack Overflow';
+        }
+
+        
+    }
+
+    pop() {
+        if(this.stackControl.length === 0) {
+            return 'Stack Underflow';
+        } else {
+            return this.stackControl.pop();
+        }
+        
+    }
+
 }
+
+
