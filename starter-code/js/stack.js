f1865d684079f625684b029020d604dec7f6ab07
Pedro Castañeda <pcramos19@gmail.com>
2020-01-22 20:09:03 +0100

    done stack

---
+++ starter-code/js/stack.js
@@ -0,0 +1,46 @@
+let stack = new StackDataStructure ()
+let stackDOM = document.querySelector(".stack");
+let addBtn = document.getElementById("add").addEventListener("click", addItem);
+let takeBtn = document.getElementById("take").addEventListener("click", takeItem);
+let valueStack = "";
+let valueStackList= "";
+
+
+
+
+printList();
+
+function printList() {
+    stackItem = "";
+
+    for (let i=0; i<stack.MAX_SIZE; i++){
+        stackItem = document.createElement("li");
+        stackItem.classList.add ("empty")
+        stackDOM.appendChild(stackItem);
+    }
+}
+
+function addItem(){
+    if (stack.canPush()){
+        valueStack = document.querySelector(".input-lg").value;
+        valueStackList = document.querySelectorAll(".empty");
+        valueStackList[valueStackList.length-1].innerHTML = valueStack;
+        valueStackList[valueStackList.length-1].classList.remove("empty");
+        valueStackList[valueStackList.length-1].classList.add("full");
+        stack.push(valueStack);
+    } else {
+        alert (`Estoy hasta los huevos`)
+    }
+}
+
+function takeItem(){
+    if (stack.isEmpty()){ 
+        alert (`Estoy fundioooooooooo`)
+    } else {
+        valueStackList = document.querySelectorAll(".full");
+        valueStackList[0].innerHTML = "";
+        valueStackList[0].classList.remove("full");
+        valueStackList[0].classList.add("empty");
+        stack.pop(valueStack);
+    }
+}
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,40 @@
-function StackDataStructure () {
+class StackDataStructure {
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }
+
+    isEmpty(){
+        if (this.stackControl.length === 0){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    canPush(){
+        if (this.MAX_SIZE === this.stackControl.length){
+            return false
+        } else {
+            return true
+        }
+    }
+
+    push(elem){
+        if (this.stackControl.length === this.MAX_SIZE){
+            return `Stack Overflow`
+        } else {
+            this.stackControl.push(elem)
+        } return this.stackControl
+    }
+
+    pop(elem){
+        if (this.stackControl.length === 0){
+            return `Stack Underflow`
+        } else {
+            return this.stackControl.pop(elem)
+        }
+    }
+
+
 }
