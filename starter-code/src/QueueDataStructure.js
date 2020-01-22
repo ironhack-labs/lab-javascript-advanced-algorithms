c6e2296030e7436f18b8ebec1e8db49b2d540bbc
Nelson <nelsoncc1812@gmail.com>
2020-01-22 18:29:30 +0000

    Jasmine done ¬¬

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,4 +1,30 @@
 class QueueDataStructure {
 
+    constructor() {
+        this.queueControl = []
+        this.MAX_SIZE=10
+    }
+
+    isEmpty() {
+        return this.queueControl.length ? false : true
+    }
+
+    canEnqueue() {
+        return this.queueControl.length < this.MAX_SIZE ? true : false
+    }
+
+    enqueue(elm) {
+
+        if (!this.canEnqueue()) return "Queue Overflow"
+        this.queueControl.unshift(elm)
+        return this.queueControl
+    }
+
+    dequeue() {
+
+        if (this.isEmpty()) return "Queue Underflow"
+        let elm = this.queueControl.pop()
+        return elm
+    }
 
 }
