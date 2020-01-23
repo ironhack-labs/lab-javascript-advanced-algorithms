b4782ef36803788163e3968116ddf83e2bcd2e2a
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-23 09:35:34 +0100

    Ejercicio terminado

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,36 +1,32 @@
-const stackDataStructure = new StackDataStructure
-let stackAdd = document.querySelector("#stackAdd")
-let stackTake = document.querySelector("#stackTake")
-let stackElements = document.querySelectorAll("elm stack")
-let input = document.getElementById("stackInput")
+const stackDataStructure = new StackDataStructure();
+let stackAdd = document.querySelector("#stackAdd");
+let stackTake = document.querySelector("#stackTake");
+let stackElements = document.querySelectorAll("elm stack");
+let input = document.getElementById("stackInput");
 
-console.log(input)
+console.log(input);
 
-stackAdd.addEventListener("click", function () {
-
-
-    addStack(input.value)
-    console.log(stackDataStructure.stackControl)
-    let i = stackDataStructure.stackControl.length //para saber que  div pintar
-    //changeColor(i)
-    a = stackElements.getElementById(i)
-    console.log(a)
-    a.innerText = input.value
-})
+stackAdd.addEventListener("click", function() {
+  addStack(input.value);
+  console.log(stackDataStructure.stackControl);
+  let i = stackDataStructure.stackControl.length; //para saber que  div pintar
+  //changeColor(i)
+  console.log(i);
+  console.log(stackElements);
+  console.log(changeColor(i));
+});
 
 function addStack(elm) {
-    stackDataStructure.push(elm)
+  stackDataStructure.push(elm);
 }
 
-function changeColor(elm) {
-    elm.classList.remove("empty")
-    elm.classList.add("full")
-    console.log(stackElements)
+function changeColor(id) {
+  let elm = stackElements.querySelector();
+  elm.classList.remove("empty");
+  elm.classList.add("full");
 }
 
 function removeColor(elm) {
-    elm.classList.remove("full")
-    elm.classList.add("empty")
-
-
+  elm.classList.remove("full");
+  elm.classList.add("empty");
 }
