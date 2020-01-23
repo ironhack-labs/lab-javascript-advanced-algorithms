afff7b1b00bd5460a40ef098fc7fe129690e5cc1
de_Mora <miguel.uni.demora@gmail.com>
2020-01-23 09:00:10 +0100

    stack impl

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,5 +1,15 @@
 let stack = new StackDataStructure();
 
+let stackVal = document.querySelector('.stack input').value
+
+let spos = 1
+
+console.log(stackVal)
+
+let sArrli = document.querySelectorAll('.stack li')
+
+
+
 let sAddBut = document.querySelector('.stack.add')
 console.log(sAddBut)
 
@@ -7,10 +17,62 @@ let sTakeBut = document.querySelector('.stack.take')
 console.log(sTakeBut)
 
 sAddBut.addEventListener("click",()=>{
-    console.log('funciono')
+    // console.log('funciono')
+    if(stack.canPush()){
+        stackVal = document.querySelector('.stack input').value
+        stack.push(queueVal)
+        sArrli[spos].className="full"
+        sArrli[spos].innerText=stackVal
+        spos++
+        sArrli[0].innerText=""
+    }else{
+        sArrli[9].innerText=stack.push(stackVal)
+
+    }
 
 })
 
 sTakeBut.addEventListener("click",()=>{
-    console.log('funciono')
+    // console.log('funciono')
+    if(!stack.isEmpty()){
+        stackVal = document.querySelector('.stack input').value
+        spos--
+        stack.pop()
+        sArrli[spos].className="empty"
+        sArrli[spos].innerText=""
+        sArrli[9].innerText=""
+    }else{
+        sArrli[0].innerText=stack.pop()
+
+    } 
 })
+
+/* 
+
+qAddBut.addEventListener("click",()=>{
+    if(stack.canEnqueue()){
+        queueVal = document.querySelector('.queue input').value
+        queue.enqueue(queueVal)
+        qArrli[pos].className="full"
+        qArrli[pos].innerText=queueVal
+        pos++
+        qArrli[0].innerText=""
+        
+    }else{
+        qArrli[9].innerText=queue.enqueue(queueVal)
+    }
+})
+qTakeBut.addEventListener("click",()=>{
+    if(!queue.isEmpty()){
+        queueVal = document.querySelector('.queue input').value
+        pos--
+        queue.dequeue()
+        qArrli[pos].className="empty"
+        qArrli[pos].innerText=""
+        qArrli[9].innerText=""
+    }else{
+        qArrli[0].innerText=queue.dequeue()
+
+    }  
+})
+*/
