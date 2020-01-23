bd55081525225a88076b877b26a39923aad9060d
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-01-23 08:40:20 +0100

    Stack READY

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -22,25 +22,30 @@ printList()
 
 let input = ''
 let stackLi = ''
+let gif = ''
 
 function addStack() {
     if (stack.canPush()) {
+        gif = document.getElementById('gif')
         input = document.querySelector('.input-lg').value
         stackLi = document.querySelectorAll('.empty')
+        gif.classList.remove('yes')
         stackLi[stackLi.length - 1].innerHTML = input
         stackLi[stackLi.length - 1].classList.remove('empty')
-         stackLi[stackLi.length - 1].classList.add('list-group-item')
+        stackLi[stackLi.length - 1].classList.add('list-group-item')
         stackLi[stackLi.length - 1].classList.add('full')
-       
+
         stack.push(input)
     } else {
-        return alert('FUUUUUUCK')
+
+        return alert('FUUUUUULL')
     }
 }
 
 function takeStack() {
+    gif = document.getElementById('gif')
     if (stack.isEmpty()) {
-        return alert('FUUUUUUCK')
+        gif.classList.add('yes')
 
     } else {
         stackLi = document.querySelectorAll('.full')
