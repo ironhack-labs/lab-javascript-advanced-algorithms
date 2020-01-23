8cf7861764751fe6581ba1003df73a195a37b490
armetegithub <armomomete@protonmail.com>
2020-01-23 09:11:28 +0100

    se introduce e input, se cambia intercambian las clases pero sigue sin funcionar

--- starter-code/js/stack.js
+++ starter-code/js/stack.js
@@ -1,53 +1,23 @@
-
-
-let stack  =  new StackDataStructure();
+let stack = new StackDataStructure();
 
 let btnAdd = document.querySelector(".add");
-btnAdd.onclick =function(){
-    
-   let item = document.querySelector('.texto').value;
-   stack.push(item);
-   console.log(item);
-   console.log(stack.stackControl);
+btnAdd.onclick = function() {
+  let item = document.querySelector(".texto").value;
+  stack.push(item);
+  console.log(item);
+  console.log(stack.stackControl);
 
- 
+  var add = document.querySelector(".list-group").lastElementChild;
 
-   var prueba = document.querySelector(".list-group").lastElementChild;
-
-
-   prueba.innerHTML = item;
-
-   
-   
-}
-
-   
-
-
-
-
-let btnTake= document.querySelector('.take');
-
-btnTake.onclick =function(){
-
-    let column = document.querySelectorAll('.list-group-item').values;
-    stack.pop(column);
-    console.log(stack.stackControl);
-
-    
-
-
-    
+  add.innerHTML = item;
+  add.classList.remove("empty");
+  add.classList.add("full");
 };
 
+let btnTake = document.querySelector(".take");
 
-
-// function pr() {
-//     document.getElementById("result").innerHTML = document.getElementById('fname').value + " " + document.getElementById('mname').value + " " + document.getElementById('lname').value;
-//   }
-
-
-
-
-
-
+btnTake.onclick = function() {
+  let remov = document.querySelectorAll(".list-group").lastElementChild;
+  stack.pop(remov);
+  console.log(stack.stackControl);
+};
