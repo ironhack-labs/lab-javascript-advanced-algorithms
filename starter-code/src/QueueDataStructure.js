6eb816bbaecadae29c12bb889eb4094fd7f77057
Pedro Castañeda <pcramos19@gmail.com>
2020-01-23 09:35:58 +0100

    casi me cargo todo

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,40 +1,40 @@
-class QueueDataStructure {
-    constructor() {
-        this.queueControl = [];
-        this.MAX_SIZE = 10;
-    }
+// class QueueDataStructure {
+//     constructor() {
+//         this.queueControl = [];
+//         this.MAX_SIZE = 10;
+//     }
 
-    isEmpty(){
-        if (this.queueControl.length === 0){
-            return true
-        } else {
-            return false
-        }
-    }
+//     isEmpty(){
+//         if (this.queueControl.length === 0){
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
 
-    canPush(){
-        if (this.MAX_SIZE === this.queueControl.length){
-            return false
-        } else {
-            return true
-        }
-    }
+//     canPush(){
+//         if (this.MAX_SIZE === this.queueControl.length){
+//             return false
+//         } else {
+//             return true
+//         }
+//     }
 
-    push(elem){
-        if (this.queueControl.length === this.MAX_SIZE){
-            return `Queue Overflow`
-        } else {
-            this.queueControl.push(elem)
-        } return this.queueControl
-    }
+//     push(elem){
+//         if (this.queueControl.length === this.MAX_SIZE){
+//             return `Queue Overflow`
+//         } else {
+//             this.queueControl.push(elem)
+//         } return this.queueControl
+//     }
 
-    pop(elem){
-        if (this.queueControl.length === 0){
-            return `Queue Underflow`
-        } else {
-            return this.queueControl.pop(elem)
-        }
-    }
+//     shift(elem){
+//         if (this.queueControl.length === 0){
+//             return `Queue Underflow`
+//         } else {
+//             return this.queueControl.shift(elem)
+//         }
+//     }
 
 
-}
+// }
