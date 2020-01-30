c2ad69d75b48878b9d8df50a2f2cc7be449c25bd
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-01-30 20:54:13 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
 function QueueDataStructure () {
+    this.queueControl = [];
+    this.MAX_SIZE = 10;
+
+    this.isEmpty = () => {
+        return this.queueControl.length === 0 ? true : false;
+    }
+
+    this.canEnqueue = () => {
+        return this.queueControl.length < this.MAX_SIZE ? true : false;
+    }
+
+    this.enqueue = arg1 => {
+        result = this.canEnqueue();
+        return result === false ? "Queue Overflow" : (this.queueControl.unshift(arg1), this.queueControl);
+    }
+
+    this.dequeue = () => {
+        return this.queueControl.length === 0
+        ? 'Queue Underflow'
+        : this.queueControl.pop()
+    }
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  // This is already the constructor
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = () => {
+    return this.stackControl.length === 0 ? true : false;
+  };
+
+  this.canPush = () => {
+    return this.stackControl.length < this.MAX_SIZE ? true : false;
+  };
+
+  this.push = arg1 => {
+    const result = this.canPush(arg1);
+    return result === true
+      ? (this.stackControl.push(arg1), this.stackControl)
+      : "Stack Overflow";
+  };
+
+  this.pop = () => {
+    return this.stackControl.length === 0
+      ? "Stack Underflow"
+      : this.stackControl.pop();
+  };
 }
+
+// structure = new StackDataStructure();
+// //console.log(structure.isEmpty());
+// structure.push(19);
+
+// structure.MAX_SIZE = 1;
+// structure.push(18);
