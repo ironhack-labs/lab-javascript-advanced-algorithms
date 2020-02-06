3b926678223035344041c79e94cce5a8d13bbc01
Alex Panko <alexander.panko@gmail.com>
2020-02-06 22:37:44 +0100

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,53 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+    queueControl = []
+    MAX_SIZE = 10
+    isEmpty() {
+        if (this.queueControl.length > 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+    canEnqueue() {
+        if (this.queueControl.length == this.MAX_SIZE) {
+            return false
+        } else {
+            return true
+        }
+    }
+    enqueue(num) {
+        if (this.canEnqueue() == true) {
+            this.queueControl.unshift(num)
+            return this.queueControl
+        } else {
+            return 'Queue Overflow'
+        }
+    }
+    dequeue() {
+        if (this.queueControl.length == 0) {
+            return 'Queue Underflow'
+        } else {
+            return this.queueControl.pop()
+        }
+    }
+}
+
+let newQueue = new QueueDataStructure();
+let myQueueArray = document.querySelectorAll(".queueBox")
+
+document.getElementById("addQueue").addEventListener('click', addQueueFunction)
+function addQueueFunction() {
+    let newElement = document.getElementById("inputQueue").value;
+    newQueue.enqueue(newElement);
+    document.getElementById('inputQueue').value = "";
+    for (let index = 0; index < newQueue.queueControl.length; index++) {
+        myQueueArray[index].innerHTML = newQueue.queueControl[index];
+    }
+}
+
+document.getElementById("takeQueue").addEventListener('click', takeQueueFunction)
+function takeQueueFunction() {
+    newQueue.dequeue();
+    let i = newQueue.queueControl.length
+    myQueueArray[i].innerHTML = ""
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,53 @@
-function StackDataStructure () {
+class StackDataStructure {
+    stackControl = []
+    MAX_SIZE = 10
+    isEmpty() {
+        if (this.stackControl.length > 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+    canPush() {
+        if (this.stackControl.length == this.MAX_SIZE) {
+            return false
+        } else {
+            return true
+        }
+    }
+    push(num) {
+        if (this.canPush() == true) {
+            this.stackControl.push(num)
+            return this.stackControl
+        } else {
+            return 'Stack Overflow'
+        }
+    }
+    pop() {
+        if (this.stackControl.length == 0) {
+            return 'Stack Underflow'
+        } else {
+            return this.stackControl.pop()
+        }
+    }
+}
+
+let newStack = new StackDataStructure();
+let myArray = document.querySelectorAll(".stackBox")
+
+document.getElementById("addStack").addEventListener('click', addStackFunction)
+function addStackFunction() {
+    let newElement = document.getElementById("inputStack").value;
+    newStack.push(newElement);
+    document.getElementById('inputStack').value = "";
+    for (let index = 0; index < newStack.stackControl.length; index++) {
+        myArray[9-index].innerHTML = newStack.stackControl[index];
+    }
+}
+
+document.getElementById("takeStack").addEventListener('click', takeStackFunction)
+function takeStackFunction() {
+    newStack.pop();
+    let i = newStack.stackControl.length
+    myArray[9-i].innerHTML = ""
 }
