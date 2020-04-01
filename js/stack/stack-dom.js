aa3c6590a62878ef3e83a1f43a844c2456862943
Ruben Narvaez <rubnarvaez@gmail.com>
2020-04-01 23:51:45 +0200

    Done
    Done
    y

--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -1,38 +1,62 @@
-const stackList = document.getElementById('stack-list');
-const stackInput = document.getElementById('stack-input');
-const container = document.getElementById('container');
-const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
-const addStackBtn = document.getElementById('add-stack');
-const takeStackBtn = document.getElementById('take-stack');
+const stackList = document.getElementById("stack-list");
+const stackInput = document.getElementById("stack-input");
+const container = document.getElementById("container");
+const warningTopStack = document.querySelector("#stack-container .warning-top");
+const warningBottomStack = document.querySelector(
+  "#stack-container .warning-bottom"
+);
+const addStackBtn = document.getElementById("add-stack");
+const takeStackBtn = document.getElementById("take-stack");
 
 const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  stackInput.value = " ";
 };
 
 const renderListStack = () => {
-  // ... your code goes here
+  let listBlocks;
+
+  for (let i = 0; i < newStack.MAX_SIZE; i++) {
+    listBlocks = document.createElement("li");
+    listBlocks.classList.add("inactive");
+    stackList.appendChild(listBlocks);
+  }
 };
 
-renderListStack();
+renderListStack({ warningBottomStack });
 
 const generateWarningStack = type => {
-  if (type === 'underflow') {
-    // ... your code goes here
-  } else if (type === 'overflow') {
-    // ... your code goes here
+  if (type === "underflow") {
+    warningBottomStack.innerHTML = "underflow";
+    warningBottomStack.style.display = "block";
+  } else if (type === "overflow") {
+    warningTopStack.innerHTML = "overflow";
+    warningBottomStack.style.display = "block";
   }
 };
 
 const addToStack = () => {
-  // ... your code goes here
-};
+  if (newStack.canPush()) {
+    newStack.push(stackInput.value);
+    let newActive = (document.querySelectorAll("li").innerHTML =
+      stackInput.value);
 
+    //newActiveArray[] //DEBO SELECCIONAR LA FILA INACTIVA MAS BAJA
+    console.log(newActiveArray);
+
+    clearStackInput();
+  } else {
+    generateWarningTopStack;
+  }
+};
 const removeFromStack = () => {
-  // ... your code goes here
+  if (newStack.isEmpty()) {
+    generateWarningBottomStack;
+  }
 };
 
-addStackBtn.addEventListener('click', addToStack);
-takeStackBtn.addEventListener('click', removeFromStack);
+addStackBtn.addEventListener("click", addToStack);
+takeStackBtn.addEventListener("click", removeFromStack);
+
+console.log(document.querySelector("#stack-input").innerHTML);
