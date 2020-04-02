eab1c9708fbddfb2ee6c9611c1aa79e7b092ff67
Julian Bajo <jbajopinedo@gmail.com>
2020-04-02 02:30:48 +0200

    Ejercicio estructuras Stack y Queue realizado completado

--- js/queue/queue-dom.js
+++ js/queue/queue-dom.js
@@ -9,28 +9,98 @@ const queue = new QueueDataStructure();
 
 const clearQueueInput = () => {
   // ... your code goes here
+  queueInput.value = ''
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+  // Generamos la lista de cajas
+  // donde se van a ir añadiendo o borrando las cajas nuevas.
+  for (let i = 0; i < queue.MAX_SIZE; i++) {
+    const liItem = document.createElement('li')
+    liItem.classList.add('inactive')
+    liItem.id = `item${i + 1}`
+    queueUL.appendChild(liItem)
+  }
 };
 
 generateListQueue();
 
+// Activamos la aparicion de los mensajes de overflow y de underflow
 const generateWarningQueue = type => {
   if (type === 'underflow') {
     // ... your code goes here
+
+    warningBottomQueue.style.display = 'block'
+    warningBottomQueue.innerHTML = queue.dequeue(type)
+
   } else if (type === 'overflow') {
     // ... your code goes here
+
+    warningTopQueue.style.display = 'block'
+    warningTopQueue.innerHTML = queue.enqueue(type)
+
   }
 };
 
 const addToQueue = () => {
   // ... your code goes here
+
+  //Comprobamos que haya espacio en el queue
+
+  if (queue.canEnqueue()) {
+
+    //Desactivamos el mensaje de error    
+    warningBottomQueue.style.display = 'none'
+    
+
+    //Añadimos el valor ingresado por el usuario al array.
+    queue.enqueue(queueInput.value)
+
+    //Limpiamos la linea de ingreso de texto
+    clearQueueInput()
+
+    //Activamos las cajas de la lista 
+    queueUL.querySelector(`#item${queue.queueControl.length}`).className = 'active'
+
+    //Añadimos al DOM el contenido que ingreso el usuario a las cajas 
+    let index = 0
+    queueUL.querySelectorAll('.active').forEach(elm => {
+      elm.innerHTML = queue.queueControl[index]
+      index++
+    })
+
+  } else {
+
+    generateWarningQueue('overflow')
+    
+    }
+
 };
 
 const removeFromQueue = () => {
   // ... your code goes here
+
+  //Comprobamos que el array del queue no este vacio
+  if (!queue.isEmpty()) {
+   
+    //Desactivamos el mensaje de error
+    warningTopQueue.style.display = 'none'
+    
+    //Quitamos del DOM la caja 
+    const liItem = queueUL.querySelector(`#item${queue.queueControl.length}`)
+    liItem.className = 'inactive'
+    liItem.innerHTML = ''
+
+    //Eliminamos el elemento del array
+    queue.dequeue()
+
+  } else {
+
+    generateWarningQueue('underflow')
+    
+  }
+
 };
 
 addQueue.addEventListener('click', addToQueue);
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -9,29 +9,103 @@ const takeStackBtn = document.getElementById('take-stack');
 const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  stackInput.value = " "
 };
 
 const renderListStack = () => {
-  // ... your code goes here
+  //Con esta funcion genero las cajas grises inactivas de la lista,
+  //además les asigno la clase "inactive" y les añado una id para identificarlas.
+
+  for (let i = 0; i < newStack.MAX_SIZE; i++) {
+    // ... your code goes here
+    const liItem = document.createElement('li')
+    liItem.classList.add('inactive')
+    liItem.id = `item${i + 1}`
+    stackList.appendChild(liItem)
+  }
+
 };
 
 renderListStack();
+//Con esta funcion se activan los mensajes de 
+//"Stack Overflow" y "Stack Underflow"
 
 const generateWarningStack = type => {
   if (type === 'underflow') {
-    // ... your code goes here
+    // ... your code goes here    
+    warningBottomStack.innerHTML = newStack.pop(type)
+    warningBottomStack.style.display = 'block'
+    
+
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopStack.style.display = 'block'
+    warningTopStack.innerHTML = newStack.push(type)
+
   }
 };
 
 const addToStack = () => {
   // ... your code goes here
+    //Comprobamos con la funcion del ejercicio de lógica,
+    //si existe espacio en el array.
+
+  if (newStack.canPush()) {
+    
+    //Oculto el mensaje del warning "Satck Underflow" visible
+
+   warningBottomStack.style.display = 'none'
+    
+
+    //Añado al array del objeto StackDataStructure
+    // el elemento stackinput con el valor asignado
+    
+    newStack.push(stackInput.value)
+
+    //Llamamo a la funcion que reinicia el input de texto
+    
+    clearStackInput()
+
+    //Se refleja en el html cambiando las clases e incluyendo texto
+
+    const newLiItem = stackList.querySelector(`#item${newStack.stackControl.length}`)
+    newLiItem.className = 'active'
+    newLiItem.innerHTML = newStack.stackControl[newStack.stackControl.length - 1]
+
+  } else {
+
+    generateWarningStack("Stack Overflow")
+
+  } 
+      
 };
 
 const removeFromStack = () => {
   // ... your code goes here
+
+  //Comprobamos con la funcion del ejercicio de lógica,
+    //si el array esta vacío.
+
+  if (!newStack.isEmpty()) {
+
+    //Eliminamos el mensaje overflow
+    
+    warningTopStack.style.display = 'none'
+    
+    //Quitamos el elmento li del DOM
+    const newLiItem = stackList.querySelector(`#item${newStack.stackControl.length}`)
+    newLiItem.className = 'inactive'
+    newLiItem.innerHTML = ''
+
+    //Con el metodo .pop() quitamos el ultimo elemento del objetop satck data structure
+    newStack.pop()
+
+  } else {
+    
+    //Generamos el mensaje "Underflow" si el array esta vacio.
+    generateWarningStack("underflow")
+
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
