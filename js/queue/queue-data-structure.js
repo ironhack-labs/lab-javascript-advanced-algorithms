6f3a6c0b6fc8bed9febe9931a2c2fe4c9df7affa
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-06-11 00:40:07 +0200

    added functions in queue.js

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -6,21 +6,24 @@ class QueueDataStructure {
 
   display() {
     // ... your code goes here
+    return this.queueControl
   }
 
   canEnqueue() {
     // ... your code goes here
+    return this.queueControl < this.MAX_SIZE ? true : false
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.queueControl.length === 0 ? true : false
   }
-
   enqueue(item) {
     // ... your code goes here
   }
 
   dequeue() {
     // ... your code goes here
+    return this.isEmpty() ? 'Queue Underflow' : this.queueControl.pop()
   }
 }
