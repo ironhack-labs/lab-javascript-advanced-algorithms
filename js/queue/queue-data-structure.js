fca7c79310b6bb13625e1422e82153f15f288c23
toufiq23 <drrameraaron023@gmail.com>
2020-06-16 00:37:37 +0200

    Done

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,22 +5,41 @@ class QueueDataStructure {
   }
 
   display() {
-    // ... your code goes here
+		// ... your code goes here
+		return this.queueControl;
   }
 
   canEnqueue() {
-    // ... your code goes here
+		// ... your code goes here
+		if (this.queueControl.length >= this.MAX_SIZE) {
+			return false;
+		}
+		return true;
   }
 
   isEmpty() {
-    // ... your code goes here
+		// ... your code goes here
+		if (this.queueControl.length <= 0) {
+			return true;
+		}
+		return false;
   }
 
   enqueue(item) {
-    // ... your code goes here
+		// ... your code goes here
+		if (this.canEnqueue() === false) {
+			return "Queue Overflow";
+		} else {
+			this.queueControl.unshift(item);
+			return this.display();
+		}
   }
 
   dequeue() {
-    // ... your code goes here
+		// ... your code goes here
+		if (this.isEmpty() === true) {
+			return "Queue Underflow";
+		}
+		return this.queueControl.pop();
   }
 }
