53e753bf18596d11195c44118546d4a057b465b9
david <david1_43@hotmail.com>
2020-08-26 23:46:26 +0200

    done

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,21 +6,46 @@ class StackDataStructure {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length === this.MAX_SIZE) {
+      return false;
+    }
+    return true;
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
+
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true;
+    }
+    return false;
   }
 
   push(item) {
     // ... your code goes here
+
+    if (this.canPush()) {
+       this.stackControl.push(item);
+       return this.stackControl;
+    }
+    
+    if(this.stackControl.length === this.MAX_SIZE) {
+      return 'Stack Overflow';
+    }
+    
   }
 
   pop() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE && this.stackControl.length > 0) {
+      return this.stackControl.pop();
+    } else {
+      return "Stack Underflow";
+    }
   }
 }
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -10,29 +10,72 @@ const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
   // ... your code goes here
+  stackInput.value = "";
 };
 
-const renderListStack = () => {
+const renderListStack = (elemnt) => {
   // ... your code goes here
+  const newLine = document.createElement('li');
+  newLine.setAttribute('class', 'inactive');
+  stackList.appendChild(newLine);
+  
 };
 
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
+renderListStack();
 renderListStack();
 
 const generateWarningStack = type => {
   if (type === 'underflow') {
     // ... your code goes here
+    warningBottomStack.innetHTML = 'underflow'
+    warningBottomStack.style.display = 'block'
+    
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopStack.style.display = 'block'
+    warningTopStack.innetHTML = 'overflow'
+    
   }
 };
 
+const liStack = document.querySelector('.inactive')
+
 const addToStack = () => {
   // ... your code goes here
+
+  newStack.push(liStack)
+  liStack.setAttribute('class', 'active');
+  warningBottomStack.style.display = 'none'
+  if (newStack.push() === 'Stack Overflow') {
+    generateWarningStack('overflow')
+  }
 };
 
-const removeFromStack = () => {
+const removeFromStack = (event) => {
   // ... your code goes here
+  if (newStack.pop() === "Stack Underflow") {
+    generateWarningStack('underflow')
+  } else {
+    liStack.setAttribute('class', 'inactive');
+    
+  if (newStack.stackControl.length <= 0) {
+    warningTopStack.style.display = 'none';
+      //console.log(newStack.stackControl.length)
+  }
+    //renderListStack()
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
 takeStackBtn.addEventListener('click', removeFromStack);
+
+
+// console.log(liStack.style)
