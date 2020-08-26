746e1dddb73c255951dd598a5f7a46e9cb5a65ba
Pedro Conde <condepedro968@gmail.com>
2020-08-26 22:48:07 +0200

    adding changes

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -4,23 +4,30 @@ class StackDataStructure {
     this.MAX_SIZE = 10;
   }
 
-  canPush() {
-    // ... your code goes here
+  canPush() {               //Para checkear si podemos añadir elementos en stack para evitar buff. overflow
+    if (this.stackControl.length < this.MAX_SIZE) return true
+    return false
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }
 
-  isEmpty() {
-    // ... your code goes here
+  isEmpty() {               //Para checkear si podemos obtener elementos del stack para evitar buff. underflow
+    if (this.stackControl.length === 0) return true
+    return false
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()){
+        this.stackControl.push(item)
+        return this.stackControl
+    }
+    return 'Stack Overflow'
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) return 'Stack Underflow'
+    return this.stackControl.pop()
   }
 }
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -9,29 +9,58 @@ const takeStackBtn = document.getElementById('take-stack');
 const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  stackInput.value = '';
 };
 
 const renderListStack = () => {
-  // ... your code goes here
-};
+  warningTopStack.style.display = 'none';
+  warningBottomStack.style.display = 'none';
+  stackList.innerHTML = '';
+  let length = newStack.display().length;
+  let size = 10 - length;
+
+  newStack.display().forEach(item => {     // Crea un tag li por cada elemento que se meta en del array
+    let li = document.createElement('li');
+    li.innerHTML = item;
+    li.className = 'active';
+    stackList.appendChild(li);   // Este metodo agrega un nodo alfinal de la lista de un elemento hijo de un elemeto padre especif.
+  });
+  
+  for (let i = 0; i < size ; i++) {   // Crea el tag li segun la longitud del array (arr size = 10)
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    stackList.appendChild(li);
+  }
+
+}
 
 renderListStack();
 
 const generateWarningStack = type => {
-  if (type === 'underflow') {
-    // ... your code goes here
-  } else if (type === 'overflow') {
-    // ... your code goes here
+  if (type === 'Stack Underflow') {
+    warningBottomStack.style.display = 'block';
+    warningBottomStack.innerText = type;
+  } else if(type === 'Stack Overflow') {
+    warningTopStack.style.display = 'block';
+    warningTopStack.innerText = type;
   }
 };
 
 const addToStack = () => {
-  // ... your code goes here
+  if (newStack.push(stackInput.value) === 'Stack Overflow') {
+    generateWarningStack('Stack Overflow');
+  } else {
+    clearStackInput();
+    renderListStack();
+  }
 };
 
 const removeFromStack = () => {
-  // ... your code goes here
+  if (newStack.pop() === 'Stack Underflow') {
+    generateWarningStack('Stack Underflow');
+  } else {
+    renderListStack();
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
