8a1c68583ea2cb9c0a2feed52ddff86d744973bf
JuanBatkis <juanbatkis@gmail.com>
2020-10-12 17:48:01 -0300

    All tasks done

--- js/queue/queue-dom.js
+++ js/queue/queue-dom.js
@@ -8,29 +8,55 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new QueueDataStructure();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  const value = queueInput.value;
+  queueInput.value = '';
+  return value;
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  for (let index = 0; index < queue.MAX_SIZE; index++) {
+    queueUL.innerHTML += `<li class="inactive"></li>`;
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = type => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.innerHTML = 'underflow';
+    warningBottomQueue.style.display = 'block';
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.innerHTML = 'overflow';
+    warningTopQueue.style.display = 'block';
   }
 };
 
 const addToQueue = () => {
-  // ... your code goes here
+  if (queue.canEnqueue()) {
+    queue.enqueue(clearQueueInput());
+    warningBottomQueue.style.display = 'none';
+    const items = document.querySelectorAll('#queue-list li');
+
+    for (let index = 0; index < queue.queueControl.length; index++) {
+      items[index].innerHTML = queue.queueControl[index];
+      items[index].classList = 'active';
+    }
+  } else {
+    generateWarningQueue('overflow');
+  }
 };
 
 const removeFromQueue = () => {
-  // ... your code goes here
+  if (queue.isEmpty()) {
+    generateWarningQueue('underflow');
+  } else {
+    warningTopQueue.style.display = 'none';
+    const items = document.querySelectorAll('#queue-list .active');
+    const lastItem = items[items.length - 1];
+    lastItem.classList = 'inactive';
+    lastItem.innerHTML = '';
+    queue.dequeue();
+  }
 };
 
 addQueue.addEventListener('click', addToQueue);
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -9,29 +9,53 @@ const takeStackBtn = document.getElementById('take-stack');
 const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  const value = stackInput.value;
+  stackInput.value = '';
+  return value;
 };
 
 const renderListStack = () => {
-  // ... your code goes here
+  for (let index = 0; index < newStack.MAX_SIZE; index++) {
+    stackList.innerHTML += '<li class="inactive"></li>'
+  }
 };
 
 renderListStack();
 
 const generateWarningStack = type => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomStack.innerHTML = 'underflow';
+    warningBottomStack.style.display = 'block';
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopStack.innerHTML = 'overflow';
+    warningTopStack.style.display = 'block';
   }
 };
 
 const addToStack = () => {
-  // ... your code goes here
+  if (newStack.canPush()) {
+    warningBottomStack.style.display = 'none';
+    const firstItem = document.querySelector('#stack-list .inactive');
+    firstItem.classList = 'active';
+    const value = clearStackInput();
+    firstItem.innerHTML = value;
+    newStack.push(value);
+  } else {
+    generateWarningStack('overflow');
+  }
 };
 
 const removeFromStack = () => {
-  // ... your code goes here
+  if (newStack.isEmpty()) {
+    generateWarningStack('underflow');
+  } else {
+    warningTopStack.style.display = 'none';
+    const items = document.querySelectorAll('#stack-list .active');
+    const lastItem = items[items.length - 1];
+    lastItem.classList = 'inactive';
+    lastItem.innerHTML = '';
+    newStack.pop();
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
