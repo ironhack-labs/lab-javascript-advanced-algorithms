b4fe64f2640e54a8c9e10d5c92c496d7c9386544
Roma Dorado <doradoroma@gmail.com>
2020-10-28 23:50:59 +0100

    [WEB-MAD-DE]Jose Dorado

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,22 +5,48 @@ class QueueDataStructure {
   }
 
   display() {
+    return this.queueControl
     // ... your code goes here
   }
 
   canEnqueue() {
+
+    if (this.queueControl.length = this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
     // ... your code goes here
   }
 
   isEmpty() {
+
+    if (this.canEnqueue >= 0) {
+      return false
+    } else {
+      return true
+    }
     // ... your code goes here
   }
 
   enqueue(item) {
+
+    if (this.canEnqueue()) {
+      this.queueControl.shift(item)
+      return this.queueControl
+    } else {
+      return 'Queue Overflow'
+    }
     // ... your code goes here
   }
 
   dequeue() {
+    if (this.dequeue()) {
+      this.queueControl.length = 0
+      return 'Queue Underflow'
+    } else {
+      return this.queueControl.pop()
+    }
     // ... your code goes here
   }
 }
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,22 +5,51 @@ class StackDataStructure {
   }
 
   canPush() {
+
+    if (this.stackControl < 10) {
+      return true
+    } else {
+      return false
+    }
+
     // ... your code goes here
   }
 
   display() {
+
+  return this.stackControl
     // ... your code goes here
   }
 
   isEmpty() {
+
+    if (this.stackControl <= 0) {
+      return true
+    } else {
+      return false
+    }
+
     // ... your code goes here
   }
 
   push(item) {
+      
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      return 'Stack Overflow'
+
+    }
     // ... your code goes here
   }
 
   pop() {
+    if (this.stackControl.length === 0) {
+      return 'Stack Overflow'
+    } else {
+      return this.stackControl.pop()
+    }
     // ... your code goes here
   }
 }
