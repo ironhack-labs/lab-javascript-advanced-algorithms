6e7fdd384329a5af18348259b59f0980e4f9f157
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-28 17:25:02 +0100

    commit

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -26,7 +26,7 @@ class StackDataStructure {
 
   isEmpty() {
     // ... your code goes here
-    if(this.stackControl.lenght >= 0){ 
+    if(this.stackControl.lenght <= 0){ 
     return true
   } else {
     return false
