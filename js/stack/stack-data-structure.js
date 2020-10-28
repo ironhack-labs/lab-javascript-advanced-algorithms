570a02d6074ecb6406d39cf2e8440f7dfdf1be7d
manuelbarreda <manubarreda69@gmail.com>
2020-10-28 18:15:31 +0100

    add stack logic

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,21 +6,45 @@ class StackDataStructure {
 
   canPush() {
     // ... your code goes here
+    
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    };
+
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    };
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush() === true) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
   }
 
   pop() {
     // ... your code goes here
+   if (this.isEmpty() === true) {
+     return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
   }
 }
