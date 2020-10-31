aaeaee0e2806508b82e2d71081ba7ca21d1b9559
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-31 16:33:51 -0600

    Stack methods usage Should add the indicated element to the stack Should return that stack is not empty when adding an element Should return the stack when an element is inserted Should insert the elements in the received order Should return 'Stack Overflow' if the stack is full Should return false if the stack is full

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,7 +6,13 @@ class StackDataStructure {
   //() to check if we can add new elements to the stack (to avoid the stack overflow), a
   canPush() {
     // ... your code goes here
-    return true
+
+    if (this.MAX_SIZE <= this.stackControl.length) {
+      return false;
+    } else {
+
+      return true
+    }
   }
 
   display() {
@@ -15,10 +21,10 @@ class StackDataStructure {
   // check if we can take an element from the stack (to prevent the stack underflow).
   isEmpty() {
     // ... your code goes here
-    if (this.stackControl.length == 0 ) {
+    if (this.stackControl.length == 0) {
 
       return true
-    }else {
+    } else {
       return false
     }
 
@@ -27,8 +33,8 @@ class StackDataStructure {
 
   push(item) {
     // ... your code goes here
-    if (this.MAX_SIZE == this.stackControl.length  ) {
-      return  "Stack Overflow"
+    if (this.MAX_SIZE == this.stackControl.length) {
+      return "Stack Overflow"
     } else {
       this.stackControl.push(item);
 
