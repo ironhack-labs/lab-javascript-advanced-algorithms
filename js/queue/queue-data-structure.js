033e9ab2205250641c669f5650368c67fb9ce0f6
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-31 16:53:14 -0600

    Queue methods usage Should enqueue the indicated element to the queue Should return that queue is not empty when enqueuing an element Should return the queue when an element is queued

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -33,6 +33,16 @@ class QueueDataStructure {
 
   enqueue(item) {
     // ... your code goes here
+    if (this.MAX_SIZE == this.queueControl.length) {
+      return "Stack Overflow"
+    } else {
+      this.queueControl.push(item);
+
+      this.queueControl[this.queueControl.length - 1]
+
+      return this.queueControl
+    }
+
   }
 
   dequeue() {
