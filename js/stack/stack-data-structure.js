448060c162863105919e77c213eadd12c48124ce
asierbayon <bayonasier@gmail.com>
2020-11-19 17:02:13 +0100

    feat: iterations 1 & 2

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,22 +5,25 @@ class StackDataStructure {
   }
 
   canPush() {
-    // ... your code goes here
+    return (this.stackControl.length < this.MAX_SIZE);
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return (this.stackControl.length === 0)
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl; 
+    } return 'Stack Overflow';
   }
 
   pop() {
-    // ... your code goes here
+    return this.isEmpty() ? 'Stack Underflow' : this.stackControl.pop();
   }
 }
