3f76e7499eb6d2907fe63e700d1d40ec182aa9a1
Abel Andrés <abelandres.gtc@gmail.com>
2021-01-20 20:58:22 +0100

    add stack logic

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,21 +6,45 @@ class StackDataStructure {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
   }
 
   display() {
     // ... your code goes here
+    
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
     // ... your code goes here
+    if (this.stackControl.length <= this.MAX_SIZE) {
+      this.stackControl.push(item)
+      if (this.stackControl.length > this.MAX_SIZE) {
+        return `Stack Overflow`
+      }
+      return this.stackControl      
+    }
   }
 
   pop() {
     // ... your code goes here
+    if (this.stackControl.length >= 0) {      
+      if (this.stackControl.length <= 0) {
+        return `Stack Underflow`
+      }
+      return this.stackControl.pop()
+    }
   }
 }
