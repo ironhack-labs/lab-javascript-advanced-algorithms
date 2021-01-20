d0bc7d13e8239c53b961c3a2b160d99e117d06d2
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-01-20 18:34:18 +0100

    Stack Jasmine test passed

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,7 +5,7 @@ class StackDataStructure {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE
   }
 
   display() {
@@ -13,14 +13,23 @@ class StackDataStructure {
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()){
+
+    this.stackControl.push(item)
+    return this.stackControl
+
+  } else {return 'Stack Overflow'}
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+
+      return 'Stack Underflow'
+      
+      } else {return this.stackControl[(this.stackControl.length-1)]}
   }
 }
