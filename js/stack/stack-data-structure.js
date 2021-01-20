9d5e597e17bf3ed917be4584c37ce5a34b3d934d
Barbara Diaz <barbarad91@gmail.com>
2021-01-20 14:08:01 +0100

    Create canPush and push methods

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,26 +1,31 @@
 class StackDataStructure {
-  constructor() {
-    this.stackControl = [];
-    this.MAX_SIZE = 10;
-  }
+    constructor() {
+        this.stackControl = [];
+        this.MAX_SIZE = 10;
+    }
 
-  canPush() {
-    // ... your code goes here
-  }
+    canPush() {
+        return (this.stackControl.length < this.MAX_SIZE)
+    }
 
-  display() {
-    // ... your code goes here
-  }
+    display() {
+        // ... your code goes here
+    }
 
-  isEmpty() {
-    // ... your code goes here
-  }
+    isEmpty() {
+        return (this.stackControl.length === 0)
+    }
 
-  push(item) {
-    // ... your code goes here
-  }
+    push(item) {
+        if (this.canPush()) {
+            this.stackControl[this.stackControl.length] = item
+            return this.stackControl
+        } else {
+            return 'Stack Overflow'
+        }
+    }
 
-  pop() {
-    // ... your code goes here
-  }
+    pop() {
+        // ... your code goes here
+    }
 }
