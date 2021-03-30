488047140b9508478e206697c5e0ed0c10724fc1
Silinde87 <paurodriguezmolina@gmail.com>
2021-03-30 16:46:41 +0200

    Done Iteration 1

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,26 +1,29 @@
 class StackDataStructure {
-  constructor() {
-    this.stackControl = [];
-    this.MAX_SIZE = 10;
-  }
+	constructor() {
+		this.stackControl = [];
+		this.MAX_SIZE = 10;
+	}
 
-  canPush() {
-    // ... your code goes here
-  }
+	canPush() {
+		return this.stackControl.length === this.MAX_SIZE ? false : true;
+	}
 
-  display() {
-    // ... your code goes here
-  }
+	display() {
+		// ... your code goes here
+	}
 
-  isEmpty() {
-    // ... your code goes here
-  }
+	isEmpty() {
+		return this.stackControl.length === 0 ? true : false;
+	}
 
-  push(item) {
-    // ... your code goes here
-  }
+	push(item) {
+		if (!this.canPush()) return "Stack Overflow";
+		this.stackControl.push(item);
+		return this.stackControl;
+	}
 
-  pop() {
-    // ... your code goes here
-  }
+	pop() {
+		if (this.isEmpty()) return "Stack Underflow";
+		return this.stackControl.pop();
+	}
 }
