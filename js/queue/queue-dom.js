ad287464eeac182ba75b3ce01e4a4527e61a503f
Mario Ortiz <ortiz.mario22@gmail.com>
2021-03-31 20:53:07 +0200

    just in time

--- js/queue/queue-dom.js
+++ js/queue/queue-dom.js
@@ -9,28 +9,91 @@ const queue = new QueueDataStructure();
 
 const clearQueueInput = () => {
   // ... your code goes here
+  inputName = queueInput.value = ""
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+  inputName = queueInput.value
+  return inputName
+
 };
 
 generateListQueue();
 
 const generateWarningQueue = type => {
   if (type === 'underflow') {
+
     // ... your code goes here
+    warningBottomQueue.style.display = "block"
+    warningBottomQueue.textContent = 'underflow'
+
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopQueue.style.display = "block"
+    warningTopQueue.textContent = 'overflow'
   }
 };
 
 const addToQueue = () => {
   // ... your code goes here
+  inputName = generateListQueue()
+
+  if (queue.canEnqueue()) {
+    queueArray = queue.enqueue(1)
+    const liElems = document.querySelectorAll("#queue-list> li")
+    warningBottomQueue.style.display = "none"
+    warningBottomQueue.textContent = ''
+
+    if (queueArray.length > 1) {
+
+      for (let i = queueArray.length - 1; i >= 1; i--) {
+
+        liElems[i].textContent = liElems[i - 1].textContent
+        liElems[i].classList.add('active')
+        liElems[i].classList.remove('inactive')
+
+      }
+    }
+
+    liElems[0].classList.add('active')
+    liElems[0].classList.remove('inactive')
+    liElems[0].textContent = inputName
+
+    clearQueueInput()
+
+  }
+  else {
+
+    value = 'overflow'
+    generateWarningQueue(value)
+  }
+
 };
 
 const removeFromQueue = () => {
   // ... your code goes here
+  if (queue.isEmpty()) {
+
+    value = 'underflow'
+    generateWarningQueue(value)
+
+  } else {
+
+    queue.dequeue()
+    queueArray = queue.queueControl.shift()
+
+    queueLength = queue.queueControl.length
+    let liElemsActive = document.querySelectorAll("#queue-list> li.active")
+
+    warningTopQueue.style.display = "none"
+    warningTopQueue.textContent = ''
+    liElemsActive[queueLength].classList.add('inactive')
+    liElemsActive[queueLength].classList.remove('active')
+    liElemsActive[queueLength].textContent = ''
+
+  }
+
 };
 
 addQueue.addEventListener('click', addToQueue);
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -10,28 +10,82 @@ const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
   // ... your code goes here
+  inputName = stackInput.value = ""
 };
 
 const renderListStack = () => {
   // ... your code goes here
+  inputName = stackInput.value
+  //clearStackInput()
+  return inputName
 };
 
+let dangerBottom = document.querySelector("#stack-container>.warning-bottom")
 renderListStack();
 
 const generateWarningStack = type => {
+
   if (type === 'underflow') {
     // ... your code goes here
+    dangerBottom.style.display = "block"
+    dangerBottom.textContent = 'underflow'
+
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopStack.style.display = "block"
+    warningTopStack.textContent = 'overflow'
+
   }
 };
 
+
 const addToStack = () => {
+  inputName = renderListStack()
+
   // ... your code goes here
+  if (newStack.canPush()) {
+
+    dangerBottom.style.display = "none"
+    dangerBottom.textContent = ''
+    stackArray = newStack.push(1)
+    const liElems = document.querySelectorAll("#stack-list> li")
+
+    liElems[stackArray.length - 1].classList.add('active')
+    liElems[stackArray.length - 1].classList.remove('inactive')
+    liElems[stackArray.length - 1].textContent = inputName
+    clearStackInput()
+
+  } else {
+    type = 'overflow'
+    generateWarningStack(type)
+
+  }
+
 };
 
 const removeFromStack = () => {
   // ... your code goes here
+  if (newStack.isEmpty()) {
+
+    type = 'underflow'
+    generateWarningStack(type)
+
+  } else {
+
+    stackArray = newStack.stackControl
+    popElmt = stackArray.pop()
+
+    let liElemsActive = document.querySelectorAll("#stack-list> li.active")
+    console.log(liElemsActive);
+    const activeElm = liElemsActive.length
+
+    warningTopStack.style.display = "none"
+    warningTopStack.textContent = ''
+    liElemsActive[activeElm - 1].classList.add('inactive')
+    liElemsActive[activeElm - 1].classList.remove('active')
+    liElemsActive[activeElm - 1].textContent = ""
+
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
