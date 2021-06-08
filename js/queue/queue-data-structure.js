7498b1dc95ece2529339fa1d90b1edd699500c8a
Pablo Molleda <pmolledadelrio@gmail.com>
2021-06-08 18:44:06 +0200

    queue done

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,22 +5,48 @@ class Queue {
   }
 
   canEnqueue() {
+
+    if (this.queueControl.length >= this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
     // ... your code goes here
   }
 
   isEmpty() {
+    if (this.queueControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
     // ... your code goes here
   }
 
   enqueue(item) {
+    if (!this.canEnqueue()) {
+      throw new Error('QUEUE_OVERFLOW') 
+    }
+
+    this.queueControl.push(item)
+      
+      return this.queueControl
     // ... your code goes here
   }
 
   dequeue() {
+    if (!!this.isEmpty()) {
+      throw new Error('QUEUE_UNDERFLOW')
+    } else {
+
+    return this.queueControl.shift()
+
+  }
     // ... your code goes here
   }
 
   display() {
+    return this.queueControl
     // ... your code goes here
   }  
 }
