2ae09526cd47485669902660793c6cdfeb056826
pablorodri <pablorodri.r@gmail.com>
2021-06-08 18:27:41 +0200

    Solved lab

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,28 +1,50 @@
 class Stack {
   constructor() {
+    //PROPIEDADES = VALORES
+    //valores dentro del constructor
     this.stackControl = [];
     this.MAX_SIZE = 10;
   }
 
+  //METODOS = FUNCTIONES
+  //fuera del constructor funciones del obj
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else return false
   }
 
   isEmpty() {
+    if (this.stackControl.length == 0) {
+      return true
+    } else return false
     // ... your code goes here
   }
 
   push(item) {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else { throw new Error('STACK_OVERFLOW') };
+
+
     // ... your code goes here
   }
 
-  pop() {
+  pop(item) {
+    if (this.stackControl.length == 0) {
+      throw new Error('STACK_UNDERFLOW')
+    } else {
+      let x = this.stackControl.pop(item)
+      return x
+    }
+
     // ... your code goes here
   }
-
   display() {
+    return this.stackControl
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
