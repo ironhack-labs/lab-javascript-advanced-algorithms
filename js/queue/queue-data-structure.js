e8c08c02e5e90f6be6aab1927570f028eb53959a
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-09 00:41:54 +0200

    WDFTMAY2021 ENRIQUE VOZMEDIANO

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,24 +5,59 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
-  }
 
+    if (this.queueControl < this.MAX_SIZE) {
+      return true
+    } else if (this.queueControl = 10) {
+      return false
+    }
+    //return ((this.queueControl < this.MAX_SIZE === true) && (this.queueControl = 0 === true)) //|| (this.queueControl >= 10 === false))
+    //   if (this.queueControl < this.MAX_SIZE) {
+    //     ///=== true && this.queueControl >= 10 === true)
+    //     // ... your code goes here
+    //   } else (this.queueControl >= 10)
+    //   return false
+    // } 
+  }
   isEmpty() {
+
+    return (this.queueControl = 0 === true || this.queueControl > 0 === false) //sorprendentemente se escribir esto y me resuelve mas problemas.
+    // if (this.queueControl = 0) {
+    //   return true
+    // } else {
+    //   return false //practicando
+    // }
+    // }                            /no funciona
+    // if (this.queueControl > 0) {
+    //   return false
+    // }
     // ... your code goes here
   }
 
   enqueue(item) {
+    if (this.canEnqueue) {
+      this.queueControl.push(item)
+    } else ("QUEUE_OVERFLOW")
+
+
     // ... your code goes here
   }
 
   dequeue() {
-    // ... your code goes here
+    // ... your code goes here array.splice(start, deleteCount)
+    // Como no he sabido devolver los items restantes de los anteriores aquí no llego.
+    if (this.queueControl >= 10) {
+      this.queueControl.splice(0, 1)
+      return this.queueControl //
+
+    }
+
+
   }
 
   display() {
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,24 +5,57 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return (this.stackControl < this.MAX_SIZE)
+
+
+
+
+    // if ( < 10) {
+    //   return true
+    // } if else (Stack > 0){
+    //   return true
+    // } else
+    // ... your code goes here 
+    //!!20.31!!
   }
 
   isEmpty() {
+    if (this.stackControl = 0 || this.stackControl != 0) {
+      return this.stackControl
+    }
+
+    //return (this.stackControl = 0 === true || this.stackControl != 0 != true)
+    //return this.stackControl.length === 0 hours later
+    //primer día que uso === !=
+
     // ... your code goes here
   }
 
   push(item) {
+
+    if
+    //return (this.canPush === this.stackControl.push(item))
+
+    // if (this.stackControl < this.MAX_SIZE) {
+    //   this.stackControl.push(item)
+    // } return this.stackControl.length // PORQUE NO TIENE SENTIDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO should return the stack when an element is inserted
+
+
+    //
+    // HE INTENTADO QUE DEVOLVIERA LA LONGITUD DE this.MAX_SIZE PERO HA SIDO EN VANO .length not works // mucho
+
+
+
+
     // ... your code goes here
   }
-
   pop() {
     // ... your code goes here
   }
 
   display() {
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
