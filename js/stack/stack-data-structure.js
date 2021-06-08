5b4b31a8e6a4c073c5645c502f3ae199cbe0f7db
Pablo Molleda <pmolledadelrio@gmail.com>
2021-06-08 18:22:11 +0200

    stack done

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,24 +5,49 @@ class Stack {
   }
 
   canPush() {
+
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
     // ... your code goes here
   }
 
   isEmpty() {
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
     // ... your code goes here
   }
 
   push(item) {
+    if (!this.canPush()) {
+      throw new Error('STACK_OVERFLOW') 
+    }
+    this.stackControl.push(item)
+      
+      return this.stackControl
+
+    }
     // ... your code goes here
-  }
+
 
   pop() {
+    if (!!this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW')
+    }
+
+    return this.stackControl.pop()
     // ... your code goes here
   }
 
   display() {
+    return this.stackControl
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
