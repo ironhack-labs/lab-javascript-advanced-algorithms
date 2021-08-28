fe23d1ecf44d0a87e453137133de9e0feb76d304
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-28 21:18:22 +0200

    Iteration 2 completed

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -1,28 +1,43 @@
 class Queue {
-  constructor() {
-    this.queueControl = [];
-    this.MAX_SIZE = 10;
-  }
+	constructor() {
+		this.queueControl = [];
+		this.MAX_SIZE = 10;
+	}
 
-  canEnqueue() {
-    // ... your code goes here
-  }
+	canEnqueue() {
+		// ... your code goes here
+		return this.queueControl.length === this.MAX_SIZE ? false : true;
+	}
 
-  isEmpty() {
-    // ... your code goes here
-  }
+	isEmpty() {
+		// ... your code goes here
+		return this.queueControl.length != 0 ? false : true;
+	}
 
-  enqueue(item) {
-    // ... your code goes here
-  }
+	enqueue(item) {
+		// ... your code goes here
+		if (this.canEnqueue()) {
+			this.queueControl.push(item);
+		} else {
+			throw new Error('QUEUE_OVERFLOW');
+		}
 
-  dequeue() {
-    // ... your code goes here
-  }
+		return this.queueControl;
+	}
 
-  display() {
-    // ... your code goes here
-  }  
+	dequeue() {
+		// ... your code goes here
+		if (this.isEmpty()) {
+			throw new Error('QUEUE_UNDERFLOW');
+		} else {
+			return this.queueControl.shift();
+		}
+	}
+
+	display() {
+		// ... your code goes here
+		return this.queueControl;
+	}
 }
 
 // This is required to enable the automated tests, please ignore it.
