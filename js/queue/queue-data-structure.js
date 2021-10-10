53025a4c79c478dec7c1a9fc6c8955d54b886ce5
Brigi Horvath <brigi.horvath82@gmail.com>
2021-10-10 15:12:06 +0200

    Iteration 2

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,24 +5,33 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    return this.queueControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.queueControl.length === 0;
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item);
+    } else {
+      throw new Error('QUEUE_OVERFLOW');
+    }
+    return this.queueControl;
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('QUEUE_UNDERFLOW');
+    } else {
+      return this.queueControl.shift();
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
