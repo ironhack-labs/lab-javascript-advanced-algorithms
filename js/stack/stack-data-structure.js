f8c9c4bfcd40b6d03482439bb82bd63e184c34de
Brigi Horvath <brigi.horvath82@gmail.com>
2021-10-10 14:34:07 +0200

    Iteration 1

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,24 +5,33 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
+    return this.stackControl;
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW');
+    } else {
+      return this.stackControl.pop();
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
