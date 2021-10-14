44df6e21be640f22a08938a0e7b1f5c841121d3a
Philipp Kastl <picastle@posteo.de>
2021-10-14 16:51:22 +0200

    add iteration 1

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,24 +5,36 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return !(this.MAX_SIZE <= this.stackControl.length);
   }
 
   isEmpty() {
     // ... your code goes here
+    return !this.stackControl.length != 0;
   }
 
   push(item) {
     // ... your code goes here
+    if (!this.canPush()) {
+      throw new Error('STACK_OVERFLOW');
+    }
+    this.stackControl.push(item);
+
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW');
+    }
+    return this.stackControl.pop();
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
