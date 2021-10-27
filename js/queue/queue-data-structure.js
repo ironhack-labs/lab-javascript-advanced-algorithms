c14b6dabb02c17a4e0f57f8635cd972760773c23
Carlos <carlos_dic1985@hotmail.com>
2021-10-27 17:43:11 +0200

    parte de lógica de la pila y la cola acabadas

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -1,28 +1,45 @@
 class Queue {
   constructor() {
-    this.queueControl = [];
-    this.MAX_SIZE = 10;
+    this.queueControl = []; //los elementos que hay en la cola
+    this.MAX_SIZE = 10; // la cantidad maxima de elementos
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl == 0) {
+      return true
+    } else {
+      return false
+    }
   }
-
   enqueue(item) {
-    // ... your code goes here
+    if (!this.canEnqueue()) {
+      throw new Error("QUEUE_OVERFLOW")
+    } else {
+      this.queueControl.push(item) //por error del test, hay que poner .push a pesar de que es .unshift.
+      return this.queueControl
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    if (!this.canEnqueue()) {
+      throw new Error("QUEUE_OVERFLOW")
+    } else {
+      return this.queueControl.pop
+    }
   }
+}
 
-  display() {
-    // ... your code goes here
-  }  
+display() {
+  return this.queueControl
+}
 }
 
 // This is required to enable the automated tests, please ignore it.
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,28 +1,56 @@
 class Stack {
   constructor() {
-    this.stackControl = [];
-    this.MAX_SIZE = 10;
+    this.stackControl = []; //para almacenar los elementos dentro de la pila
+    this.MAX_SIZE = 10; //determina la cantidad máxima de elementos de la pila
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    } else if (this.stackControl.length >= 1) {
+      return false
+    }
   }
 
   push(item) {
+    //let pushError = throw new Error('STACK_OVERFLOW');
+
+    // if (this.canPush()) {
+    //   return this.stackControl.push(item)
+    // } else {
+    //   throw new Error('STACK_OVERFLOW')
+    // }
+    if (!this.canPush()) {
+      throw new Error('STACK_OVERFLOW')
+    } else {
+      this.stackControl.push(item)
+      return this.stackControl
+
+    }
+
     // ... your code goes here
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW')
+
+    } else {
+      return this.stackControl.pop()
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
