a5102cc1dd1d299a04258e9b44cc30095568e32e
Lisa Medina Gutiérrez <lisa.medinag@gmail.com>
2021-10-27 17:22:43 +0200

    Iteration 1 & 2 finished

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,24 +5,50 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl.length === this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item);
+      return this.queueControl;
+    } else {
+      throw new Error('QUEUE_OVERFLOW');
+
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    // if (this.isEmpty() === "false"){
+    //   const itemPoped = this.queueControl.pop();
+    //   return itemPoped;
+    // } else {
+    //   throw new Error('QUEUE_UNDERFLOW');
+    // }
+
+    if (this.isEmpty()){
+      throw new Error('QUEUE_UNDERFLOW');
+    } else {
+      const itemPoped = this.queueControl.shift();
+      return itemPoped;
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,23 +5,44 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length === this.MAX_SIZE){
+      return false;
+    } else {
+      return true;
+    } 
   }
 
+  
+
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0){
+      return true;
+    } else {
+      return false;
+    } 
   }
 
   push(item) {
-    // ... your code goes here
+    
+    if (this.canPush()){
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
+   
   }
-
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()){
+      throw new Error('STACK_UNDERFLOW');
+    } else {
+      const itemPoped = this.stackControl.pop();
+      return itemPoped;
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
   }  
 }
 
