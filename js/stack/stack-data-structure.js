dbe49fc2fc7f9c50909c8d2dba877936d34e76c1
Fernando Cardona <fernandocardona@MacBook-Pro-de-FernandoCardona.local>
2021-10-27 18:07:03 +0200

    done half

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,23 +5,54 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    //... your code goes here
+    // 
+    if (this.stackControl.length < this.MAX_SIZE){
+      console.log('hola2');
+      return true;  
+    }
+   else{ 
+      return false;
+    }    
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true;
+
+    }
+    else {
+      return false
+    }
   }
 
-  push(item) {
+  push(item) { 
     // ... your code goes here
+   
+    if (this.canPush() ){ 
+       this.stackControl.push(item);
+       return this.stackControl
+
+    } else {
+      throw new Error( "STACK_OVERFLOW");
+    }
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty() === false ){ 
+      
+      return this.stackControl.pop(); 
+
+   } else {
+     throw new Error( "STACK_UNDERFLOW");
+   }
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl
   }  
 }
 
