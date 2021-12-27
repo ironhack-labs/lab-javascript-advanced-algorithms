b6fca0106ee39ace31c2b2aabd01f4db9b0862ec
ricomasgu <ricard.comas.gomez@gmail.com>
2021-12-27 21:18:25 +0100

    Iteration 2

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -6,22 +6,34 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    return this.queueControl.length!==this.MAX_SIZE;
   }
 
   isEmpty() {
     // ... your code goes here
+    return !this.queueControl.length;
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(!this.canEnqueue()){
+      throw new Error('QUEUE_OVERFLOW');
+    }
+    this.queueControl.push(item);
+    return this.queueControl;
   }
 
   dequeue() {
     // ... your code goes here
+    if(this.isEmpty()){
+      throw new Error('QUEUE_UNDERFLOW');
+    }
+    return this.queueControl.shift();
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl;
   }  
 }
 
