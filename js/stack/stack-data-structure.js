df9d64fcc48dc737dd118acefcae89a2ea1472b9
ricomasgu <ricard.comas.gomez@gmail.com>
2021-12-27 19:48:13 +0100

    Iteration 1

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -6,22 +6,34 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    return this.stackControl.length!==this.MAX_SIZE;
   }
 
   isEmpty() {
     // ... your code goes here
+    return !this.stackControl.length;
   }
 
   push(item) {
     // ... your code goes here
+    if(!this.canPush()){
+      throw new Error('STACK_OVERFLOW');
+    }
+    this.stackControl.push(item);
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    if(this.isEmpty()){
+      throw new Error('STACK_UNDERFLOW');
+    }
+    return this.stackControl.pop();
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
