cd3338e4bc905ed16cb9734adb2877bdfd820aa2
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-01-19 17:53:02 +0100

    done

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,23 +5,52 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE){
+      return true
+    } else {
+      return false
+    }
   }
+
 
   isEmpty() {
-    // ... your code goes here
-  }
+    
+    if(this.stackControl.length === 0){
+      return true
+  } else {
+      return false
 
+  } 
+  }
   push(item) {
-    // ... your code goes here
+    
+    if(this.canPush()){
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW'); 
+    }
+
   }
 
   pop() {
-    // ... your code goes here
+    
+    if(!this.isEmpty()){
+      
+      const element = this.stackControl.pop()
+      return element
+      
+    } else {
+
+      throw new Error('STACK_UNDERFLOW');
+    }
+    
+
   }
 
   display() {
-    // ... your code goes here
+    
+    return this.stackControl
   }  
 }
 
