5c527fd07d505151b967072a3a7de6c22ec1ae45
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-01-19 17:36:57 +0100

    [MAD PP] Ángela Rueda y Hiba berbe

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,19 +5,43 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    
+    if (this.stackControl.length >=this.MAX_SIZE ) {
+      return false 
+    }else { 
+      return true
+    }
   }
+    // ... your code goes here
+  
 
   isEmpty() {
+    if (this.stackControl.length ===0 ){
+      return true
+    } else { 
+      return false
+    }
     // ... your code goes here
   }
 
   push(item) {
+    if (this.canPush()) {
+       this.stackControl.push(item)
+       return this.stackControl
+    } else {
+     throw new Error('STACK_OVERFLOW');
+    
+    }
     // ... your code goes here
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty!==0()) {
+      this.stackControl.pop()
+    }else{ 
+     throw new Error('STACK_UNDERFLOW')
+    }  
+  // ... your code goes here
   }
 
   display() {
