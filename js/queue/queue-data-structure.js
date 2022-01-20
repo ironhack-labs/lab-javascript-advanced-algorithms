cc230d9a5608e362b0727ca4a291bde256cc1fca
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-01-20 22:48:21 +0100

    completed the logic, still to do the dom manipulation

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -4,24 +4,57 @@ class Queue {
     this.MAX_SIZE = 10;
   }
 
+  getLength() {
+    let counter = 0;
+    while (this.queueControl[counter] !== undefined) {
+      counter++;
+    }
+    return counter;
+  }
+
   canEnqueue() {
-    // ... your code goes here
+    let length = this.getLength();
+    if (length >= this.MAX_SIZE) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    let length = this.getLength();
+    return length === 0;
   }
 
   enqueue(item) {
-    // ... your code goes here
+    let length = this.getLength();
+    let canDo = this.canEnqueue();
+    if (canDo) {
+      this.queueControl[length] = item;
+      return this.queueControl;
+    } else {
+      throw new Error("QUEUE_OVERFLOW")
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    let empty = this.isEmpty();
+    let length = this.getLength();
+    let temporaryArray =[];
+    let removedElement = this.queueControl[0];
+    if (!empty) {
+      for (let i = 1; i < length; i++) {
+        temporaryArray[i -1] = this.queueControl[i];
+      }
+      this.queueControl = temporaryArray;
+      return removedElement; 
+    } else {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.queueControl;
   }  
 }
 
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -3,27 +3,57 @@ class Stack {
     this.stackControl = [];
     this.MAX_SIZE = 10;
   }
+  
+  getLength() {
+    let counter = 0;
+    while (this.stackControl[counter] !== undefined) {
+      counter++;
+    }
+    return counter;
+  }
 
   canPush() {
-    // ... your code goes here
+    let length = this.getLength();
+    if (length === 0) {
+      return true;
+    }
+    if (length >= this.MAX_SIZE) {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    let length = this.getLength();
+    return length === 0;
   }
 
   push(item) {
-    // ... your code goes here
+    let length = this.getLength();
+    if (length >= this.MAX_SIZE) {
+      throw new Error("STACK_OVERFLOW");
+    } else {
+      this.stackControl[length] = item;
+    return this.stackControl;
+    }
   }
 
   pop() {
-    // ... your code goes here
+    let length = this.getLength();
+    if (length === 0) {
+      throw new Error("STACK_UNDERFLOW");
+    }
+    return this.stackControl[length - 1];
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
   }  
 }
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
+
+let stack = new Stack();
+stack.stackControl = [1, 2, 3];
+let result = stack.push("Nico");
+console.log("Returned array after push: ", result);
