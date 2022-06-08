bd27338420b73ba95639128286da6e81aa41f516
CarolinaArlone <carolinaarlone@gmail.com>
2022-06-08 16:33:32 +0200

    [MAD PP 0522] Carolina Arlone y Ovi Catalin Stanescu

--- js/queue/queue-data-structure.js
+++ js/queue/queue-data-structure.js
@@ -5,24 +5,41 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    }
+    throw new Error('QUEUE_OVERFLOW')
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.isEmpty) {
+      throw new Error('QUEUE_UNDERFLOW')
+    } else {
+      return this.queueControl.shift()
+    }
+
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,28 +1,46 @@
 class Stack {
   constructor() {
-    this.stackControl = [];
-    this.MAX_SIZE = 10;
+    this.stackControl = [];//almacena elemento dentro de la pila
+    this.MAX_SIZE = 10;//cantidad máxima de elementos que puede tener una pila
   }
 
   canPush() {
-    // ... your code goes here
+
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW')
+    } else {
+      return this.stackControl.pop()
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
