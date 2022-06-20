83571b00a60c7d4b4110abf9905fe0accd1a7f8b
Kristen Wegner <klwegner@gmail.com>
2022-06-19 22:11:53 -0400

    done

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,24 +5,41 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length !== 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush() === false) {
+      throw new Error('STACK_OVERFLOW');
+    } else {
+      this.stackControl.push(item);
+    }
+    return this.stackControl;
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty() === true) {
+      throw new Error('STACK_UNDERFLOW');
+    } else {
+      return this.stackControl.pop();
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
