e6a31cb6176e31fac1c793649dfc15336c2b8a38
Chlotte <charlottealleaume99@gmail.com>
2022-08-02 17:38:20 +0200

    implementation of queues and arrays done - tested

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -4,26 +4,40 @@ class Queue {
     this.MAX_SIZE = 10;
   }
 
-  canEnqueue() {
-    // ... your code goes here
-  }
+  canEnqueue() { return (this.queueControl.length < this.MAX_SIZE); }
 
-  isEmpty() {
-    // ... your code goes here
-  }
+  isEmpty() { return (this.queueControl.length === 0); }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue() === true)
+      this.queueControl.push(item);
+    else
+      throw new Error('QUEUE_OVERFLOW');
+    return(this.queueControl);
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.isEmpty() === false)
+      return(this.queueControl.shift());
+    else
+      throw new Error('QUEUE_UNDERFLOW');
   }
 
-  display() {
-    // ... your code goes here
-  }  
+  display() { return([... this.queueControl]); }  
 }
+
+let q = new Queue();
+let arr = [1, 2, 3, 4, 5];
+for (let i of arr){
+  q.enqueue(i);
+  console.log('q -> ', q);
+}
+
+for (let i of arr){
+  q.dequeue();
+  console.log('q -> ', q);
+}
+
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Queue;
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -4,26 +4,30 @@ class Stack {
     this.MAX_SIZE = 10;
   }
 
-  canPush() {
-    // ... your code goes here
-  }
+  canPush() { return (this.stackControl.length < this.MAX_SIZE); }
 
-  isEmpty() {
-    // ... your code goes here
-  }
+  isEmpty() { return (this.stackControl.length === 0); }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush() === true)
+      this.stackControl.push(item);
+    else
+      throw new Error('STACK_OVERFLOW');
+    return(this.stackControl);
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty() === false)
+      return(this.stackControl.pop());
+    else
+      throw new Error('STACK_UNDERFLOW');
   }
 
-  display() {
-    // ... your code goes here
-  }  
+  display() { return([... this.stackControl]); }  
 }
+
+let s = new Stack();
+console.log(s.push('lol'));
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
