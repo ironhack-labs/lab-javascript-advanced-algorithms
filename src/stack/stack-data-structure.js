4204bba04cabade72ca02a93946643dab8333f81
RedValerian <r.pino.camacho@gmail.com>
2022-08-17 15:46:06 +0200

    Week 2 - Day 3

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,24 +5,29 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return !(this.stackControl.length === this.MAX_SIZE);
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0 ? true : false;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
   pop() {
-    // ... your code goes here
+    this.stackControl.pop();
   }
 
   display() {
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
