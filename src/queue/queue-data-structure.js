44d5ed6c6c17b111a2539bb295ea2f3be8ca5ba3
Daniel Gonzalez <danigonzalezsierra@gmail.com>
2022-08-17 17:12:04 +0200

    lab whitout bonnus

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,23 +5,31 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    return this.queueControl.length < this.MAX_SIZE ? true : false 
   }
-
   isEmpty() {
-    // ... your code goes here
+    return this.queueControl.length === 0 ? true : false
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if(this.canEnqueue()){
+      this.queueControl.push(item)
+      return this.queueControl
+    }else {
+      throw new Error('QUEUE_OVERFLOW');
+    } 
   }
 
   dequeue() {
-    // ... your code goes here
+   if (!this.isEmpty()) {
+     return this.queueControl.shift()
+    } else {
+      throw new Error('QUEUE_UNDERFLOW');
+     }
   }
 
   display() {
-    // ... your code goes here
+   return this.queueControl
   }  
 }
 
