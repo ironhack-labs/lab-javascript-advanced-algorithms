79de0141f15a90d307a5bbce60a234bd7b1b1a73
juca25 <jserranocucarella@gmail.com>
2022-08-17 17:11:58 +0200

    done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,33 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE 
   }
-
+  
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0 
+    
   }
 
   push(item) {
-    // ... your code goes here
-  }
-
-  pop() {
-    // ... your code goes here
-  }
+    if(this.canPush()){
+    this.stackControl.push(item)
+    return this.stackControl} 
+    else {
+      throw new Error('STACK_OVERFLOW')}
+    }
+    
+    pop() {
+      
+      if(!this.isEmpty()){
+      return  this.stackControl.pop()
+      } 
+      else {
+        throw new Error('STACK_UNDERFLOW')}
+      }
 
   display() {
-    // ... your code goes here
+   return this.stackControl   
   }  
 }
 
