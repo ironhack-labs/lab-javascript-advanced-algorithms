431e54f68746b874581f904f50b128c3bad64f4d
Amanda Ngo <amanda.ngo172@gmail.com>
2022-08-23 13:32:46 -0500

    Finished lab

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,37 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    if(this.stackControl.length == 0){
+      return true;
+    }
+    else{return false}
   }
 
   push(item) {
-    // ... your code goes here
+    if(this.canPush()){
+      this.stackControl.push(item);
+      return this.stackControl;
+    }
+    else{
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if(!this.isEmpty()){
+      return this.stackControl.pop();
+    }
+    else{
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
   }  
 }
 
