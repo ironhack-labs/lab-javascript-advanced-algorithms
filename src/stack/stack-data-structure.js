c91320f3853b8bf754d552d3c9e6b65a907e7af5
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-09-13 21:58:47 +0200

    work on 'stack iteration' but with some problem with method canPush

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,24 +6,49 @@ class Stack {
 
   canPush() {
     // ... your code goes here
-  }
+    let result;
+    if (this.stackControl.length >= 0 && this.stackControl.length < 10) { 
+      result = true;
+    } else {result = false}
+      return result
+    }
+    
 
   isEmpty() {
     // ... your code goes here
+    if(this.stackControl.length){
+      return false
+    }
+    return true
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()){
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty()){
+      throw new Error('STACK_UNDERFLOW')
+    } else {
+      console.log(this.stackControl)
+       return this.stackControl.pop()
+    }
   }
+
 
   display() {
     // ... your code goes here
+    this.stackControl.forEach((stack) => 
+     stack)
   }  
-}
 
+}
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
