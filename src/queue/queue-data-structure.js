7d0b1bddafb5a10a50997428892b4c0f6286270e
Arash Kaffashi <arash_kaffashi@hotmail.com>
2022-10-21 23:52:31 -0400

    done: Iteration 2 - Queue

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -1,29 +1,32 @@
 class Queue {
-  constructor() {
-    this.queueControl = [];
-    this.MAX_SIZE = 10;
-  }
+	constructor() {
+		this.queueControl = [];
+		this.MAX_SIZE = 10;
+	}
 
-  canEnqueue() {
-    // ... your code goes here
-  }
+	canEnqueue() {
+		return this.queueControl.length < this.MAX_SIZE;
+	}
 
-  isEmpty() {
-    // ... your code goes here
-  }
+	isEmpty() {
+		return this.queueControl.length === 0;
+	}
 
-  enqueue(item) {
-    // ... your code goes here
-  }
+	enqueue(item) {
+		if (!this.canEnqueue()) throw new Error("QUEUE_OVERFLOW");
+		this.queueControl.push(item);
+		return this.queueControl;
+	}
 
-  dequeue() {
-    // ... your code goes here
-  }
+	dequeue() {
+		if (this.isEmpty()) throw new Error("QUEUE_UNDERFLOW");
+		return this.queueControl.shift();
+	}
 
-  display() {
-    // ... your code goes here
-  }  
+	display() {
+		return this.queueControl;
+	}
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
