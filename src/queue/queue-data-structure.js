41a52a24a1b5f75d667928a3740969a58ea4d90c
Teresa Chaves <ttahaves@hotmail.com>
2022-10-25 16:52:35 +0200

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,15 +5,32 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    }
+    else {
+      throw new Error('QUEUE_OVERFLOW');
+    }
   }
 
   dequeue() {
@@ -22,7 +39,7 @@ class Queue {
 
   display() {
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
