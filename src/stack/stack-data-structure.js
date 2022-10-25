0a0ea2fadfa676085550a00f21d0b88e7d1ed6c5
Hector Briceño <hector.mario86@gmail.com>
2022-10-25 18:04:43 +0200

    "Solved lab"

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,40 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length === 0 || this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush() === true) {
+      this.stackControl.push(item)
+      return this.stackControl
+
+    } else { throw new Error('STACK_OVERFLOW') }
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty() === false) {
+      return this.stackControl.pop()
+    } else {
+      throw new Error('STACK_UNDERFLOW')
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
-
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
