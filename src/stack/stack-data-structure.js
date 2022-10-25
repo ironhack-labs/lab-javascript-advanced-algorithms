87468e1c0a7d65a766c0c4d93141e1d2d4859500
sijincodes <sijinmathews03@gmail.com>
2022-10-25 21:48:38 +0200

    WDFT Berlin OCT2022 -sijin

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,24 +6,38 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) return true;
+    return false;
   }
 
   isEmpty() {
     // ... your code goes here
+    if (!this.stackControl.length) return true;
+    else if (this.stackControl.length > 0) return false;
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    }
+    throw new Error("STACK_OVERFLOW");
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty()) throw new Error("STACK_UNDERFLOW");
+    else {
+      return this.stackControl.pop();
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
