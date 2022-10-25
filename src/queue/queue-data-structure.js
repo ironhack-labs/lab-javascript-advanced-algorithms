a1d9b6c3ca80daf5685f89dee6fd7b739932d1c2
Alberto Naval <albertonaval.dev@gmail.com>
2022-10-25 16:53:32 +0200

    Second Iteration

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,23 +6,32 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    return this.queueControl < this.MAX_SIZE
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.queueControl.length === 0
   }
 
   enqueue(item) {
     // ... your code goes here
+      if (this.canEnqueue()) {
+      this.queueControl.unshift(item)
+      return this.queueControl
+    } else {
+      throw new Error('QUEUE_OVERFLOW')
+    }
   }
 
   dequeue() {
     // ... your code goes here
+
   }
 
   display() {
     // ... your code goes here
-  }  
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,23 +6,41 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.stackControl.length === 0
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
+
+
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW')
+    } else {
+      return this.stackControl.pop()
+      //return this.stackControl[this.stackControl.length-1]
+    }
   }
+
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
