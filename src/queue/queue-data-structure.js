58c17bdb57749153bd0b974a7990596643d30318
CibránMC <cibran.melendez@gmail.com>
2022-10-25 18:36:14 +0200

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,23 +5,32 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if(this.queueControl.length < this.MAX_SIZE) return true
+    else return false
   }
 
   isEmpty() {
-    // ... your code goes here
+    if(this.queueControl.length === 0)
+      return true
+    else return false
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if(this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    }else throw new Error ("QUEUE_OVERFLOW")
   }
 
   dequeue() {
-    // ... your code goes here
+    if(!this.isEmpty()) {
+    let remove = this.queueControl.shift()
+    return remove
+    }else throw Error("QUEUE_UNDERFLOW")
   }
 
   display() {
-    // ... your code goes here
+    return this.queueControl
   }  
 }
 
