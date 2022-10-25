a111f671a6ad96e418fda50b5860dbb825d4f82a
NaomiBurguesMontesdeoca <naomi.burgues@gmail.com>
2022-10-25 16:51:35 +0200

    Solved lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,25 +5,46 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    }
+    else {
+      throw new Error('QUEUE_OVERFLOW');
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.queueControl.pop()
+
+    }
+    else {
+      throw new Error('QUEUE_UNDERFLOW')
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
+
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Queue;
