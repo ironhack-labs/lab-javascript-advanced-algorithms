9211804fa35bd3c93112ea4f31014c2f69898119
ssafali <safaksafali@gmail.com>
2022-10-25 16:12:56 +0200

    BER-OCT-22. Solved lab. Safak.

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,40 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if(this.queueControl.length >= this.MAX_SIZE) {
+      return false;
+    }
+    return true;
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.queueControl.length === 0;
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(this.canEnqueue()) {
+      this.queueControl.push(item);
+    } else if (!this.canEnqueue()) {
+      throw new Error("QUEUE_OVERFLOW");
+    }
+    return this.queueControl;
   }
 
   dequeue() {
     // ... your code goes here
+    if(this.isEmpty()) {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
+    let toBeRemoved = this.queueControl[0];
+    this.queueControl.shift();
+    return toBeRemoved;
   }
 
   display() {
     // ... your code goes here
+    this.queueControl;
   }  
 }
 
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,39 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if(this.stackControl.length >= this.MAX_SIZE) {
+      return false;
+    }
+    return true;
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.stackControl.length === 0;
   }
 
   push(item) {
     // ... your code goes here
+    if(this.canPush()) {
+      this.stackControl.push(item);
+    } else if (!this.canPush()) {
+      throw new Error("STACK_OVERFLOW");
+    }
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    if(this.isEmpty()) {
+      throw new Error("STACK_UNDERFLOW");
+    }
+    let toBeRemoved = this.stackControl[this.stackControl.length-1];
+    this.stackControl.pop();
+    return toBeRemoved;
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
   }  
 }
 
