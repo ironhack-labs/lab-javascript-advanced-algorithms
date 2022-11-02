e32db65fc7f62062c80e02d865a6bde17fbe6957
Lena Araújo <lenaaraujo92@gmail.com>
2022-11-02 13:04:29 -0300

    iteration 1 - only two tests not passing

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,40 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl >= this.MAX_SIZE) {
+      return false;
+    }
+    return true;
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length) {
+      return false;
+    }
+    return true;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
