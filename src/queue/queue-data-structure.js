1f3f1bd90f3fc55efb573eadb0874f1b010b3540
David Mullally <mullallydavid7@gmail.com>
2022-11-30 11:21:44 +0100

    complete

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,24 +6,65 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true;
+    } else if (this.queueControl.length === this.MAX_SIZE) {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue() === true) {
+      this.queueControl.unshift(item);
+      return this.queueControl;
+    } else {
+      throw new Error("QUEUE_OVERFLOW");
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.queueControl.push();
+    } else {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
+/*push(item) {
+    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
+  }
+
+  pop() {
+    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
+  }*/
+
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,24 +6,46 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else if (this.stackControl.length === this.MAX_SIZE) {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
