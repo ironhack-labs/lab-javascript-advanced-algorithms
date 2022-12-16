95696afd3dc761f3a8f73c3d14b9700d6e7cb99e
Matthew Schröder <matthewschroeder94@gmail.com>
2022-12-16 15:30:33 +0100

    iteration 1 complete

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,42 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if(this.stackControl.length < this.MAX_SIZE)
+    {return true}
+    else if (this.stackControl.length === this.MAX_SIZE) {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
-  }
+    if(this.stackControl.length === 0)
+    {return true}
+    else if (this.stackControl.length > 0) {
+      return false
+    }
+    }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()){
+      this.stackControl.push(item)
+      return this.stackControl
+    }
+    else {
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()){
+      return this.stackControl.pop();
+    }
+    else {
+      throw new Error('STACK_UNDERFLOW');
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
