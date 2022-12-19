43af63c0675543aaa6bed70bb9e59554ba0effc3
Thomas Augot <thomas.augot@hotmail.fr>
2022-12-19 15:53:59 +0100

    iteration 1 solved, some tests are failing, to be checked later

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,24 +6,48 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length === 10) {
+      return false;
+    } else if (this.stackControl.length < 10) {
+      return true;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length > 0) {
+      return false;
+    } else if (this.stackControl.length === 0) {
+      return true;
+    }
   }
 
   push(item) {
     // ... your code goes here
+    let addItem = this.canPush();
+    if ((addItem = true)) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
     // ... your code goes here
+    let isEmpty = this.isEmpty();
+    if ((isEmpty = false)) {
+      this.stackControl.pop(item);
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
