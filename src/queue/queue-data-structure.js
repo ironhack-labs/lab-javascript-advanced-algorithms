a96078bdfd8541b8825e1809ed0090c3ba03c147
Thalita dos Reis <dosreistha@gmail.com>
2022-12-30 18:46:14 +0100

    lab done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,23 +5,41 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if(this.queueControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if(this.queueControl.length === 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item);
+      return this.queueControl;
+    } else {
+      throw new Error('QUEUE_OVERFLOW');
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('QUEUE_UNDERFLOW');
+    } else {
+      let removeElement = this.queueControl.shift();
+      return removeElement;
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.queueControl;
   }  
 }
 
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,41 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if(this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if(this.stackControl.length === 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW');
+    } else {
+      let removeElement = this.stackControl.pop();
+      return removeElement;
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
