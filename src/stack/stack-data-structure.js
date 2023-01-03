75530b52de6c9b1449baa3f44e3c2d80c6c69773
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-03 14:16:46 +0100

    Add push item, pop

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -15,11 +15,22 @@ class Stack {
 	}
 
 	push(item) {
-		// ... your code goes here
+		if (this.canPush()) {
+			this.stackControl.push(item);
+		}
+		if (this.stackControl[this.stackControl.length - 1] === item) return this.stackControl;
+		else {
+			throw new Error('STACK_OVERFLOW');
+		}
 	}
 
 	pop() {
-		// ... your code goes here
+		if (!this.isEmpty()) {
+			return this.stackControl.pop();
+		}
+		if (this.isEmpty()) {
+			throw new Error('STACK_UNDERFLOW');
+		}
 	}
 
 	display() {
