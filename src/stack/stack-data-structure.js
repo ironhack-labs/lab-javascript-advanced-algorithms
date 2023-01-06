831ee4afee13b933eb5eac45949a0066d364e8f6
BodyElvis <bodymoliki@gmail.com>
2023-01-06 11:22:21 +0100

    Lab-Solution-Advanced-Algorithms

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,24 +5,33 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0;
   }
 
   push(item) {
-    // ... your code goes here
+    if (!this.canPush()) {
+      throw new Error("STACK_OVERFLOW");
+    }
+    this.stackControl.push(item);
+    return this;
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error("STACK_UNDERFLOW");
+    }
+    return this.stackControl.pop();
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
+  
+  
 }
 
 // This is required to enable the automated tests, please ignore it.
