ebcae5cedf57b178af6acfcaf8fa3b1d92c2c27a
Neilly <neilpilarca@gmail.com>
2023-01-16 14:01:16 +0100

    solved Lab stack

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,24 +6,46 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush() === true) {
+      this.stackControl.push(item);
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty() === false) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
