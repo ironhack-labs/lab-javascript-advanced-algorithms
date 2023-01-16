00381786db73fceff847a98a230b4923b6de7fbe
pgtognoni <pgtognoni@gmail.com>
2023-01-16 17:35:43 -0300

    advance algorithms practice

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,48 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else return false
   }
 
   isEmpty() {
     // ... your code goes here
+    
+    if (this.queueControl.length < 1) {
+      return true
+    } else return false
+  
   }
 
   enqueue(item) {
     // ... your code goes here
+    const newArr = JSON.parse(JSON.stringify(this.queueControl));
+
+    if(this.canEnqueue()) {
+      newArr.push(item);
+      this.queueControl = newArr;
+
+      return this.queueControl;
+    } else throw new Error('QUEUE_OVERFLOW')
   }
 
   dequeue() {
     // ... your code goes here
+    const newArr = JSON.parse(JSON.stringify(this.queueControl));
+
+    if(this.isEmpty()) {
+      throw new Error('QUEUE_UNDERFLOW')
+    } else {
+      const element = newArr.shift();
+      this.queueControl = newArr;
+      return element;
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl
   }  
 }
 
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,52 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if(this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if(this.stackControl.length < 1) {
+      return true
+    } else 
+      return false
+    
   }
 
   push(item) {
     // ... your code goes here
+    const newStack = JSON.parse(JSON.stringify(this.stackControl));
+    
+    if (this.canPush()) {
+      
+      newStack.push(item);
+      this.stackControl = newStack;
+
+      return this.stackControl;
+    } else throw new Error ('STACK_OVERFLOW')
+    
   }
 
   pop() {
     // ... your code goes here
+    const newStack = JSON.parse(JSON.stringify(this.stackControl));
+
+    if (this.isEmpty()) {
+      throw new Error ('STACK_UNDERFLOW')
+    } else {
+      const element = newStack.pop();
+      this.stackControl = newStack;
+      return element;
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
