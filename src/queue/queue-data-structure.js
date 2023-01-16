fa866bb62323ff2a93a24ccde6857b1824170d70
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-01-16 14:17:14 +0100

    [FT-WD-Berlin-W2D1-lab] Indrajeet Patil

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,25 +5,32 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    return this.queueControl.length < this.MAX_SIZE ? true : false;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.queueControl.length === 0 ? true : false;
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.queueControl.length === this.MAX_SIZE) {
+      throw new Error("QUEUE_OVERFLOW");
+    }
+    this.queueControl.push(item);
+    return this.queueControl;
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
+    return this.queueControl.shift();
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,33 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE ? true : false;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0 ? true : false;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.stackControl.length === this.MAX_SIZE) {
+      throw new Error("STACK_OVERFLOW");
+    }
+    this.stackControl.push(item);
+    return this.stackControl;
   }
 
   pop() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) {
+      throw new Error("STACK_UNDERFLOW");
+    }
+
+    return this.stackControl.pop();
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
