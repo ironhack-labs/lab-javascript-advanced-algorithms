57a16d80d8ef325c6934829b5544220b0c95a9d8
Neilly <neilpilarca@gmail.com>
2023-01-16 14:06:49 +0100

    solve Lab queues

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,24 +6,46 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue() === true) {
+      this.queueControl.push(item);
+    } else {
+      throw new Error("QUEUE_OVERFLOW");
+    }
+    return this.queueControl;
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.isEmpty() === false) {
+      return this.queueControl.shift();
+    } else {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
