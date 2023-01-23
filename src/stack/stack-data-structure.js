6b9b7ad175c8ea5a341d86e6165842ced6bd678a
Joao Raposo <raposo2281@gmail.com>
2023-01-23 16:54:59 +0000

    Solved with 2 errors

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,33 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if(this.stackControl.length === 0) return true;
+    else return false;
   }
 
   isEmpty() {
     // ... your code goes here
+    if(this.stackControl.length === 0) return true;
+    else return false;
   }
 
   push(item) {
     // ... your code goes here
+    if(this.stackControl.length >= this.MAX_SIZE) throw new Error('STACK_OVERFLOW')
+    this.stackControl.push(item);
+    return this.stackControl;
   }
 
   pop() {
     // ... your code goes here
+    if(this.stackControl.length <= 0) throw new Error('STACK_UNDERFLOW')
+    return this.stackControl.pop();
+    
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
