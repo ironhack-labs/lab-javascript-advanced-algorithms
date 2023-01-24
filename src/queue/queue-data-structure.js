47835c5a13f43f773f84aef91220512c3f3a030f
Catalina Fernandez <catalinaf97@gmail.com>
2023-01-24 19:39:07 +0100

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,46 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (!this.queueControl.length) {
+      return true
+    } else {
+      return false
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+    } else {
+      throw new Error('QUEUE_OVERFLOW')
+    }
+
+    return this.queueControl
   }
 
   dequeue() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      const pop = this.queueControl.shift()
+      return pop
+    } else {
+      throw new Error('QUEUE_UNDERFLOW')
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl
   }  
 }
 
