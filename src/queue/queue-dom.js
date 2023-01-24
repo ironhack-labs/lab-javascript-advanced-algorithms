75bee9d46b8a14a9b651c05ccf5c9aece634df28
Bernardo Vera <bvera.ruiz@gmail.com>
2023-01-24 18:35:45 +0100

    done js. DOM with error

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -1,36 +1,55 @@
 const queueUL = document.querySelector('.list-queue');
 const queueInput = document.querySelector('.queue-input');
+const container = document.getElementById('container');
 const warningTopQueue = document.querySelector('#queue-container .warning-top');
-const warningBottomQueue = document.querySelector(
-  '#queue-container .warning-bottom'
-);
+const warningBottomQueue = document.querySelector('#queue-container .warning-bottom');
 const addQueue = document.querySelector('.btn-add-queue');
 const dequeue = document.querySelector('.btn-take-dequeue');
 
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
+
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
   try {
-    // ... your code goes here
+    queue.push(queueInput.value)
   } catch (error) {
+    generateWarningQueue('overflow')
     // there was an overflow error, handle it
   }
 };
@@ -38,7 +57,9 @@ const addToQueue = () => {
 const removeFromQueue = () => {
   try {
     // ... your code goes here
+    queue.pop();
   } catch (error) {
+    generateWarningQueue('underflow');
     // there was an underflow error, handle it
   }
 };
