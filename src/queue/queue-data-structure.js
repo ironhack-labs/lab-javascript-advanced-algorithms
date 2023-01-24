69a36bdf539e7aac75bf0838c991b99a8bf0ddba
jelloreda <jaimelloredavelasco@gmail.com>
2023-01-24 17:40:02 +0100

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,23 +6,43 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    }
+    return false
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    }
+    return false
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    }
+
+    throw new Error('QUEUE_OVERFLOW');
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('QUEUE_UNDERFLOW');
+    }
+    const removedElm = this.queueControl.shift()
+    return removedElm
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,23 +6,43 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    }
+    return false
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    }
+    return false
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    }
+
+    throw new Error('STACK_OVERFLOW');
   }
 
   pop() {
     // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW');
+    }
+    const removedElm = this.stackControl.pop()
+    return removedElm
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
