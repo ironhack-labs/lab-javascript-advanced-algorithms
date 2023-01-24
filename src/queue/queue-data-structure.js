64fb4015e31b8eca979b86d2e4cd1c9412751742
aldidjamil <alditamadjamil@gmail.com>
2023-01-24 21:57:38 +0100

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,23 +6,57 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    }
+    else (this.queueControl.length > this.MAX_SIZE)
+    return false
+
   }
 
   isEmpty() {
     // ... your code goes here
+
+    if (this.queueControl.length === 0) {
+      return true
+    }
+
+    else (this.queueControl.length > 0)
+    { return false }
+
   }
 
   enqueue(item) {
     // ... your code goes here
+
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+    }
+    else {
+      throw new Error('QUEUE_OVERFLOW')
+    }
+
+    return this.queueControl
   }
 
   dequeue() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      const elementRemoved = this.queueControl.shift()
+      return elementRemoved
+    }
+    else {
+      throw new Error('QUEUE_UNDERFLOW')
+    }
+
+
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -2,27 +2,60 @@ class Stack {
   constructor() {
     this.stackControl = [];
     this.MAX_SIZE = 10;
+
+
   }
 
   canPush() {
     // ... your code goes here
+
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    }
+    else (this.stackControl.length > this.MAX_SIZE)
+    return false
+
   }
 
   isEmpty() {
     // ... your code goes here
+
+    if (this.stackControl.length === 0) {
+      return true
+    }
+
+    else (this.stackControl.length > 0)
+    { return false }
+
   }
 
   push(item) {
     // ... your code goes here
+
+    if (this.canPush()) {
+      this.stackControl.push(item)
+    }
+    else {
+      throw new Error('STACK_OVERFLOW')
+    }
+
+    return this.stackControl
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      const elementRemoved = this.stackControl.pop()
+      return elementRemoved
+    }
+    else {
+      throw new Error('STACK_UNDERFLOW')
+    }
+
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
