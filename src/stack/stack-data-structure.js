6405f7c220e63aaa2d18e9c0c6b56aa96538f5c0
AngelesVFA <spotifyasuna21@gmail.com>
2023-01-24 19:19:54 +0100

    [MAD DE] Angeles Figueredo

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,37 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+   if(this.stackControl.length >= this.MAX_SIZE){
+    return false
+   } 
+   return true
   }
 
   isEmpty() {
-    // ... your code goes here
+    if(this.stackControl.length ==0){
+      return true
+    } 
+    return false
   }
 
   push(item) {
-    // ... your code goes here
+    if(this.canPush()){
+      this.stackControl.push(item)
+      return this.stackControl}
+    
+    throw new Error('STACK_OVERFLOW') 
   }
 
   pop() {
-    // ... your code goes here
+    if(this.stackControl.length === 0){
+      throw new Error('STACK_UNDERFLOW')
+    }else{
+      return this.stackControl.pop()
+    }
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
