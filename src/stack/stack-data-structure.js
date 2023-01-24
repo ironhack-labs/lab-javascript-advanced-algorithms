57a3c2f06d2ff8a543600903d50022f76c565ff3
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-01-24 18:42:13 +0100

    done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,23 +6,49 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    }
+    return false
+
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    }
+    return false
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
     // ... your code goes here
+
+    if (this.isEmpty()) {
+      throw new Error('STACK_UNDERFLOW')
+
+
+    } else {
+      return this.stackControl.pop()
+
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
