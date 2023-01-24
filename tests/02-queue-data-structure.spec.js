0da4d3a7d51798c9effffc802bedba7711332189
Juan R <juangnba@hotmail.com>
2023-01-24 17:32:18 +0100

    done

--- tests/02-queue-data-structure.spec.js
+++ tests/02-queue-data-structure.spec.js
@@ -1,4 +1,4 @@
-describe('Queue', () => {
+describe("Queue", () => {
   let queue;
 
   beforeEach(() => {
@@ -6,27 +6,27 @@ describe('Queue', () => {
   });
 
   describe('Property "queueControl"', () => {
-    it('should be an array used to store the elements of the queue', () => {
+    it("should be an array used to store the elements of the queue", () => {
       expect(Array.isArray(queue.queueControl)).toBe(true);
     });
   });
 
   describe('Property "MAX_SIZE"', () => {
-    it('should be a maximum number of items to avoid the queue overflow', () => {
-      expect(typeof queue.MAX_SIZE).toBe('number');
+    it("should be a maximum number of items to avoid the queue overflow", () => {
+      expect(typeof queue.MAX_SIZE).toBe("number");
     });
   });
 
   describe('Method "canEnqueue"', () => {
-    it('should be declared', () => {
-      expect(typeof queue.canEnqueue).toBe('function');
+    it("should be declared", () => {
+      expect(typeof queue.canEnqueue).toBe("function");
     });
 
-    it('should return true if queue is empty', () => {
+    it("should return true if queue is empty", () => {
       expect(queue.canEnqueue()).toBe(true);
     });
 
-    it('should return false if queue is full', () => {
+    it("should return false if queue is full", () => {
       queue.MAX_SIZE = 1;
       queue.queueControl.push(1);
       expect(queue.canEnqueue()).toBe(false);
@@ -34,37 +34,37 @@ describe('Queue', () => {
   });
 
   describe('Method "isEmpty"', () => {
-    it('should be declared', () => {
-      expect(typeof queue.isEmpty).toBe('function');
+    it("should be declared", () => {
+      expect(typeof queue.isEmpty).toBe("function");
     });
 
-    it('should return true if queue is empty, false if it contains any elements', () => {
+    it("should return true if queue is empty, false if it contains any elements", () => {
       expect(queue.isEmpty()).toBe(true);
     });
 
-    it('should return false if queue contains any elements', () => {
+    it("should return false if queue contains any elements", () => {
       queue.queueControl.push(1);
       expect(queue.isEmpty()).toBe(false);
     });
   });
 
   describe('Method "enqueue"', () => {
-    it('should be declared', () => {
-      expect(typeof queue.enqueue).toBe('function');
+    it("should be declared", () => {
+      expect(typeof queue.enqueue).toBe("function");
     });
 
-    it('should add the indicated element to the end of the queue', () => {
+    it("should add the indicated element to the end of the queue", () => {
       const value = 19;
       queue.enqueue(value);
       expect(queue.queueControl.length).toBe(1);
       expect(queue.queueControl[queue.queueControl.length - 1]).toBe(value);
     });
 
-    it('should return the queue when an element is inserted', () => {
+    it("should return the queue when an element is inserted", () => {
       expect(queue.enqueue(19)).toEqual([19]);
     });
 
-    it('should insert the elements in the received order', () => {
+    it("should insert the elements in the received order", () => {
       queue.enqueue(19);
       queue.enqueue(88);
       expect(queue.queueControl).toEqual([19, 88]);
@@ -73,35 +73,34 @@ describe('Queue', () => {
     it('should throw error with message "QUEUE_OVERFLOW" if the queue is full', () => {
       queue.MAX_SIZE = 1;
       queue.enqueue(19);
-      expect(() => queue.enqueue(88)).toThrow(new Error('QUEUE_OVERFLOW'));
+      expect(() => queue.enqueue(88)).toThrow(new Error("QUEUE_OVERFLOW"));
     });
   });
 
   describe('Method "dequeue"', () => {
-    it('should be declared', () => {
-      expect(typeof queue.dequeue).toBe('function');
+    it("should be declared", () => {
+      expect(typeof queue.dequeue).toBe("function");
     });
 
-    it('should return the element removed from the beggining of the queue', () => {
+    it("should return the element removed from the beggining of the queue", () => {
       queue.enqueue(19);
       queue.enqueue(88);
       expect(queue.dequeue()).toBe(19);
     });
 
     it('should throw error with message "QUEUE_UNDERFLOW" if there are no elements in the queue', () => {
-      expect(() => queue.dequeue()).toThrow(new Error('QUEUE_UNDERFLOW'));
+      expect(() => queue.dequeue()).toThrow(new Error("QUEUE_UNDERFLOW"));
     });
-  });  
+  });
 
   describe('Method "display"', () => {
-    it('should be declared', () => {
-      expect(typeof queue.display).toBe('function');
+    it("should be declared", () => {
+      expect(typeof queue.display).toBe("function");
     });
 
-    it('should return contents of queue', () => {
+    it("should return contents of queue", () => {
       queue.enqueue(10);
       expect(queue.display()).toEqual([10]);
     });
   });
-
 });
