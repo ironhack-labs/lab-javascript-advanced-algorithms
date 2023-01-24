65ca269dd2af1a13e5e022f6e148952cfb3674fe
IreneValor <irenevalor@gmail.com>
2023-01-24 23:33:54 +0100

    Solved lab

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -4,26 +4,71 @@ class Stack {
     this.MAX_SIZE = 10;
   }
 
+  
+
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
+    
   }
 
+
+
+
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+
   }
+  
+
+  
 
   push(item) {
     // ... your code goes here
+
+    if (this.canPush()) {
+      this.stackControl.push(item)
+    }
+    else {
+      throw new Error('STACK_OVERFLOW')
+    }
+
+    return this.stackControl
   }
+    
+
+
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      const element = this.stackControl.pop()
+      return element
+    }
+    else {
+      throw new Error('STACK_UNDERFLOW')
+    }
   }
+ 
+  
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
+
+ 
+    
+    // ... your code goes here
+  
+  
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
