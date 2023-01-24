91bd8b377e17387cf601eea5a1ba06f8477157fd
alvmorsie <122047484+alvmorsie@users.noreply.github.com>
2023-01-24 20:23:19 +0100

    Solved lab

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -11,10 +11,28 @@ const queue = new Queue();
 
 const clearQueueInput = () => {
   // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = newQueue.display().length;
+  let size = newQueue.MAX_SIZE - length;
+  newQueue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
@@ -22,24 +40,35 @@ generateListQueue();
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
     // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
   try {
     // ... your code goes here
+
+
   } catch (error) {
     // there was an overflow error, handle it
+
   }
 };
 
 const removeFromQueue = () => {
   try {
     // ... your code goes here
+
+
   } catch (error) {
     // there was an underflow error, handle it
+
+
   }
 };
 
