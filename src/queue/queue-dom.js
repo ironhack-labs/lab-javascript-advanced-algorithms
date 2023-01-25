5cb0ae54ace061baae40ff9e5489a59719a05c27
Diego Rodriguez <godiedri@gmail.com>
2023-01-25 09:04:32 +0100

    solved lab

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -7,14 +7,36 @@ const warningBottomQueue = document.querySelector(
 const addQueue = document.querySelector('.btn-add-queue');
 const dequeue = document.querySelector('.btn-take-dequeue');
 
-const queue = new Queue();
+const Queue = new Queue();
 
 const clearQueueInput = () => {
   // ... your code goes here
+
+  queueInput.value = ''
+
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueList.innerHTML = '';
+  let length = Queue.display().length;
+  let size = Queue.MAX_SIZE - length;
+  Queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueList.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueList.appendChild(li);
+  }
+
 };
 
 generateListQueue();
@@ -22,16 +44,31 @@ generateListQueue();
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
     // ... your code goes here
+
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
+
   } else if (type === 'overflow') {
     // ... your code goes here
+
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
+
   }
 };
 
 const addToQueue = () => {
   try {
     // ... your code goes here
+
+    generateWarningQueue('overflow');
+
   } catch (error) {
     // there was an overflow error, handle it
+
+    clearQueueInput();
+    renderListQueue();
+
   }
 };
 
