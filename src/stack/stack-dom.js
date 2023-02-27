47464ad4609f96de9c5f21dd6079cc50abc80375
Peter Sekan <sekan.pete@gmail.com>
2023-02-27 22:01:21 +0100

    Solved

--- src/stack/stack-dom.js
+++ src/stack/stack-dom.js
@@ -1,64 +1,71 @@
-const stackList = document.getElementById('stack-list');
-const stackInput = document.getElementById('stack-input');
-const container = document.getElementById('container');
-const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
-const addStackBtn = document.getElementById('add-stack');
-const takeStackBtn = document.getElementById('take-stack');
+const stackList = document.getElementById("stack-list");
+const stackInput = document.getElementById("stack-input");
+const container = document.getElementById("container");
+const warningTopStack = document.querySelector("#stack-container .warning-top");
+const warningBottomStack = document.querySelector(
+  "#stack-container .warning-bottom"
+);
+const addStackBtn = document.getElementById("add-stack");
+const takeStackBtn = document.getElementById("take-stack");
 
 const newStack = new Stack();
 
 const clearStackInput = () => {
-  stackInput.value = '';
+  stackInput.value = "";
 };
 
 const renderListStack = () => {
-  warningTopStack.style.display = 'none';
-  warningBottomStack.style.display = 'none';
-  stackList.innerHTML = '';
+  warningTopStack.style.display = "none";
+  warningBottomStack.style.display = "none";
+  stackList.innerHTML = "";
   let length = newStack.display().length;
   let size = newStack.MAX_SIZE - length;
-  newStack.display().forEach(item => {
-    let li = document.createElement('li');
-    li.className = 'active';
+  newStack.display().forEach((item) => {
+    let li = document.createElement("li");
+    li.className = "active";
     li.innerText = item;
     stackList.appendChild(li);
   });
   for (let i = 0; i < size; i++) {
-    let li = document.createElement('li');
-    li.className = 'inactive';
-    li.innerHTML = '&nbsp;';
+    let li = document.createElement("li");
+    li.className = "inactive";
+    li.innerHTML = "&nbsp;";
     stackList.appendChild(li);
   }
 };
 renderListStack();
 
-const generateWarningStack = type => {
-  if (type === 'underflow') {
-    warningBottomStack.style.display = 'block';
+const generateWarningStack = (type) => {
+  if (type === "underflow") {
+    warningBottomStack.style.display = "block";
     warningBottomStack.innerText = type;
-  } else if (type === 'overflow') {
-    warningTopStack.style.display = 'block';
+  } else if (type === "overflow") {
+    warningTopStack.style.display = "block";
     warningTopStack.innerText = type;
   }
 };
 
 const addToStack = () => {
-  if (newStack.push(stackInput.value) === 'Stack Overflow') {
-    generateWarningStack('overflow');
-  } else {
+  try {
+    // ... your code goes here
+    newStack.push(stackInput.value);
     clearStackInput();
     renderListStack();
+  } catch (error) {
+    generateWarningStack("overflow");
   }
 };
 
 const removeFromStack = () => {
-  if (newStack.pop() === 'Stack Underflow') {
-    generateWarningStack('underflow');
-  } else {
+  try {
+    // ... your code goes here
+    newStack.pop();
+    clearStackInput();
     renderListStack();
+  } catch (error) {
+    generateWarningStack("underflow");
   }
 };
 
-addStackBtn.addEventListener('click', addToStack);
-takeStackBtn.addEventListener('click', removeFromStack);
+addStackBtn.addEventListener("click", addToStack);
+takeStackBtn.addEventListener("click", removeFromStack);
