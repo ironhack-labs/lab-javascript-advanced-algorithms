b20b4b174a1a26186d114a7243e7a0e5db6880be
valyy151 <marin.valenta1@gmail.com>
2023-02-27 05:55:23 +0100

    Initial Commit

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -1,28 +1,30 @@
 class Stack {
-  constructor() {
-    this.stackControl = [];
-    this.MAX_SIZE = 10;
-  }
+	constructor() {
+		this.stackControl = [];
+		this.MAX_SIZE = 10;
+	}
 
-  canPush() {
-    // ... your code goes here
-  }
+	canPush() {
+		if (this.stackControl.length < this.MAX_SIZE) {
+			return true;
+		} else return false;
+	}
 
-  isEmpty() {
-    // ... your code goes here
-  }
+	isEmpty() {
+		// ... your code goes here
+	}
 
-  push(item) {
-    // ... your code goes here
-  }
+	push(item) {
+		// ... your code goes here
+	}
 
-  pop() {
-    // ... your code goes here
-  }
+	pop() {
+		// ... your code goes here
+	}
 
-  display() {
-    // ... your code goes here
-  }  
+	display() {
+		// ... your code goes here
+	}
 }
 
 // This is required to enable the automated tests, please ignore it.
