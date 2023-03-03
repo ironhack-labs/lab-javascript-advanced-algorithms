9cf30dc20351fd4075ff5e2ca495d87e8962e621
Cedric Aim <cedric.aim@gmail.com>
2023-03-03 12:55:17 +0100

    javascript iterations and bug fixed on test

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,30 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return !this.stackControl.length;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    }
+    throw new Error("STACK_OVERFLOW");
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) return this.stackControl.pop();
+    throw new Error("STACK_UNDERFLOW");
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
