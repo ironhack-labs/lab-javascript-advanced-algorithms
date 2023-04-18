56b06f54cf76252a9239c0c6e4f427e08cfc247b
Manuel Pérez <mperezpradosjose@gmail.com>
2023-04-18 18:22:41 +0200

    Realizado con el dom a medias

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,24 +5,45 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      console.log(false);
+      return false
+    } else {
+      console.log(true);
+      return true
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
-    // ... your code goes here
+
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
+
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop()
+    } else {
+      throw new Error('STACK_UNDERFLOW');
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
