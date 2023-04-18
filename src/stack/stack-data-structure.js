d806331217369449ad3802d21de4f5b4328aeb31
Helena Sánchez <helena.sangar@gmail.com>
2023-04-18 22:03:47 +0200

    sin dom

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,57 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
+    // console.log(this.stackControl)
   }
+
+
 
   isEmpty() {
-    // ... your code goes here
+
+    // let emptyStack = this.MAX_SIZE - this.stackControl.length
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false
+    }
+
+    //console.log(emptyStack)
   }
+
 
   push(item) {
-    // ... your code goes here
+
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
+
+
   pop() {
-    // ... your code goes here
+
+    if (this.isEmpty() === false) {
+      return this.stackControl.pop();
+
+    } else {
+      throw new Error('STACK_UNDERFLOW');
+    }
+
   }
 
   display() {
-    // ... your code goes here
-  }  
-}
+    return this.stackControl;
+  }
 
+}
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
