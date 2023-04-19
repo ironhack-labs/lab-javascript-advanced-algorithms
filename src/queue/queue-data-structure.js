85607d7f24f36f755c5956b5dc8fbf4971545f60
CristianMausque <cristianmausque@gmail.com>
2023-04-19 10:25:16 +0200

    siento la tardanza

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,24 +5,46 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else { return false }
+
+
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (!this.queueControl.length) {
+      return true
+    } else if (this.queueControl.length > 0) {
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+
+      return this.queueControl
+
+    } else {
+      throw new Error('QUEUE_OVERFLOW')
+    } // ... your code goes here
   }
 
   dequeue() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      const variable = this.queueControl.shift()
+      return variable
+    } else {
+
+      throw new Error('QUEUE_UNDERFLOW')
+
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,59 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else { return false }
+
+
+
   }
 
   isEmpty() {
-    // ... your code goes here
+
+    if (!this.stackControl.length) {
+      return true
+    } else if (this.stackControl.length > 0) {
+      return false
+    }
+
   }
 
   push(item) {
-    // ... your code goes here
+
+
+    if (this.canPush()) {
+      this.stackControl.push(item)
+
+      return this.stackControl
+
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+
+
+
+    if (!this.isEmpty()) {
+      const variable = this.stackControl.pop()
+      return variable
+    } else {
+
+      throw new Error('STACK_UNDERFLOW')
+
+    }
+
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
+
+
+
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+
