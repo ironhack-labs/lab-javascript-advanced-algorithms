d09ffeb0a463cae6431b50033f39f9e280b5ade3
Fernando Durban <ferbrizie@gmail.com>
2023-04-19 10:54:30 +0200

    Done

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -10,28 +10,47 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = " ";
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queue.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queue.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an overflow error, handle it
+  if (queueInput.push(queueInput.value) === 'Queue Overflow') {
+    generateWarningQueue('overflow');
+  } else {
+    clearQueueInput();
+    generateListQueue();
   }
 };
 
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,24 +5,41 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    }
+    else
+      return false
+
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true
+    }
+    else return false
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush() == true) {
+      this.stackControl.push(item)
+    }
+    else throw new Error('STACK_OVERFLOW');
+    return this.stackControl
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty() === false) {
+      let elementopopeado = this.stackControl.pop()
+      return elementopopeado
+    }
+    else throw new Error('STACK_UNDERFLOW');
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
