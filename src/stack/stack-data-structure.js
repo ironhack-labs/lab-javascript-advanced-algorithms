d8f0bbdf359984f51f4350213736c88c35ddb819
Walter Bernal <wltrbm@gmail.com>
2023-04-27 16:52:03 -0700

    Iteration 1 - The Stack class

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,28 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length === this.MAX_SIZE ? false : true
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0 ? true : false
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } 
+    throw new Error ("STACK_OVERFLOW")
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) return this.stackControl.pop()
+    throw new Error("STACK_UNDERFLOW")
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
