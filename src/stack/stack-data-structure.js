fe837218b26605742270b54da075146d7931ad6f
churrundo <churrundo@gmail.com>
2023-04-29 20:41:51 -0600

    finish stack section

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,35 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+   if(this.stackControl.length<this.MAX_SIZE) {
+    return true
+  }else {return false}
   }
 
-  isEmpty() {
-    // ... your code goes here
+  isEmpty(){
+    if(this.stackControl.length===0) {
+      return true
+    }else {return false}
+    
+    
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()){
+      this.stackControl.push(item);
+      return this.stackControl
+    }else {throw new Error("STACK_OVERFLOW")};
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()){
+      throw new Error("STACK_UNDERFLOW");
+      
+    }else {return this.stackControl.pop()};
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
