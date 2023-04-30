39588bf344c836d326f8162daab3707d68b9638b
churrundo <churrundo@gmail.com>
2023-04-29 20:48:35 -0600

    finish queue section

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,37 +5,42 @@ class Stack {
   }
 
   canPush() {
-   if(this.stackControl.length<this.MAX_SIZE) {
-    return true
-  }else {return false}
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  isEmpty(){
-    if(this.stackControl.length===0) {
-      return true
-    }else {return false}
-    
-    
+  isEmpty() {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
-    if (this.canPush()){
+    if (this.canPush()) {
       this.stackControl.push(item);
-      return this.stackControl
-    }else {throw new Error("STACK_OVERFLOW")};
+      return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
-    if (this.isEmpty()){
+    if (this.isEmpty()) {
       throw new Error("STACK_UNDERFLOW");
-      
-    }else {return this.stackControl.pop()};
+    } else {
+      return this.stackControl.pop();
+    }
   }
 
   display() {
-    return this.stackControl
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
