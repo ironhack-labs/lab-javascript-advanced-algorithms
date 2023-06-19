4033d10039cd72eafd54a9ae169879fb4a5c1fcd
Driztnafein <127021511+Driztnafein@users.noreply.github.com>
2023-06-19 17:40:00 +0100

    Solved lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,24 +5,43 @@ class Queue {
   }
 
   canEnqueue() {
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
     // ... your code goes here
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    } else {
+      return false// ... your code goes here
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+   if (this.canEnqueue()) {
+    this.queueControl.push(item);
+    return this.queueControl
+   } else {
+    throw new Error("QUEUE_OVERFLOW");
+   } // ... your code goes here
   }
 
   dequeue() {
+    if (!this.isEmpty()) {
+      return this.queueControl.shift();
+    } else {
+      throw new Error("QUEUE_UNDERFLOW")
+    }
     // ... your code goes here
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl// ... your code goes here
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
