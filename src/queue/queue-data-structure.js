f380cc18f1005975d50ef9d7205a8ffa8a4a2e48
nacho <nachosanson6@gmail.com>
2023-07-19 19:28:06 +0200

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,24 +5,44 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+      return this.queueControl
+    } else {
+      //throw new Error('QUEUE_OVERFLOW') JASMINE
+      return 'Queue Overflow'
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.queueControl.length > 0) {
+      return this.queueControl.shift()
+
+    } else {
+      //throw new Error('QUEUE_UNDERFLOW') JASMINE
+      return 'Queue Underflow'
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
