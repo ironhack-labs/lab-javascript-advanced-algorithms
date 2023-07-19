400fdb57320d51e5712c8ff306e40d2b11459485
H1V35 <higesjorge@gmail.com>
2023-07-19 18:24:37 +0200

    Solved Lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,50 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (!this.queueControl.length) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (!this.canEnqueue()) {
+      return 'Queue Overflow'
+      // throw new Error('QUEUE_OVERFLOW')
+    }
+    else {
+      this.queueControl.push(item)
+      return this.queueControl
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.isEmpty()) {
+      return 'Queue Underflow'
+      // throw new Error('QUEUE_UNDERFLOW')
+    }
+    else {
+      return this.queueControl.shift()
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl
   }  
 }
 
--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -11,10 +11,28 @@ const queue = new Queue();
 
 const clearQueueInput = () => {
   // ... your code goes here
-};
+  queueInput.value = ''
+}
 
 const generateListQueue = () => {
   // ... your code goes here
+  warningTopQueue.style.display = 'none'
+  warningBottomQueue.style.display = 'none'
+  queueUL.innerHTML = ''
+  let length = queue.display().length
+  let size = queue.MAX_SIZE - length
+  queue.display().forEach(item => {
+    let li = document.createElement('li')
+    li.className = 'active'
+    li.innerText = item
+    queueUL.appendChild(li)
+  })
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li')
+    li.className = 'inactive'
+    li.innerHTML = '&nbsp;'
+    queueUL.appendChild(li)
+  }
 };
 
 generateListQueue();
@@ -22,26 +40,45 @@ generateListQueue();
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
     // ... your code goes here
+    warningBottomQueue.style.display = 'block'
+    warningBottomQueue.innerText = type
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopQueue.style.display = 'block'
+    warningTopQueue.innerText = type
   }
 };
 
 const addToQueue = () => {
   try {
     // ... your code goes here
+    if (queue.enqueue(queueInput.value) === 'Queue Overflow') {
+      throw new Error('error')
+    }
+    else {
+      clearQueueInput()
+      generateListQueue()
+    }
   } catch (error) {
     // there was an overflow error, handle it
+    generateWarningQueue('overflow')
   }
-};
+}
 
 const removeFromQueue = () => {
   try {
     // ... your code goes here
+    if (queue.dequeue() === 'Queue Underflow') {
+      throw new Error ('error')
+    } 
+    else {
+      generateListQueue()
+    }
   } catch (error) {
     // there was an underflow error, handle it
+    generateWarningQueue('underflow')
   }
-};
+}
 
 addQueue.addEventListener('click', addToQueue);
 dequeue.addEventListener('click', removeFromQueue);
