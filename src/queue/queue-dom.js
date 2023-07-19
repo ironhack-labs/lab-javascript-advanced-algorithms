344978845bbc6292cfb1da84824e59f9a96a6ff9
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-07-19 20:26:04 +0200

    Lab Done

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -1,3 +1,4 @@
+/*VETE AL FINAL CLAUDIA*/
 const queueUL = document.querySelector('.list-queue');
 const queueInput = document.querySelector('.queue-input');
 const warningTopQueue = document.querySelector('#queue-container .warning-top');
@@ -9,35 +10,62 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 
 const queue = new Queue();
 
-const clearQueueInput = () => {
-  // ... your code goes here
+const clearQueueInput = () => { // para limpiar el input, donde escribes el item a incluir
+  queueInput.value = '';
 };
 
-const generateListQueue = () => {
-  // ... your code goes here
+const generateListQueue = () => {/*Propiedades de CSS*/
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an overflow error, handle it
+  //try {
+  if (queue.enqueue(queueInput.value) === 'Queue Overflow') {
+    generateWarningQueue('overflow');
+  } else {
+    clearQueueInput();
+    generateListQueue();
   }
+  // } catch (error) {
+  //   // there was an overflow error, handle it
+  // }
 };
 
 const removeFromQueue = () => {
   try {
-    // ... your code goes here
+    if (queue.dequeue() === 'Queue underflow') {
+      generateWarningQueue('underflow');
+    } else {
+      generateListQueue();
+    }
   } catch (error) {
     // there was an underflow error, handle it
   }
@@ -45,3 +73,8 @@ const removeFromQueue = () => {
 
 addQueue.addEventListener('click', addToQueue);
 dequeue.addEventListener('click', removeFromQueue);
+
+
+/*En esta lo primero que hay que hacer es rellenar el js de las queue que es como el 
+de las stack, que nos dan hecha. Teniendo en cuenta que algunos métodos cambian, como 
+el push y el pop (fijate en las horas de data.js de cada una de ellas!!)*/
