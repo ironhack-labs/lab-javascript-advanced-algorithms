6ffadc20a20dd741eaed85a6a167b3031de06c2a
samuelemarino04 <samuele12601@gmail.com>
2023-07-19 19:59:26 +0200

    not done

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -10,36 +10,61 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueList.innerHTML = '';
+  let length = newQueue.display().length;
+  let size = newQueue.MAX_SIZE - length;
+  newQueue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueList.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueList.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
   try {
-    // ... your code goes here
+    if
+      (newQueue.enqueue(queueInput.value) === 'Queue Overflow') {
+      generateWarningQueue('overflow');
+    }
   } catch (error) {
-    // there was an overflow error, handle it
+    clearQueueInput();
+    renderListQueue();
   }
 };
 
 const removeFromQueue = () => {
   try {
-    // ... your code goes here
+    if
+      (newQueue.dequeue() === 'Queue Underflow') {
+      generateWarningQueue('underflow');
+    }
   } catch (error) {
-    // there was an underflow error, handle it
+    renderListStack()
   }
 };
 
