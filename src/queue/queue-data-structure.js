062aeec93cad6a2343a0644da7e4bf7be43c44f3
helena-141 <helen_141@msn.com>
2023-07-20 14:37:05 +0200

    done

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,21 +6,51 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length >= this.MAX_SIZE) {
+      return false
+    }
+    else {
+      return true
+    }
   }
 
   isEmpty() {
     // ... your code goes here
-  }
+    if (this.queueControl.length > 0) {
+      return false
+    }
+    else {
+      return true
+    }
+
+}
 
   enqueue(item) {
+    
+    if (this.canEnqueue()) {
+      this.queueControl.push(item)
+    
+      return this.queueControl
+    }
+    else {
+      throw new Error('QUEUE_OVERFLOW')
+    }
+    
     // ... your code goes here
   }
 
   dequeue() {
+    if (!this.isEmpty()) {
+      return this.queueControl.shift()
+    }
+    else {
+      throw new Error('QUEUE_UNDERFLOW')
+    }
     // ... your code goes here
   }
 
   display() {
+    return this.queueControl
     // ... your code goes here
   }  
 }
