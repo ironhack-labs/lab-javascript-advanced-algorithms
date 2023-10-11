4bb08401e609e0bbbb964609ce8dfc88a1c30e2d
paussaanz <paula@sanz.eu>
2023-10-11 14:13:04 +0200

    lab done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,34 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
-  }
+if (this.stackControl.length === 0){
+  return true
+} 
+return false 
+ }
+
 
   push(item) {
-    // ... your code goes here
+if (this.canPush() === true){
+  this.stackControl.push(item)
+  return this.stackControl
+}
+throw new Error('STACK_OVERFLOW')
   }
 
   pop() {
-    // ... your code goes here
+if (this.isEmpty() === false){
+  return this.stackControl.pop()
+}
+throw new Error('STACK_UNDERFLOW')
   }
 
   display() {
-    // ... your code goes here
+return this.stackControl
   }  
 }
 
