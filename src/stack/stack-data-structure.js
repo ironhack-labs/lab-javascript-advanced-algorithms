f8bcb87b0e8ee06aa74bfe7d84f376f2241e6165
Jormag <jorgemamblonagomez@gmail.com>
2023-10-17 17:12:40 +0200

    done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,24 +5,38 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    }
+    return false
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (!this.stackControl.length) {
+      return true
+    }
+    return false
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop()
+    }
+    throw new Error('STACK_UNDERFLOW')
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
