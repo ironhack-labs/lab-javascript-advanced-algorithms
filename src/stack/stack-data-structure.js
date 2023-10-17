6603df0366e3d426083fad8333a9fb61d0e24f96
Carlos Rodriguez <carlos97sdg@gmail.com>
2023-10-17 18:15:05 +0200

    done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,43 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+
+    }
   }
 
+
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty() === false) {
+      return this.stackControl.pop()
+    } else {
+      throw new Error('STACK_UNDERFLOW')
+    }
+  }
+  display() {
+    return this.stackControl
   }
 
-  display() {
-    // ... your code goes here
-  }  
 }
-
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
