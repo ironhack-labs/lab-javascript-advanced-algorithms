25ccafdf51f193f863776a0bf9c3da5624fc61be
Cesaria Colazzo <csr.colazzo@gmail.com>
2023-10-17 20:11:53 +0200

    done

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -10,38 +10,74 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
+
 
 generateListQueue();
 
+
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
   try {
-    // ... your code goes here
+    queue.enqueue(queueInput.value)
+    clearQueueInput();
+    generateListQueue()
   } catch (error) {
-    // there was an overflow error, handle it
+    generateWarningQueue('overflow∫')
   }
 };
 
 const removeFromQueue = () => {
   try {
-    // ... your code goes here
+    queue.dequeue()
+    generateListQueue()
   } catch (error) {
-    // there was an underflow error, handle it
+    generateWarningQueue('underflow')
   }
 };
 
 addQueue.addEventListener('click', addToQueue);
 dequeue.addEventListener('click', removeFromQueue);
+Collapse
+
+
+
+
+
+
+
+
+
+
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,66 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true
+    } else {
+      return false
+    }
+
   }
 
+
+
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length > 0) {
+      return false
+    } else {
+      return true
+    }
+
+
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+
+    } else {
+
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      let removed = this.stackControl.pop()
+      return removed
+
+
+
+
+
+
+    } throw new Error('STACK_UNDERFLOW')
+
+
   }
 
+
+
+
+
+
+
+
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+
+  }
+  // ... your code goes here
 }
+
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
