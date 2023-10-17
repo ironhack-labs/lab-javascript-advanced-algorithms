5cd25f4c3b7ccb7eaf0e2bd08129056aca1d2c06
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-10-17 19:11:55 +0200

    solved lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,21 +6,47 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if(this.queueControl.length<this.MAX_SIZE){
+      return true
+    }
+    else{
+      return false
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if(this.queueControl.length===0){
+      return true
+    }
+    else{
+      return false
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(this.canEnqueue()){
+      this.queueControl.push(item)
+      return this.queueControl
+    }
+    else{
+      throw new Error('QUEUE_OVERFLOW')
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if(!this.isEmpty()){
+      return this.queueControl.shift()
+    }
+    else{
+      throw new Error("QUEUE_UNDERFLOW")
+    }
   }
 
   display() {
+    return this.queueControl
     // ... your code goes here
   }  
 }
