f2d92ca2e1675b772ba77bc1a557680167aed859
Christian Doeller <mail@christiandoeller.de>
2023-12-17 19:12:35 +0100

    finished logic

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,44 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length < this.MAX_SIZE){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length === 0){
+      return true;
+    }else {
+      return false;
+    }
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue() === false){
+      throw new Error('QUEUE_OVERFLOW');
+    } else {
+      this.queueControl.push(item);
+      return this.queueControl;
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.isEmpty() === true){
+      throw new Error('QUEUE_UNDERFLOW');
+    } else {
+      return this.queueControl.shift();
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl;
   }  
 }
 
