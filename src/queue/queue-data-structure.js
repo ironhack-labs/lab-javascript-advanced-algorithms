1b9512b4842cea3b941933a4aa2cd7891f306e50
Madhushree B <boylemadhushree@gmail.com>
2024-03-07 17:39:16 +0100

    Iteration 2

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,34 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    return this.queueControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.queueControl.length === 0;
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(this.canEnqueue() === false){
+      throw new Error('QUEUE_OVERFLOW')
+    }
+    this.queueControl.push(item);
+    return this.queueControl;
   }
 
   dequeue() {
     // ... your code goes here
+    if(this.isEmpty() === true){
+      throw new Error('QUEUE_UNDERFLOW')
+    }
+    return this.queueControl.shift();
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl;
   }  
 }
 
