cddf4bdb6e44077d03f0125872a6c191785f2cd9
Madhushree B <boylemadhushree@gmail.com>
2024-03-07 17:18:55 +0100

    Iteration 1

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,47 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if(this.stackControl.length < this.MAX_SIZE){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
     // ... your code goes here
-  }
+    if(this.stackControl > 0) {
+      return false
+    } else {
+      return true;
+    }
+    }
 
   push(item) {
     // ... your code goes here
+    if(this.canPush() === true){
+      this.stackControl.push(item);
+      return this.stackControl;
+
+    } else {
+      throw new Error("STACK_OVERFLOW")
+    }
   }
 
   pop() {
     // ... your code goes here
+    if(this.isEmpty() === false ){
+      this.stackControl.pop();
+      return this.stackControl;
+      // TO DO
+    } else {
+      throw new Error("STACK_UNDERFLOW")
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
